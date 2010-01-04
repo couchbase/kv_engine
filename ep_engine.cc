43f395717d4a102c5b1236967edb87758ddf921e
Trond Norbye <Trond.Norbye@gmail.com>
2010-01-04 15:08:26 +0100

    Create an engine built on top of the kvstore

---
+++ ep_engine.cc
@@ -0,0 +1,210 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+#include "config.h"
+#include <assert.h>
+
+#include <memcached/engine.h>
+
+#include "ep_engine.h"
+
+/**
+ * Helper function to avoid typing in the long cast all over the place
+ * @param handle pointer to the engine
+ * @return the engine as a class
+ */
+static inline EventuallyPersistentEngine* getHandle(ENGINE_HANDLE* handle)
+{
+    return reinterpret_cast<EventuallyPersistentEngine*>(handle);
+}
+
+// The Engine API specifies C linkage for the functions..
+extern "C" {
+
+    static const char* EvpGetInfo(ENGINE_HANDLE* handle)
+    {
+        return "EP engine v0.1";
+    }
+
+    static ENGINE_ERROR_CODE EvpInitialize(ENGINE_HANDLE* handle,
+                                           const char* config_str)
+    {
+        return getHandle(handle)->initialize(config_str);
+    }
+
+    static void EvpDestroy(ENGINE_HANDLE* handle)
+    {
+        getHandle(handle)->destroy();
+    }
+
+    static ENGINE_ERROR_CODE EvpItemAllocate(ENGINE_HANDLE* handle,
+                                             const void* cookie,
+                                             item **item,
+                                             const void* key,
+                                             const size_t nkey,
+                                             const size_t nbytes,
+                                             const int flags,
+                                             const rel_time_t exptime)
+    {
+        return getHandle(handle)->itemAllocate(cookie, item, key,
+                                               nkey, nbytes, flags, exptime);
+    }
+
+    static ENGINE_ERROR_CODE EvpItemDelete(ENGINE_HANDLE* handle,
+                                           const void* cookie,
+                                           item* item)
+    {
+        return getHandle(handle)->itemDelete(cookie, item);
+    }
+
+    static void EvpItemRelease(ENGINE_HANDLE* handle,
+                               const void *cookie,
+                               item* item)
+    {
+        getHandle(handle)->itemRelease(cookie, item);
+    }
+
+    static ENGINE_ERROR_CODE EvpGet(ENGINE_HANDLE* handle,
+                                    const void* cookie,
+                                    item** item,
+                                    const void* key,
+                                    const int nkey)
+    {
+        return getHandle(handle)->get(cookie, item, key, nkey);
+    }
+
+    static ENGINE_ERROR_CODE EvpGetStats(ENGINE_HANDLE* handle,
+                                         const void* cookie,
+                                         const char* stat_key,
+                                         int nkey,
+                                         ADD_STAT add_stat)
+    {
+        return getHandle(handle)->getStats(cookie, stat_key, nkey, add_stat);
+    }
+
+    static ENGINE_ERROR_CODE EvpStore(ENGINE_HANDLE* handle,
+                                      const void *cookie,
+                                      item* item,
+                                      uint64_t *cas,
+                                      ENGINE_STORE_OPERATION operation)
+    {
+        return getHandle(handle)->store(cookie, item, cas, operation);
+    }
+
+    static ENGINE_ERROR_CODE EvpArithmetic(ENGINE_HANDLE* handle,
+                                           const void* cookie,
+                                           const void* key,
+                                           const int nkey,
+                                           const bool increment,
+                                           const bool create,
+                                           const uint64_t delta,
+                                           const uint64_t initial,
+                                           const rel_time_t exptime,
+                                           uint64_t *cas,
+                                           uint64_t *result)
+    {
+        return ENGINE_ENOTSUP;
+    }
+
+    static ENGINE_ERROR_CODE EvpFlush(ENGINE_HANDLE* handle,
+                                      const void* cookie, time_t when)
+    {
+        return getHandle(handle)->flush(cookie, when);
+    }
+
+    static void EvpResetStats(ENGINE_HANDLE* handle, const void *cookie)
+    {
+        (void) cookie;
+        return getHandle(handle)->resetStats();
+    }
+
+    static ENGINE_ERROR_CODE EvpUnknownCommand(ENGINE_HANDLE* handle,
+                                               const void* cookie,
+                                               protocol_binary_request_header *request,
+                                               ADD_RESPONSE response)
+    {
+        if (response(NULL, 0, NULL, 0, NULL, 0,
+                     PROTOCOL_BINARY_RAW_BYTES,
+                     PROTOCOL_BINARY_RESPONSE_UNKNOWN_COMMAND, 0, cookie)) {
+            return ENGINE_SUCCESS;
+        } else {
+            return ENGINE_FAILED;
+        }
+    }
+
+    static uint64_t EvpItemGetCas(const item *item) {
+        return 0;
+    }
+
+    static void EvpItemSetCas(item *item, uint64_t cas) {
+        // empty
+    }
+
+    static const char *EvpItemGetKey(const item *it) {
+        return static_cast<const Item*>(it)->key.c_str();
+    }
+
+    static char *EvpItemGetData(const item *it) {
+        return static_cast<const Item*>(it)->data;
+    }
+
+    static uint8_t EvpItemGetClsid(const item *item) {
+        return 0;
+    }
+
+    /**
+     * The only public interface to the eventually persistance engine.
+     * Allocate a new instance and initialize it
+     * @param interface the highest interface the server supports (we only support
+     *                  interface 1)
+     * @param get_server_api callback function to get the server exported API
+     *                  functions
+     * @param handle Where to return the new instance
+     * @return ENGINE_SUCCESS on success
+     */
+    ENGINE_ERROR_CODE create_instance(uint64_t interface,
+                                      GET_SERVER_API get_server_api,
+                                      ENGINE_HANDLE **handle)
+    {
+        SERVER_HANDLE_V1 *api;
+        api = static_cast<SERVER_HANDLE_V1 *>(get_server_api(1));
+        if (interface != 1 || api == NULL) {
+            return ENGINE_ENOTSUP;
+        }
+
+        EventuallyPersistentEngine *engine;
+        engine = new struct EventuallyPersistentEngine(api);
+        if (engine == NULL) {
+            return ENGINE_ENOMEM;
+        }
+
+        *handle = reinterpret_cast<ENGINE_HANDLE*> (engine);
+        return ENGINE_SUCCESS;
+    }
+
+} // C linkage
+
+EventuallyPersistentEngine::EventuallyPersistentEngine(SERVER_HANDLE_V1 *sApi) :
+    backend(new EventuallyPersistentStore(new Sqlite3("/usr/tmp/test.db")))
+{
+    interface.interface = 1;
+    ENGINE_HANDLE_V1::get_info = EvpGetInfo;
+    ENGINE_HANDLE_V1::initialize = EvpInitialize;
+    ENGINE_HANDLE_V1::destroy = EvpDestroy;
+    ENGINE_HANDLE_V1::allocate = EvpItemAllocate;
+    ENGINE_HANDLE_V1::remove = EvpItemDelete;
+    ENGINE_HANDLE_V1::release = EvpItemRelease;
+    ENGINE_HANDLE_V1::get = EvpGet;
+    ENGINE_HANDLE_V1::get_stats = EvpGetStats;
+    ENGINE_HANDLE_V1::reset_stats = EvpResetStats;
+    ENGINE_HANDLE_V1::store = EvpStore;
+    ENGINE_HANDLE_V1::arithmetic = EvpArithmetic;
+    ENGINE_HANDLE_V1::flush = EvpFlush;
+    ENGINE_HANDLE_V1::unknown_command = EvpUnknownCommand;
+    ENGINE_HANDLE_V1::item_get_cas = EvpItemGetCas;
+    ENGINE_HANDLE_V1::item_set_cas = EvpItemSetCas;
+    ENGINE_HANDLE_V1::item_get_key = EvpItemGetKey;
+    ENGINE_HANDLE_V1::item_get_data = EvpItemGetData;
+    ENGINE_HANDLE_V1::item_get_clsid = EvpItemGetClsid;
+
+    serverApi = *sApi;
+    backend->reset();
+}
---
+++ ep_engine.h
@@ -0,0 +1,193 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+#include "locks.hh"
+#include "ep.hh"
+#include "sqlite-base.hh"
+
+using namespace kvtest;
+
+extern "C" {
+    EXPORT_FUNCTION
+    ENGINE_ERROR_CODE create_instance(uint64_t interface,
+                                      GET_SERVER_API get_server_api,
+                                      ENGINE_HANDLE **handle);
+
+    static const char *EvpItemGetKey(const item *it);
+    static char *EvpItemGetData(const item *it);
+}
+
+/**
+ * Statistic information collected by the eventually persistent engine
+ */
+struct engine_stats {
+    /** All access to the members should be guarded by the lock */
+    pthread_mutex_t lock;
+    // @todo add interesting stats
+};
+
+/**
+ * The Item structure we use to pass information between the memcached
+ * core and the backend. Please note that the kvstore don't store these
+ * objects, so we do have an extra layer of memory copying :(
+ */
+class Item : public item {
+private:
+    friend class EventuallyPersistentEngine;
+    friend const char *EvpItemGetKey(const item *it);
+    friend char *EvpItemGetData(const item *it);
+
+    Item(const void* k, const size_t nk, const size_t nb,
+         const int fl, const rel_time_t exp) :
+        key(static_cast<const char*>(k), nk)
+    {
+        nkey = nk;
+        nbytes = nb;
+        flags = fl;
+        iflag = 0;
+        exptime = exp;
+        data = new char[nbytes];
+    }
+
+    Item(const std::string &k, const int fl, const rel_time_t exp,
+         const void *dta, const size_t nb) :
+        key(k)
+    {
+        nkey = key.length();
+        nbytes = nb;
+        flags = fl;
+        iflag = 0;
+        exptime = exp;
+        data = new char[nbytes];
+        memcpy(data, dta, nbytes);
+    }
+
+    ~Item() {
+        delete []data;
+    }
+
+    std::string key;
+    char *data;
+};
+
+/**
+ * I don't care about the set callbacks right now, but since this is a
+ * demo, let's dump core if one of them fails so that we can debug it later
+ */
+class IgnoreCallback : public Callback<bool>
+{
+    virtual void callback(bool &value) {
+        assert(value);
+    }
+};
+
+/**
+ *
+ */
+class EventuallyPersistentEngine : public ENGINE_HANDLE_V1 {
+public:
+    ENGINE_ERROR_CODE initialize(const char* config)
+    {
+        return ENGINE_SUCCESS;
+    }
+
+    void destroy()
+    {
+        // empty
+    }
+
+    ENGINE_ERROR_CODE itemAllocate(const void* cookie,
+                                   item** item,
+                                   const void* key,
+                                   const size_t nkey,
+                                   const size_t nbytes,
+                                   const int flags,
+                                   const rel_time_t exptime)
+    {
+        *item = new Item(key, nkey, nbytes, flags, exptime);
+        if (*item == NULL) {
+            return ENGINE_ENOMEM;
+        } else {
+            return ENGINE_SUCCESS;
+        }
+    }
+
+    ENGINE_ERROR_CODE itemDelete(const void* cookie, item *item)
+    {
+        return ENGINE_ENOTSUP;
+
+    }
+
+    void itemRelease(const void* cookie, item *item)
+    {
+        delete (Item*)item;
+    }
+
+    ENGINE_ERROR_CODE get(const void* cookie,
+                          item** item,
+                          const void* key,
+                          const int nkey)
+    {
+        std::string k(static_cast<const char*>(key), nkey);
+        RememberingCallback<GetValue> getCb;
+        backend->get(k, getCb);
+        getCb.waitForValue();
+        if (getCb.val.success) {
+            *item = new Item(k, 0, 0, getCb.val.value.c_str(),
+                             getCb.val.value.length());
+            return ENGINE_SUCCESS;
+        } else {
+            return ENGINE_KEY_ENOENT;
+        }
+    }
+
+    ENGINE_ERROR_CODE getStats(const void* cookie,
+                               const char* stat_key,
+                               int nkey,
+                               ADD_STAT add_stat)
+    {
+        LockHolder lh(&stats.lock);
+        if (stat_key == NULL) {
+            // @todo add interesting stats
+        }
+        return ENGINE_SUCCESS;
+    }
+
+    ENGINE_ERROR_CODE store(const void *cookie,
+                            item* item,
+                            uint64_t *cas,
+                            ENGINE_STORE_OPERATION operation)
+    {
+        Item *it = static_cast<Item*>(item);
+        if (operation == OPERATION_SET) {
+            backend->set(it->key, it->data, ignoreCallback);
+            return ENGINE_SUCCESS;
+        } else {
+            return ENGINE_ENOTSUP;
+        }
+    }
+
+    ENGINE_ERROR_CODE flush(const void *cookie, time_t when)
+    {
+        if (when != 0) {
+            return ENGINE_ENOTSUP;
+        }
+
+        return ENGINE_SUCCESS;
+    }
+
+    void resetStats()
+    {
+        LockHolder lh(&stats.lock);
+        // @todo reset statistics
+    }
+
+private:
+    EventuallyPersistentEngine(SERVER_HANDLE_V1 *sApi);
+    friend ENGINE_ERROR_CODE create_instance(uint64_t interface,
+                                             GET_SERVER_API get_server_api,
+                                             ENGINE_HANDLE **handle);
+
+    SERVER_HANDLE_V1 serverApi;
+    IgnoreCallback ignoreCallback;
+    KVStore *backend;
+    struct engine_stats stats;
+};
