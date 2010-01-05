1e5100642f50da781b09fea2c5fdef9c6eb057ac
Trond Norbye <Trond.Norbye@gmail.com>
2010-01-05 09:52:21 +0100

    Refactor: Remove unused code and fix indentation

--- ep.hh
+++ ep.hh
@@ -1,3 +1,4 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 #ifndef EP_HH
 #define EP_HH 1
 
@@ -18,11 +19,11 @@
 #include "locks.hh"
 
 struct ep_stats {
-   time_t dirtyAge;
-   time_t dirtyAgeHighWat;
-   size_t queue_size;
-   size_t flusher_todo;
-   time_t commit_time;
+    time_t dirtyAge;
+    time_t dirtyAgeHighWat;
+    size_t queue_size;
+    size_t flusher_todo;
+    time_t commit_time;
 };
 
 // Forward declaration for StoredValue
@@ -30,209 +31,209 @@ class HashTable;
 
 class StoredValue {
 public:
-   StoredValue() {
-      next = NULL;
-      value = NULL;
-      nvalue = 0;
-      dirtied = 0;
-   }
-   StoredValue(std::string &k, const char *v, size_t nv, StoredValue *n) {
-      key = k;
-      value = NULL;
-      dirtied = 0;
-      setValue(v, nv);
-      next = n;
-   }
-   ~StoredValue() {
-      free((void*)value);
-      value = NULL;
-   }
-   void markDirty() {
-      if (!isDirty()) {
-         dirtied = time(NULL);
-      }
-   }
-   // returns time this object was dirtied.
-   time_t markClean() {
-      time_t rv = dirtied;
-      dirtied = 0;
-      return rv;
-   }
-   bool isDirty() {
-      return dirtied != 0;
-   }
-   bool isClean() {
-      return dirtied == 0;
-   }
-   const char* getValue(size_t *size_out) {
-      *size_out = nvalue;
-      return value;
-   }
-   void setValue(const char *v, const size_t nv) {
-      free((void*)value);
-      value = (const char *)calloc(nv, sizeof(char));
-      nvalue = nv;
-      memcpy((char*)value, v, nvalue);
-      markDirty();
-   }
+    StoredValue() {
+        next = NULL;
+        value = NULL;
+        nvalue = 0;
+        dirtied = 0;
+    }
+    StoredValue(std::string &k, const char *v, size_t nv, StoredValue *n) {
+        key = k;
+        value = NULL;
+        dirtied = 0;
+        setValue(v, nv);
+        next = n;
+    }
+    ~StoredValue() {
+        free((void*)value);
+        value = NULL;
+    }
+    void markDirty() {
+        if (!isDirty()) {
+            dirtied = time(NULL);
+        }
+    }
+    // returns time this object was dirtied.
+    time_t markClean() {
+        time_t rv = dirtied;
+        dirtied = 0;
+        return rv;
+    }
+    bool isDirty() {
+        return dirtied != 0;
+    }
+    bool isClean() {
+        return dirtied == 0;
+    }
+    const char* getValue(size_t *size_out) {
+        *size_out = nvalue;
+        return value;
+    }
+    void setValue(const char *v, const size_t nv) {
+        free((void*)value);
+        value = (const char *)calloc(nv, sizeof(char));
+        nvalue = nv;
+        memcpy((char*)value, v, nvalue);
+        markDirty();
+    }
 private:
 
-   friend class HashTable;
+    friend class HashTable;
 
-   time_t dirtied;
-   std::string key;
-   const char *value;
-   size_t nvalue;
-   StoredValue *next;
-   DISALLOW_COPY_AND_ASSIGN(StoredValue);
+    time_t dirtied;
+    std::string key;
+    const char *value;
+    size_t nvalue;
+    StoredValue *next;
+    DISALLOW_COPY_AND_ASSIGN(StoredValue);
 };
 
 typedef enum {
-   NOT_FOUND, WAS_CLEAN, WAS_DIRTY
+    NOT_FOUND, WAS_CLEAN, WAS_DIRTY
 } mutation_type_t;
 
 class HashTable {
 public:
 
-   // Construct with number of buckets and locks.
-   HashTable(size_t s = 196613, size_t l = 193) {
-      size = s;
-      n_locks = l;
-      active = true;
-      values = (StoredValue**)calloc(s, sizeof(StoredValue**));
-      mutexes = (pthread_mutex_t*)calloc(l, sizeof(pthread_mutex_t));
-      for (int i = 0; i < (int)n_locks; i++) {
-         pthread_mutex_init(&mutexes[i], NULL);
-      }
-   }
+    // Construct with number of buckets and locks.
+    HashTable(size_t s = 196613, size_t l = 193) {
+        size = s;
+        n_locks = l;
+        active = true;
+        values = (StoredValue**)calloc(s, sizeof(StoredValue**));
+        mutexes = (pthread_mutex_t*)calloc(l, sizeof(pthread_mutex_t));
+        for (int i = 0; i < (int)n_locks; i++) {
+            pthread_mutex_init(&mutexes[i], NULL);
+        }
+    }
 
-   ~HashTable() {
-      clear();
-      for (int i = 0; i < (int)n_locks; i++) {
-         pthread_mutex_destroy(&mutexes[i]);
-      }
-      free(mutexes);
-      free(values);
-      mutexes = NULL;
-      values = NULL;
-      active = false;
-   }
+    ~HashTable() {
+        clear();
+        for (int i = 0; i < (int)n_locks; i++) {
+            pthread_mutex_destroy(&mutexes[i]);
+        }
+        free(mutexes);
+        free(values);
+        mutexes = NULL;
+        values = NULL;
+        active = false;
+    }
 
-   void clear() {
-      assert(active);
-      for (int i = 0; i < (int)size; i++) {
-         LockHolder lh(getMutex(i));
-         while (values[i]) {
-            StoredValue *v = values[i];
-            values[i] = v->next;
+    void clear() {
+        assert(active);
+        for (int i = 0; i < (int)size; i++) {
+            LockHolder lh(getMutex(i));
+            while (values[i]) {
+                StoredValue *v = values[i];
+                values[i] = v->next;
+                delete v;
+            }
+        }
+    }
+
+    StoredValue *find(std::string &key) {
+        assert(active);
+        int bucket_num = bucket(key);
+        LockHolder lh(getMutex(bucket_num));
+        return unlocked_find(key, bucket_num);
+    }
+
+    // True if this existed and was clean
+    mutation_type_t set(std::string &key, std::string &val) {
+        return set(key, val.c_str(), val.size());
+    }
+
+    mutation_type_t set(std::string &key, const char *val, size_t nbytes) {
+        assert(active);
+        mutation_type_t rv = NOT_FOUND;
+        int bucket_num = bucket(key);
+        LockHolder lh(getMutex(bucket_num));
+        StoredValue *v = unlocked_find(key, bucket_num);
+        if (v) {
+            rv = v->isClean() ? WAS_CLEAN : WAS_DIRTY;
+            v->setValue(val, nbytes);
+        } else {
+            v = new StoredValue(key, val, nbytes, values[bucket_num]);
+            values[bucket_num] = v;
+        }
+        return rv;
+    }
+
+    StoredValue *unlocked_find(std::string &key, int bucket_num) {
+        StoredValue *v = values[bucket_num];
+        while (v) {
+            if (key.compare(v->key) == 0) {
+                return v;
+            }
+            v = v->next;
+        }
+        return NULL;
+    }
+
+    inline int bucket(std::string &key) {
+        assert(active);
+        int h=5381;
+        int i=0;
+        const char *str=key.c_str();
+
+        for(i=0; str[i] != 0x00; i++) {
+            h = ((h << 5) + h) ^ str[i];
+        }
+
+        return abs(h) % (int)size;
+    }
+
+    // Get the mutex for a bucket (for doing your own lock management)
+    inline pthread_mutex_t *getMutex(int bucket_num) {
+        assert(active);
+        assert(bucket_num < (int)size);
+        assert(bucket_num >= 0);
+        int lock_num = bucket_num % (int)n_locks;
+        assert(lock_num < (int)n_locks);
+        assert(lock_num >= 0);
+        return &mutexes[lock_num];
+    }
+
+    // True if it existed
+    bool del(std::string &key) {
+        assert(active);
+        int bucket_num = bucket(key);
+        LockHolder lh(getMutex(bucket_num));
+
+        StoredValue *v = values[bucket_num];
+
+        // Special case empty bucket.
+        if (!v) {
+            return false;
+        }
+
+        // Special case the first one
+        if (key.compare(v->key) == 0) {
+            values[bucket_num] = v->next;
             delete v;
-         }
-      }
-   }
-
-   StoredValue *find(std::string &key) {
-      assert(active);
-      int bucket_num = bucket(key);
-      LockHolder lh(getMutex(bucket_num));
-      return unlocked_find(key, bucket_num);
-   }
-
-   // True if this existed and was clean
-   mutation_type_t set(std::string &key, std::string &val) {
-      return set(key, val.c_str(), val.size());
-   }
-
-   mutation_type_t set(std::string &key, const char *val, size_t nbytes) {
-      assert(active);
-      mutation_type_t rv = NOT_FOUND;
-      int bucket_num = bucket(key);
-      LockHolder lh(getMutex(bucket_num));
-      StoredValue *v = unlocked_find(key, bucket_num);
-      if (v) {
-         rv = v->isClean() ? WAS_CLEAN : WAS_DIRTY;
-         v->setValue(val, nbytes);
-      } else {
-         v = new StoredValue(key, val, nbytes, values[bucket_num]);
-         values[bucket_num] = v;
-      }
-      return rv;
-   }
-
-   StoredValue *unlocked_find(std::string &key, int bucket_num) {
-      StoredValue *v = values[bucket_num];
-      while (v) {
-         if (key.compare(v->key) == 0) {
-            return v;
-         }
-         v = v->next;
-      }
-      return NULL;
-   }
-
-   inline int bucket(std::string &key) {
-      assert(active);
-      int h=5381;
-      int i=0;
-      const char *str=key.c_str();
-
-      for(i=0; str[i] != 0x00; i++) {
-         h = ((h << 5) + h) ^ str[i];
-      }
-
-      return abs(h) % (int)size;
-   }
-
-   // Get the mutex for a bucket (for doing your own lock management)
-   inline pthread_mutex_t *getMutex(int bucket_num) {
-      assert(active);
-      assert(bucket_num < (int)size);
-      assert(bucket_num >= 0);
-      int lock_num = bucket_num % (int)n_locks;
-      assert(lock_num < (int)n_locks);
-      assert(lock_num >= 0);
-      return &mutexes[lock_num];
-   }
-
-   // True if it existed
-   bool del(std::string &key) {
-      assert(active);
-      int bucket_num = bucket(key);
-      LockHolder lh(getMutex(bucket_num));
-
-      StoredValue *v = values[bucket_num];
-
-      // Special case empty bucket.
-      if (!v) {
-         return false;
-      }
-
-      // Special case the first one
-      if (key.compare(v->key) == 0) {
-         values[bucket_num] = v->next;
-         delete v;
-         return true;
-      }
-
-      while (v->next) {
-         if (key.compare(v->next->key) == 0) {
-            StoredValue *tmp = v->next;
-            v->next = v->next->next;
-            delete tmp;
             return true;
-         }
-      }
+        }
 
-      return false;
-   }
+        while (v->next) {
+            if (key.compare(v->next->key) == 0) {
+                StoredValue *tmp = v->next;
+                v->next = v->next->next;
+                delete tmp;
+                return true;
+            }
+        }
+
+        return false;
+    }
 
 private:
-   size_t            size;
-   size_t            n_locks;
-   bool              active;
-   StoredValue     **values;
-   pthread_mutex_t  *mutexes;
+    size_t            size;
+    size_t            n_locks;
+    bool              active;
+    StoredValue     **values;
+    pthread_mutex_t  *mutexes;
 
-   DISALLOW_COPY_AND_ASSIGN(HashTable);
+    DISALLOW_COPY_AND_ASSIGN(HashTable);
 };
 
 // Forward declaration
@@ -241,72 +242,72 @@ class Flusher;
 class EventuallyPersistentStore : public KVStore {
 public:
 
-   EventuallyPersistentStore(KVStore *t, size_t est=32768);
+    EventuallyPersistentStore(KVStore *t, size_t est=32768);
 
-   ~EventuallyPersistentStore();
+    ~EventuallyPersistentStore();
 
-   void set(std::string &key, std::string &val,
-            Callback<bool> &cb);
+    void set(std::string &key, std::string &val,
+             Callback<bool> &cb);
 
-   void set(std::string &key, const char *val, size_t nbytes,
-            Callback<bool> &cb);
+    void set(std::string &key, const char *val, size_t nbytes,
+             Callback<bool> &cb);
 
-   void get(std::string &key, Callback<GetValue> &cb);
+    void get(std::string &key, Callback<GetValue> &cb);
 
-   void del(std::string &key, Callback<bool> &cb);
+    void del(std::string &key, Callback<bool> &cb);
 
-   void getStats(struct ep_stats *out);
+    void getStats(struct ep_stats *out);
 
-   void reset();
+    void reset();
 
 private:
 
-   void queueDirty(std::string &key);
-   void flush(bool shouldWait);
-   void flushSome(std::queue<std::string> *q, Callback<bool> &cb);
-   void initQueue();
+    void queueDirty(std::string &key);
+    void flush(bool shouldWait);
+    void flushSome(std::queue<std::string> *q, Callback<bool> &cb);
+    void initQueue();
 
-   friend class Flusher;
+    friend class Flusher;
 
-   KVStore                 *underlying;
-   size_t                   est_size;
-   Flusher                 *flusher;
-   HashTable                storage;
-   pthread_mutex_t          mutex;
-   pthread_cond_t           cond;
-   std::queue<std::string> *towrite;
-   pthread_t                thread;
-   struct ep_stats          stats;
-   DISALLOW_COPY_AND_ASSIGN(EventuallyPersistentStore);
+    KVStore                 *underlying;
+    size_t                   est_size;
+    Flusher                 *flusher;
+    HashTable                storage;
+    pthread_mutex_t          mutex;
+    pthread_cond_t           cond;
+    std::queue<std::string> *towrite;
+    pthread_t                thread;
+    struct ep_stats          stats;
+    DISALLOW_COPY_AND_ASSIGN(EventuallyPersistentStore);
 };
 
 class Flusher {
 public:
-   Flusher(EventuallyPersistentStore *st) {
-      store = st;
-      running = true;
-   }
-   ~Flusher() {
-      stop();
-   }
-   void stop() {
-      running = false;
-   }
-   void run() {
-      try {
-         while(running) {
-            store->flush(true);
-         }
-         std::cout << "Shutting down flusher." << std::endl;
-      } catch(std::runtime_error &e) {
-         std::cerr << "Exception in executor loop: "
-                   << e.what() << std::endl;
-         assert(false);
-      }
-   }
+    Flusher(EventuallyPersistentStore *st) {
+        store = st;
+        running = true;
+    }
+    ~Flusher() {
+        stop();
+    }
+    void stop() {
+        running = false;
+    }
+    void run() {
+        try {
+            while(running) {
+                store->flush(true);
+            }
+            std::cout << "Shutting down flusher." << std::endl;
+        } catch(std::runtime_error &e) {
+            std::cerr << "Exception in executor loop: "
+                      << e.what() << std::endl;
+            assert(false);
+        }
+    }
 private:
-   EventuallyPersistentStore *store;
-   volatile bool running;
+    EventuallyPersistentStore *store;
+    volatile bool running;
 };
 
 #endif /* EP_HH */
--- sqlite-kvstore.hh
+++ sqlite-kvstore.hh
@@ -1,3 +1,4 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 #ifndef SQLITE_BASE_H
 #define SQLITE_BASE_H 1
 
@@ -11,72 +12,72 @@
 class PreparedStatement {
 public:
 
-   /**
-    * Construct a prepared statement.
-    *
-    * @param d the DB where the prepared statement will execute
-    * @param query the query to prepare
-    */
-   PreparedStatement(sqlite3 *d, const char *query);
+    /**
+     * Construct a prepared statement.
+     *
+     * @param d the DB where the prepared statement will execute
+     * @param query the query to prepare
+     */
+    PreparedStatement(sqlite3 *d, const char *query);
 
-   /**
-    * Clean up.
-    */
-   ~PreparedStatement();
+    /**
+     * Clean up.
+     */
+    ~PreparedStatement();
 
-   /**
-    * Bind a null-terminated string parameter to a binding in
-    * this statement.
-    *
-    * @param pos the binding position (starting at 1)
-    * @param s the value to bind
-    */
-   void bind(int pos, const char *s);
+    /**
+     * Bind a null-terminated string parameter to a binding in
+     * this statement.
+     *
+     * @param pos the binding position (starting at 1)
+     * @param s the value to bind
+     */
+    void bind(int pos, const char *s);
 
-   /**
-    * Bind a string parameter to a binding in this statement.
-    *
-    * @param pos the binding position (starting at 1)
-    * @param s the value to bind
-    * @param nbytes number of bytes in the string.
-    */
-   void bind(int pos, const char *s, size_t nbytes);
+    /**
+     * Bind a string parameter to a binding in this statement.
+     *
+     * @param pos the binding position (starting at 1)
+     * @param s the value to bind
+     * @param nbytes number of bytes in the string.
+     */
+    void bind(int pos, const char *s, size_t nbytes);
 
-   /**
-    * Execute a prepared statement that does not return results.
-    *
-    * @return how many rows were affected
-    */
-   int execute();
+    /**
+     * Execute a prepared statement that does not return results.
+     *
+     * @return how many rows were affected
+     */
+    int execute();
 
-   /**
-    * Execute a prepared statement that does return results
-    * and/or return the next row.
-    *
-    * @return true if there are more rows after this one
-    */
-   bool fetch();
+    /**
+     * Execute a prepared statement that does return results
+     * and/or return the next row.
+     *
+     * @return true if there are more rows after this one
+     */
+    bool fetch();
 
-   /**
-    * Reset the bindings.
-    *
-    * Call this before reusing a prepared statement.
-    */
-   void reset();
+    /**
+     * Reset the bindings.
+     *
+     * Call this before reusing a prepared statement.
+     */
+    void reset();
 
-   /**
-    * Get the value at a given column in the current row.
-    *
-    * Use this along with fetch.
-    *
-    * @param x the column number (starting at 1)
-    * @return the value
-    */
-   const char *column(int x);
+    /**
+     * Get the value at a given column in the current row.
+     *
+     * Use this along with fetch.
+     *
+     * @param x the column number (starting at 1)
+     * @return the value
+     */
+    const char *column(int x);
 
 private:
-   sqlite3      *db;
-   sqlite3_stmt *st;
+    sqlite3      *db;
+    sqlite3_stmt *st;
 };
 
 /**
@@ -85,126 +86,126 @@ private:
 class BaseSqlite3 : public KVStore {
 public:
 
-   /**
-    * Construct an instance of sqlite with the given database name.
-    */
-   BaseSqlite3(const char *fn);
+    /**
+     * Construct an instance of sqlite with the given database name.
+     */
+    BaseSqlite3(const char *fn);
 
-   /**
-    * Cleanup.
-    */
-   ~BaseSqlite3();
+    /**
+     * Cleanup.
+     */
+    ~BaseSqlite3();
 
-   /**
-    * Reset database to a clean state.
-    */
-   void reset();
+    /**
+     * Reset database to a clean state.
+     */
+    void reset();
 
-   /**
-    * Begin a transaction (if not already in one).
-    */
-   void begin();
+    /**
+     * Begin a transaction (if not already in one).
+     */
+    void begin();
 
-   /**
-    * Commit a transaction (unless not currently in one).
-    */
-   void commit();
+    /**
+     * Commit a transaction (unless not currently in one).
+     */
+    void commit();
 
-   /**
-    * Rollback a transaction (unless not currently in one).
-    */
-   void rollback();
-
-protected:
-
-   /**
-    * Shortcut to execute a simple query.
-    *
-    * @param query a simple query with no bindings to execute directly
-    */
-   void execute(const char *query);
-
-   /**
-    * After setting up the DB, this is called to initialize our
-    * prepared statements.
-    */
-   virtual void initStatements() {}
-
-   /**
-    * When tearing down, tear down the statements set up by
-    * initStatements.
-    */
-   virtual void destroyStatements() {}
-
-   /**
-    * Set up the tables.
-    */
-   virtual void initTables() {}
-
-   /**
-    * Clean up the tables.
-    */
-   virtual void destroyTables() {}
+    /**
+     * Rollback a transaction (unless not currently in one).
+     */
+    void rollback();
 
 protected:
-   /**
-    * Direct access to the DB.
-    */
-   sqlite3 *db;
+
+    /**
+     * Shortcut to execute a simple query.
+     *
+     * @param query a simple query with no bindings to execute directly
+     */
+    void execute(const char *query);
+
+    /**
+     * After setting up the DB, this is called to initialize our
+     * prepared statements.
+     */
+    virtual void initStatements() {}
+
+    /**
+     * When tearing down, tear down the statements set up by
+     * initStatements.
+     */
+    virtual void destroyStatements() {}
+
+    /**
+     * Set up the tables.
+     */
+    virtual void initTables() {}
+
+    /**
+     * Clean up the tables.
+     */
+    virtual void destroyTables() {}
+
+protected:
+    /**
+     * Direct access to the DB.
+     */
+    sqlite3 *db;
 
 private:
 
-   const char *filename;
-   bool intransaction;
+    const char *filename;
+    bool intransaction;
 
-   void open();
-   void close();
+    void open();
+    void close();
 };
 
 class Sqlite3 : public BaseSqlite3 {
 public:
 
-   Sqlite3(const char *path, bool is_auditable=false) : BaseSqlite3(path) {
-      ins_stmt = sel_stmt = del_stmt = NULL;
-      auditable = is_auditable;
-   }
+    Sqlite3(const char *path, bool is_auditable=false) : BaseSqlite3(path) {
+        ins_stmt = sel_stmt = del_stmt = NULL;
+        auditable = is_auditable;
+    }
 
-   /**
-    * Overrides set() to call the char* variant.
-    */
-   void set(std::string &key, std::string &val, Callback<bool> &cb);
+    /**
+     * Overrides set() to call the char* variant.
+     */
+    void set(std::string &key, std::string &val, Callback<bool> &cb);
 
-   /**
-    * Overrides set().
-    */
-   void set(std::string &key, const char *val, size_t nbytes,
-            Callback<bool> &cb);
+    /**
+     * Overrides set().
+     */
+    void set(std::string &key, const char *val, size_t nbytes,
+             Callback<bool> &cb);
 
-   /**
-    * Overrides get().
-    */
-   void get(std::string &key, Callback<GetValue> &cb);
+    /**
+     * Overrides get().
+     */
+    void get(std::string &key, Callback<GetValue> &cb);
 
-   /**
-    * Overrides del().
-    */
-   void del(std::string &key, Callback<bool> &cb);
+    /**
+     * Overrides del().
+     */
+    void del(std::string &key, Callback<bool> &cb);
 
 protected:
 
-   void initStatements();
+    void initStatements();
 
-   void destroyStatements();
+    void destroyStatements();
 
-   void initTables();
+    void initTables();
 
-   void destroyTables();
+    void destroyTables();
 
 private:
-   bool               auditable;
-   PreparedStatement *ins_stmt;
-   PreparedStatement *sel_stmt;
-   PreparedStatement *del_stmt;
+    bool               auditable;
+    PreparedStatement *ins_stmt;
+    PreparedStatement *sel_stmt;
+    PreparedStatement *del_stmt;
 };
 
 #endif /* SQLITE_BASE_H */
