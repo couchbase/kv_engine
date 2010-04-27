dbd2dff856dd0f2c72e70c5e0ebbc447490f9eef
Dustin Sallings <dustin@spy.net>
2010-04-27 10:34:32 +0530

    Moved flusher implementation out of header file.
    Change-Id: I01d549e6aec7cbe552fc848085ca01ab49f6537f
    Reviewed-on: https://review.northscale.com:8443/1324
    Reviewed-by: Sean Lynch <seanl@literati.org>
    Tested-by: Sean Lynch <seanl@literati.org>

--- flusher.hh
+++ flusher.hh
@@ -12,56 +12,18 @@ public:
     ~Flusher() {
         stop();
     }
-    void stop() {
-        running = false;
-    }
-    void initialize() {
-        rel_time_t start = ep_current_time();
-        store->warmup();
-        store->stats.warmupTime = ep_current_time() - start;
-        store->stats.warmupComplete = true;
-        hasInitialized = true;
-    }
 
-    void run() {
-        running = true;
-        if (!hasInitialized) {
-            initialize();
-        }
-        try {
-            while (running) {
-                rel_time_t start = ep_current_time();
+    void stop();
 
-                int n = store->flush(true);
-                if (n > 0) {
-                    rel_time_t sleep_end = start + n;
-                    while (running && ep_current_time() < sleep_end) {
-                        sleep(1);
-                    }
-                }
-            }
-            std::stringstream ss;
-            ss << "Shutting down flusher (Write of all dirty items)"
-               << std::endl;
-            getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "%s",
-                             ss.str().c_str());
-            store->stats.min_data_age = 0;
-            store->flush(false);
-            getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "Flusher stopped\n");
-        } catch(std::runtime_error &e) {
-            std::stringstream ss;
-            ss << "Exception if flusher loop: " << e.what() << std::endl;
-            getLogger()->log(EXTENSION_LOG_WARNING, NULL, "%s",
-                             ss.str().c_str());
-            assert(false);
-        }
-        // Signal our completion.
-        store->flusherStopped();
-    }
+    void initialize();
+
+    void run(void);
 private:
     EventuallyPersistentStore *store;
     volatile bool running;
     bool hasInitialized;
+
+    int doFlush(bool shouldWait);
 };
 
 #endif /* FLUSHER_H */
