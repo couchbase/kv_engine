b8cbc367a16f975757312e6fa3058c9444609ebe
Trond Norbye <Trond.Norbye@gmail.com>
2010-04-30 09:05:26 +0200

    Seed the cas counter with 1 instead of 0 (cas wildcard)
    Change-Id: I0d48807f66f1cdc9ddcd656a787bec29e1a4e759
    Reviewed-on: https://review.northscale.com:8443/1336
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- item.cc
+++ item.cc
@@ -6,7 +6,7 @@ static void devnull(uint64_t current)
     (void)current;
 }
 
-uint64_t Item::casCounter = 0;
+uint64_t Item::casCounter = 1;
 uint64_t Item::casNotificationFrequency = 10000;
 void (*Item::casNotifier)(uint64_t) = devnull;
 Mutex Item::casMutex;
