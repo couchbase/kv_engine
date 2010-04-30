couchbase/kv_engine

commit 7cdcbc40bd93c1389cdb0a92ba5784766250141c

Remove preserveCas argument to set and add.
Also removed redundant add method, instead using a default value for
isDirty.

Change-Id: I4567a42f11c1dd1714dc17748dac7ce34d68b9f1
Reviewed-on: https://review.northscale.com:8443/1332
Reviewed-by: Dustin Sallings <dustin@spy.net>
Tested-by: Dustin Sallings <dustin@spy.net>

stored-value.hh
@@ -158,7 +158,7 @@ public:
         return unlocked_find(key, bucket_num);
     }
 
-    mutation_type_t set(const Item &val, bool preserveCas = false) {
+    mutation_type_t set(const Item &val) {
         assert(active);
         mutation_type_t rv = NOT_FOUND;
         int bucket_num = bucket(val.getKey());
@@ -169,19 +169,14 @@ public:
             if (val.getCas() != 0 && val.getCas() != v->getCas()) {
                 return INVALID_CAS;
             }
-            if (!preserveCas) {
-                itm.setCas();
-            }
+            itm.setCas();
             rv = v->isClean() ? WAS_CLEAN : WAS_DIRTY;
             v->setValue(itm.getData(), itm.getNBytes(),
                         itm.getFlags(), itm.getExptime(),
                         itm.getCas());
         } else {
-            if (!preserveCas) {
-                if (itm.getCas() != 0) {
-                    return INVALID_CAS;
-                }
-                itm.setCas();
+            if (itm.getCas() != 0) {
+                return INVALID_CAS;
             }
 
             v = new StoredValue(itm, values[bucket_num]);
@@ -190,7 +185,7 @@ public:
         return rv;
     }
 
-    bool add(const Item &val, bool isDirty, bool preserveCas = false) {
+    bool add(const Item &val, bool isDirty = true) {
         assert(active);
         int bucket_num = bucket(val.getKey());
         LockHolder lh(getMutex(bucket_num));
@@ -199,18 +194,12 @@ public:
             return false;
         } else {
             Item &itm = const_cast<Item&>(val);
-            if (!preserveCas) {
-                itm.setCas();
-            }
+            itm.setCas();
             v = new StoredValue(itm, values[bucket_num], isDirty);
             values[bucket_num] = v;
         }
 
         return true;
-    }
-
-    bool add(const Item &val) {
-        return add(val, true);
     }
 
     StoredValue *unlocked_find(const std::string &key, int bucket_num) {

commit e870577cb00a44d6b1ba064f7a9daa5c0515888f

Fixed regression introduced in 7cdcbc40bd93c1389cdb0a92ba5784766250141c
Change-Id: I9d2adac63b5d8c84e0f4bf19c479d06a7a6f02dc
Reviewed-on: https://review.northscale.com:8443/1337
Reviewed-by: Dustin Sallings <dustin@spy.net>
Tested-by: Dustin Sallings <dustin@spy.net>

stored-value.hh
@@ -178,7 +178,7 @@ public:
             if (itm.getCas() != 0) {
                 return INVALID_CAS;
             }
-
+            itm.setCas();
             v = new StoredValue(itm, values[bucket_num]);
             values[bucket_num] = v;
         }