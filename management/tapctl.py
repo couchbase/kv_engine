cfe439863c7e5fd6d9841b3d311b3d3295fd3d0d
Jayesh Jose <jjose@zynga.com>
2010-05-04 09:59:42 +0530

    Added tapctl.py to control tap client connection
    - start/stop tap client connection
    - set tap peer
    
    Change-Id: I9ba354fc340fb889d039537e6256d9a438c2d00c
    Reviewed-on: http://review.northscale.com:8080/235
    Tested-by: Dustin Sallings <dustin@spy.net>
    Reviewed-by: Dustin Sallings <dustin@spy.net>

---
+++ management/tapctl.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+Tap control for ep-engine.
+
+"""
+
+import sys
+import socket
+import string
+import random
+import struct
+import exceptions
+import time
+
+import mc_bin_client
+
+def usage():
+    print >> sys.stderr, "Usage: %s host:port start|stop|status for start/stop replication" % sys.argv[0]
+    print >> sys.stderr, "  or   %s host:port set param value" % sys.argv[0]
+    print >> sys.stderr, ""
+    print >> sys.stderr, " Available params:"
+    print >> sys.stderr, "    tap_peer  - set the replication master"
+    sys.exit(1)
+
+if __name__ == '__main__':
+    try:
+        hp, cmd = sys.argv[1:3]
+        host, port = hp.split(':')
+        port = int(port)
+    except ValueError:
+        usage()
+
+    mc = mc_bin_client.MemcachedClient(host, port)
+    f = {'stop': mc.stop_replication,
+         'start': mc.start_replication,
+         'set': mc.set_tap_param,
+         'status':mc.stats}.get(cmd)
+
+    if not f:
+        usage()
+
+    print "Issuing %s command" % cmd
+    if cmd != 'status':
+        try:
+            f(*sys.argv[3:])
+        except mc_bin_client.MemcachedError, e:
+            print "ERROR: %s" % e.msg
+
+
+    stats = mc.stats()
+    print
+    print "Current replication stats: "
+    print "ep_replication_peer  : %s" % stats['ep_replication_peer']
+    print "ep_replication_state : %s" % stats['ep_replication_state']
+    print "ep_replication_status: %s" % stats['ep_replication_status']
+    print
+
