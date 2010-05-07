95e78f46b6bc79a44bba380c584d6f37bbecfec5
Sean Lynch <seanl@literati.org>
2010-05-07 17:56:48 +0530

    Add time functions to common.hh
    Change-Id: Ic9a0bdda6722ec841f6e1952fb4d37c041bf93da
    Reviewed-on: http://review.northscale.com:8080/31
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- common.hh
+++ common.hh
@@ -1,11 +1,32 @@
 #ifndef COMMON_H
 #define COMMON_H 1
 
+#include <math.h>
+#include <sys/time.h>
+
 // Stolen from http://google-styleguide.googlecode.com/svn/trunk/cppguide.xml
 // A macro to disallow the copy constructor and operator= functions
 // This should be used in the private: declarations for a class
 #define DISALLOW_COPY_AND_ASSIGN(TypeName)      \
     TypeName(const TypeName&);                  \
     void operator=(const TypeName&)
+
+inline void advance_tv(struct timeval &tv, const double secs) {
+    double ip, fp;
+    fp = modf(secs, &ip);
+    int usec = (fp * 1e6) + tv.tv_usec;
+    int quot = usec / 1000000;
+    int rem = usec % 1000000;
+    tv.tv_sec = ip + tv.tv_sec + quot;
+    tv.tv_usec = rem;
+}
+
+inline bool less_tv(const struct timeval &tv1, const struct timeval &tv2) {
+    if (tv1.tv_sec == tv2.tv_sec) {
+        return tv1.tv_usec < tv2.tv_usec;
+    } else {
+        return tv1.tv_sec < tv2.tv_sec;
+    }
+}
 
 #endif /* COMMON_H */
