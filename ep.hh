2470231b3c0acca86b1eb915b7ae660f40c06c5a
Dustin Sallings <dustin@spy.net>
2010-05-17 10:36:14 -0700

    Moved stats into a separate file.
    Change-Id: Ib4c17bc56818674332f545db03dec1c77fa6a8ec
    Reviewed-on: http://review.northscale.com:8080/84
    Reviewed-by: Sean Lynch <seanl@literati.org>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- ep.hh
+++ ep.hh
@@ -20,6 +20,7 @@
 
 #include <memcached/engine.h>
 
+#include "stats.hh"
 #include "kvstore.hh"
 #include "locks.hh"
 #include "sqlite-kvstore.hh"
@@ -39,61 +40,6 @@ extern EXTENSION_LOGGER_DESCRIPTOR *getLogger(void);
 extern "C" {
     extern rel_time_t (*ep_current_time)();
 }
-
-struct ep_stats {
-    // How long it took us to load the data from disk.
-    time_t warmupTime;
-    // Whether we're warming up.
-    bool warmupComplete;
-    // Number of records warmed up.
-    size_t warmedUp;
-    // size of the input queue
-    size_t queue_size;
-    // Size of the in-process (output) queue.
-    size_t flusher_todo;
-    // Objects that were rejected from persistence for being too fresh.
-    size_t tooYoung;
-    // Objects that were forced into persistence for being too old.
-    size_t tooOld;
-    // Number of items persisted.
-    size_t totalPersisted;
-    // Cumulative number of items added to the queue.
-    size_t totalEnqueued;
-    // Number of times an item flush failed.
-    size_t flushFailed;
-    // How long an object is dirty before written.
-    rel_time_t dirtyAge;
-    rel_time_t dirtyAgeHighWat;
-    // How old persisted data was when it hit the persistence layer
-    rel_time_t dataAge;
-    rel_time_t dataAgeHighWat;
-    // How long does it take to do an entire flush cycle.
-    rel_time_t flushDuration;
-    rel_time_t flushDurationHighWat;
-    // Amount of time spent in the commit phase.
-    rel_time_t commit_time;
-    // Total number of items; this would be total_items if we recycled
-    // items, but we don't right now.
-    size_t curr_items;
-    // Beyond this point are config items
-    // Minimum data age before a record can be persisted
-    uint32_t min_data_age;
-    // Maximum data age before a record is forced to be persisted
-    uint32_t queue_age_cap;
-    // Current tap queue size.
-    size_t tap_queue;
-    // Total number of tap messages sent.
-    size_t tap_fetched;
-};
-
-struct key_stats {
-    uint64_t cas;
-    rel_time_t exptime;
-    rel_time_t dirtied;
-    rel_time_t data_age;
-    uint32_t flags;
-    bool dirty;
-};
 
 // Forward declaration
 class Flusher;
