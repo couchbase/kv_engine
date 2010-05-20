a89d7fb296205df77f428a32103070e4166e5104
Dustin Sallings <dustin@spy.net>
2010-05-19 18:16:17 -0700

    Rewrote atomic_ptr_test using new test primitive.
    Change-Id: I4e7ce8e3d68776fa1dbf7fe4733bb4499a4d7117
    Reviewed-on: http://review.northscale.com:8080/118
    Reviewed-by: Sean Lynch <seanl@literati.org>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- t/atomic_ptr_test.cc
+++ t/atomic_ptr_test.cc
@@ -3,6 +3,8 @@
 
 #include "atomic.hh"
 #include "locks.hh"
+#include "threadtests.hh"
+
 #define NUM_THREADS 50
 #define NUM_TIMES 1000000
 
@@ -26,84 +28,59 @@ private:
 
 Atomic<int> Doodad::numInstances(0);
 
-struct thread_args {
-    SyncObject mutex;
-    SyncObject gate;
-    int counter;
+class AtomicPtrTest : public Generator<bool> {
+public:
+
+    AtomicPtrTest(RCPtr<Doodad> p) : ptr(p) {}
+
+    bool operator()() {
+        for (int i = 0; i < NUM_TIMES; ++i) {
+            switch (rand() % 4) {
+            case 0:
+                ptr.reset(new Doodad);
+                break;
+            case 1:
+                {
+                    RCPtr<Doodad> d(new Doodad);
+                    ptr.reset(d);
+                }
+                break;
+            case 2:
+                {
+                    RCPtr<Doodad> d(new Doodad);
+                    d.reset();
+                }
+                break;
+            case 3:
+                {
+                    RCPtr<Doodad> d(ptr);
+                    d.reset();
+                }
+                break;
+            default:
+                assert(false);
+            }
+        }
+
+        return true;
+    }
+
+private:
     RCPtr<Doodad> ptr;
 };
 
-void *launch_test_thread(void *arg) {
-    struct thread_args *args = static_cast<struct thread_args *>(arg);
-    int i(0);
-    LockHolder lh(args->mutex);
-    args->gate.acquire();
-    args->counter++;
-    args->gate.release();
-    args->gate.notify();
-    args->mutex.wait();
-    lh.unlock();
-    for (i = 0; i < NUM_TIMES; ++i) {
-        switch (rand() % 4) {
-        case 0:
-            args->ptr.reset(new Doodad);
-            break;
-        case 1:
-            {
-                RCPtr<Doodad> d(new Doodad);
-                args->ptr.reset(d);
-            }
-            break;
-        case 2:
-            {
-                RCPtr<Doodad> d(new Doodad);
-                d.reset();
-            }
-            break;
-        case 3:
-            {
-                RCPtr<Doodad> d(args->ptr);
-                d.reset();
-            }
-            break;
-        default:
-            assert(false);
-        }
-    }
+static void testAtomicPtr() {
+    // Just do a bunch.
+    RCPtr<Doodad> dd;
+    AtomicPtrTest *testGen = new AtomicPtrTest(dd);
 
-    return static_cast<void *>(0);
+    getCompletedThreads<bool>(NUM_THREADS, testGen);
+
+    delete testGen;
+    dd.reset();
+    assert(Doodad::getNumInstances() == 0);
 }
 
 int main() {
-    pthread_t threads[NUM_THREADS];
-    int i(0), rc(0), result(-1);
-    struct thread_args args;
-
-    args.counter = 0;
-
-    for (i = 0; i < NUM_THREADS; ++i) {
-        rc = pthread_create(&threads[i], NULL, launch_test_thread, &args);
-        assert(rc == 0);
-    }
-
-    // Wait for all threads to reach the starting gate
-    int counter;
-    while (true) {
-        LockHolder lh(args.gate);
-        counter = args.counter;
-        if (counter == NUM_THREADS) {
-            break;
-        }
-        args.gate.wait();
-    }
-    args.mutex.notify();
-
-    for (i = 0; i < NUM_THREADS; ++i) {
-        rc = pthread_join(threads[i], reinterpret_cast<void **>(&result));
-        assert(rc == 0);
-        assert(result == 0);
-    }
-    args.ptr.reset();
-    assert(Doodad::getNumInstances() == 0);
-    return 0;
+    testAtomicPtr();
 }
