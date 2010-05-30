94cf33b85148a743b4fc9509588eba6d70efee3a
Dustin Sallings <dustin@spy.net>
2010-05-29 21:29:41 -0700

    Move the bulk of the dispatcher implementation into the .cc file.
    Change-Id: Ie3160a01bfb7696db70bdf8f2bc8691eeca0717c
    Reviewed-on: http://review.northscale.com:8080/247
    Tested-by: Matt Ingenthron <ingenthr@gmail.com>
    Reviewed-by: Matt Ingenthron <ingenthr@gmail.com>

--- dispatcher.hh
+++ dispatcher.hh
@@ -104,90 +104,14 @@ public:
     }
 
     TaskId schedule(shared_ptr<DispatcherCallback> callback, int priority=0,
-                    double sleeptime=0) {
-        getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "Scheduling a new task\n");
-        LockHolder lh(mutex);
-        TaskId task(new Task(callback, priority, sleeptime));
-        queue.push(task);
-        mutex.notify();
-        return TaskId(task);
-    }
+                    double sleeptime=0);
 
-    TaskId wake(TaskId task) {
-        cancel(task);
-        TaskId oldTask(task);
-        TaskId newTask(new Task(*oldTask));
-        queue.push(newTask);
-        mutex.notify();
-        return TaskId(newTask);
-    }
+    TaskId wake(TaskId task);
 
     void start();
+    void stop();
 
-    void run() {
-        getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "Dispatcher starting\n");
-        while (state == dispatcher_running) {
-            LockHolder lh(mutex);
-            if (queue.empty()) {
-                // Wait forever
-                mutex.wait();
-            } else {
-                TaskId task = queue.top();
-                assert(task);
-                LockHolder tlh(task->mutex);
-                switch (task->state) {
-                case task_sleeping:
-                    {
-                        struct timeval tv;
-                        gettimeofday(&tv, NULL);
-                        if (less_tv(tv, task->waketime)) {
-                            tlh.unlock();
-                            mutex.wait(task->waketime);
-                        } else {
-                            task->state = task_running;
-                        }
-                    }
-                    break;
-                case task_running:
-                    queue.pop();
-                    lh.unlock();
-                    tlh.unlock();
-                    try {
-                        if(task->run(*this, TaskId(task))) {
-                            reschedule(task);
-                        }
-                    } catch (std::exception& e) {
-                        std::cerr << "exception caught in task " << task->name << ": " << e.what() << std::endl;
-                    } catch(...) {
-                        std::cerr << "Caught a fatal exception in task" << task->name <<std::endl;
-                    }
-                    break;
-                case task_dead:
-                    queue.pop();
-                    break;
-                default:
-                    throw std::runtime_error("Unexpected state for task");
-                }
-            }
-        }
-
-        state = dispatcher_stopped;
-        mutex.notify();
-        getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "Dispatcher exited\n");
-    }
-
-    void stop() {
-        LockHolder lh(mutex);
-        if (state == dispatcher_stopped || state == dispatcher_stopping) {
-            return;
-        }
-        getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "Stopping dispatcher\n");
-        state = dispatcher_stopping;
-        mutex.notify();
-        lh.unlock();
-        pthread_join(thread, NULL);
-        getLogger()->log(EXTENSION_LOG_DEBUG, NULL, "Dispatcher stopped\n");
-    }
+    void run();
 
     void snooze(TaskId t, double sleeptime) {
         t->snooze(sleeptime);
