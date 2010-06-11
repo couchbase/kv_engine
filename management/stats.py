27e26dd56899a7552f140090948508e2bb070623
Patrick Galbraith <patg@northscale.com>
2010-06-11 18:16:34 -0400

    BUG: #1185 - created stats.py script to obtain vkey stats
    Change-Id: I27613b31045b0ddd4ab8768110e65670da2e1951
    Reviewed-on: http://review.northscale.com:8080/426
    Tested-by: Dustin Sallings <dustin@spy.net>
    Reviewed-by: Dustin Sallings <dustin@spy.net>

---
+++ management/stats.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+import clitool
+import sys
+
+def stats_vkey(mc, key):
+    cmd = 'vkey ' + key
+    vbs = mc.stats(cmd)
+    for stat in vbs:
+        print "stat: %s value: %s" % (stat, vbs[stat])
+
+if __name__ == '__main__':
+
+    c = clitool.CliTool()
+
+    c.addCommand('vkey', stats_vkey, 'keyname')
+
+    c.execute()
