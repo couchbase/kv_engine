a4c8f8395bd4aa26b104c5690bc777f589f1e5ff
Dustin Sallings <dustin@spy.net>
2010-06-12 00:45:37 -0700

    Use some pragmas that make the test db a bit faster.
    This also increases the coverage a bit.
    
    Change-Id: Ibf4492a5edc2ca53f2b3c2729990e45e44e470fa
    Reviewed-on: http://review.northscale.com:8080/430
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

---
+++ t/test_pragma.sql
@@ -0,0 +1,8 @@
+--
+-- This is good for testing.  It's bad for production.
+-- Don't deploy stuff like this.
+--
+-- For more info:  http://www.sqlite.org/pragma.html
+--
+pragma synchronous = OFF;
+pragma journal_mode = OFF;
