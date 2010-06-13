1f3239c6f82d87fd039a79281ebc6e4cb5328512
Dustin Sallings <dustin@spy.net>
2010-06-13 11:13:57 -0700

    Stats vkey output formatting cleanup.
    Change-Id: I455f1bc64c4b09d06305dd624a1b6092d099dc14
    Reviewed-on: http://review.northscale.com:8080/471
    Tested-by: Patrick M. Galbraith <patg@northscale.com>
    Reviewed-by: Patrick M. Galbraith <patg@northscale.com>

--- management/stats.py
+++ management/stats.py
@@ -3,11 +3,17 @@
 import clitool
 import sys
 
+def stats_formatter(stats, prefix=" "):
+    longest = max((len(x) + 2) for x in stats.keys())
+    for stat, val in sorted(stats.items()):
+        s = stat + ":"
+        print "%s%s%s" % (prefix, s.ljust(longest), val)
+
 def stats_vkey(mc, key):
     cmd = 'vkey ' + key
     vbs = mc.stats(cmd)
-    for stat in vbs:
-        print "stat: %s value: %s" % (stat, vbs[stat])
+    print "verification for key", key
+    stats_formatter(vbs)
 
 if __name__ == '__main__':
 
