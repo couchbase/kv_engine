da6d6c3b32fe485689eec5edec575158ab8e78ff
Matt Ingenthron <ingenthr@cep.net>
2010-06-16 11:09:45 -0700

    Backport: If tap_idle_timeout == 0, just wait "forever"
    This is part of the fix for bug 1357.
    
    It is a backport of
    e6994f1f97f055b0e6efbddeb44c9a035c945d34
    http://review.northscale.com:8080/#change,578
    
    Change-Id: Iee46fd4ee1d77ddaa33d1a7f1918c4e93f55117e
    Reviewed-on: http://review.northscale.com:8080/589
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- ep_engine.h
+++ ep_engine.h
@@ -382,9 +382,7 @@ public:
         }
 
         if (tapIdleTimeout == 0) {
-            getLogger()->log(EXTENSION_LOG_WARNING, NULL,
-                             "Invalid value for tap_idle_timeout specified. Using default\n");
-            tapIdleTimeout = DEFAULT_TAP_IDLE_TIMEOUT;
+            tapIdleTimeout = (size_t)-1;
         }
 
         if (ret == ENGINE_SUCCESS) {
@@ -1116,8 +1114,13 @@ private:
         }
 
         if (shouldPause) {
-            double sleeptime = nextTapNoop - ep_current_time();
-            tapNotifySync.wait(sleeptime);
+            double diff = nextTapNoop - ep_current_time();
+            if (diff > 0) {
+                tapNotifySync.wait(diff);
+            } else {
+                tapNotifySync.wait();
+            }
+
             if (shutdown) {
                 return;
             }
