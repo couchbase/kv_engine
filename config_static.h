cc251c2f13c80e27efe1a3e1bcd20f12040ebe17
Trond Norbye <trond.norbye@gmail.com>
2010-07-20 13:07:13 +0200

    Moved the AH_BOTTOM parts to config_static.h
    Change-Id: Ia765613f0f64f6a9a941394cd0954e883c8276bf
    Reviewed-on: http://review.northscale.com/1276
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

---
+++ config_static.h
@@ -0,0 +1,42 @@
+/* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/* Consider this file as an extension to config.h, just that it contains
+ * static text. The intention is to reduce the number of #ifdefs in the rest
+ * of the source files without having to put all of them in AH_BOTTOM
+ * in configure.ac.
+ */
+#ifndef CONFIG_STATIC_H
+#define CONFIG_STATIC_H 1
+
+#ifdef HAVE_INTTYPES_H
+#include <inttypes.h>
+#endif
+
+#if ((defined (__SUNPRO_C) || defined(__SUNPRO_CC)) || defined __GNUC__)
+#define EXPORT_FUNCTION __attribute__ ((visibility("default")))
+#else
+#define EXPORT_FUNCTION
+#endif
+
+#if HAVE_ARPA_INET_H
+#include <arpa/inet.h>
+#endif
+
+#ifdef HAVE_NETDB_H
+#include <netdb.h>
+#endif
+
+#ifndef HAVE_GETHRTIME
+typedef uint64_t hrtime_t;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+    extern hrtime_t gethrtime(void);
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
+#endif
