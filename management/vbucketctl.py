84865675ab661527fd8538ff435a3f203c5b0e17
Keith Batten <kbatten@gmail.com>
2010-08-18 16:22:28 -0700

    bug 1988 - added optional sasl auth
    Change-Id: I57fed1cfa580cce831ba5ba3e8f536a3694c4d6e
    Reviewed-on: http://review.northscale.com/2013
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

--- management/vbucketctl.py
+++ management/vbucketctl.py
@@ -2,17 +2,29 @@
 
 import clitool
 
-def listvb(mc):
+def listvb(mc,auth="none",username="",password=""):
+    if auth == "sasl":
+        mc.sasl_auth_plain(username,password)
     vbs = mc.stats('vbucket')
     for (vb, state) in sorted(list(vbs.items())):
         print "vbucket", vb[3:], state
+
+def setvb(mc,vbid,vbstate,auth="none",username="",password=""):
+    if auth == "sasl":
+        mc.sasl_auth_plain(username,password)
+    mc.set_vbucket_state(vbid,vbstate)
+
+def rmvb(mc,vbid,auth="none",username="",password=""):
+    if auth == "sasl":
+        mc.sasl_auth_plain(username,password)
+    mc.delete_vbucket(vbid)
 
 if __name__ == '__main__':
 
     c = clitool.CliTool()
 
-    c.addCommand('list', listvb)
-    c.addCommand('set', 'set_vbucket_state', 'set [vbid] [vbstate]')
-    c.addCommand('rm', 'delete_vbucket', 'rm [vbid]')
+    c.addCommand('list', listvb, 'list sasl [username] [password]')
+    c.addCommand('set', setvb, 'set [vbid] [vbstate] sasl [username] [password]')
+    c.addCommand('rm', rmvb, 'rm [vbid] sasl [username] [password]')
 
     c.execute()
