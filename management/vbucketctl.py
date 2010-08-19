27cf59fcbae34914aa8d511b6423f97dec1bee08
Dustin Sallings <dustin@spy.net>
2010-08-19 11:44:50 -0700

    Python style updates.
    Change-Id: Ibab6eee217cb49b14537353c0e7e541ce9641c7b
    Reviewed-on: http://review.northscale.com/2035
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- management/vbucketctl.py
+++ management/vbucketctl.py
@@ -2,21 +2,21 @@
 
 import clitool
 
-def listvb(mc,auth="none",username="",password=""):
+def listvb(mc, auth="none", username="", password=""):
     if auth == "sasl":
-        mc.sasl_auth_plain(username,password)
+        mc.sasl_auth_plain(username, password)
     vbs = mc.stats('vbucket')
     for (vb, state) in sorted(list(vbs.items())):
         print "vbucket", vb[3:], state
 
-def setvb(mc,vbid,vbstate,auth="none",username="",password=""):
+def setvb(mc, vbid, vbstate, auth="none", username="", password=""):
     if auth == "sasl":
-        mc.sasl_auth_plain(username,password)
-    mc.set_vbucket_state(vbid,vbstate)
+        mc.sasl_auth_plain(username, password)
+    mc.set_vbucket_state(vbid, vbstate)
 
-def rmvb(mc,vbid,auth="none",username="",password=""):
+def rmvb(mc, vbid, auth="none", username="", password=""):
     if auth == "sasl":
-        mc.sasl_auth_plain(username,password)
+        mc.sasl_auth_plain(username, password)
     mc.delete_vbucket(vbid)
 
 if __name__ == '__main__':
