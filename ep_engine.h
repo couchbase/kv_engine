5aabe9f6e92093ee6f092f5bd661289adc208786
Trond Norbye <trond.norbye@gmail.com>
2010-09-03 15:11:14 +0200

    Removed dead/unused code
    Change-Id: I8333ad921504238a6478026e07086e99a10376a0
    Reviewed-on: http://review.northscale.com/2234
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- ep_engine.h
+++ ep_engine.h
@@ -44,51 +44,8 @@ extern "C" {
                                       GET_SERVER_API get_server_api,
                                       ENGINE_HANDLE **handle);
     void *EvpNotifyTapIo(void*arg);
-    void EvpClockHandler(const void *cookie,
-                         ENGINE_EVENT_TYPE type,
-                         const void *event_data,
-                         const void *cb_data);
 }
 
-#ifdef linux
-/* /usr/include/netinet/in.h defines macros from ntohs() to _bswap_nn to
- * optimize the conversion functions, but the prototypes generate warnings
- * from gcc. The conversion methods isn't the bottleneck for my app, so
- * just remove the warnings by undef'ing the optimization ..
- */
-#undef ntohs
-#undef ntohl
-#undef htons
-#undef htonl
-#endif
-
-class BoolCallback : public Callback<bool> {
-public:
-    void callback(bool &theValue) {
-        value = theValue;
-    }
-
-    bool getValue() {
-        return value;
-    }
-
-private:
-    bool value;
-};
-
-class BoolPairCallback : public Callback<std::pair<bool, int64_t> > {
-public:
-    void callback(std::pair<bool, int64_t> &theValue) {
-        value = theValue.first;
-    }
-
-    bool getValue() {
-        return value;
-    }
-
-private:
-    bool value;
-};
 
 // Forward decl
 class BinaryMessage;
