17bf24bc495256e663d72f8241932c5015e68d6f
Dustin Sallings <dustin@spy.net>
2010-09-13 13:15:31 -0700

    Some flusher doc updates.
    Change-Id: Ia9bf1a027d74a0dfc333765dbddd04009f618251
    Reviewed-on: http://review.northscale.com/2386
    Reviewed-by: Dustin Sallings <dustin@spy.net>
    Tested-by: Dustin Sallings <dustin@spy.net>

--- flusher.hh
+++ flusher.hh
@@ -17,6 +17,9 @@ enum flusher_state {
 
 class Flusher;
 
+/**
+ * A DispatcherCallback adaptor over Flusher.
+ */
 class FlusherStepper : public DispatcherCallback {
 public:
     FlusherStepper(Flusher* f) : flusher(f) { }
@@ -25,12 +28,17 @@ private:
     Flusher *flusher;
 };
 
+/**
+ * Manage persistence of data for an EventuallyPersistentStore.
+ */
 class Flusher {
 public:
+
     Flusher(EventuallyPersistentStore *st, Dispatcher *d) :
         store(st), _state(initializing), dispatcher(d),
         flushQueue(NULL) {
     }
+
     ~Flusher() {
         if (_state != stopped) {
             getLogger()->log(EXTENSION_LOG_WARNING, NULL,
