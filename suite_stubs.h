a5899de188ad175744e90762c21c1c4a37d0daed
Dustin Sallings <dustin@spy.net>
2010-10-23 16:22:13 -0700

    Build and run generated test suite (disabled by default).
    The generated test suite runs a variety of sequences of commands and
    ensures the engine leaves stuff in the expected order.
    
    Change-Id: I0ba76c69e667c7393bf3998c013767afe48fb360
    Reviewed-on: http://review.membase.org/3393
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

---
+++ suite_stubs.h
@@ -0,0 +1,28 @@
+#ifndef TESTSUITE_H
+#define TESTSUITE_H 1
+
+#include "ep_testsuite.h"
+
+bool teardown(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void delay(int amt);
+
+void add(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void append(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void decr(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void decrWithDefault(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void prepend(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void flush(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void del(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void set(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void incr(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void incrWithDefault(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+
+void checkValue(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1, const char* exp);
+void assertNotExists(ENGINE_HANDLE *h, ENGINE_HANDLE_V1 *h1);
+void assertHasError(void);
+void assertHasNoError(void);
+
+extern int expiry;
+extern struct test_harness testHarness;
+
+#endif /* TESTSUITE_H */
