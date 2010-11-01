b1ad66f2ff72c687a4bd7b51feda7a005c727724
Dustin Sallings <dustin@spy.net>
2010-10-31 21:47:16 -0700

    Added a simple RingBuffer.
    Will use this for tracking previous dispatcher jobs.
    
    Change-Id: I13148a56bb501aabd1914a312031b80aa4149510
    Reviewed-on: http://review.membase.org/3449
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

---
+++ ringbuffer.hh
@@ -0,0 +1,78 @@
+#ifndef RINGBUFFER_HH
+#define RINGBUFFER_HH
+
+#include <cassert>
+#include <vector>
+#include <algorithm>
+
+#include "common.hh"
+
+/**
+ * A RingBuffer holds a fixed number of elements of type T.
+ */
+template <typename T>
+class RingBuffer {
+public:
+
+    /**
+     * Construct a RingBuffer to hold the given number of elements.
+     */
+    explicit RingBuffer(size_t s) : pos(0), max(s), wrapped(false) {
+        storage = new T[max];
+    }
+
+    ~RingBuffer() {
+        delete[] storage;
+    }
+
+    /**
+     * How many elements are currently stored in this ring buffer?
+     */
+    size_t size() {
+        return wrapped ? max : pos;
+    }
+
+    /**
+     * Add an object to the RingBuffer.
+     */
+    void add(T ob) {
+        if (pos == max) {
+            wrapped = true;
+            pos = 0;
+        }
+        storage[pos++] = ob;
+    }
+
+    /**
+     * Remove all items.
+     */
+    void reset() {
+        pos = 0;
+        wrapped = 0;
+    }
+
+    /**
+     * Copy out the contents of this RingBuffer into the a vector.
+     */
+    std::vector<T> contents() {
+        std::vector<T> rv;
+        rv.resize(size());
+        size_t copied(0);
+        if (wrapped && pos != max) {
+            std::copy(storage + pos, storage + max, rv.begin());
+            copied = max - pos;
+        }
+        std::copy(storage, storage + pos, rv.begin() + copied);
+        return rv;
+    }
+
+private:
+    T *storage;
+    size_t pos;
+    size_t max;
+    bool wrapped;
+
+    DISALLOW_COPY_AND_ASSIGN(RingBuffer);
+};
+
+#endif /* RINGBUFFER_HH */
---
+++ t/ringbuffer_test.cc
@@ -0,0 +1,70 @@
+#include "config.h"
+
+#include <iostream>
+#include <cassert>
+#include <vector>
+
+#include "ringbuffer.hh"
+
+static void testEmpty() {
+    RingBuffer<int> rb(10);
+    assert(rb.size() == 0);
+    std::vector<int> v(rb.contents());
+    assert(v.size() == 0);
+}
+
+static void testPartial() {
+    RingBuffer<int> rb(10);
+    rb.add(1);
+    rb.add(2);
+    rb.add(3);
+    assert(rb.size() == 3);
+    std::vector<int> v(rb.contents());
+    assert(v.size() == 3);
+    std::vector<int> expected;
+    expected.push_back(1);
+    expected.push_back(2);
+    expected.push_back(3);
+
+    assert(v == expected);
+}
+
+static void testFull() {
+    RingBuffer<int> rb(3);
+    rb.add(1);
+    rb.add(2);
+    rb.add(3);
+    assert(rb.size() == 3);
+    std::vector<int> v(rb.contents());
+    assert(v.size() == 3);
+    std::vector<int> expected;
+    expected.push_back(1);
+    expected.push_back(2);
+    expected.push_back(3);
+
+    assert(v == expected);
+}
+
+static void testWrapped() {
+    RingBuffer<int> rb(2);
+    rb.add(1);
+    rb.add(2);
+    rb.add(3);
+    assert(rb.size() == 2);
+    std::vector<int> v(rb.contents());
+    assert(v.size() == 2);
+    std::vector<int> expected;
+    expected.push_back(2);
+    expected.push_back(3);
+    assert(v == expected);
+}
+
+int main() {
+
+    testEmpty();
+    testPartial();
+    testFull();
+    testWrapped();
+
+    return 0;
+}
