3d39122deaf85ca343c8c139f8c1435813ce54f3
Dustin Sallings <dustin@spy.net>
2011-01-19 13:10:31 -0800

    Allow configuration of shard patterns in dbconvert.
    Change-Id: I71e27b0dc479bd3939f729cc2a163b354f8c8d84
    Reviewed-on: http://review.membase.org/4282
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- management/dbconvert.cc
+++ management/dbconvert.cc
@@ -16,7 +16,8 @@ using namespace std;
 
 static KVStore *getStore(EPStats &st,
                          const char *path,
-                         const char *strategyName) {
+                         const char *strategyName,
+                         const char *shardPattern) {
     db_type dbStrategy;
 
     if (!KVStore::stringToType(strategyName, dbStrategy)) {
@@ -24,7 +25,6 @@ static KVStore *getStore(EPStats &st,
         exit(EX_USAGE);
     }
 
-    const char *shardPattern("%d/%b-%i.sqlite");
     const char *initFile(NULL);
     const char *postInitFile(NULL);
     size_t nVBuckets(1024);
@@ -105,7 +105,9 @@ static void usage(const char *cmd) {
          << endl
          << "Optional arguments:" << endl
          << "  --src-strategy=someStrategy (default=multiDB)" << endl
+         << "  --src-pattern=shardPattern (default=%d/%b-%i.sqlite)" << endl
          << "  --dest-strategy=someStrategy (default=multiMTVBDB)" << endl
+         << "  --dest-pattern=somePattern (default=%d/%b-%i.mb)" << endl
          << "  --remove-crlf" << endl
          << "  --txn-size=someNumber (default=10000)" << endl
          << "  --report-every=someNumber (default=10000)" << endl;
@@ -116,13 +118,17 @@ int main(int argc, char **argv) {
     const char *cmd(argv[0]);
     const char *srcPath(NULL), *srcStrategy("multiDB");
     const char *destPath(NULL), *destStrategy("multiMTVBDB");
+    const char *srcShardPattern("%d/%b-%i.sqlite");
+    const char *destShardPattern("%d/%b-%i.mb");
     size_t txnSize(10000), reportEvery(10000);
     int killCrlf(0);
 
     /* options descriptor */
     static struct option longopts[] = {
         { "src-strategy",  required_argument, NULL,      's' },
+        { "src-pattern",   required_argument, NULL,      'p' },
         { "dest-strategy", required_argument, NULL,      'S' },
+        { "dest-pattern",  required_argument, NULL,      'P' },
         { "remove-crlf",   no_argument,       &killCrlf, 'x' },
         { "txn-size",      required_argument, NULL,      't' },
         { "report-every",  required_argument, NULL,      'r' },
@@ -135,8 +141,14 @@ int main(int argc, char **argv) {
         case 's':
             srcStrategy = optarg;
             break;
+        case 'p':
+            srcShardPattern = optarg;
+            break;
         case 'S':
             destStrategy = optarg;
+            break;
+        case 'P':
+            destShardPattern = optarg;
             break;
         case 't':
             txnSize = static_cast<size_t>(atoi(optarg));
@@ -164,8 +176,10 @@ int main(int argc, char **argv) {
 
     EPStats srcStats, destStats;
 
-    KVStore *src(getStore(srcStats, srcPath, srcStrategy));
-    KVStore *dest(getStore(destStats, destPath, destStrategy));
+    KVStore *src(getStore(srcStats, srcPath,
+                          srcStrategy, srcShardPattern));
+    KVStore *dest(getStore(destStats, destPath,
+                           destStrategy, destShardPattern));
 
     Mover mover(dest, txnSize, killCrlf, reportEvery);
     cout << "Each . represents " << reportEvery << " items moved." << endl;
