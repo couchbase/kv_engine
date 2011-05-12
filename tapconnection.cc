f736bfab194ad4fc65d52417fbc73f403189a9fb
Dustin Sallings <dustin@spy.net>
2011-05-11 20:09:17 -0700

    Fix tap pending_disk_backfill stat.
    The name of the stat and the implementation are entirely the opposite
    of what we present.
    
    Change-Id: I24ac894c941e6d61ed598862c013e0531dcaf888
    Reviewed-on: http://review.membase.org/6184
    Tested-by: Trond Norbye <trond.norbye@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- tapconnection.cc
+++ tapconnection.cc
@@ -660,7 +660,7 @@ void TapConnection::addStats(ADD_STAT add_stat, const void *c) {
     addTapStat("suspended", this, isSuspended(), add_stat, c);
     addTapStat("paused", this, paused, add_stat, c);
     addTapStat("pending_backfill", this, pendingBackfill, add_stat, c);
-    addTapStat("pending_disk_backfill", this, !isPendingDiskBackfill(),
+    addTapStat("pending_disk_backfill", this, isPendingDiskBackfill(),
                add_stat, c);
     if (reconnects > 0) {
         addTapStat("reconnects", this, reconnects, add_stat, c);
