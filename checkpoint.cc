ce5e4b417e5e923d81398d9cb9c42819b053a095
Chiyoung Seo <chiyoung.seo@gmail.com>
2011-06-03 03:32:39 -0700

    Fix to the bug in setting TAP cursor's offset in registration.
    Change-Id: I5d3b5d368d2925baab4dc79ea2937651799c95d7
    Reviewed-on: http://review.couchbase.org/6744
    Reviewed-by: Bin Cui <bin.cui@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- checkpoint.cc
+++ checkpoint.cc
@@ -330,7 +330,9 @@ bool CheckpointManager::registerTAPCursor(const std::string &name, uint64_t chec
         // If the checkpoint to start with is not found, set the TAP cursor to the current
         // open checkpoint. This case requires the full materialization through backfill.
         it = --(checkpointList.end());
-        CheckpointCursor cursor(it, (*it)->begin(), 0, closedCheckpointOnly);
+        CheckpointCursor cursor(it, (*it)->begin(),
+                            numItems - ((*it)->getNumItems() + 1), // 1 is for checkpoint start item
+                            closedCheckpointOnly);
         tapCursors[name] = cursor;
         (*it)->incrReferenceCounter();
     } else {
