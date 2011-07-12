82050d5da0e4be70b0131eb996873e1f686b8d68
Filipe David Manana <fdmanana@apache.org>
2011-07-12 18:44:43 +0100

    Fix test suite compilation errors
    This fixes some missing function declaration errors:
    
      CXX    ep_testsuite_la-ep_testsuite.lo
    cc1plus: warnings being treated as errors
    ep_testsuite.cc: In function ‘test_result prepare(engine_test_t*)’:
    ep_testsuite.cc:5425: error: no previous declaration for ‘test_result prepare(engine_test_t*)’ [-Wmissing-declarations]
    ep_testsuite.cc: In function ‘void cleanup(engine_test_t*, test_result)’:
    ep_testsuite.cc:5447: error: no previous declaration for ‘void cleanup(engine_test_t*, test_result)’ [-Wmissing-declarations]
    ep_testsuite.cc: In function ‘bool teardown_suite()’:
    ep_testsuite.cc:5965: error: no previous declaration for ‘bool teardown_suite()’ [-Wmissing-declarations]
    make[2]: *** [ep_testsuite_la-ep_testsuite.lo] Error 1
    
    Change-Id: I2363240c962a09740301a4a85f0559bc04981bcd
    Reviewed-on: http://review.couchbase.org/7913
    Tested-by: Filipe David Borba Manana <fdmanana@gmail.com>
    Reviewed-by: Dustin Sallings <dustin@spy.net>

--- ep_testsuite.h
+++ ep_testsuite.h
@@ -14,6 +14,13 @@ engine_test_t* get_tests(void);
 MEMCACHED_PUBLIC_API
 bool setup_suite(struct test_harness *th);
 
+MEMCACHED_PUBLIC_API
+bool teardown_suite();
+
+enum test_result prepare(engine_test_t *test);
+
+void cleanup(engine_test_t *test, enum test_result result);
+
 #ifdef __cplusplus
 }
 #endif
