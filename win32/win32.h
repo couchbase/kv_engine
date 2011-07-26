f16fdff99d2c8d4c9b9525d6b2d3616a0e71898b
Bin Cui <bin.cui@gmail.com>
2011-07-25 18:52:39 -0700

    Need to use winsock api for tcp calls on windows
    Use WSARecv instead of recv() and WSAGetLastError for retrieving
    error code.
    
    Change-Id: I55d2a84747f5d19ce95746445f5b23a668245176
    Reviewed-on: http://review.couchbase.org/8360
    Tested-by: Bin Cui <bin.cui@gmail.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- win32/win32.h
+++ win32/win32.h
@@ -28,4 +28,51 @@
 
 #define setsockopt(_socket, _level, _option_name, _option_value, _option_len) \
         setsockopt((_socket), (_level), (_option_name), (char*)(_option_value), (_option_len))
+
+static inline void mapErr(int error) {
+    switch(error) {
+        default:
+            errno = ECONNRESET;
+            break;
+        case WSAEPFNOSUPPORT:
+            errno = EAFNOSUPPORT;
+            break;
+        case WSA_IO_PENDING:
+        case WSATRY_AGAIN:
+            errno = EAGAIN;
+            break;
+        case WSAEWOULDBLOCK:
+            errno = EWOULDBLOCK;
+            break;
+        case WSAEMSGSIZE:
+            errno = E2BIG;
+            break;
+        case WSAECONNRESET:
+            errno = 0;
+            break;
+    }
+}
+
+
+#define recv(a,b,c,d) mem_recv(a,b,c,d)
+
+static inline size_t mem_recv(int s, void *buf, int len, int unused)
+{
+    DWORD flags = 0;
+    DWORD dwBufferCount;
+    WSABUF wsabuf = { len, (char *)buf };
+    int error;
+
+    if(WSARecv((SOCKET)s, &wsabuf, 1, &dwBufferCount, &flags,
+              NULL, NULL) == 0) {
+        return dwBufferCount;
+    }
+    error = WSAGetLastError();
+    if (error == WSAECONNRESET) {
+        return 0;
+    }
+    mapErr(error);
+    return -1;
+}
+
 #endif /* WIN32_H */
