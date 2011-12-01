442497b156a734db4f82d881c9c3c2e03733e436
Chiyoung Seo <chiyoung.seo@gmail.com>
2011-12-01 09:48:00 -0800

    [Backport] Reset tap queue size when the queue is empty.
    We use a separate varaible to keep track of the queue size because
    std::list's size() actually scans the list to return its size,
    which is an expensive operation. When the queue becomes empty, the
    size variable should be reset.
    
    Change-Id: Ia0728078a2c1397072c97b20a87bb0fd6746fdcc
    Reviewed-on: http://review.couchbase.org/11407
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- tapconnection.cc
+++ tapconnection.cc
@@ -1371,7 +1371,7 @@ queued_item TapProducer::next(bool &shouldPause) {
     if (!queue->empty()) {
         queued_item qi = queue->front();
         queue->pop_front();
-        --queueSize;
+        queueSize = queue->empty() ? 0 : queueSize - 1;
         if (queueMemSize > sizeof(queued_item)) {
             queueMemSize.decr(sizeof(queued_item));
         } else {
