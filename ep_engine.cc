f671a7f6e1ab7b023580e28b98ca04da23e48f3d
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-01-13 17:07:21 -0800

    MB-4632 Meaningful error should be given to the restore client.
    Change-Id: Ie47aba5f628283400850d817c5b3575468592316
    Reviewed-on: http://review.couchbase.org/12359
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- ep_engine.cc
+++ ep_engine.cc
@@ -4045,7 +4045,9 @@ ENGINE_ERROR_CODE EventuallyPersistentEngine::handleRestoreCmd(const void *cooki
 {
     LockHolder lh(restore.mutex);
     if (restore.manager == NULL) { // we need another "mode" variable
-        if (response(NULL, 0, NULL, 0, NULL, 0, PROTOCOL_BINARY_RAW_BYTES,
+        std::string msg = "Restore mode is not enabled.";
+        if (response(NULL, 0, NULL, 0, msg.c_str(), msg.length(),
+                     PROTOCOL_BINARY_RAW_BYTES,
                      PROTOCOL_BINARY_RESPONSE_NOT_SUPPORTED, 0, cookie)) {
             return ENGINE_SUCCESS;
         }
