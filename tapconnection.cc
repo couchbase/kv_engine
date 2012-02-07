7253094c215c289fb10f7c1ba494da5453eccc1b
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-02-07 14:10:19 -0800

    Don't reschedule checkpoint items only for the registered TAP.
    If the TAP connection is for the registered client that is only
    interested in items from the closed checkpoints, only checkpoint
    items should not be rescheduled because its cursor is going to
    be reset to the beginning of its current checkpoint upon
    the reconnection. On the other hand, other TAP opaque messages
    should be rescheduled for the registered client.
    
    Change-Id: I7f0a267b935d7efe7bab5ca776fd111b74b96abb
    Reviewed-on: http://review.couchbase.org/13069
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- tapconnection.cc
+++ tapconnection.cc
@@ -427,17 +427,7 @@ void TapProducer::clearQueues_UNLOCKED() {
 
 void TapProducer::rollback() {
     LockHolder lh(queueLock);
-    if (registeredTAPClient && closedCheckpointOnly) {
-        // If the connection is for a registered TAP client that is only interested in closed
-        // checkpoints, we don't need to resend unACKed items to the client because its replication
-        // cursor is reset to the beginning of the checkpoint to which the cursor currently belongs.
-        tapLog.clear();
-        clearQueues_UNLOCKED();
-        seqno = initialAckSequenceNumber;
-        seqnoReceived = seqno -1;
-        checkpointMsgCounter = 0;
-        return;
-    }
+    bool reschedule_mutations = !(registeredTAPClient && closedCheckpointOnly);
 
     size_t checkpoint_msg_sent = 0;
     std::vector<uint16_t> backfillVBs;
@@ -457,7 +447,9 @@ void TapProducer::rollback() {
         case TAP_CHECKPOINT_START:
         case TAP_CHECKPOINT_END:
             ++checkpoint_msg_sent;
-            addCheckpointMessage_UNLOCKED(i->item);
+            if (reschedule_mutations) {
+                addCheckpointMessage_UNLOCKED(i->item);
+            }
             break;
         case TAP_FLUSH:
             addEvent_UNLOCKED(i->item);
@@ -475,7 +467,9 @@ void TapProducer::rollback() {
                             "TAP Checkpoint State for VBucket %d Not Found", i->vbucket);
                     }
                 }
-                addEvent_UNLOCKED(i->item);
+                if (reschedule_mutations) {
+                    addEvent_UNLOCKED(i->item);
+                }
             }
             break;
         case TAP_OPAQUE:
