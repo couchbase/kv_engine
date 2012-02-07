b59d2c282bae96eed3f4529d86aa0e22b5be4d2d
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-02-07 15:58:52 -0800

    Revert "Don't reschedule checkpoint items only for registered TAP"
    This changes breaks other tests for incremental backup clients.
    
    This reverts commit 3810cb80775b674eb7c955fa971a58adad66f5cb.
    
    Change-Id: I9ec15d0e5e244c13d90d767b8c41a5b2e05b78d7
    Reviewed-on: http://review.couchbase.org/13073
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- tapconnection.cc
+++ tapconnection.cc
@@ -427,7 +427,17 @@ void TapProducer::clearQueues_UNLOCKED() {
 
 void TapProducer::rollback() {
     LockHolder lh(queueLock);
-    bool reschedule_mutations = !(registeredTAPClient && closedCheckpointOnly);
+    if (registeredTAPClient && closedCheckpointOnly) {
+        // If the connection is for a registered TAP client that is only interested in closed
+        // checkpoints, we don't need to resend unACKed items to the client because its replication
+        // cursor is reset to the beginning of the checkpoint to which the cursor currently belongs.
+        tapLog.clear();
+        clearQueues_UNLOCKED();
+        seqno = initialAckSequenceNumber;
+        seqnoReceived = seqno -1;
+        checkpointMsgCounter = 0;
+        return;
+    }
 
     size_t checkpoint_msg_sent = 0;
     std::vector<uint16_t> backfillVBs;
@@ -447,9 +457,7 @@ void TapProducer::rollback() {
         case TAP_CHECKPOINT_START:
         case TAP_CHECKPOINT_END:
             ++checkpoint_msg_sent;
-            if (reschedule_mutations) {
-                addCheckpointMessage_UNLOCKED(i->item);
-            }
+            addCheckpointMessage_UNLOCKED(i->item);
             break;
         case TAP_FLUSH:
             addEvent_UNLOCKED(i->item);
@@ -467,9 +475,7 @@ void TapProducer::rollback() {
                             "TAP Checkpoint State for VBucket %d Not Found", i->vbucket);
                     }
                 }
-                if (reschedule_mutations) {
-                    addEvent_UNLOCKED(i->item);
-                }
+                addEvent_UNLOCKED(i->item);
             }
             break;
         case TAP_OPAQUE:
