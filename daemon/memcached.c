757689e298f8e09e6e4572087b3cdd5f69140d54
Mike Wiederhold <mike@couchbase.com>
2012-03-09 17:17:18 -0800

    MB-4058: Stats cachedump returns "SERVER ERROR Not implemented"
    stats cachedump was returning errors such as out of memory
    or errors saying the cachdump command wasn't in the correct
    format. Since we don't implement this we should just always
    return a not iplemented error.
    
    Change-Id: Icc835e24ccf5bf3b67fe1081d2433a898f9faad5
    Reviewed-on: http://review.couchbase.org/13854
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- daemon/memcached.c
+++ daemon/memcached.c
@@ -4148,6 +4148,7 @@ static char *process_stat(conn *c, token_t *tokens, const size_t ntokens) {
     } else if (strcmp(subcommand, "settings") == 0) {
         process_stat_settings(&append_stats, c);
     } else if (strcmp(subcommand, "cachedump") == 0) {
+#ifdef FUTURE
         char *buf = NULL;
         unsigned int bytes = 0, id, limit = 0;
 
@@ -4167,10 +4168,11 @@ static char *process_stat(conn *c, token_t *tokens, const size_t ntokens) {
             return NULL;
         }
 
-#ifdef FUTURE
         buf = item_cachedump(id, limit, &bytes);
-#endif
         write_and_free(c, buf, bytes);
+        return NULL;
+#endif
+        out_string(c, "SERVER_ERROR Not supported");
         return NULL;
     } else if (strcmp(subcommand, "aggregate") == 0) {
         server_stats(&append_stats, c, true);
