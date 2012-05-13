b71c2cbe2f1066a73da0beb61efea44b47b1b5e4
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-05-12 18:27:16 -0700

    Revert "Don't set TAP expiration time in the normal operation."
    This reverts commit 33f4c7d741863adf34c5595fd837e0b45c03431b.
    
    Change-Id: Ie30316c38910314e724dc83bbca0c055bc1815aa
    Reviewed-on: http://review.couchbase.org/15950
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- tapconnection.cc
+++ tapconnection.cc
@@ -115,6 +115,10 @@ TapProducer::TapProducer(EventuallyPersistentEngine &theEngine,
     evaluateFlags();
     queue = new std::list<queued_item>;
 
+    if (supportAck) {
+        expiryTime = ep_current_time() + ackGracePeriod;
+    }
+
     if (cookie != NULL) {
         setReserved(true);
     }
@@ -709,6 +713,7 @@ ENGINE_ERROR_CODE TapProducer::processAck(uint32_t s,
     std::list<TapLogElement>::iterator iter = tapLog.begin();
     ENGINE_ERROR_CODE ret = ENGINE_SUCCESS;
 
+    expiryTime = ep_current_time() + ackGracePeriod;
     if (isSeqNumRotated && s < seqnoReceived) {
         // if the ack seq number is rotated, reset the last seq number of each vbucket to 0.
         std::map<uint16_t, TapCheckpointState>::iterator it = tapCheckpointState.begin();
