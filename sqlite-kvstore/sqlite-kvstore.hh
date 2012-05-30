66016b798e5c8e54dccba87a25e49e5320d44655
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-05-29 20:24:46 -0700

    MB-5401 Reopen database upon failures from the database layer
    Failures from the underlying database layer can happen by various
    reasons. For example, if a database file is deleted by mistake or
    a database directory is mounted to the NFS that sometimes has a
    connection issue, there would be failures from the underlying
    database layer. In this case, we should reopen database to get
    a new handle.
    
    Change-Id: Iaf67d7eac195c2e1c4a5c71ca26b6d89b11adfff
    Reviewed-on: http://review.couchbase.org/16508
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Jin Lim <jin@couchbase.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Reviewed-on: http://review.couchbase.org/16674
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- sqlite-kvstore/sqlite-kvstore.hh
+++ sqlite-kvstore/sqlite-kvstore.hh
@@ -240,6 +240,13 @@ private:
         db = NULL;
     }
 
+    void reopen() {
+        assert(strategy);
+        commit();
+        close();
+        open();
+    }
+
     shared_ptr<SqliteStrategy> strategy;
 
     bool intransaction;
