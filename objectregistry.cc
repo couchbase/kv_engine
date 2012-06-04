f5621212dd69222f4f7d837dd81658b24c25a384
Mike Wiederhold <mike@couchbase.com>
2012-06-04 15:07:19 -0700

    MB-5444: Use abort instead of assert in allocation hooks
    Assert calls memory allocating functions and causes recursive
    hooks calls.
    
    Change-Id: If0c6f640e4f4d654514c872e99653882d8cb56dd
    Reviewed-on: http://review.couchbase.org/17124
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- objectregistry.cc
+++ objectregistry.cc
@@ -125,7 +125,9 @@ bool ObjectRegistry::memoryAllocated(size_t mem) {
    }
    EPStats &stats = engine->getEpStats();
    stats.totalMemory.incr(mem);
-   assert(stats.totalMemory.get() < GIGANTOR);
+   if (stats.totalMemory.get() < GIGANTOR) {
+        abort();
+   }
    return true;
 }
 
@@ -136,6 +138,8 @@ bool ObjectRegistry::memoryDeallocated(size_t mem) {
    }
    EPStats &stats = engine->getEpStats();
    stats.totalMemory.decr(mem);
-   assert(stats.totalMemory.get() < GIGANTOR);
+   if (stats.totalMemory.get() < GIGANTOR) {
+       abort();
+   }
    return true;
 }
