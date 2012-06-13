debaa5fb3bd47262c92d46e2b2e3136a0a0067c8
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-06-12 19:38:10 -0700

    MB-5540 Set vbucket Id correctly in mc_bin_client::get_vb_state()
    Change-Id: Iab0b701c7fd4efab222ed197a217f3787aa6463f
    Reviewed-on: http://review.couchbase.org/17182
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- management/mc_bin_client.py
+++ management/mc_bin_client.py
@@ -234,8 +234,9 @@ class MemcachedClient(object):
         return self._doCmd(memcacheConstants.CMD_SET_VBUCKET_STATE, '', '', state)
 
     def get_vbucket_state(self, vbucket):
-        return self._doCmd(memcacheConstants.CMD_GET_VBUCKET_STATE,
-                           str(vbucket), '')
+        assert isinstance(vbucket, int)
+        self.vbucketId = vbucket
+        return self._doCmd(memcacheConstants.CMD_GET_VBUCKET_STATE, '', '')
 
     def delete_vbucket(self, vbucket):
         assert isinstance(vbucket, int)
