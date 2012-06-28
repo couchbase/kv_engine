e21eb4509f5d0539da39fda3951bd6ef29d083e7
Liang Guo <liang@couchbase.com>
2012-06-27 19:33:39 -0700

    CBD-408 PersistenceCallback do not evict referenced replica items
    This change is required for working-set replica node mirroring.
    
    Change-Id: I889cfe53b62bfed3a01813996ec78bef36c32cc3
    Reviewed-on: http://review.couchbase.org/17696
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- ep.cc
+++ ep.cc
@@ -2088,7 +2088,8 @@ public:
                         double lower = static_cast<double>(stats->mem_low_wat);
                         if (current > lower) {
                             // Check if the key was already visited by all the cursors.
-                            if (vb->checkpointManager.eligibleForEviction(v->getKey())) {
+                            if (!v->isReferenced() &&
+                                vb->checkpointManager.eligibleForEviction(v->getKey())) {
                                 v->ejectValue(*stats, vb->ht);
                             }
                         }
