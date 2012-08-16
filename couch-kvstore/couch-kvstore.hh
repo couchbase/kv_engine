f8a017aa08e81ef7b97ec74b1ca30e943de9fdd5
Chiyoung Seo <chiyoung.seo@gmail.com>
2012-08-16 14:14:28 -0700

    MB-6254 Add doxygen style comments to CouchKVStore class
    Change-Id: I2788a3721bbea5f600b89cdc768ee5b8b80b8389
    Reviewed-on: http://review.couchbase.org/19756
    Reviewed-by: Chiyoung Seo <chiyoung.seo@gmail.com>
    Tested-by: Chiyoung Seo <chiyoung.seo@gmail.com>

--- couch-kvstore/couch-kvstore.hh
+++ couch-kvstore/couch-kvstore.hh
@@ -18,6 +18,9 @@
 class CouchKVStoreStats {
 
 public:
+    /**
+     * Default constructor
+     */
     CouchKVStoreStats() :
       docsCommitted(0), numOpen(0), numClose(0),
       numLoadedVb(0), numGetFailure(0), numSetFailure(0),
@@ -77,17 +80,46 @@ typedef union {
 
 const size_t COUCHSTORE_METADATA_SIZE(2 * sizeof(uint32_t) + sizeof(uint64_t));
 
+/**
+ * Class representing a document to be persisted in couchstore.
+ */
 class CouchRequest
 {
 public:
+    /**
+     * Constructor
+     *
+     * @param it Item instance to be persisted
+     * @param rev vbucket database revision number
+     * @param cb persistence callback
+     * @param del flag indicating if it is an item deletion or not
+     */
     CouchRequest(const Item &it, int rev, CouchRequestCallback &cb, bool del);
 
+    /**
+     * Get the vbucket id of a document to be persisted
+     *
+     * @return vbucket id of a document
+     */
     uint16_t getVBucketId(void) {
         return vbucketId;
     }
+
+    /**
+     * Get the revision number of the vbucket database file
+     * where the document is persisted
+     *
+     * @return revision number of the corresponding vbucket database file
+     */
     int getRevNum(void) {
         return fileRevNum;
     }
+
+    /**
+     * Get the couchstore Doc instance of a document to be persisted
+     *
+     * @return pointer to the couchstore Doc instance of a document
+     */
     Doc *getDbDoc(void) {
         if (deleteItem) {
             return NULL;
@@ -95,28 +127,75 @@ public:
             return &dbDoc;
         }
     }
+
+    /**
+     * Get the couchstore DocInfo instance of a document to be persisted
+     *
+     * @return pointer to the couchstore DocInfo instance of a document
+     */
     DocInfo *getDbDocInfo(void) {
         return &dbDocInfo;
     }
+
+    /**
+     * Get the callback instance for SET
+     *
+     * @return callback instance for SET
+     */
     Callback<mutation_result> *getSetCallback(void) {
         return callback.setCb;
     }
+
+    /**
+     * Get the callback instance for DELETE
+     *
+     * @return callback instance for DELETE
+     */
     Callback<int> *getDelCallback(void) {
         return callback.delCb;
     }
+
+    /**
+     * Get the sequence number of a document to be persisted
+     *
+     * @return sequence number of a document
+     */
     int64_t getItemId(void) {
         return itemId;
     }
+
+    /**
+     * Get the time in ns elapsed since the creation of this instance
+     *
+     * @return time in ns elapsed since the creation of this instance
+     */
     hrtime_t getDelta() {
         return (gethrtime() - start) / 1000;
     }
+
+    /**
+     * Get the length of a document body to be persisted
+     *
+     * @return length of a document body
+     */
     size_t getNBytes() {
         return valuelen;
     }
-    bool   isDelete() {
+
+    /**
+     * Return true if the document to be persisted is for DELETE
+     *
+     * @return true if the document to be persisted is for DELETE
+     */
+    bool isDelete() {
         return deleteItem;
     };
 
+    /**
+     * Get the key of a document to be persisted
+     *
+     * @return key of a document to be persisted
+     */
     const std::string& getKey(void) const {
         return key;
     }
@@ -138,20 +217,29 @@ private :
 };
 
 /**
- * Couchstore kv-store
+ * KVStore with couchstore as the underlying storage system
  */
 class CouchKVStore : public KVStore
 {
 public:
     /**
-     * Build it!
+     * Constructor
+     *
+     * @param theEngine EventuallyPersistentEngine instance
+     * @param read_only flag indicating if this kvstore instance is for read-only operations
      */
     CouchKVStore(EventuallyPersistentEngine &theEngine,
                  bool read_only = false);
+
+    /**
+     * Copy constructor
+     *
+     * @param from the source kvstore instance
+     */
     CouchKVStore(const CouchKVStore &from);
 
     /**
-     * Cleanup.
+     * Deconstructor
      */
     virtual ~CouchKVStore() {
         close();
@@ -164,6 +252,8 @@ public:
 
     /**
      * Begin a transaction (if not already in one).
+     *
+     * @return true if the transaction is started successfully
      */
     bool begin(void) {
         assert(!isReadOnly());
@@ -174,7 +264,7 @@ public:
     /**
      * Commit a transaction (unless not currently in one).
      *
-     * Returns false if the commit fails.
+     * @return true if the commit is completed successfully.
      */
     bool commit(void);
 
@@ -190,60 +280,135 @@ public:
 
     /**
      * Query the properties of the underlying storage.
+     *
+     * @return properties of the underlying storage system
      */
     StorageProperties getStorageProperties(void);
 
     /**
-     * Overrides set().
+     * Insert or update a given document.
+     *
+     * @param itm instance representing the document to be inserted or updated
+     * @param cb callback instance for SET
      */
-    void set(const Item &item, Callback<mutation_result> &cb);
+    void set(const Item &itm, Callback<mutation_result> &cb);
 
     /**
-     * Overrides get().
+     * Retrieve the document with a given key from the underlying storage system.
+     *
+     * @param key the key of a document to be retrieved
+     * @param rowid the sequence number of a document
+     * @param vb vbucket id of a document
+     * @param cb callback instance for GET
      */
     void get(const std::string &key, uint64_t rowid,
              uint16_t vb, Callback<GetValue> &cb);
 
     /**
-     * Overrides getMulti().
+     * Retrieve the multiple documents from the underlying storage system at once.
+     *
+     * @param vb vbucket id of a document
+     * @param itms list of items whose documents are going to be retrieved
      */
     void getMulti(uint16_t vb, vb_bgfetch_queue_t &itms);
 
     /**
-     * Overrides del().
+     * Delete a given document from the underlying storage system.
+     *
+     * @param itm instance representing the document to be deleted
+     * @param rowid the sequence number of a document
+     * @param cb callback instance for DELETE
      */
     void del(const Item &itm, uint64_t rowid,
              Callback<int> &cb);
 
+    /**
+     * Delete a given vbucket database instance from the underlying storage system
+     *
+     * @param vbucket vbucket id
+     * @return true if the vbucket deletion is completed successfully.
+     */
     bool delVBucket(uint16_t vbucket);
 
+    /**
+     * Retrieve the list of persisted vbucket states
+     *
+     * @return vbucket state map instance where key is vbucket id and
+     * value is vbucket state
+     */
     vbucket_map_t listPersistedVbuckets(void);
 
+    /**
+     * Retrieve ths list of persisted engine stats
+     *
+     * @param stats map instance where the persisted engine stats will be added
+     */
     void getPersistedStats(std::map<std::string, std::string> &stats);
 
     /**
-     * Take a snapshot of the stats in the main DB.
+     * Persist a snapshot of the engine stats in the underlying storage.
+     *
+     * @param engine_stats map instance that contains all the engine stats
+     * @return true if the snapshot is done successfully
      */
-    bool snapshotStats(const std::map<std::string, std::string> &m);
+    bool snapshotStats(const std::map<std::string, std::string> &engine_stats);
 
     /**
-    * Take a snapshot of the vbucket states in the main DB.
-    */
-    bool snapshotVBuckets(const vbucket_map_t &m);
+     * Persist a snapshot of the vbucket states in the underlying storage system.
+     *
+     * @param vb_stats map instance that contains all the vbucket states
+     * @return true if the snapshot is done successfully
+     */
+    bool snapshotVBuckets(const vbucket_map_t &vb_states);
 
     /**
-     * Overrides dump
+     * Retrieve all the documents from the underlying storage system.
+     *
+     * @param cb callback instance to process each document retrieved
      */
     void dump(shared_ptr<Callback<GetValue> > cb);
+
+    /**
+     * Retrieve all the documents for a given vbucket from the storage system.
+     *
+     * @param vb vbucket id
+     * @param cb callback instance to process each document retrieved
+     */
     void dump(uint16_t vb, shared_ptr<Callback<GetValue> > cb);
+
+    /**
+     * Retrieve all the keys from the underlying storage system.
+     *
+     * @param vbids list of vbucket ids whose document keys are going to be retrieved
+     * @param cb callback instance to process each key retrieved
+     */
     void dumpKeys(const std::vector<uint16_t> &vbids,  shared_ptr<Callback<GetValue> > cb);
+
+    /**
+     * Retrieve the list of keys and their meta data for a given
+     * vbucket, which were deleted.
+     * @param vb vbucket id
+     * @param cb callback instance to process each key and its meta data
+     */
     void dumpDeleted(uint16_t vb,  shared_ptr<Callback<GetValue> > cb);
+
+    /**
+     * Does the underlying storage system support key-only retrieval operations?
+     *
+     * @return true if key-only retrieval is supported
+     */
     bool isKeyDumpSupported() {
         return true;
     }
 
     /**
-     * Overrides warmup
+     * Warmup function
+     *
+     * @param lf access log instance that contains the list of resident
+     * items before shutdown
+     * @param vbmap map instance where key is vbucket id and value is its state
+     * @param cb callback instance for warmup
+     * @param estimate callback instance for estimating warmup completion
      */
     size_t warmup(MutationLog &lf,
                   const std::map<uint16_t, vbucket_state> &vbmap,
@@ -251,14 +416,39 @@ public:
                   Callback<size_t> &estimate);
 
     /**
-     * Overrides getEstimatedItemCount
+     * Get the estimated number of items that are going to be loaded during warmup.
+     *
+     * @param items number of estimated items to be loaded during warmup
+     * @return true if the estimation is completed successfully
      */
     bool getEstimatedItemCount(size_t &items);
 
-    void addStats(const std::string &prefix, ADD_STAT add_stat, const void *c);
+    /**
+     * Perform the pre-optimizations before persisting dirty items
+     *
+     * @param items list of dirty items that can be pre-optimized
+     */
+    void optimizeWrites(std::vector<queued_item> &items);
+
+    /**
+     * Add all the kvstore stats to the stat response
+     *
+     * @param prefix stat name prefix
+     * @param add_stat upstream function that allows us to add a stat to the response
+     * @param cookie upstream connection cookie
+     */
+    void addStats(const std::string &prefix, ADD_STAT add_stat, const void *cookie);
+
+    /**
+     * Add all the kvstore timings stats to the stat response
+     *
+     * @param prefix stat name prefix
+     * @param add_stat upstream function that allows us to add a stat to the response
+     * @param cookie upstream connection cookie
+     */
     void addTimingStats(const std::string &prefix, ADD_STAT add_stat,
                         const void *c);
-    void optimizeWrites(std::vector<queued_item> &items);
+
     void processTxnSizeChange(size_t txn_size) {
         (void) txn_size;
     }
