79dba0ceba54c7e9fcda5761f9609b86705afb04
Aaron Miller <apage43@ninjawhale.com>
2012-10-23 17:57:17 -0700

    MB-7006 Add tests for JSON validation
    Change-Id: I7ece614adf3dc6c8be92d6168d3e80f4aae7418c
    Reviewed-on: http://review.couchbase.org/21905
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Steve Yen <steve.yen@gmail.com>
    Tested-by: Steve Yen <steve.yen@gmail.com>

---
+++ tests/module_tests/json_test.cc
@@ -0,0 +1,26 @@
+#include "config.h"
+#include <cassert>
+#include <iostream>
+
+#include "tools/JSON_checker.h"
+#include "common.hh"
+
+#define check(expr, msg) {if(!(expr)) \
+    { std::cerr << "JSON test failed: " << msg << std::endl; exit(1); }}
+#define CHECK_JSON(X) checkUTF8JSON((const unsigned char *)X, sizeof(X) - 1)
+static void testJSON() {
+    check(CHECK_JSON("{\"test\": 12}"), "simple json checks as OK");
+    check(CHECK_JSON("{\"test\": [[[[[[[[[[[[[[[[[[[[[[12]]]]]]]]]]]]]]]]]]]]]]}"),
+            "deep json checks as OK");
+    check(!CHECK_JSON("{\"test\": [[[[[[[[[[[[[[[[[[[[[[12]]]]]]]]]]]]]]]]]]]]]]]]}"),
+            "bad deep json is not OK");
+    check(!CHECK_JSON("{bad stuff}"), "bad json starting with { is not OK");
+    check(CHECK_JSON("null"), "bare values are OK");
+    check(CHECK_JSON("99"), "bare numbers are OK");
+    check(!CHECK_JSON("{\"test\xFF\": 12}"), "bad UTF-8 is not OK");
+}
+
+int main(int argc, char **argv) {
+    (void)argc; (void)argv;
+    testJSON();
+}
