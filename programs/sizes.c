d40c581931dcfab0ef47e1dc16fd874230036f62
Trond Norbye <trond.norbye@gmail.com>
2013-10-03 09:39:24 +0200

    Update sizes to print the real size of the conn
    Change-Id: I24934f387c8f81c89aa29a647469b20fa9cbfd9e
    Reviewed-on: http://review.couchbase.org/29329
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- programs/sizes.c
+++ programs/sizes.c
@@ -7,7 +7,21 @@ static void display(const char *name, size_t size) {
     printf("%s\t%d\n", name, (int)size);
 }
 
+static long calc_conn_size(void) {
+   long ret = sizeof(conn);
+   ret += DATA_BUFFER_SIZE; /* read */
+   ret += DATA_BUFFER_SIZE; /* write */
+   ret += (sizeof(item *) * ITEM_LIST_INITIAL);
+   ret += (sizeof(char *) * SUFFIX_LIST_INITIAL);
+   ret += (sizeof(struct iovec) * IOV_LIST_INITIAL);
+   ret += (sizeof(struct msghdr) * MSG_LIST_INITIAL);
+   return ret;
+}
+
+
 int main(int argc, char **argv) {
+
+
 
     display("Slab Stats", sizeof(struct slab_stats));
     display("Thread stats",
@@ -17,7 +31,7 @@ int main(int argc, char **argv) {
     display("Settings", sizeof(struct settings));
     display("Libevent thread",
             sizeof(LIBEVENT_THREAD));
-    display("Connection", sizeof(conn));
+    display("Connection", calc_conn_size());
 
     printf("----------------------------------------\n");
 
