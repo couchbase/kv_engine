a11574c8818b8802b62de1539235c37a002cf756
Mike Wiederhold <mike@couchbase.com>
2013-11-21 16:14:31 -0800

    Fix compile errors from cmake move
    Change-Id: I626835f4bbe4ba58cdd00ac9d2603bb445583479
    Reviewed-on: http://review.couchbase.org/30476
    Reviewed-by: David Liao <david.liao@couchbase.com>
    Tested-by: Michael Wiederhold <mike@couchbase.com>

--- src/common.h
+++ src/common.h
@@ -164,11 +164,11 @@ inline bool parseUint64(const char *str, uint64_t *out) {
     errno = 0;
     *out = 0;
     char *endptr;
-    unsigned long long ull = strtoull(str, &endptr, 10);
+    uint64_t ull = strtoull(str, &endptr, 10);
     if (errno == ERANGE)
         return false;
     if (xisspace(*endptr) || (*endptr == '\0' && endptr != str)) {
-        if ((long long) ull < 0) {
+        if ((int64_t) ull < 0) {
             /* only check for negative signs in the uncommon case when
              * the unsigned number is so big that it's negative as a
              * signed number. */
