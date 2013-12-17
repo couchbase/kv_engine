d8243b47e816269146a28635cca56e5f4f672998
Trond Norbye <trond.norbye@gmail.com>
2013-12-17 11:11:27 +0100

    Add prototypes for the atomic functions for win32
    No implementation currently. Just trying to make the
    other stuff compile so far.
    
    Change-Id: I168c92e0f36f610a6c5def57d16b8985b319139b
    Reviewed-on: http://review.couchbase.org/31196
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Trond Norbye <trond.norbye@gmail.com>

--- src/atomic/msvc_atomics.h
+++ src/atomic/msvc_atomics.h
@@ -22,11 +22,52 @@
 
 #include "config.h"
 
-#define ep_sync_add_and_fetch(a, b)
-#define ep_sync_bool_compare_and_swap(a, b, c)
-#define ep_sync_fetch_and_add(a, b)
-#define ep_sync_lock_release(a)
-#define ep_sync_lock_test_and_set(a, b)
-#define ep_sync_synchronize()
+#include <memcached/engine.h>
+
+int ep_sync_lock_test_and_set(volatile int *dest, int value);
+void ep_sync_lock_release(volatile int *dest);
+void ep_sync_synchronize(void);
+rel_time_t ep_sync_add_and_fetch(volatile uint64_t *dest, uint64_t value);
+rel_time_t ep_sync_add_and_fetch(volatile uint32_t *dest, uint32_t value);
+int ep_sync_add_and_fetch(volatile int *dest, int value);
+uint8_t ep_sync_add_and_fetch(volatile uint8_t *dest, uint8_t value);
+int ep_sync_fetch_and_add(volatile int *dest, int value);
+uint64_t ep_sync_fetch_and_add(volatile uint64_t *dest, uint64_t value);
+uint32_t ep_sync_fetch_and_add(volatile uint32_t *dest, uint32_t value);
+hrtime_t ep_sync_fetch_and_add(volatile hrtime_t *dest, hrtime_t value);
+bool ep_sync_bool_compare_and_swap(volatile uint8_t *dest, uint8_t prev, uint8_t next);
+bool ep_sync_bool_compare_and_swap(volatile bool *dest, bool prev, bool next);
+bool ep_sync_bool_compare_and_swap(volatile int *dest, int prev, int next);
+bool ep_sync_bool_compare_and_swap(volatile unsigned int *dest,
+    unsigned int prev,
+    unsigned int next);
+bool ep_sync_bool_compare_and_swap(volatile uint64_t *dest,
+    uint64_t prev,
+    uint64_t next);
+
+/*
+* Unfortunately C++ isn't all that happy about assinging everything to/from a
+* void pointer without a cast, so we need to add extra functions.
+* Luckily we know that the size_t is big enough to keep a pointer, so
+* we can reuse the size_t function for we already defined
+*/
+class QueuedItem;
+typedef std::queue<QueuedItem> ItemQueue;
+typedef std::queue<int> IntQueue;
+class VBucket;
+class VBucketHolder;
+class Doodad;
+class Blob;
+
+bool ep_sync_bool_compare_and_swap(ItemQueue * volatile *dest, ItemQueue *prev, ItemQueue *next);
+bool ep_sync_bool_compare_and_swap(VBucket* volatile* dest, VBucket* prev, VBucket* next);
+bool ep_sync_bool_compare_and_swap(Blob* volatile* dest, Blob* prev, Blob* next);
+bool ep_sync_bool_compare_and_swap(QueuedItem* volatile* dest, QueuedItem* prev, QueuedItem* next);
+bool ep_sync_bool_compare_and_swap(VBucketHolder* volatile* dest, VBucketHolder* prev, VBucketHolder* next);
+bool ep_sync_bool_compare_and_swap(Doodad* volatile* dest, Doodad* prev, Doodad* next);
+bool ep_sync_bool_compare_and_swap(IntQueue * volatile *dest, IntQueue *prev, IntQueue *next);
+
+
+typedef int pthread_key_t;
 
 #endif  // SRC_ATOMIC_MSVC_ATOMICS_H
