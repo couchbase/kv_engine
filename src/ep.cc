44d302dd013d766d042a32f009cec097fd799642
Mike Wiederhold <mike@couchbase.com>
2014-04-08 00:39:34 -0700

    MB-10772: Fix issue with vbucket getting out of sync during vb reset
    During a vbucket reset which happens during a tap initial backfill
    we should just delete the vbucket on disk even if there are no
    items because certain data such as the vbucket high seqno and
    failover table can get out of sync. Since tap will only be used for
    upgrades then it should be okay to just remove the optimization of
    just resetting some of the values in the vbucket without touching
    disk.
    
    Change-Id: I8b91b67bc4537c07f69b719fd464b4b20083576f
    Reviewed-on: http://review.couchbase.org/35441
    Tested-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Chiyoung Seo <chiyoung@couchbase.com>

--- src/ep.cc
+++ src/ep.cc
@@ -1123,14 +1123,6 @@ bool EventuallyPersistentStore::resetVBucket(uint16_t vbid) {
     RCPtr<VBucket> vb = vbMap.getBucket(vbid);
     if (vb) {
         vbucket_state_t vbstate = vb->getState();
-        if (vb->getNumItems(eviction_policy) == 0) { // Already reset?
-            // The first checkpoint for active vbucket should start with id 2.
-            uint64_t start_chk_id = (vbstate == vbucket_state_active) ? 2 : 0;
-            vb->checkpointManager.setOpenCheckpointId(start_chk_id);
-            vbMap.setPersistenceCheckpointId(vbid, 0);
-            vbMap.setPersistenceSeqno(vbid, 0);
-            return true;
-        }
 
         vbMap.removeBucket(vbid);
         lh.unlock();
