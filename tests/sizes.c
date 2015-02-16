3e5273a738b529a186775e3961daa7af0870bff6
Dave Rigby <daver@couchbase.com>
2015-02-16 11:37:10 +0000

    Report binary protocol opcode usage in memcached_sizes
    Count the number of binary protocol opcodes used, and show a map of
    them. Example output:
    
        Binary protocl opcodes used  128 / 255
        Opcode map:     (# = Used, . = Free)
    
        0123456789abcdef
        00 XXXXXXXXXXXXXXXX
        10 XXXXXXXXXXXXXXXX
        20 XXXXXXXXX.......
        30 XXXXXXXXXXXXXXXX
        40 XXXXXXXX........
        50 XXXXXXXXXXXXXXXX
        60 ................
        70 ................
        80 XXXX.XXX.XX.....
        90 .XXXXX.X......XX
        a0 XXXXXXXXXXX.XXX.
        b0 XXXXXXXX........
        c0 .XX.............
        d0 ................
        e0 ................
        f0 XXXXXX..........
    
    Change-Id: I950d2ac87e95b5f673eca5da8136ba5c529aa4fb
    Reviewed-on: http://review.couchbase.org/46883
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/sizes.c
+++ tests/sizes.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 #include "daemon/memcached.h"
+#include "utilities/protocol2text.c"
 
 static void display(const char *name, size_t size) {
     printf("%s\t%d\n", name, (int)size);
@@ -15,6 +16,31 @@ static long calc_conn_size(void) {
    return ret;
 }
 
+static unsigned int count_used_opcodes(void) {
+    unsigned int used_opcodes = 0;
+    for (uint8_t opcode = 0; opcode < 255; opcode++) {
+        if (memcached_opcode_2_text(opcode) != NULL) {
+            used_opcodes++;
+        }
+    }
+    return used_opcodes;
+}
+
+static void display_used_opcodes(void) {
+    printf("Opcode map:     (# = Used, . = Free)\n\n");
+    printf("   0123456789abcdef");
+    for (unsigned int opcode = 0; opcode < 256; opcode++) {
+        if (opcode % 16 == 0) {
+            printf("\n%02x ", opcode & ~0xf);
+        }
+        if (memcached_opcode_2_text(opcode) != NULL) {
+            putchar('X');
+        } else {
+            putchar('.');
+        }
+    }
+    putchar('\n');
+}
 
 int main(int argc, char **argv) {
 
@@ -34,6 +60,9 @@ int main(int argc, char **argv) {
 
     display("libevent thread cumulative", sizeof(LIBEVENT_THREAD));
     display("Thread stats cumulative\t", sizeof(struct thread_stats));
+    printf("Binary protocol opcodes used\t%u / %u\n",
+           count_used_opcodes(), 256);
+    display_used_opcodes();
 
     return 0;
 }
