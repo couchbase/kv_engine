19affd1ac552310f4c7e5f97fc120cc5ef2ec973
abhinavdangeti <abhinav@couchbase.com>
2015-03-20 13:31:52 -0700

    MB-14003: Set the bySeqno of the HashTable item correctly
    Context: addTAPBackfillItem
    
    Change-Id: I825635cb50b4130dca311ee247cf157c09a76d92
    Reviewed-on: http://review.couchbase.org/48560
    Reviewed-by: Michael Wiederhold <mike@couchbase.com>
    Reviewed-by: Sundararaman Sridharan <sundar@couchbase.com>
    Reviewed-by: Sriram Ganesan <sriram@couchbase.com>
    Tested-by: abhinav dangeti <abhinav@couchbase.com>

--- src/ep.cc
+++ src/ep.cc
@@ -851,8 +851,9 @@ ENGINE_ERROR_CODE EventuallyPersistentStore::addTAPBackfillItem(const Item &itm,
         ret = ENGINE_KEY_EEXISTS;
         break;
     case WAS_DIRTY:
-        // If a given backfill item is already dirty, don't queue it again.
-        break;
+        // FALLTHROUGH, to ensure the bySeqno for the hashTable item is
+        // set correctly, and also the sequence numbers are ordered correctly.
+        // (MB-14003)
     case NOT_FOUND:
         // FALLTHROUGH
     case WAS_CLEAN:
