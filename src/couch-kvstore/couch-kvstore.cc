1432c65610bb90d143e42bbfb642d715d42c0d0c
abhinavdangeti <abhinav@couchbase.com>
2015-04-01 15:10:32 -0700

    MB-14079: If mkdir() returns EEXIST, do now throw exception
    With this change, we wouldn't need to make a stat() system
    call, so getting rid of that part.
    
    Change-Id: If6f7a0eacdcb84be5762df097db7dfd2ee1fe860
    Reviewed-on: http://review.couchbase.org/49071
    Reviewed-by: Chiyoung Seo <chiyoung@couchbase.com>
    Tested-by: abhinav dangeti <abhinav@couchbase.com>

--- src/couch-kvstore/couch-kvstore.cc
+++ src/couch-kvstore/couch-kvstore.cc
@@ -1307,28 +1307,11 @@ void CouchKVStore::open() {
     // TODO intransaction, is it needed?
     intransaction = false;
 
-    struct stat dbstat;
-    bool havedir = false;
-
-    int stat_ret = stat(dbname.c_str(), &dbstat);
-    if (stat_ret == 0 && (dbstat.st_mode & S_IFDIR) == S_IFDIR) {
-        havedir = true;
-    }
-
-    int stat_errno = 0;
-    if (stat_ret == -1) {
-        stat_errno = errno;
-    }
-
-    if (!havedir) {
-        if (mkdir(dbname.c_str(), S_IRWXU) == -1) {
+    if (mkdir(dbname.c_str(), S_IRWXU) == -1) {
+        if (errno != EEXIST) {
             std::stringstream ss;
             ss << "Warning: Failed to create data directory ["
                << dbname << "]: " << strerror(errno);
-            if (stat_ret == -1) {
-                ss << "; stat() had failed with error: "
-                   << strerror(stat_errno);
-            }
             throw std::runtime_error(ss.str());
         }
     }
