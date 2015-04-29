couchbase/kv_engine

commit c4ca56d7bc0c6b0d42dac30e220afe02d321bbec

MB-13647: Refactor debug printing routines to be accessible outside memcached.c
Make key_to_printable_buffer() to be non-static; and split the
sanitizing into a seperate inner function (buf_to_printable_buffer) so
it can be used without adding a prefix (for example to print
subdocument paths). Move them both to new files: debug_helpers.{cc,h}

Change-Id: Ia42c612157c5930a63c216de6fa6fba40ce3cc5e
Reviewed-on: http://review.couchbase.org/50063
Tested-by: buildbot <build@couchbase.com>
Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

daemon/debug_helpers.cc
@@ -1,0 +1,57 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include "debug_helpers.h"
+
+#include <cctype>
+#include <cstdio>
+
+ssize_t buf_to_printable_buffer(char *dest, size_t destsz,
+                                const char *src, size_t srcsz)
+{
+    char *ptr = dest;
+    if (srcsz > destsz) {
+        srcsz = destsz;
+    }
+
+    for (size_t ii = 0; ii < srcsz; ++ii, ++src, ++ptr) {
+        if (std::isgraph(*src)) {
+            *ptr = *src;
+        } else {
+            *ptr = '.';
+        }
+    }
+
+    *ptr = '\0';
+    return (ssize_t)(ptr - dest);
+}
+
+ssize_t key_to_printable_buffer(char *dest, size_t destsz, SOCKET client,
+                                bool from_client, const char *prefix,
+                                const char *key, size_t nkey)
+{
+    ssize_t nw = std::snprintf(dest, destsz, "%c%d %s ",
+                               from_client ? '>' : '<',
+                               (int)client, prefix);
+    if (nw == -1) {
+        return -1;
+    }
+    char* ptr = dest + nw;
+    destsz -= nw;
+    return buf_to_printable_buffer(ptr, destsz, key, nkey);
+}
+

CMakeLists.txt
@@ -183,6 +183,8 @@ ADD_EXECUTABLE(memcached
                daemon/config_parse.h
                daemon/connections.c
                daemon/connections.h
+               daemon/debug_helpers.cc
+               daemon/debug_helpers.h
                daemon/hash.c
                daemon/ioctl.c
                daemon/mcaudit.c

daemon/debug_helpers.h
@@ -1,0 +1,62 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/*
+ * Helper functions for debug output.
+ */
+
+#pragma once
+
+#include "config.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Insert a char* into a buffer, but replace all non-printable characters with
+ * a '.'
+ *
+ * @param dest where to store the output
+ * @param destsz size of destination buffer
+ * @param src the string to add to the buffer
+ * @param srcsz the number of bytes in src
+ * @return number of bytes in dest if success, -1 otherwise
+ */
+ssize_t buf_to_printable_buffer(char *dest, size_t destsz,
+                                const char *src, size_t srcsz);
+
+/**
+ * Insert a key into a buffer, but replace all non-printable characters
+ * with a '.'.
+ *
+ * @param dest where to store the output
+ * @param destsz size of destination buffer
+ * @param prefix string to insert before the data
+ * @param client the client we are serving
+ * @param from_client set to true if this data is from the client
+ * @param key the key to add to the buffer
+ * @param nkey the number of bytes in the key
+ * @return number of bytes in dest if success, -1 otherwise
+ */
+ssize_t key_to_printable_buffer(char *dest, size_t destsz, SOCKET client,
+                                bool from_client, const char *prefix,
+                                const char *key, size_t nkey);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+

daemon/memcached.c
@@ -15,6 +15,7 @@
  */
 #include "config.h"
 #include "config_parse.h"
+#include "debug_helpers.h"
 #include "memcached.h"
 #include "memcached/extension_loggers.h"
 #include "memcached/audit_interface.h"
@@ -604,51 +605,6 @@ static void* binary_get_request(conn *c) {
  */
 static char* binary_get_key(conn *c) {
     return c->read.curr - (c->binary_header.request.keylen);
-}
-
-/**
- * Insert a key into a buffer, but replace all non-printable characters
- * with a '.'.
- *
- * @param dest where to store the output
- * @param destsz size of destination buffer
- * @param prefix string to insert before the data
- * @param client the client we are serving
- * @param from_client set to true if this data is from the client
- * @param key the key to add to the buffer
- * @param nkey the number of bytes in the key
- * @return number of bytes in dest if success, -1 otherwise
- */
-static ssize_t key_to_printable_buffer(char *dest, size_t destsz,
-                                       SOCKET client, bool from_client,
-                                       const char *prefix,
-                                       const char *key,
-                                       size_t nkey)
-{
-    char *ptr;
-    ssize_t ii;
-    ssize_t nw = snprintf(dest, destsz, "%c%d %s ", from_client ? '>' : '<',
-                          (int)client, prefix);
-    if (nw == -1) {
-        return -1;
-    }
-
-    ptr = dest + nw;
-    destsz -= nw;
-    if (nkey > destsz) {
-        nkey = destsz;
-    }
-
-    for (ii = 0; ii < nkey; ++ii, ++key, ++ptr) {
-        if (isgraph(*key)) {
-            *ptr = *key;
-        } else {
-            *ptr = '.';
-        }
-    }
-
-    *ptr = '\0';
-    return (ssize_t)(ptr - dest);
 }
 
 /**

daemon/memcached.h
@@ -483,7 +483,6 @@ void perform_callbacks(ENGINE_EVENT_TYPE type,
 
 const char* get_server_version(void);
 
-
 /**
  * Connection-related functions
  */

commit 35aabddc0453f2cff21f30babeb04db6012ebb03

Fix WIN32 build failure caused by c4ca56d
MSVC doesn't have an snprintf() function, instead it is named
_snprintf(). We therefore `#define snprintf _snprintf` to allow code
calling snprintf() to work on WIN32. Unfortunately this has the
side-effect of causing std::snprintf() to be expanded to
std::_snprintf(), which doesn't exist.

Simple solution (taken here) is just to call snprintf() without the
std:: prefix.

Change-Id: I299db1caccc48965310daef8b0d84cdf93804086
Reviewed-on: http://review.couchbase.org/50381
Tested-by: buildbot <build@couchbase.com>
Reviewed-by: Dave Rigby <daver@couchbase.com>

daemon/debug_helpers.cc
@@ -44,9 +44,8 @@ ssize_t key_to_printable_buffer(char *dest, size_t destsz, SOCKET client,
                                 bool from_client, const char *prefix,
                                 const char *key, size_t nkey)
 {
-    ssize_t nw = std::snprintf(dest, destsz, "%c%d %s ",
-                               from_client ? '>' : '<',
-                               (int)client, prefix);
+    ssize_t nw = snprintf(dest, destsz, "%c%d %s ", from_client ? '>' : '<',
+                          (int)client, prefix);
     if (nw == -1) {
         return -1;
     }
@@ -54,4 +53,3 @@ ssize_t key_to_printable_buffer(char *dest, size_t destsz, SOCKET client,
     destsz -= nw;
     return buf_to_printable_buffer(ptr, destsz, key, nkey);
 }
-