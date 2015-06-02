788c1ec6946f7b1b3b6ee6469ddff7dbf9c22b7b
abhinavdangeti <abhinav@couchbase.com>
2015-06-02 11:44:14 -0700

    MB-15206: Check and add new checkpoint upon receiving snapshot end
    In the DCP consumer, upon receiving a snapshot end message,
    check and add a new checkpoint for the replica vbucket, to
    ensure that items that are in the current checkpoint do not
    take up a lot of memory. Any old unreferenced checkpoints
    will be removed by the checkpoint-remover and item-pager
    daemons.
    
    Change-Id: I9eb61fb9e71661e4245de9f92f595a9300abffb9
    Reviewed-on: http://review.couchbase.org/51682
    Reviewed-by: Chiyoung Seo <chiyoung@couchbase.com>
    Tested-by: abhinav dangeti <abhinav@couchbase.com>

--- src/dcp-stream.cc
+++ src/dcp-stream.cc
@@ -1187,9 +1187,10 @@ void PassiveStream::handleSnapshotEnd(RCPtr<VBucket>& vb, uint64_t byseqno) {
     if (byseqno == cur_snapshot_end) {
         if (cur_snapshot_type == disk && vb->isBackfillPhase()) {
             vb->setBackfillPhase(false);
-            uint64_t id = vb->checkpointManager.getOpenCheckpointId() + 1;
-            vb->checkpointManager.checkAndAddNewCheckpoint(id, vb);
         }
+
+        uint64_t id = vb->checkpointManager.getOpenCheckpointId() + 1;
+        vb->checkpointManager.checkAndAddNewCheckpoint(id, vb);
 
         if (cur_snapshot_ack) {
             LockHolder lh(streamMutex);
