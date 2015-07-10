ab974094ca78326970260dfdb5476d6f94954715
Trond Norbye <trond.norbye@gmail.com>
2015-07-10 10:28:49 +0200

    Refactor: move StatsCounter to its own file
    Change-Id: I3a8bd0915238dd9431ef5af902fde0c98c373130
    Reviewed-on: http://review.couchbase.org/53061
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

--- daemon/stats.h
+++ daemon/stats.h
@@ -1,7 +1,137 @@
-/* stats */
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#pragma once
 
-#ifndef STATS_H
-#define STATS_H
+#include <cstdint>
+#include "stats_counter.h"
+
+/**
+ * Stats stored per-thread.
+ */
+struct thread_stats {
+    thread_stats() {
+        reset();
+    }
+
+    void reset() {
+        cmd_get = 0;
+        get_hits = 0;
+        get_misses = 0;
+        cmd_set = 0;
+        delete_hits = 0;
+        cas_hits = 0;
+        cas_badval = 0;
+        delete_misses = 0;
+        incr_misses = 0;
+        decr_misses = 0;
+        incr_hits = 0;
+        decr_hits = 0;
+        cas_misses = 0;
+        bytes_written = 0;
+        bytes_read = 0;
+        cmd_flush = 0;
+        conn_yields = 0;
+        auth_cmds = 0;
+        auth_errors = 0;
+
+        rbufs_allocated = 0;
+        rbufs_loaned = 0;
+        rbufs_existing = 0;
+        wbufs_allocated = 0;
+        wbufs_loaned = 0;
+
+        iovused_high_watermark = 0;
+        msgused_high_watermark = 0;
+    }
+
+    StatsCounter<uint64_t> cmd_get;
+    StatsCounter<uint64_t> get_hits;
+    StatsCounter<uint64_t> get_misses;
+    StatsCounter<uint64_t> cmd_set;
+    StatsCounter<uint64_t> delete_hits;
+    StatsCounter<uint64_t> cas_hits;
+    StatsCounter<uint64_t> cas_badval;
+    StatsCounter<uint64_t> delete_misses;
+    StatsCounter<uint64_t> incr_misses;
+    StatsCounter<uint64_t> decr_misses;
+    StatsCounter<uint64_t> incr_hits;
+    StatsCounter<uint64_t> decr_hits;
+    StatsCounter<uint64_t> cas_misses;
+    StatsCounter<uint64_t> bytes_read;
+    StatsCounter<uint64_t> bytes_written;
+    StatsCounter<uint64_t> cmd_flush;
+    StatsCounter<uint64_t> conn_yields; /* # of yields for connections (-R option)*/
+    StatsCounter<uint64_t> auth_cmds;
+    StatsCounter<uint64_t> auth_errors;
+    /* # of read buffers allocated. */
+    StatsCounter<uint64_t> rbufs_allocated;
+    /* # of read buffers which could be loaned (and hence didn't need to be allocated). */
+    StatsCounter<uint64_t> rbufs_loaned;
+    /* # of read buffers which already existed (with partial data) on the connection
+       (and hence didn't need to be allocated). */
+    StatsCounter<uint64_t> rbufs_existing;
+    /* # of write buffers allocated. */
+    StatsCounter<uint64_t> wbufs_allocated;
+    /* # of write buffers which could be loaned (and hence didn't need to be allocated). */
+    StatsCounter<uint64_t> wbufs_loaned;
+
+    // Right now we're protecting both the "high watermark" variables
+    // between the same mutex
+    std::mutex mutex;
+
+    /* Highest value iovsize has got to */
+    int iovused_high_watermark;
+    /* High value conn->msgused has got to */
+    int msgused_high_watermark;
+};
+
+/**
+ * Listening port.
+ */
+struct listening_port {
+    int port;
+    int curr_conns;
+    int maxconns;
+};
+
+/**
+ * Global stats.
+ */
+struct stats {
+    /** Number of connections used by the server itself (listen ports etc). */
+    std::atomic<unsigned int> daemon_conns;
+
+    /** The current number of connections to the server */
+    std::atomic<unsigned int> curr_conns;
+
+    /** The total number of connections to the server since start (or reset) */
+    std::atomic<unsigned int> total_conns;
+
+    /** The current number of allocated connection objects */
+    std::atomic<unsigned int> conn_structs;
+
+    /** The number of times I reject a client */
+    std::atomic<uint64_t> rejected_conns;
+
+    // TODO[C++]: convert listening_ports to std::vector.
+    struct listening_port *listening_ports;
+};
+
+
 
 struct thread_stats *get_thread_stats(conn *c);
 
@@ -61,5 +191,3 @@ struct thread_stats *get_thread_stats(conn *c);
         } \
     } \
 }
-
-#endif /* STATS_H */
---
+++ daemon/stats_counter.h
@@ -0,0 +1,74 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#pragma once
+
+#include <atomic>
+
+/**
+ * The StatsCounter is used to track stat values by using an
+ * std::atomic<> but use relaxed memory ordering.
+ */
+template <typename T>
+class StatsCounter {
+public:
+    StatsCounter() {
+        value.store(0, std::memory_order_relaxed);
+    }
+    StatsCounter(const StatsCounter &other) {
+        value.store(other.value.load(std::memory_order_relaxed),
+                    std::memory_order_relaxed);
+    }
+
+    operator T() const {
+        return value.load(std::memory_order_relaxed);
+    }
+
+    T load() const {
+        return value.load(std::memory_order_relaxed);
+    }
+
+    StatsCounter & operator += (const T rhs) {
+        value.fetch_add(rhs, std::memory_order_relaxed);
+        return *this;
+    }
+
+    StatsCounter & operator += (const StatsCounter &rhs) {
+        value.fetch_add(rhs.value.load(std::memory_order_relaxed),
+                        std::memory_order_relaxed);
+        return *this;
+    }
+
+    void operator++() {
+        value.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    void operator++(int) {
+        value.fetch_add(1, std::memory_order_relaxed);
+    }
+
+    StatsCounter & operator = (T val) {
+        value.store(val, std::memory_order_relaxed);
+        return *this;
+    }
+
+    void reset() {
+        value.store(0, std::memory_order_relaxed);
+    }
+
+private:
+    std::atomic<T> value;
+};
