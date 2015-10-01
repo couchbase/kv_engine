0c678c0842ff016f3a5e0fe371889138eb207a31
abhinavdangeti <abhinav@couchbase.com>
2015-09-30 17:40:14 -0700

    MB-16421: BGFetch to restore an item that is non-resident
    In a full eviction scenario, a bgfetch is to restore an item
    not just if the hash table item is a temp-initial item, but
    if the hash table item is non-resident as well.
    
    Change-Id: I127b0cbe7034133a656b046cd4022635be23aedd
    Reviewed-on: http://review.couchbase.org/55680
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Chiyoung Seo <chiyoung@couchbase.com>

--- src/ep.cc
+++ src/ep.cc
@@ -1420,17 +1420,27 @@ void EventuallyPersistentStore::completeBGFetch(const std::string &key,
                 status = ENGINE_SUCCESS;
             }
         } else {
+            bool restore = false;
             if (v && v->isResident()) {
                 status = ENGINE_SUCCESS;
-            }
-
-            bool restore = false;
-            if (eviction_policy == VALUE_ONLY &&
-                v && !v->isResident() && !v->isDeleted()) {
-                restore = true;
-            } else if (eviction_policy == FULL_EVICTION &&
-                       v && v->isTempInitialItem()) {
-                restore = true;
+            } else {
+                switch (eviction_policy) {
+                    case VALUE_ONLY:
+                        if (v && !v->isResident() && !v->isDeleted()) {
+                            restore = true;
+                        }
+                        break;
+                    case FULL_EVICTION:
+                        if (v) {
+                            if (v->isTempInitialItem() ||
+                                (!v->isResident() && !v->isDeleted())) {
+                                restore = true;
+                            }
+                        }
+                        break;
+                    default:
+                        throw std::logic_error("Unknown eviction policy");
+                }
             }
 
             if (restore) {
@@ -1515,17 +1525,27 @@ void EventuallyPersistentStore::completeBGFetchMulti(uint16_t vbId,
                 status = ENGINE_SUCCESS;
             }
         } else {
+            bool restore = false;
             if (v && v->isResident()) {
                 status = ENGINE_SUCCESS;
-            }
-
-            bool restore = false;
-            if (eviction_policy == VALUE_ONLY &&
-                v && !v->isResident() && !v->isDeleted()) {
-                restore = true;
-            } else if (eviction_policy == FULL_EVICTION &&
-                       v && v->isTempInitialItem()) {
-                restore = true;
+            } else {
+                switch (eviction_policy) {
+                    case VALUE_ONLY:
+                        if (v && !v->isResident() && !v->isDeleted()) {
+                            restore = true;
+                        }
+                        break;
+                    case FULL_EVICTION:
+                        if (v) {
+                            if (v->isTempInitialItem() ||
+                                (!v->isResident() && !v->isDeleted())) {
+                                restore = true;
+                            }
+                        }
+                        break;
+                    default:
+                        throw std::logic_error("Unknown eviction policy");
+                }
             }
 
             if (restore) {
