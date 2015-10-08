653d91940475bd8e4f2b14167a1a079d724a2610
abhinav dangeti <abhinav@couchbase.com>
2015-10-08 00:47:08 +0000

    Revert "Fix data race in CouchKVStore stats access"
    (as this breaks windows:
    C:\Program Files (x86)\Microsoft Visual Studio 12.0\VC\INCLUDE\vector(1708) : error C2558: class 'Couchbase::RelaxedAtomic<uint64_t>' : no copy constructor available or copy constructor is declared 'explicit')
    
    This reverts commit a847eb3aa610956ba738098d45767c012ed8bdfb.
    
    Change-Id: I05c57a1be697c50f4ae091e277c684bc2f8afdd5
    Reviewed-on: http://review.couchbase.org/55887
    Reviewed-by: abhinav dangeti <abhinav@couchbase.com>
    Tested-by: abhinav dangeti <abhinav@couchbase.com>

--- src/couch-kvstore/couch-kvstore.h
+++ src/couch-kvstore/couch-kvstore.h
@@ -20,7 +20,6 @@
 
 #include "config.h"
 #include "libcouchstore/couch_db.h"
-#include <relaxed_atomic.h>
 
 #include <map>
 #include <string>
@@ -528,12 +527,7 @@ private:
     void removeCompactFile(const std::string &filename);
 
     const std::string dbname;
-
-    // Map of the fileRev for each vBucket. Using RelaxedAtomic so
-    // stats gathering (doDcpVbTakeoverStats) can get a snapshot
-    // without having to lock.
-    std::vector<Couchbase::RelaxedAtomic<uint64_t>> dbFileRevMap;
-
+    std::vector<uint64_t>dbFileRevMap;
     uint16_t numDbFiles;
     std::vector<CouchRequest *> pendingReqsQ;
     bool intransaction;
@@ -541,13 +535,11 @@ private:
     /* all stats */
     CouchKVStoreStats   st;
     couch_file_ops statCollectingFileOps;
-    /* deleted docs in each file, indexed by vBucket. RelaxedAtomic
-       to allow stats access witout lock */
-    std::vector<Couchbase::RelaxedAtomic<size_t>> cachedDeleteCount;
+    /* deleted docs in each file, indexed by vBucket */
+    std::vector<size_t> cachedDeleteCount;
 
-    /* non-deleted docs in each file, indexed by vBucket.
-       RelaxedAtomic to allow stats access witout lock. */
-    std::vector<Couchbase::RelaxedAtomic<size_t>> cachedDocCount;
+    /* non-deleted docs in each file, indexed by vBucket */
+    std::vector<size_t> cachedDocCount;
 
     /* pending file deletions */
     AtomicQueue<std::string> pendingFileDeletions;
