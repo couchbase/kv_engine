199216c816c42f6b32252a7e0a2797c3c81b055f
Trond Norbye <trond.norbye@gmail.com>
2015-10-14 10:44:36 +0200

    We don't need a C interface to the audit methods
    Change-Id: I355e09fad9164ad5a92986244931beae869ac30a
    Reviewed-on: http://review.couchbase.org/56113
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- daemon/mcaudit.h
+++ daemon/mcaudit.h
@@ -16,53 +16,47 @@
  */
 #pragma once
 
-#ifdef __cplusplus
-extern "C" {
-#endif
 
-    /**
-     * Send an audit event for an authentication failure
-     *
-     * @param c the connection object performing the sasl auth
-     * @param reason the textual description of why auth failed
-     */
-    void audit_auth_failure(const Connection *c, const char *reason);
+/**
+ * Send an audit event for an authentication failure
+ *
+ * @param c the connection object performing the sasl auth
+ * @param reason the textual description of why auth failed
+ */
+void audit_auth_failure(const Connection* c, const char* reason);
 
-    /**
-     * Send an audit event for a successful authentication
-     *
-     * @param c the connection object performing the sasl auth
-     */
-    void audit_auth_success(const Connection *c);
+/**
+ * Send an audit event for a successful authentication
+ *
+ * @param c the connection object performing the sasl auth
+ */
+void audit_auth_success(const Connection* c);
 
-    /**
-     * Send an audit event for bucket flush
-     * @param c the connection performing the operation
-     * @param bucket the name of the bucket
-     */
-    void audit_bucket_flush(const Connection *c, const char *bucket);
+/**
+ * Send an audit event for bucket flush
+ * @param c the connection performing the operation
+ * @param bucket the name of the bucket
+ */
+void audit_bucket_flush(const Connection* c, const char* bucket);
 
-    /**
-     * Send an audit event for a DCP Open
-     *
-     * param c the connection object performing DCP Open
-     */
-    void audit_dcp_open(const Connection *c);
+/**
+ * Send an audit event for a DCP Open
+ *
+ * param c the connection object performing DCP Open
+ */
+void audit_dcp_open(const Connection* c);
 
-    /*
-     * Send an audit event for command access failure
-     *
-     * param c the connection object performing DCP Open
-     */
-    void audit_command_access_failed(const Connection *c);
+/*
+ * Send an audit event for command access failure
+ *
+ * param c the connection object performing DCP Open
+ */
+void audit_command_access_failed(const Connection* c);
 
-    /**
-     * Send an audit event for a invalid and thus rejected packet
-     *
-     * param c the connection object performing DCP Open
-     */
-    void audit_invalid_packet(const Connection *c);
+/**
+ * Send an audit event for a invalid and thus rejected packet
+ *
+ * param c the connection object performing DCP Open
+ */
+void audit_invalid_packet(const Connection* c);
 
-#ifdef __cplusplus
-}
-#endif
