couchbase/kv_engine

commit 7d88d0bba08f07c667ef167f79d3a81dc5f5a825

MB-16689: Handle EMFILE on file logger rotation
Check for EMFILE (too many open files) when opening / rotating log
files in the file logger.

- If we fail to open a new file at rotation time, log the failure
  ("Failed to open next logfile: Too many open files - disabling file
  logging.") at the end of the current log, and temporary disable
  logging (to prevent filling up disk space as we cannot rotate any
  longer).

- Upon every flush (timed, and when log buffers get to a given size),
  attempt to (re)open the log file if it previously failed. If/when we
  succeed, log this ("Restarting file logging") in the new log file.

For example:

    $ cat log_test_emfile.0.txt
    2015-11-09T14:50:12.652583Z WARNING Restarting file logging
    ...
    <<< HIT EMFILE >>>
    2015-11-09T14:50:12.653617Z WARNING Failed to open next logfile: Too many open files - disabling file logging. Messages at 'WARNING' or higher still output to babysitter log file
    <<< FDs NOW AVAILABLE >>>

    $ cat log_test_emfile.1.txt
    2015-11-09T14:50:13.065720Z WARNING Restarting file logging.

Includes a regression test.

Change-Id: Ia31941cddbc711086a82ebd482aeaa63f09db88b
Reviewed-on: http://review.couchbase.org/56627
Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
Tested-by: buildbot <build@couchbase.com>

extensions/loggers/file_logger.cc
@@ -231,6 +231,40 @@ static const char *severity2string(EXTENSION_LOG_LEVEL sev) {
     }
 }
 
+/* Formats a log entry (consisting {time, frac_of_second, severity, message})
+ * into the specified buffer.
+ * Returns the number of characters for the prefix (timestamp / severity).
+ */
+static int format_log_entry(char* buffer, size_t buf_len, time_t time,
+                            uint32_t frac_of_second,
+                            EXTENSION_LOG_LEVEL severity, const char* message) {
+    ISOTime::ISO8601String timestamp;
+    ISOTime::generatetimestamp(timestamp, time, frac_of_second);
+    const int prefix_len = snprintf(buffer, buf_len, "%s %s ", timestamp.data(),
+                                   severity2string(severity));
+    if (prefix_len < 0) {
+        return 0;
+    }
+
+    const int msglen = snprintf(buffer + prefix_len, buf_len - prefix_len,
+                                "%s", message);
+    if (msglen < 0) {
+        return 0;
+    }
+
+    if (size_t(prefix_len + msglen) > (buf_len - 1)) {
+        // Message cropped.
+        std::cerr << "Message too big to fit in event. Full message: "
+                  << message;
+        std::cerr.flush();
+
+        const char cropped[] = " [cut]";
+        snprintf(buffer + (buf_len - sizeof(cropped)), sizeof(cropped),
+                 "%s", cropped);
+    }
+    return prefix_len;
+}
+
 /* Takes the syslog compliant event and calls the native logging functionality */
 static void syslog_event_receiver(SyslogEvent *event) {
     uint8_t syslog_severity = event->prival & 7; /* Mask out all but 3 least-significant bits */
@@ -254,25 +288,9 @@ static void syslog_event_receiver(SyslogEvent *event) {
     }
 
     char buffer[2048];
-    ISOTime::ISO8601String timestamp;
-    ISOTime::generatetimestamp(timestamp, event->time, event->time_secfrac);
-    int offset = snprintf(buffer, sizeof(buffer), "%s %s ", timestamp.data(),
-                          severity2string(severity));
-    int prefixlen = offset;
-    int datalen = static_cast<int>(strlen(event->msg));
-
-    if (static_cast<size_t>((prefixlen + datalen)) >= sizeof(buffer)) {
-        std::cerr << buffer
-                  << "Message too big to fit in event. Full message: "
-                  << event->msg;
-        std::cerr.flush();
-
-        memcpy(buffer + offset, event->msg, sizeof(buffer) - prefixlen - 7);
-        memcpy(buffer + sizeof(buffer) - 7, " [cut]", 6);
-        buffer[sizeof(buffer) - 1] = '\0';
-    } else {
-        strcat(buffer + offset, event->msg);
-    }
+    const int prefixlen = format_log_entry(buffer, sizeof(buffer),
+                                           event->time, event->time_secfrac,
+                                           severity, event->msg);
 
     if (severity >= current_log_level || severity >= output_level) {
         if (severity >= output_level) {
@@ -363,13 +381,11 @@ static FILE *open_logfile(const char *fnm) {
     static unsigned int next_id = 0;
     char fname[1024];
     FILE *ret;
-    do {
-        sprintf(fname, "%s.%d.%s", fnm, next_id++, extension);
-    } while (access(fname, F_OK) == 0);
-    ret = iops.open(fname, "wb");
-    if (!ret) {
-        fprintf(stderr, "Failed to open memcached log file\n");
+    sprintf(fname, "%s.%d.%s", fnm, next_id, extension);
+    while (access(fname, F_OK) == 0) {
+        sprintf(fname, "%s.%d.%s", fnm, ++next_id, extension);
     }
+    ret = iops.open(fname, "wb");
     return ret;
 }
 
@@ -379,26 +395,56 @@ static void close_logfile(FILE *fp) {
     }
 }
 
-static FILE *reopen_logfile(FILE *old, const char *fnm) {
+static FILE *rotate_logfile(FILE *old, const char *fnm) {
+    FILE* new_log = open_logfile(fnm);
+    if (new_log == NULL && old != NULL) {
+        // Can't open the logfile. We already output messages of
+        // sufficient level to stderr, let's hope that's enough.
+        std::string msg("Failed to open next logfile: " +
+                        std::string(strerror(errno)) +
+                        " - disabling file logging. Messages at '" +
+                        severity2string(output_level) +
+                        "' or higher still output to babysitter log file\n");
+        struct timeval now;
+        cb_get_timeofday(&now);
+        char log_entry[1024];
+        format_log_entry(log_entry, sizeof(log_entry), now.tv_sec, now.tv_usec,
+                         EXTENSION_LOG_WARNING, msg.c_str());
+
+        iops.write(old, log_entry, strlen(log_entry));
+        iops.flush(old);
+        // Send to stderr for good measure.
+        (void)fwrite(log_entry, sizeof(char), strlen(log_entry), stderr);
+    }
     close_logfile(old);
-    return open_logfile(fnm);
+    return new_log;
 }
 
 static size_t flush_pending_io(FILE *file, struct logbuffer *lb) {
     size_t ret = 0;
     if (lb->offset > 0) {
-        char *ptr = lb->data;
-        size_t towrite = ret = lb->offset;
-
-        while (towrite > 0) {
-            int nw = iops.write(file, ptr, towrite);
-            if (nw > 0) {
-                ptr += nw;
-                towrite -= nw;
+        if (file) {
+            char *ptr = lb->data;
+            size_t towrite = ret = lb->offset;
+            while (towrite > 0) {
+                int nw = iops.write(file, ptr, towrite);
+                if (nw > 0) {
+                    ptr += nw;
+                    towrite -= nw;
+                }
             }
+            lb->offset = 0;
+            iops.flush(file);
+        } else {
+            // Cannot write as have no FD, however we also don't want
+            // to leave the logbuffer as-is as that would result in it
+            // filling up and producers being deadlocked. Therefore
+            // discard the logbuffer contents.
+            // Note that any message at output_level is always logged
+            // to stderr (for babysitter) so those messages will not
+            // be lost.
+            lb->offset = 0;
         }
-        lb->offset = 0;
-        iops.flush(file);
     }
 
     return ret;
@@ -419,7 +465,6 @@ static FILE *fp;
 static void logger_thead_main(void* arg)
 {
     size_t currsize = 0;
-    fp = open_logfile(reinterpret_cast<const char*>(arg));
 
     struct timeval tp;
     cb_get_timeofday(&tp);
@@ -440,9 +485,29 @@ static void logger_thead_main(void* arg)
             /* Perform file IO without the lock */
             cb_mutex_exit(&mutex);
 
+            /* In case we failed to open the log file last time (e.g. EMFILE),
+               re-attempt now. */
+            if (fp == NULL) {
+                fp = open_logfile(reinterpret_cast<const char*>(arg));
+                if (fp != NULL) {
+                    // Record that the log is back online.
+                    struct timeval now;
+                    cb_get_timeofday(&now);
+                    char log_entry[1024];
+                    format_log_entry(log_entry, sizeof(log_entry),
+                                     now.tv_sec, now.tv_usec,
+                                     EXTENSION_LOG_WARNING,
+                                     "Restarting file logging\n");
+
+                    iops.write(fp, log_entry, strlen(log_entry));
+                    // Send to stderr for good measure.
+                    (void)fwrite(log_entry, sizeof(char), strlen(log_entry), stderr);
+                }
+            }
+
             currsize += flush_pending_io(fp, buffers + curr);
             if (currsize > cyclesz) {
-                fp = reopen_logfile(fp, reinterpret_cast<const char*>(arg));
+                fp = rotate_logfile(fp, reinterpret_cast<const char*>(arg));
                 currsize = 0;
             }
             cb_mutex_enter(&mutex);

CMakeLists.txt
@@ -519,12 +519,22 @@ ENDIF(NOT WIN32)
 INSTALL(FILES man/man4/memcached.json.4.pdf
         DESTINATION doc)
 
-ADD_EXECUTABLE(memcached_logger_test tests/logger_test.cc)
+ADD_EXECUTABLE(memcached_logger_test
+               tests/logger_test_common.cc
+               tests/logger_test.cc)
 TARGET_LINK_LIBRARIES(memcached_logger_test mcd_util file_logger dirutils)
 ADD_TEST(memcached-logger-test-rotate memcached_logger_test rotate)
 # There are too many spurious test failures
 #ADD_TEST(memcached-logger-test-dedupe memcached_logger_test dedupe)
 
+IF (NOT WIN32)
+   # Test makes use of {get,set}rlimit() which don't exist on Windows.
+   ADD_EXECUTABLE(memcached_logger_emfile_test
+                  tests/logger_test_common.cc
+                  tests/logger_emfile_test.cc)
+   TARGET_LINK_LIBRARIES(memcached_logger_emfile_test mcd_util file_logger dirutils)
+   ADD_TEST(memcached-logger-emfile-test memcached_logger_emfile_test)
+ENDIF (NOT WIN32)
 
 IF (${CMAKE_MAJOR_VERSION} LESS 3)
     SET_TARGET_PROPERTIES(mcd_util PROPERTIES INSTALL_NAME_DIR

tests/logger_emfile_test.cc
@@ -1,0 +1,134 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/*
+ * Test how logger handles running out of file descriptors (EMFILE)
+ */
+
+#include "logger_test_common.h"
+
+#include <cstdio>
+#include <cstdlib>
+#include <fcntl.h>
+#include <unistd.h>
+#include <platform/cbassert.h>
+#include <platform/dirutils.h>
+#include <sys/resource.h>
+
+void wait_for_log_to_contain(int fd, const char* log_message) {
+    // read() gives no guarantees of how the data will be chunked, so accumulate
+    // one lines' worth (up to \n) in a string.
+    std::string line;
+    while (true) {
+        char buffer[1024];
+        ssize_t bytes_read = read(fd, buffer, sizeof(buffer));
+        if (bytes_read > 0) {
+            line.append(buffer, bytes_read);
+        } else if (bytes_read == 0) {
+            usleep(10);
+        } else {
+            perror("read failed:");
+            return;
+        }
+
+        if (line.find('\n')) {
+            if (line.find(log_message) != std::string::npos) {
+                // Pass - found the warning in our log about rotation.
+                break;
+            } else {
+                line.clear();
+            }
+        }
+    }
+}
+
+int main() {
+
+    // Timeout if takes longer than 30s.
+    alarm(30);
+
+    // Clean out any old files.
+    std::vector<std::string> files;
+    files = CouchbaseDirectoryUtilities::findFilesWithPrefix("log_test_emfile");
+    if (!files.empty()) {
+        remove_files(files);
+    }
+
+    // Bring down out open file limit to a more conservative level (to
+    // save using up a huge number of user / system FDs).
+    struct rlimit rlim;
+    if (getrlimit(RLIMIT_NOFILE, &rlim) != 0) {
+        fprintf(stderr, "Failed to get getrlimit number of files\n");
+        exit(1);
+    }
+
+    rlim.rlim_cur = 100;
+    if (setrlimit(RLIMIT_NOFILE, &rlim) != 0) {
+        fprintf(stderr, "Failed to setrlimit number of files\n");
+        exit(2);
+    }
+
+    // Open the logger
+    EXTENSION_ERROR_CODE ret = memcached_extensions_initialize(
+            "unit_test=true;loglevel=warning;cyclesize=50;"
+            "buffersize=100;sleeptime=1;filename=log_test_emfile",
+            get_server_api);
+    cb_assert(ret == EXTENSION_SUCCESS);
+
+    // Wait for first log file to be created, and open it
+    int log_file;
+    while ((log_file = open("log_test_emfile.0.txt", O_RDONLY)) == -1) {
+        usleep(10);
+    }
+
+    // Consume all available FD so we cannot open any more files
+    // (i.e. rotation will fail).
+    std::vector<FILE*> FDs;
+    FILE* file;
+    while ((file = std::fopen(".", "r")) != NULL) {
+        FDs.emplace_back(file);
+    }
+
+    // add log entry, enough to trigger a rotation attempt.
+    logger->log(EXTENSION_LOG_DETAIL, NULL,
+               "test_emfile: Log line which should be in log_test_emfile.0.log");
+    logger->log(EXTENSION_LOG_DETAIL, NULL,
+               "test_emfile: Log line which should be in log_test_emfile.0.log");
+
+    // read() gives no guarantees of how the data will be chunked, so accumulate
+    // one lines' worth (up to \n) in a string.
+    wait_for_log_to_contain(log_file, "Failed to open next logfile");
+
+    // Close extra FDs so we can now print.
+    for (auto f : FDs) {
+        std::fclose(f);
+    }
+
+    // Wait for second log file to be created, and open it
+    close(log_file);
+    while ((log_file = open("log_test_emfile.1.txt", O_RDONLY)) == -1) {
+        usleep(10);
+    }
+
+    // add log entry, enough to trigger a rotation attempt.
+    logger->log(EXTENSION_LOG_DETAIL, NULL,
+               "test_emfile: Log line which should be in log_test_emfile.1.log");
+
+    wait_for_log_to_contain(log_file, "Restarting file logging");
+
+    logger->shutdown(false);
+}

tests/logger_test.cc
@@ -1,93 +1,17 @@
 /* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-#include <stdio.h>
-#include <cstdlib>
+
+#include "logger_test_common.h"
+
+#include <platform/cbassert.h>
+#include <platform/dirutils.h>
+
 #include <cstring>
+#include <cstdio>
 #include <iostream>
 #include <sstream>
-
-#include <platform/dirutils.h>
-#include <extensions/protocol_extension.h>
-#include <memcached/config_parser.h>
-#include <platform/cbassert.h>
+#include <vector>
 
 using namespace std;
-
-EXTENSION_LOGGER_DESCRIPTOR *logger;
-
-extern "C" {
-    static EXTENSION_LOG_LEVEL get_log_level(void);
-    static bool register_extension(extension_type_t type, void *extension);
-    static void register_callback(ENGINE_HANDLE *eh,
-                                  ENGINE_EVENT_TYPE type,
-                                  EVENT_CALLBACK cb,
-                                  const void *cb_data);
-    static SERVER_HANDLE_V1 *get_server_api(void);
-}
-
-
-static EXTENSION_LOG_LEVEL get_log_level(void)
-{
-    return EXTENSION_LOG_DETAIL;
-}
-
-static bool register_extension(extension_type_t type, void *extension)
-{
-    cb_assert(type == EXTENSION_LOGGER);
-    logger = reinterpret_cast<EXTENSION_LOGGER_DESCRIPTOR *>(extension);
-    return true;
-}
-
-static void register_callback(ENGINE_HANDLE *eh,
-                              ENGINE_EVENT_TYPE type,
-                              EVENT_CALLBACK cb,
-                              const void *cb_data)
-{
-    (void)eh;
-    (void)type;
-    (void)cb;
-    (void)cb_data;
-}
-
-static SERVER_HANDLE_V1 *get_server_api(void)
-{
-    static int init;
-    static SERVER_CORE_API core_api;
-    static SERVER_COOKIE_API server_cookie_api;
-    static SERVER_STAT_API server_stat_api;
-    static SERVER_LOG_API server_log_api;
-    static SERVER_EXTENSION_API extension_api;
-    static SERVER_CALLBACK_API callback_api;
-    static ALLOCATOR_HOOKS_API hooks_api;
-    static SERVER_HANDLE_V1 rv;
-
-    if (!init) {
-        init = 1;
-
-        core_api.parse_config = parse_config;
-        server_log_api.get_level = get_log_level;
-        extension_api.register_extension = register_extension;
-        callback_api.register_callback = register_callback;
-
-        rv.interface = 1;
-        rv.core = &core_api;
-        rv.stat = &server_stat_api;
-        rv.extension = &extension_api;
-        rv.callback = &callback_api;
-        rv.log = &server_log_api;
-        rv.cookie = &server_cookie_api;
-        rv.alloc_hooks = &hooks_api;
-    }
-
-    return &rv;
-}
-
-static void remove_files(std::vector<std::string> &files) {
-    for (std::vector<std::string>::iterator iter = files.begin();
-         iter != files.end();
-         ++iter) {
-        CouchbaseDirectoryUtilities::rmrf(*iter);
-    }
-}
 
 static void test_rotate(void) {
     EXTENSION_ERROR_CODE ret;

tests/logger_test_common.cc
@@ -1,0 +1,87 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/*
+ * Common helper code for all logger tests.
+ */
+
+#include "logger_test_common.h"
+
+#include <platform/cbassert.h>
+#include <platform/dirutils.h>
+
+EXTENSION_LOGGER_DESCRIPTOR *logger;
+
+EXTENSION_LOG_LEVEL get_log_level(void)
+{
+    return EXTENSION_LOG_DETAIL;
+}
+
+bool register_extension(extension_type_t type, void *extension)
+{
+    cb_assert(type == EXTENSION_LOGGER);
+    logger = reinterpret_cast<EXTENSION_LOGGER_DESCRIPTOR *>(extension);
+    return true;
+}
+
+void register_callback(ENGINE_HANDLE *eh,
+                       ENGINE_EVENT_TYPE type,
+                       EVENT_CALLBACK cb,
+                       const void *cb_data)
+{
+}
+
+SERVER_HANDLE_V1 *get_server_api(void)
+{
+    static int init;
+    static SERVER_CORE_API core_api;
+    static SERVER_COOKIE_API server_cookie_api;
+    static SERVER_STAT_API server_stat_api;
+    static SERVER_LOG_API server_log_api;
+    static SERVER_EXTENSION_API extension_api;
+    static SERVER_CALLBACK_API callback_api;
+    static ALLOCATOR_HOOKS_API hooks_api;
+    static SERVER_HANDLE_V1 rv;
+
+    if (!init) {
+        init = 1;
+
+        core_api.parse_config = parse_config;
+        server_log_api.get_level = get_log_level;
+        extension_api.register_extension = register_extension;
+        callback_api.register_callback = register_callback;
+
+        rv.interface = 1;
+        rv.core = &core_api;
+        rv.stat = &server_stat_api;
+        rv.extension = &extension_api;
+        rv.callback = &callback_api;
+        rv.log = &server_log_api;
+        rv.cookie = &server_cookie_api;
+        rv.alloc_hooks = &hooks_api;
+    }
+
+    return &rv;
+}
+
+void remove_files(std::vector<std::string> &files) {
+    for (std::vector<std::string>::iterator iter = files.begin();
+         iter != files.end();
+         ++iter) {
+        CouchbaseDirectoryUtilities::rmrf(*iter);
+    }
+}

tests/logger_test_common.h
@@ -1,0 +1,38 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2015 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#pragma once
+
+#include <extensions/protocol_extension.h>
+
+#include <string>
+#include <vector>
+
+extern EXTENSION_LOGGER_DESCRIPTOR *logger;
+
+extern "C" {
+    EXTENSION_LOG_LEVEL get_log_level(void);
+    bool register_extension(extension_type_t type, void *extension);
+    void register_callback(ENGINE_HANDLE *eh,
+                           ENGINE_EVENT_TYPE type,
+                           EVENT_CALLBACK cb,
+                           const void *cb_data);
+    SERVER_HANDLE_V1 *get_server_api(void);
+}
+
+// Removes all files in the specified vector
+void remove_files(std::vector<std::string> &files);

commit fb7bc39af8464c279bcaa615ba0218dc580d5872

MB-17045: Ensure that logfiles are not truncated when re-opening after EMFILE
In the error scenario where memcached has run out of file descriptors,
and then subsequently recovers (i.e. more FDs are made available), it
may incorrectly attempt to re-open the previous logfile, truncating it
in the process.

This is due to the search for the next file in open_logfile() starting
from the same position as previously, instead of one after the last
successful open.

Fix the search algorithm, and for additional paranoia always append to
any logfile we open.

This bug was introduced by commit 7d88d0b - MB-16689: Handle EMFILE on
file logger rotation.

Change-Id: I6cdadb8608ea84d5ab7e08ab6694e7006abf28df
Reviewed-on: http://review.couchbase.org/57721
Well-Formed: buildbot <build@couchbase.com>
Tested-by: buildbot <build@couchbase.com>
Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

extensions/loggers/file_logger.cc
@@ -355,14 +355,22 @@ static FILE *open_logfile(const char *fnm) {
     static unsigned int next_id = 0;
     char fname[1024];
     FILE *ret;
-    sprintf(fname, "%s.%d.%s", fnm, next_id, extension);
-    while (access(fname, F_OK) == 0) {
-        sprintf(fname, "%s.%d.%s", fnm, ++next_id, extension);
-    }
 
-    ret = fopen(fname, "wb");
+    // Search for the next logfile number which doesn't yet exist.
+    // There's a subtlety here - if we have run out of file descriptors (EMFILE) -
+    // access() can fail with ENOENT, which means we need to ensure that we only
+    // search from *after* the last successful open call - i.e. try_id always
+    // starts from one after the last successful open, otherwise we can
+    // re-open the existing log file and overwrite it!
+    unsigned int try_id = next_id;
+    do {
+        sprintf(fname, "%s.%d.%s", fnm, try_id++, extension);
+    } while (access(fname, F_OK) == 0);
+
+    ret = fopen(fname, "ab");
     if (ret != nullptr) {
         setbuf(ret, nullptr);
+        next_id = try_id;
     }
 
     return ret;