eafddf602627eb75c36913534bba4d4bb274d88c
abhinavdangeti <abhinav@couchbase.com>
2016-01-04 09:00:31 -0800

    [BP] MB-16292: Replica/Pending vb state switched in takeover packet.
    The encoding of the takeover packet changes the value of what
    ep-engine uses. The receive side isn't undoing the switch so
    ep-engine thinks it set state to pending, but it gets changed
    to replica.
    
    Change-Id: Ic97bb70ca7b7264bd50989e2b9e52338dee8cbf8
    Reviewed-on: http://review.couchbase.org/58013
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- daemon/memcached.c
+++ daemon/memcached.c
@@ -2655,6 +2655,10 @@ static ENGINE_ERROR_CODE dcp_message_set_vbucket_state(const void* cookie,
         return ENGINE_E2BIG;
     }
 
+    if (!is_valid_vbucket_state_t(state)) {
+        return ENGINE_EINVAL;
+    }
+
     memset(packet.bytes, 0, sizeof(packet.bytes));
     packet.message.header.request.magic =  (uint8_t)PROTOCOL_BINARY_REQ;
     packet.message.header.request.opcode = (uint8_t)PROTOCOL_BINARY_CMD_DCP_SET_VBUCKET_STATE;
@@ -2662,23 +2666,7 @@ static ENGINE_ERROR_CODE dcp_message_set_vbucket_state(const void* cookie,
     packet.message.header.request.bodylen = htonl(1);
     packet.message.header.request.opaque = opaque;
     packet.message.header.request.vbucket = htons(vbucket);
-
-    switch (state) {
-    case vbucket_state_active:
-        packet.message.body.state = 0x01;
-        break;
-    case vbucket_state_pending:
-        packet.message.body.state = 0x02;
-        break;
-    case vbucket_state_replica:
-        packet.message.body.state = 0x03;
-        break;
-    case vbucket_state_dead:
-        packet.message.body.state = 0x04;
-        break;
-    default:
-        return ENGINE_EINVAL;
-    }
+    packet.message.body.state = (uint8_t)state;
 
     memcpy(c->write.curr, packet.bytes, sizeof(packet.bytes));
     add_iov(c, c->write.curr, sizeof(packet.bytes));
