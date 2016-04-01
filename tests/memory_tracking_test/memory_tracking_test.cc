4046ab37e5485cf4d6556ccb01c55943eea70b8c
Dave Rigby <daver@couchbase.com>
2016-04-01 15:05:44 +0100

    Convert memory_tracking_test to GTest
    As part of MB-18940 I needed to expand the testing of the memory
    tracker. Ultimately the changes were contained to ep-engine, however
    in the process I wanted to be able to use GTest for memory tracking
    tests. This patch converts the (memcached) memory tracking tests to
    the GTest framework.
    
    Change-Id: I157769cebdab10a334102a0581f7f324e35f39b0
    Reviewed-on: http://review.couchbase.org/62289
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- tests/memory_tracking_test/memory_tracking_test.cc
+++ tests/memory_tracking_test/memory_tracking_test.cc
@@ -24,13 +24,15 @@
 
 #include "daemon/alloc_hooks.h"
 
-std::atomic_size_t alloc_size;
+#include <gtest/gtest.h>
 
 // Test pointer in global scope to prevent compiler optimizing malloc/free away
 // via DCE.
 char* p;
 
-extern "C" {
+class MemoryTrackerTest : public ::testing::Test {
+public:
+    // callback function for when memory is allocated.
     static void NewHook(const void* ptr, size_t) {
         if (ptr != NULL) {
             void* p = const_cast<void*>(ptr);
@@ -38,6 +40,7 @@ extern "C" {
         }
     }
 
+    // callback function for when memory is deleted.
     static void DeleteHook(const void* ptr) {
         if (ptr != NULL) {
             void* p = const_cast<void*>(ptr);
@@ -45,92 +48,107 @@ extern "C" {
         }
     }
 
-    static void TestThread(void* arg) {
-        alloc_size = 0;
+    // function executed by our accounting test thread.
+    static void AccountingTestThread(void* arg);
 
-        // Test new & delete //////////////////////////////////////////////////
-        p = new char();
-        cb_assert(alloc_size > 0);
-        delete p;
-        cb_assert(alloc_size == 0);
+    static std::atomic_size_t alloc_size;
+};
 
-        // Test new[] & delete[] //////////////////////////////////////////////
-        p = new char[100];
-        cb_assert(alloc_size >= 100);
-        delete []p;
-        cb_assert(alloc_size == 0);
+std::atomic_size_t MemoryTrackerTest::alloc_size;
 
-        // Test malloc() / free() /////////////////////////////////////////////
-        p = static_cast<char*>(malloc(sizeof(char) * 10));
-        cb_assert(alloc_size >= 10);
-        free(p);
-        cb_assert(alloc_size == 0);
 
-        // Test realloc() /////////////////////////////////////////////////////
-        p = static_cast<char*>(malloc(1));
-        cb_assert(alloc_size >= 1);
+void MemoryTrackerTest::AccountingTestThread(void* arg) {
+    alloc_size = 0;
 
-        // Allocator may round up allocation sizes; so it's hard to
-        // accurately predict how much alloc_size will increase. Hence
-        // we just increase by a "large" amount and check at least half that
-        // increment.
-        size_t prev_size = alloc_size;
-        p = static_cast<char*>(realloc(p, sizeof(char) * 100));
-        cb_assert(alloc_size >= (prev_size + 50));
+    // Test new & delete //////////////////////////////////////////////////
+    p = new char();
+    EXPECT_GT(alloc_size, 0);
+    delete p;
+    EXPECT_EQ(0, alloc_size);
 
-        prev_size = alloc_size;
-        p = static_cast<char*>(realloc(p, 1));
-        cb_assert(alloc_size < prev_size);
+    // Test new[] & delete[] //////////////////////////////////////////////
+    p = new char[100];
+    EXPECT_GE(alloc_size, 100);
+    delete []p;
+    EXPECT_EQ(0, alloc_size);
 
-        prev_size = alloc_size;
-        char* q = static_cast<char*>(realloc(NULL, 10));
-        cb_assert(alloc_size >= prev_size + 10);
+    // Test malloc() / free() /////////////////////////////////////////////
+    p = static_cast<char*>(malloc(sizeof(char) * 10));
+    EXPECT_GE(alloc_size, 10);
+    free(p);
+    EXPECT_EQ(0, alloc_size);
 
-        free(p);
-        free(q);
-        cb_assert(alloc_size == 0);
+    // Test realloc() /////////////////////////////////////////////////////
+    p = static_cast<char*>(malloc(1));
+    EXPECT_GE(alloc_size, 1);
 
-        // Test calloc() //////////////////////////////////////////////////////
-        p = static_cast<char*>(calloc(sizeof(char), 20));
-        cb_assert(alloc_size >= 20);
-        free(p);
-        cb_assert(alloc_size == 0);
+    // Allocator may round up allocation sizes; so it's hard to
+    // accurately predict how much alloc_size will increase. Hence
+    // we just increase by a "large" amount and check at least half that
+    // increment.
+    size_t prev_size = alloc_size;
+    p = static_cast<char*>(realloc(p, sizeof(char) * 100));
+    EXPECT_GE(alloc_size, (prev_size + 50));
 
-        // Test indirect use of malloc() via strdup() /////////////////////////
-        p = strdup("random string");
-        cb_assert(alloc_size >= sizeof("random string"));
-        free(p);
-        cb_assert(alloc_size == 0);
+    prev_size = alloc_size;
+    p = static_cast<char*>(realloc(p, 1));
+    EXPECT_LT(alloc_size, prev_size);
+
+    prev_size = alloc_size;
+    char* q = static_cast<char*>(realloc(NULL, 10));
+    EXPECT_GE(alloc_size, prev_size + 10);
+
+    free(p);
+    free(q);
+    EXPECT_EQ(0, alloc_size);
+
+    // Test calloc() //////////////////////////////////////////////////////
+    p = static_cast<char*>(calloc(sizeof(char), 20));
+    EXPECT_GE(alloc_size, 20);
+    free(p);
+    EXPECT_EQ(0, alloc_size);
+
+    // Test indirect use of malloc() via strdup() /////////////////////////
+    p = strdup("random string");
+    EXPECT_GE(alloc_size, sizeof("random string"));
+    free(p);
+    EXPECT_EQ(0, alloc_size);
 
 #if defined(HAVE_MEMALIGN)
-        // Test memalign //////////////////////////////////////////////////////
-        p = static_cast<char*>(memalign(16, 64));
-        cb_assert(alloc_size >= 64);
-        free(p);
-        cb_assert(alloc_size == 0);
+    // Test memalign //////////////////////////////////////////////////////
+    p = static_cast<char*>(memalign(16, 64));
+    EXPECT_GE(alloc_size, 64);
+    free(p);
+    EXPECT_EQ(0, alloc_size);
 
-        // Test posix_memalign ////////////////////////////////////////////////
-        void* ptr;
-        cb_assert(posix_memalign(&ptr, 16, 64) == 0);
-        cb_assert(alloc_size >= 64);
-        free(ptr);
-        cb_assert(alloc_size == 0);
+    // Test posix_memalign ////////////////////////////////////////////////
+    void* ptr;
+    cb_assert(posix_memalign(&ptr, 16, 64) == 0);
+    EXPECT_GE(alloc_size, 64);
+    free(ptr);
+    EXPECT_EQ(0, alloc_size);
 #endif
-    }
 }
 
-int main(void) {
-   init_alloc_hooks();
+// Test that the various memory allocation / deletion functions are correctly
+// accounted for, when run in a parallel thread.
+TEST_F(MemoryTrackerTest, Accounting) {
+    mc_add_new_hook(NewHook);
+    mc_add_delete_hook(DeleteHook);
 
-   mc_add_new_hook(NewHook);
-   mc_add_delete_hook(DeleteHook);
+    cb_thread_t tid;
+    ASSERT_EQ(0, cb_create_thread(&tid, AccountingTestThread, 0, 0));
+    ASSERT_EQ(0, cb_join_thread(tid));
 
-   cb_thread_t tid;
-   cb_assert(cb_create_thread(&tid, TestThread, 0, 0) == 0);
-   cb_assert(cb_join_thread(tid) == 0);
+    mc_remove_new_hook(NewHook);
+    mc_remove_delete_hook(DeleteHook);
+}
 
-   mc_remove_new_hook(NewHook);
-   mc_remove_delete_hook(DeleteHook);
 
-   return 0;
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+
+    init_alloc_hooks();
+
+    return RUN_ALL_TESTS();
 }
