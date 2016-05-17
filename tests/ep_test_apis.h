cac6c736db563146bbddf3a5c3486f75b9d99db2
Jim Walker <jim@couchbase.com>
2016-05-17 17:41:10 +0100

    MB-19636: Initialise failovers correctly from 2.5.x vbstate
    (Note: backport of MB-19635 to 3.0.x branch).
    
    When loading a vb file, don't force the failover table data
    to be ("[{\"id\":0,\"seq\":0}]"); if the file doesn't contain
    any data.
    
    Change-Id: I41673bf848fcbab9b616edec5c7fd2ab9a3ddd6b
    Reviewed-on: http://review.couchbase.org/64156
    Well-Formed: buildbot <build@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- tests/ep_test_apis.h
+++ tests/ep_test_apis.h
@@ -40,7 +40,17 @@ static void checkeqfn(T exp, T got, const char *msg, const char *file, const int
     }
 }
 
+template <typename T>
+static void checknefn(T exp, T got, const char *msg, const char *file, const int linenum) {
+    if (exp == got) {
+        std::stringstream ss;
+        ss << "Expected `" << exp << "' to not equal `" << got << "' - " << msg;
+        abort_msg(ss.str().c_str(), file, linenum);
+    }
+}
+
 #define checkeq(a, b, c) checkeqfn(a, b, c, __FILE__, __LINE__)
+#define checkne(a, b, c) checknefn(a, b, c, __FILE__, __LINE__)
 
 #ifdef __cplusplus
 extern "C" {
