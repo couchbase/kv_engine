6e0a4993d2ec77233b40119db2560cf7d5458034
Dave Rigby <daver@couchbase.com>
2016-08-04 12:52:00 +0100

    Add unmerged-commits.py
    Script to show which commit(s) are not yet merged between our release
    branches.
    
    Example output (branches slightly hacked to show interesting results):
    
        Commits in 'couchbase/3.0.x' not present in 'couchbase/sherlock':
    
        Commits in 'couchbase/sherlock' not present in '5edb7b1':
        + 297e7b9dbe92f25feebaf2243d31b65cb9388a2e MB-20330: ReaderLockHolder with no lvalue
        + 60f5d2f0c5c70821adc2b0964c123ba61c51ead3 MB-20312: Initialise snapshot task priority
        + 1301ca609be559248af78d6fa52ce766dd8e4915 MB-20307: Re-enable dcp ep_dcp_dead_conn_count
    
        Commits in '5edb7b1' not present in 'couchbase/master':
        + 6e38fe8891e4e63996cecaa55b0e23accaf6f83b MB-19113: Address false positive lock inversion seen with test_mb16357
        + 3bd9fa40f9cc597ffc6b4f9743c75bf4f8f6e4d6 MB-19153: Break circular dependency while deleting bucket
        + 6f7bfa3de30c21275687985a3ee6bcf92053f741 MB-16656: Send snapshotEnd as highSeqno for replica vb in GET_ALL_VB_SEQNOS call
        ...
    
    Change-Id: I4887ab87cc7622a6b52089ca2aaf7d10a8ce71f1
    Reviewed-on: http://review.couchbase.org/66487
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Reviewed-by: Will Gardner <will.gardner@couchbase.com>

---
+++ scripts/unmerged-commits.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2.7
+
+# Script to show which commit(s) are not yet merged between our release branches.
+
+import subprocess
+from __future__ import print_function
+
+class bcolors:
+    HEADER = '\033[36m'
+    OKBLUE = '\033[34m'
+    OKGREEN = '\033[32m'
+    WARNING = '\033[33m'
+    FAIL = '\033[31m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+# Set of branches to check for unmerged patches. Ordered by ancestory;
+# i.e. the oldest supported branch to the newest, which is the order
+# patches should be merged.
+branches = ['couchbase/3.0.x',
+            'couchbase/sherlock',
+            'couchbase/watson',
+            'couchbase/master']
+
+for downstream, upstream in zip(branches, branches[1:]):
+    print((bcolors.HEADER +
+           "Commits in '{}' not present in '{}':" +
+           bcolors.ENDC).format(downstream, upstream))
+    commits = subprocess.check_output(['git', 'cherry', '-v',
+                                       upstream, downstream])
+    print(commits)
+
+    count = len(commits.splitlines())
+    if count > 0:
+        print ((bcolors.WARNING +
+                "{} commits outstanding" + bcolors.ENDC).format(count))
