08386b63b941effcf4a30f5db1fc57f92d934cf8
Manu Dhundi <manu@couchbase.com>
2016-10-18 13:51:10 -0700

    MB-21300: [Address Sanitizer] Prevent underflow during hash table resize()
    http://cv.jenkins.couchbase.com/job/ep-engine-addresssanitizer-master/385
    
    If i == 0 in line 144 of HashTable::resize(), reading prime_size_table[i - 1]
    is spurious.
    
    Change-Id: I7f3135efff7a0993fb939707b912d064514e9a45
    Reviewed-on: http://review.couchbase.org/68887
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

--- src/stored-value.cc
+++ src/stored-value.cc
@@ -422,6 +422,8 @@ void HashTable::resize() {
     } else if (prime_size_table[i] < static_cast<ssize_t>(defaultNumBuckets)) {
         // Was going to be smaller than the configured ht_size.
         new_size = defaultNumBuckets;
+    } else if (0 == i) {
+        new_size = prime_size_table[i];
     } else if (isCurrently(size, prime_size_table[i-1], prime_size_table[i])) {
         // If one of the candidate sizes is the current size, maintain
         // the current size in order to remain stable.
