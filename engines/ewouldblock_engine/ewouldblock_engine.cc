6c175603f87e602028d31dde82b16b0632e29b41
Trond Norbye <trond.norbye@gmail.com>
2016-10-27 19:58:16 +0200

    Use the correct loglevel for debug output in ewb engine
    Change-Id: I04028baf68c6553ed42100b68ce1e493aba9210a
    Reviewed-on: http://review.couchbase.org/69279
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ewouldblock_engine/ewouldblock_engine.cc
+++ engines/ewouldblock_engine/ewouldblock_engine.cc
@@ -886,7 +886,7 @@ private:
         for (const auto c : suspended_map) {
             if (c.second == cookie) {
                 auto logger = gsa()->log->get_logger();
-                logger->log(EXTENSION_LOG_NOTICE, nullptr,
+                logger->log(EXTENSION_LOG_DEBUG, nullptr,
                             "Connection %p with id %u should be suspended for engine %p",
                            c.second, c.first, this);
 
@@ -902,8 +902,9 @@ private:
             pending_io_ops.push(cookie);
         }
         auto logger = gsa()->log->get_logger();
-        logger->log(EXTENSION_LOG_WARNING, nullptr,
-                    "EWB_Engine: connection %p should be resumed for engine %p", cookie, this);
+        logger->log(EXTENSION_LOG_DEBUG, nullptr,
+                    "EWB_Engine: connection %p should be resumed for engine %p",
+                    cookie, this);
 
         condvar.notify_one();
     }
