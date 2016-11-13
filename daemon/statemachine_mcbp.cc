618f9487fe635f57003ba7d7c795dac6f7512948
Trond Norbye <trond.norbye@gmail.com>
2016-11-12 20:15:37 -0800

    ListeningPort is no longer a struct
    Change-Id: Ifd0a89fbe0acc904fa3e0be8d2951f05d8ea6a8e
    Reviewed-on: http://review.couchbase.org/69878
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- daemon/statemachine_mcbp.cc
+++ daemon/statemachine_mcbp.cc
@@ -449,7 +449,7 @@ bool conn_pending_close(McbpConnection *c) {
 }
 
 bool conn_immediate_close(McbpConnection *c) {
-    struct ListeningPort *port_instance;
+    ListeningPort *port_instance;
     if (!c->isSocketClosed()) {
         throw std::logic_error("conn_immediate_close: socketDescriptor must be closed");
     }
