32d8c82c791a19e1e961b2bfb28fc3ba859f2be3
Trond Norbye <trond.norbye@gmail.com>
2016-11-25 14:33:17 +0100

    Refactor: split buffer.h and move sized_buffer to platform
    Change-Id: I99f90a618ceb1985e530fa8bc894fa6d0e383d0e
    Reviewed-on: http://review.couchbase.org/70362
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- daemon/buffer.h
+++ daemon/buffer.h
@@ -20,69 +20,38 @@
 #include <functional>
 #include <string>
 
-/* Struct representing a buffer of some known size. This is used to
- * refer to some existing region of memory which is owned elsewhere - i.e.
- * this object does not have ownership semantics.
- * A user should not free() the buf member themselves!
- */
-template <typename T>
-struct sized_buffer {
-    sized_buffer()
-        : sized_buffer(nullptr, 0) {}
+#include <platform/sized_buffer.h>
 
-    sized_buffer(T* buf_, size_t len_)
-        : buf(buf_),
-          len(len_) {}
-
-    sized_buffer(const std::string& str)
-        : buf(str.data()),
-          len(str.size()) { }
-
-    T* data() {
-        return buf;
-    }
-
-    const T* data() const {
-        return buf;
-    }
-
-    size_t size() const {
-        return len;
-    }
-
-    T* buf;
-    size_t len;
-};
-
-using char_buffer = sized_buffer<char>;
-using const_char_buffer = sized_buffer<const char>;
+// Until we've fixed our code ;)
+using char_buffer = cb::char_buffer;
+using const_char_buffer = cb::const_char_buffer;
 
 /*
  * Specialization of std::hash<> for sized_buffer & const_sized_buffer.
  */
-template<typename T>
+template <typename T>
 static size_t hash_array(const T* base, size_t len);
 
 namespace std {
-    template<>
-    struct hash<char_buffer> {
-        size_t operator()(const sized_buffer<char>& s) const {
-            return hash_array<char>(s.buf, s.len);
-        }
-    };
+template <>
+struct hash<char_buffer> {
+    size_t operator()(const char_buffer& s) const {
+        return hash_array<char>(s.buf, s.len);
+    }
+};
 
-    template<>
-    struct hash<const_char_buffer> {
-        size_t operator()(const const_char_buffer& s) const {
-            return hash_array<char>(s.buf, s.len);
-        }
-    };
+template <>
+struct hash<const_char_buffer> {
+    size_t operator()(const const_char_buffer& s) const {
+        return hash_array<char>(s.buf, s.len);
+    }
+};
 }
 
 /*
  * Simple hash function for a contiguous array of primitive types.
  */
-template<typename T>
+template <typename T>
 static size_t hash_array(const T* base, size_t len) {
     size_t rv = 5381;
 
