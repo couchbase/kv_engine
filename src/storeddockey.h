07df3775f7a9eaf5af445b0183088bcbcd596d3f
Jim Walker <jim@couchbase.com>
2016-12-05 13:12:54 +0000

    MB-21916: StoredDocKey - A DocKey container and test code
    This commit introduces StoredDocKey - a container to replace
    our use of std::string for document keys. The container can be used
    in std::map/set etc... and allows for us to store all items that are
    needed to reference a document.
    
    - the key (and the key size)
    - the document namespace (a critical piece for Collections)
    
    StoredDocKey will store a key of n-bytes in an n+1 std::string.
    The extra byte is where we store the DocNamespace.
    
    Change-Id: I1221e55d0081427c5f477773234ebbab482a6226
    Reviewed-on: http://review.couchbase.org/70680
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: buildbot <build@couchbase.com>

---
+++ src/storeddockey.h
@@ -0,0 +1,149 @@
+/*
+ *     Copyright 2016 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <type_traits>
+
+#include <memcached/dockey.h>
+
+#include "ep_types.h"
+
+/**
+ * StoredDocKey is a container for key data
+ *
+ * Internally an n byte key is stored in a n + 1 std::string.
+ *  a) We zero terminate so that data() is safe for printing as a c-string.
+ *  b) We store the DocNamespace in byte 0 (duplicated in parent class DocKey).
+ *    This is because StoredDocKey typically ends up being written to disk and
+ *    the DocNamespace forms part of the on-disk key. Pre-allocating space
+ *    for the DocNamespace means storage components don't have to create a
+ *    new buffer into which we layout DocNamespace and key data.
+ */
+class StoredDocKey : public DocKeyInterface<StoredDocKey> {
+public:
+    /**
+     * Create a StoredDocKey from key to key+nkey in docNamespace.
+     * @param key pointer to key data
+     * @param nkey byte length of key (which will be copied in)
+     * @docNamespace the namespace the key applies to
+     */
+    StoredDocKey(const uint8_t* key, size_t nkey, DocNamespace docNamespace) {
+        keydata.resize(nkey + namespaceBytes);
+        keydata.replace(
+                namespaceBytes, nkey, reinterpret_cast<const char*>(key), nkey);
+        keydata.at(0) = static_cast<std::underlying_type<DocNamespace>::type>(
+                docNamespace);
+    }
+
+    /**
+     * Create a StoredDocKey from a DocKey
+     * @param key DocKey that is to be copied-in
+     */
+    StoredDocKey(const DocKey key)
+        : StoredDocKey(key.data(), key.size(), key.getDocNamespace()) {
+    }
+
+    /**
+     * Create a StoredDocKey from a std::string. Post
+     * construction size will return std::string::size();
+     * @param key std::string to be copied-in
+     * @param docNamespace the namespace that the key applies to
+     */
+    StoredDocKey(const std::string& key, DocNamespace docNamespace)
+        : StoredDocKey(reinterpret_cast<const uint8_t*>(key.c_str()),
+                       key.size(),
+                       docNamespace) {
+    }
+
+    /**
+     * Create a StoredDocKey from a buffer that was previously
+     * obtained from getNamespacedData()
+     * Post construction size() will return nkey - 1
+     * @param key pointer to data to be copied in
+     * @param nkey the number of bytes to be copied
+     */
+    StoredDocKey(const uint8_t* key, size_t nkey)
+        : keydata(reinterpret_cast<const char*>(key), nkey) {
+    }
+
+    const uint8_t* data() const {
+        return reinterpret_cast<const uint8_t*>(
+                &keydata.data()[namespaceBytes]);
+    }
+
+    size_t size() const {
+        return keydata.size() - namespaceBytes;
+    }
+
+    DocNamespace getDocNamespace() const {
+        return DocNamespace(keydata.at(0));
+    }
+
+    const char* c_str() const {
+        return &keydata.c_str()[namespaceBytes];
+    }
+
+    /**
+     * Return the key with the DocNamespace as a prefix.
+     * Thus the "beer::bud" in the Collections namespace becomes "\1beer::bud"
+     */
+    const uint8_t* getDocNameSpacedData() const {
+        return reinterpret_cast<const uint8_t*>(keydata.data());
+    }
+
+    /**
+     * Return the size of the buffer returned by getDocNameSpacedData()
+     */
+    size_t getDocNameSpacedSize() const {
+        return keydata.size();
+    }
+
+    bool operator==(const StoredDocKey& rhs) const {
+        return keydata == rhs.keydata;
+    }
+
+    bool operator!=(const StoredDocKey& rhs) const {
+        return !(*this == rhs);
+    }
+
+    bool operator<(const StoredDocKey& rhs) const {
+        return keydata < rhs.keydata;
+    }
+
+protected:
+    static const size_t namespaceBytes = sizeof(DocNamespace);
+    std::string keydata;
+};
+
+std::ostream& operator<<(std::ostream& os, const StoredDocKey& key);
+
+static_assert(sizeof(DocNamespace) == sizeof(uint8_t),
+              "StoredDocKey: DocNamespace is too large");
+
+/**
+ * A hash function for StoredDocKey so they can be used in std::map and friends.
+ */
+namespace std {
+template <>
+struct hash<StoredDocKey> {
+    std::size_t operator()(const StoredDocKey& key) const {
+        return key.hash();
+    }
+};
+}
---
+++ tests/module_tests/storeddockey_test.cc
@@ -0,0 +1,228 @@
+/*
+ *     Copyright 2016 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+
+#include "storeddockey.h"
+
+#include <map>
+
+class StoredDocKeyTest : public ::testing::TestWithParam<DocNamespace> {};
+
+class StoredDocKeyTestCombi : public ::testing::TestWithParam<
+                                      std::tuple<DocNamespace, DocNamespace>> {
+};
+
+TEST_P(StoredDocKeyTest, constructors) {
+    // C-string/std::string
+    StoredDocKey key1("key", GetParam());
+    EXPECT_EQ(strlen("key"), key1.size());
+    EXPECT_EQ(0, std::memcmp("key", key1.data(), sizeof("key")));
+    EXPECT_EQ(GetParam(), key1.getDocNamespace());
+
+    // Raw
+    uint8_t keyRaw[4] = {'k', 'e', 'y', '!'};
+    StoredDocKey key2(keyRaw, 3, GetParam());
+    EXPECT_EQ(3, key2.size());
+    EXPECT_NE(keyRaw, key2.data());
+    EXPECT_EQ(0, std::memcmp(keyRaw, key2.data(), key2.size()));
+    EXPECT_EQ(GetParam(), key2.getDocNamespace());
+
+    // DocKey
+    DocKey docKey(keyRaw, 3, GetParam());
+    StoredDocKey key3(docKey);
+    EXPECT_EQ(3, key3.size());
+    EXPECT_NE(docKey.data(), key3.data());
+    EXPECT_EQ(0, std::memcmp(keyRaw, key3.data(), key3.size()));
+    EXPECT_EQ(GetParam(), key3.getDocNamespace());
+}
+
+TEST_P(StoredDocKeyTest, namespaced_constructor) {
+    StoredDocKey key1("key", GetParam());
+    StoredDocKey key2(key1.getDocNameSpacedData(), key1.getDocNameSpacedSize());
+    EXPECT_EQ(key1, key2);
+    EXPECT_NE(key1.data(), key2.data());
+}
+
+TEST_P(StoredDocKeyTest, copy_constructor) {
+    StoredDocKey key1("key1", GetParam());
+    StoredDocKey key2(key1);
+
+    // exterally check rather than just use ==
+    EXPECT_EQ(key1.size(), key2.size());
+    EXPECT_EQ(key1.getDocNamespace(), key2.getDocNamespace());
+    EXPECT_NE(key1.data(), key2.data()); // must be different pointers
+    EXPECT_TRUE(std::memcmp(key1.data(), key2.data(), key1.size()) == 0);
+    EXPECT_EQ(key1, key2);
+}
+
+TEST_P(StoredDocKeyTest, assignment) {
+    StoredDocKey key1("key1", GetParam());
+    StoredDocKey key2("anotherkey", GetParam());
+
+    key1 = key2;
+
+    // exterally check
+    EXPECT_EQ(key1.size(), key2.size());
+    EXPECT_EQ(key1.getDocNamespace(), key2.getDocNamespace());
+    EXPECT_NE(key1.data(), key2.data()); // must be different pointers
+    EXPECT_TRUE(std::memcmp(key1.data(), key2.data(), key1.size()) == 0);
+    EXPECT_EQ(key1, key2);
+}
+
+TEST_P(StoredDocKeyTest, cStringSafe) {
+    uint8_t raw[6] = {1, 2, 3, 4, 5, 6};
+    StoredDocKey key(raw, 5, GetParam());
+    EXPECT_EQ(0, key.data()[5]);
+    EXPECT_EQ(5, strlen(reinterpret_cast<const char*>(key.data())));
+    EXPECT_EQ(5, key.size());
+}
+
+TEST_P(StoredDocKeyTestCombi, hash) {
+    StoredDocKey key1("key1", std::get<0>(GetParam()));
+    StoredDocKey key2("key1", std::get<1>(GetParam()));
+    DocKey docKey1(key1);
+    DocKey docKey2(key2);
+    if (std::get<0>(GetParam()) != std::get<1>(GetParam())) {
+        EXPECT_NE(key1.hash(), key2.hash());
+        EXPECT_NE(docKey1.hash(), docKey2.hash());
+    } else {
+        EXPECT_EQ(key1.hash(), key2.hash());
+        EXPECT_EQ(docKey1.hash(), docKey2.hash());
+    }
+}
+
+TEST_P(StoredDocKeyTestCombi, equalityOperators) {
+    StoredDocKey key1("key1", std::get<0>(GetParam()));
+    StoredDocKey key2("key1", std::get<1>(GetParam()));
+    StoredDocKey key3("key3", std::get<0>(GetParam()));
+    StoredDocKey key4("key3", std::get<1>(GetParam()));
+
+    EXPECT_TRUE(key1 != key3);
+    EXPECT_TRUE(key2 != key4);
+    if (std::get<0>(GetParam()) == std::get<1>(GetParam())) {
+        EXPECT_TRUE(key1 == key2);
+        EXPECT_FALSE(key1 != key2);
+    } else {
+        EXPECT_FALSE(key1 == key2);
+        EXPECT_TRUE(key1 != key2);
+    }
+}
+
+TEST_P(StoredDocKeyTestCombi, lessThan) {
+    StoredDocKey key1("zzb", std::get<0>(GetParam()));
+    StoredDocKey key2(key1);
+    EXPECT_FALSE(key1 < key2); // same key
+
+    StoredDocKey key1_ns1("zzb", std::get<1>(GetParam()));
+    StoredDocKey key2_ns1(key1_ns1);
+    EXPECT_FALSE(key1_ns1 < key2_ns1); // same key
+
+    StoredDocKey key3("zza::thing", std::get<0>(GetParam()));
+    StoredDocKey key3_ns1("zza::thing", std::get<1>(GetParam()));
+
+    if (std::get<0>(GetParam()) < std::get<1>(GetParam())) {
+        // DocNamespace is compared first, so if it is less all compares will
+        // be less
+        EXPECT_TRUE(key3 < key1_ns1);
+        EXPECT_TRUE(key3 < key2_ns1);
+        EXPECT_TRUE(key3 < key3_ns1);
+        EXPECT_TRUE(key1 < key1_ns1);
+        EXPECT_TRUE(key1 < key2_ns1);
+        EXPECT_TRUE(key2 < key2_ns1);
+        EXPECT_TRUE(key2 < key1_ns1);
+    } else if (std::get<0>(GetParam()) == std::get<1>(GetParam())) {
+        // Same namespace, so it's a check of key
+        EXPECT_FALSE(key1 < key1_ns1);
+        EXPECT_TRUE(key3 < key1);
+        EXPECT_TRUE(key3 < key1_ns1);
+    } else {
+        EXPECT_FALSE(key3 < key1_ns1);
+        EXPECT_FALSE(key3 < key2_ns1);
+        EXPECT_FALSE(key3 < key3_ns1);
+        EXPECT_FALSE(key1 < key1_ns1);
+        EXPECT_FALSE(key1 < key2_ns1);
+        EXPECT_FALSE(key2 < key2_ns1);
+        EXPECT_FALSE(key2 < key1_ns1);
+    }
+}
+
+// Test that the StoredDocKey can be used in std::map
+TEST_P(StoredDocKeyTestCombi, map) {
+    std::map<StoredDocKey, int> map;
+    StoredDocKey key1("key1", std::get<0>(GetParam()));
+    StoredDocKey key2("key2", std::get<0>(GetParam()));
+
+    StoredDocKey key1_ns1("key1", std::get<1>(GetParam()));
+    StoredDocKey key2_ns1("key2", std::get<1>(GetParam()));
+
+    EXPECT_EQ(0, map.count(key1));
+    EXPECT_EQ(0, map.count(key1_ns1));
+    EXPECT_EQ(0, map.count(key2));
+    EXPECT_EQ(0, map.count(key2_ns1));
+
+    map[key1] = 1;
+    map[key1_ns1] = 101;
+
+    if (std::get<0>(GetParam()) == std::get<1>(GetParam())) {
+        EXPECT_EQ(1, map.size());
+        EXPECT_EQ(1, map.count(key1));
+        EXPECT_TRUE(map[key1] == 101);
+    } else {
+        EXPECT_EQ(2, map.size());
+        EXPECT_EQ(1, map.count(key1));
+        EXPECT_EQ(1, map.count(key1_ns1));
+        EXPECT_TRUE(map[key1] == 1);
+        EXPECT_TRUE(map[key1_ns1] == 101);
+    }
+
+    map[key2] = 2;
+    map[key2_ns1] = 102;
+
+    if (std::get<0>(GetParam()) == std::get<1>(GetParam())) {
+        EXPECT_EQ(2, map.size());
+        EXPECT_EQ(1, map.count(key1));
+        EXPECT_EQ(1, map.count(key2));
+        EXPECT_TRUE(map[key1] == 101);
+        EXPECT_TRUE(map[key2] == 102);
+    } else {
+        EXPECT_EQ(4, map.size());
+        EXPECT_EQ(1, map.count(key1));
+        EXPECT_EQ(1, map.count(key1_ns1));
+        EXPECT_EQ(1, map.count(key2));
+        EXPECT_EQ(1, map.count(key2_ns1));
+        EXPECT_TRUE(map[key1] == 1);
+        EXPECT_TRUE(map[key1_ns1] == 101);
+        EXPECT_TRUE(map[key2] == 2);
+        EXPECT_TRUE(map[key2_ns1] == 102);
+    }
+}
+
+std::vector<DocNamespace> allDocNamespaces = {{DocNamespace::DefaultCollection,
+                                               DocNamespace::Collections,
+                                               DocNamespace::System}};
+
+INSTANTIATE_TEST_CASE_P(
+        DocNamespace,
+        StoredDocKeyTestCombi,
+        ::testing::Combine(::testing::ValuesIn(allDocNamespaces),
+                           ::testing::ValuesIn(allDocNamespaces)), );
+
+INSTANTIATE_TEST_CASE_P(DocNamespace,
+                        StoredDocKeyTest,
+                        ::testing::Values(DocNamespace::DefaultCollection,
+                                          DocNamespace::Collections,
+                                          DocNamespace::System), );
