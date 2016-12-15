6d2811573abbc57f61a26394f699fe80f4406ebb
WillGardner <willg@rdner.io>
2016-12-15 22:34:40 +0000

    Remove unused functions from common.h
    Also removes a now defunct test.
    
    Change-Id: I322684836e7a4be3ffde129eb63188b889272036
    Reviewed-on: http://review.couchbase.org/71032
    Tested-by: buildbot <build@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>

--- src/common.h
+++ src/common.h
@@ -47,45 +47,6 @@
 #define UINT16_MAX 65535
 #endif /* UINT16_MAX */
 
-// Time handling functions
-inline void advance_tv(struct timeval &tv, const double secs) {
-    double ip, fp;
-    fp = modf(secs, &ip);
-    int usec = static_cast<int>(fp * 1e6) + static_cast<int>(tv.tv_usec);
-    int quot = usec / 1000000;
-    int rem = usec % 1000000;
-    tv.tv_sec = static_cast<int>(ip) + tv.tv_sec + quot;
-    tv.tv_usec = rem;
-}
-
-inline bool less_tv(const struct timeval &tv1, const struct timeval &tv2) {
-    if (tv1.tv_sec == tv2.tv_sec) {
-        return tv1.tv_usec < tv2.tv_usec;
-    } else {
-        return tv1.tv_sec < tv2.tv_sec;
-    }
-}
-
-inline bool less_eq_tv(const struct timeval &tv1, const struct timeval &tv2) {
-    if (tv1.tv_sec == tv2.tv_sec) {
-        return tv1.tv_usec <= tv2.tv_usec;
-    } else {
-        return tv1.tv_sec < tv2.tv_sec;
-    }
-}
-
-inline void set_max_tv(struct timeval &tv) {
-    tv.tv_sec = INT_MAX;  // until the year 2038!
-    tv.tv_usec = INT_MAX;
-}
-
-inline bool is_max_tv(struct timeval &tv) {
-    if (tv.tv_sec == INT_MAX && tv.tv_usec == INT_MAX) {
-        return true;
-    }
-    return false;
-}
-
 inline bool parseUint16(const char *in, uint16_t *out) {
     if (out == nullptr) {
         return false;
@@ -210,63 +171,6 @@ inline std::string hrtime2text(hrtime_t time) {
    }
 
    return ss.str();
-}
-
-/**
- * Given a vector instance with the sorted elements and a chunk size, this will creates
- * the list of chunks where each chunk represents a specific range and contains the chunk
- * size of elements within that range.
- * @param elm_list the vector instance that has the list of sorted elements to get chunked
- * @param chunk_size the size of each chunk
- * @param chunk_list the list of chunks to be returned to the caller
- */
-template <typename T>
-void createChunkListFromArray(std::vector<T> *elm_list, size_t chunk_size,
-                     std::list<std::pair<T, T> > &chunk_list) {
-    size_t counter = 0;
-    std::pair<T, T> chunk_range;
-
-    if (elm_list->empty() || chunk_size == 0) {
-        return;
-    }
-
-    typename std::vector<T>::iterator iter;
-    typename std::vector<T>::iterator iterend(elm_list->end());
-    --iterend;
-    for (iter = elm_list->begin(); iter != elm_list->end(); ++iter) {
-        ++counter;
-        if (counter == 1) {
-            chunk_range.first = *iter;
-        }
-
-        if (counter == chunk_size || iter == iterend) {
-            chunk_range.second = *iter;
-            chunk_list.push_back(chunk_range);
-            counter = 0;
-        }
-    }
-}
-
-/**
- * Return true if the elements in a given container are sorted in the ascending order.
- * @param first Forward iterator to the initial position of the container.
- * @param last Forward iterator to the final position of the container. The element
- * pointed by this iterator is not included.
- * @param compare Comparison function that returns true if the first element is less than
- * equal to the second element.
- */
-template <class ForwardIterator, class Compare>
-bool sorted(ForwardIterator first, ForwardIterator last, Compare compare) {
-    bool is_sorted = true;
-    ForwardIterator next;
-    for (; first != last; ++first) {
-        next = first;
-        if (++next != last && !compare(*first, *next)) {
-            is_sorted = false;
-            break;
-        }
-    }
-    return is_sorted;
 }
 
 #endif  // SRC_COMMON_H_
