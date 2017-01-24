couchbase/kv_engine

commit 8de67dca05df54ff46085c25dd39e84512a6eab5

[Refactor]: Merge tapQueueDirty() into queueDirty()
Both the functions have mostly similar behavior. The main difference
is tapQueueDirty() puts the item onto VB queue, queueDirty() puts the item
onto checkpoint. This difference can be handled by passing a flag.

Change-Id: I785c900c9793b4e7596ba3b0e9d7759f08786095
Reviewed-on: http://review.couchbase.org/72264
Tested-by: buildbot <build@couchbase.com>
Reviewed-by: Manu Dhundi <manu@couchbase.com>

src/kv_bucket.cc
@@ -2293,13 +2293,8 @@ ENGINE_ERROR_CODE KVBucket::deleteWithMeta(const DocKey& key,
         }
 
         vb->setMaxCasAndTrackDrift(v->getCas());
-
-        uint64_t queued_seqno;
-        if (tapBackfill) {
-            queued_seqno = vb->tapQueueDirty(v, std::move(lh), genBySeqno);
-        } else {
-            queued_seqno = vb->queueDirty(*v, &lh, genBySeqno, generateCas);
-        }
+        auto queued_seqno = vb->queueDirty(
+                *v, &lh, genBySeqno, GenerateCas::No, tapBackfill);
         if (nullptr != seqno) {
             *seqno = queued_seqno;
         }

src/vbucket.cc
@@ -703,54 +703,39 @@ size_t VBucket::getNumOfKeysInFilter() {
     }
 }
 
+/* [TBD]: Get rid of pHtLh */
 uint64_t VBucket::queueDirty(StoredValue& v,
                              std::unique_lock<std::mutex>* pHtLh,
                              const GenerateBySeqno generateBySeqno,
-                             const GenerateCas generateCas) {
+                             const GenerateCas generateCas,
+                             const bool isBackfillItem) {
     VBNotifyCtx notifyCtx;
     queued_item qi(v.toItem(false, getId()));
 
-    notifyCtx.notifyFlusher = checkpointManager.queueDirty(
-            *this, qi, generateBySeqno, generateCas);
-    v.setBySeqno(qi->getBySeqno());
-    notifyCtx.bySeqno = qi->getBySeqno();
-    notifyCtx.notifyReplication = true;
-
-    if (GenerateCas::Yes == generateCas) {
-        v.setCas(qi->getCas());
+    if (isBackfillItem) {
+        notifyCtx.notifyFlusher = queueBackfillItem(qi, generateBySeqno);
+        /* During backfill on a TAP receiver we need to update the snapshot
+         range in the checkpoint. Has to be done here because in case of TAP
+         backfill, above, we use vb.queueBackfillItem() instead of
+         vb.checkpointManager.queueDirty() */
+        if (generateBySeqno == GenerateBySeqno::Yes) {
+            checkpointManager.resetSnapshotRange();
+        }
+    } else {
+        notifyCtx.notifyFlusher = checkpointManager.queueDirty(
+                *this, qi, generateBySeqno, generateCas);
+        notifyCtx.bySeqno = qi->getBySeqno();
+        notifyCtx.notifyReplication = true;
+        if (GenerateCas::Yes == generateCas) {
+            v.setCas(qi->getCas());
+        }
     }
+
+    v.setBySeqno(qi->getBySeqno());
 
     if (pHtLh) {
         pHtLh->unlock();
     }
-
-    if (newSeqnoCb) {
-        newSeqnoCb->callback(getId(), notifyCtx);
-    }
-
-    return qi->getBySeqno();
-}
-
-/* [TBD]: Get rid of std::unique_lock<std::mutex> plh */
-uint64_t VBucket::tapQueueDirty(StoredValue* v,
-                                std::unique_lock<std::mutex> plh,
-                                const GenerateBySeqno generateBySeqno) {
-    VBNotifyCtx notifyCtx;
-    queued_item qi(v->toItem(false, getId()));
-
-    notifyCtx.notifyFlusher = queueBackfillItem(qi, generateBySeqno);
-
-    v->setBySeqno(qi->getBySeqno());
-
-    /* During backfill on a TAP receiver we need to update the snapshot
-     range in the checkpoint. Has to be done here because in case of TAP
-     backfill, above, we use vb.queueBackfillItem() instead of
-     vb.checkpointManager.queueDirty() */
-    if (GenerateBySeqno::Yes == generateBySeqno) {
-        checkpointManager.resetSnapshotRange();
-    }
-
-    plh.unlock();
 
     if (newSeqnoCb) {
         newSeqnoCb->callback(getId(), notifyCtx);
@@ -1239,9 +1224,11 @@ ENGINE_ERROR_CODE VBucket::addBackfillItem(Item& itm, const bool genBySeqno) {
     // FALLTHROUGH
     case MutationStatus::WasClean:
         setMaxCas(v->getCas());
-        tapQueueDirty(v,
-                      std::move(lh),
-                      genBySeqno ? GenerateBySeqno::Yes : GenerateBySeqno::No);
+        queueDirty(*v,
+                   &lh,
+                   genBySeqno ? GenerateBySeqno::Yes : GenerateBySeqno::No,
+                   GenerateCas::No,
+                   true);
         break;
     case MutationStatus::NeedBgFetch:
         throw std::logic_error(

src/vbucket.h
@@ -516,30 +516,16 @@ public:
      *        calling notify callback
      * @param generateBySeqno request that the seqno is generated by this call
      * @param generateCas request that the CAS is generated by this call
+     * @param isBackfillItem indicates if the item must be put onto vb queue or
+     *        onto checkpoint
      *
      * @return sequence number generated for the mutation
      */
-    uint64_t queueDirty(
-            StoredValue& v,
-            std::unique_lock<std::mutex>* pHtLh = nullptr,
-            const GenerateBySeqno generateBySeqno = GenerateBySeqno::Yes,
-            const GenerateCas generateCas = GenerateCas::Yes);
-
-    /* Queue an item for persistence following a TAP command
-     *
-     * The caller of this function must hold the lock of the hash table
-     * partition that contains the StoredValue being Queued.
-     *
-     * @param v the dirty item
-     * @param plh Hash table partition lock for the dirty item.
-     *        This function now owns the lock and unlocks it inside the function
-     * @param generateBySeqno request that the seqno is generated by this call
-     *
-     * @result seqno of the added item
-     */
-    uint64_t tapQueueDirty(StoredValue* v,
-                           std::unique_lock<std::mutex> plh,
-                           const GenerateBySeqno generateBySeqno);
+    uint64_t queueDirty(StoredValue& v,
+                        std::unique_lock<std::mutex>* pHtLh = nullptr,
+                        GenerateBySeqno generateBySeqno = GenerateBySeqno::Yes,
+                        GenerateCas generateCas = GenerateCas::Yes,
+                        bool isBackfillItem = false);
 
     /**
      * Gets the valid StoredValue for the key and deletes an expired item if

commit 339f2d494c301e9e625ef9fe2b26e91336718515

KVBucket::deleteWithMeta: Pass correct generateCas to queueDirty
Fix the bug introduced in the commit 8de67dc
(http://review.couchbase.org/#/c/72264).

Test case added for the same

Change-Id: I450c606a626b6ca7d416adcca524d75de65c43a2
Reviewed-on: http://review.couchbase.org/72317
Tested-by: buildbot <build@couchbase.com>
Reviewed-by: Dave Rigby <daver@couchbase.com>

src/kv_bucket.cc
@@ -2293,8 +2293,8 @@ ENGINE_ERROR_CODE KVBucket::deleteWithMeta(const DocKey& key,
         }
 
         vb->setMaxCasAndTrackDrift(v->getCas());
-        auto queued_seqno = vb->queueDirty(
-                *v, &lh, genBySeqno, GenerateCas::No, tapBackfill);
+        auto queued_seqno =
+                vb->queueDirty(*v, &lh, genBySeqno, generateCas, tapBackfill);
         if (nullptr != seqno) {
             *seqno = queued_seqno;
         }

tests/ep_testsuite_xdcr.cc
@@ -1885,6 +1885,98 @@ static enum test_result test_cas_regeneration(ENGINE_HANDLE *h,
 }
 
 /*
+ * Perform del_with_meta and check CAS regeneration is ok.
+ */
+static enum test_result test_cas_regeneration_del_with_meta(
+        ENGINE_HANDLE* h, ENGINE_HANDLE_V1* h1) {
+    const std::string key("key");
+    // First store a key from the past (small CAS).
+    ItemMetaData itemMeta;
+    itemMeta.revSeqno = 10;
+    itemMeta.cas = 0x1;
+    itemMeta.exptime = 0;
+    itemMeta.flags = 0xdeadbeef;
+    int force = 0;
+
+    if (strstr(testHarness.get_current_testcase()->cfg,
+               "conflict_resolution_type=lww") != nullptr) {
+        force = FORCE_ACCEPT_WITH_META_OPS;
+    }
+
+    // Set the key with a low CAS value
+    set_with_meta(h,
+                  h1,
+                  key.c_str(),
+                  key.length(),
+                  nullptr,
+                  0,
+                  0,
+                  &itemMeta,
+                  0,
+                  force);
+    checkeq(PROTOCOL_BINARY_RESPONSE_SUCCESS,
+            last_status.load(),
+            "Expected success");
+
+    check(get_meta(h, h1, key.c_str()), "Failed to get_meta");
+    // CAS must be what we set.
+    checkeq(itemMeta.cas, last_meta.cas, "CAS is not the value we stored");
+
+    itemMeta.cas++;
+
+    // Check that the code requires skip
+    del_with_meta(h,
+                  h1,
+                  key.c_str(),
+                  key.length(),
+                  0,
+                  &itemMeta,
+                  0,
+                  REGENERATE_CAS /*but no skip*/);
+    checkeq(PROTOCOL_BINARY_RESPONSE_EINVAL,
+            last_status.load(),
+            "Expected EINVAL");
+
+    del_with_meta(h,
+                  h1,
+                  key.c_str(),
+                  key.length(),
+                  0,
+                  &itemMeta,
+                  0,
+                  REGENERATE_CAS | SKIP_CONFLICT_RESOLUTION_FLAG);
+    checkeq(PROTOCOL_BINARY_RESPONSE_SUCCESS,
+            last_status.load(),
+            "Expected success");
+
+    check(get_meta(h, h1, key.c_str()), "Failed to get_meta");
+    uint64_t cas = last_meta.cas;
+    // Check item has a new CAS
+    checkne(itemMeta.cas, cas, "CAS was not regenerated");
+
+    itemMeta.cas++;
+    // All flags set should still regen the cas (lww and seqno)
+    del_with_meta(h,
+                  h1,
+                  key.c_str(),
+                  key.length(),
+                  0,
+                  &itemMeta,
+                  0,
+                  REGENERATE_CAS | SKIP_CONFLICT_RESOLUTION_FLAG | force);
+    checkeq(PROTOCOL_BINARY_RESPONSE_SUCCESS,
+            last_status.load(),
+            "Expected success");
+
+    check(get_meta(h, h1, key.c_str()), "Failed to get_meta");
+    // Check item has a new CAS
+    checkne(itemMeta.cas, last_meta.cas, "CAS was not regenerated");
+    checkne(cas, last_meta.cas, "CAS was not regenerated");
+
+    return SUCCESS;
+}
+
+/*
  * Test that we can send options and nmeta
  * The nmeta is just going to be ignored though, but should not fail
  */
@@ -2144,6 +2236,12 @@ BaseTestCase testsuite_testcases[] = {
                  test_cas_regeneration, test_setup, teardown,
                  "conflict_resolution_type=seqno",
                  prepare, cleanup),
+        TestCase("test CAS regeneration seqno del_with_meta lww",
+                 test_cas_regeneration_del_with_meta, test_setup, teardown,
+                 "conflict_resolution_type=lww", prepare, cleanup),
+        TestCase("test CAS regeneration seqno del_with_meta seqno",
+                 test_cas_regeneration_del_with_meta, test_setup, teardown,
+                 "conflict_resolution_type=seqno", prepare, cleanup),
         TestCase("test CAS options and nmeta",
                  test_cas_options_and_nmeta, test_setup, teardown,
                  "conflict_resolution_type=lww",