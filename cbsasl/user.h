b51a952ab39c596e279fc30244d5e4864c7e6486
Trond Norbye <trond.norbye@gmail.com>
2017-02-02 13:14:42 +0100

    Reformat user.h and password_database.h with clang-format
    Change-Id: Iced4de87c5d66b24e0bb714e079addd09dd87a2f
    Reviewed-on: http://review.couchbase.org/73066
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- cbsasl/user.h
+++ cbsasl/user.h
@@ -16,224 +16,216 @@
  */
 #pragma once
 
+#include <cJSON_utils.h>
 #include <cbcrypto/cbcrypto.h>
 #include <cstdint>
+#include <map>
 #include <string>
 #include <vector>
-#include <map>
-#include <cJSON_utils.h>
 #include "cbsasl_internal.h"
 
 namespace Couchbase {
-    class UserFactory;
+class UserFactory;
 
-    class User {
+class User {
+public:
+    /**
+     * To allow multiple authentication schemes we need to store
+     * some metadata for each of the different scheme.
+     */
+    class PasswordMetaData {
     public:
         /**
-         * To allow multiple authentication schemes we need to store
-         * some metadata for each of the different scheme.
+         * Create a new instance of the PasswordMetaData
          */
-        class PasswordMetaData {
-        public:
-            /**
-             * Create a new instance of the PasswordMetaData
-             */
-            PasswordMetaData()
-                : iteration_count(0) {
+        PasswordMetaData()
+            : iteration_count(0){
 
-            };
-
-            /**
-             * Create a new instance of the PasswordMetaData with
-             * the specified attributes
-             *
-             * @param h the password
-             * @param s the salt used (Base64 encoded)
-             * @param i iteration count
-             */
-            PasswordMetaData(const std::string& h,
-                             const std::string& s = "",
-                             int i = 0)
-                : salt(s),
-                  password(h),
-                  iteration_count(i) {
-
-            }
-
-            /**
-             * Create a new instance of the PasswordMetaData and
-             * initialize it from the specified JSON. The JSON *MUST*
-             * be of the following syntax:
-             *
-             *     {
-             *         "h" : "base64 encoded password",
-             *         "s" : "base64 encoded salt",
-             *         "i" : "iteration count"
-             *     }
-             *
-             * @param obj pointer to the cJSON structure
-             */
-            PasswordMetaData(cJSON* obj);
-
-            /**
-             * This is a helper function used from the unit tests
-             * to generate a JSON representation of a user
-             */
-            cJSON* to_json() const;
-
-            const std::string& getSalt() const {
-                return salt;
-            }
-
-            const std::string& getPassword() const {
-                return password;
-            }
-
-            int getIterationCount() const {
-                return iteration_count;
-            }
-
-        private:
-            // Base 64 encoded version of the salt
-            std::string salt;
-
-            // The actual password used
-            std::string password;
-
-            // The iteration count used for generating the password
-            int iteration_count;
-        };
+              };
 
         /**
-          * Create an empty user object
-          *
-          * A dummy user object is used by SCRAM sasl authentication
-          * so that the "attacking" client would need to perform the
-          * full authentication step in order to authenticate towards
-          * the server instead of returning "no such user" if we failed
-          * to look up the user (to respond with the SALT and iteration
-          * count).
-          *
-          * @param unm the username for the object (potentially empty)
-          * @param dmy should we create a dummy object or not
-          */
-        User(const std::string& unm = "", const bool dmy = true)
-            : username(unm),
-              dummy(dmy) {
-
-        }
-
-        /**
-         * Get the username for this entry
-         */
-        const std::string& getUsername() const {
-            return username;
-        }
-
-        /**
-          * Is this a dummy object or not?
-          */
-        bool isDummy() const {
-            return dummy;
-        }
-
-        /**
-         * Get the password metadata used for the requested mechanism
+         * Create a new instance of the PasswordMetaData with
+         * the specified attributes
          *
-         * @param mech the mechanism to retrieve the metadata for
-         * @return the passwod metadata
-         * @throws std::illegal_arguement if the mechanism isn't supported
+         * @param h the password
+         * @param s the salt used (Base64 encoded)
+         * @param i iteration count
          */
-        const PasswordMetaData& getPassword(const Mechanism& mech) const;
+        PasswordMetaData(const std::string& h,
+                         const std::string& s = "",
+                         int i = 0)
+            : salt(s), password(h), iteration_count(i) {
+        }
 
         /**
-         * Generate a JSON encoding of this object (it is primarily used
-         * from the test suite to generate a user database)
+         * Create a new instance of the PasswordMetaData and
+         * initialize it from the specified JSON. The JSON *MUST*
+         * be of the following syntax:
+         *
+         *     {
+         *         "h" : "base64 encoded password",
+         *         "s" : "base64 encoded salt",
+         *         "i" : "iteration count"
+         *     }
+         *
+         * @param obj pointer to the cJSON structure
          */
-        unique_cJSON_ptr to_json() const;
+        PasswordMetaData(cJSON* obj);
 
         /**
-         * Generate a textual representation of the user object (in JSON)
+         * This is a helper function used from the unit tests
+         * to generate a JSON representation of a user
          */
-        std::string to_string() const;
+        cJSON* to_json() const;
 
-    protected:
-        friend class UserFactory;
+        const std::string& getSalt() const {
+            return salt;
+        }
 
-        void generateSecrets(const Mechanism& mech,
-                             const std::string& passwd);
+        const std::string& getPassword() const {
+            return password;
+        }
 
-        std::map<Mechanism, PasswordMetaData> password;
+        int getIterationCount() const {
+            return iteration_count;
+        }
 
-        std::string username;
+    private:
+        // Base 64 encoded version of the salt
+        std::string salt;
 
-        bool dummy;
+        // The actual password used
+        std::string password;
+
+        // The iteration count used for generating the password
+        int iteration_count;
     };
 
     /**
-     * The UserFactory class is used to generate a User Object from a
-     * username and plain text password.
+      * Create an empty user object
+      *
+      * A dummy user object is used by SCRAM sasl authentication
+      * so that the "attacking" client would need to perform the
+      * full authentication step in order to authenticate towards
+      * the server instead of returning "no such user" if we failed
+      * to look up the user (to respond with the SALT and iteration
+      * count).
+      *
+      * @param unm the username for the object (potentially empty)
+      * @param dmy should we create a dummy object or not
+      */
+    User(const std::string& unm = "", const bool dmy = true)
+        : username(unm), dummy(dmy) {
+    }
+
+    /**
+     * Get the username for this entry
      */
-    class UserFactory {
-    public:
+    const std::string& getUsername() const {
+        return username;
+    }
 
-        /**
-         * Construct a new user object
-         *
-         * @param name username
-         * @param pw password
-         *
-         * @return a newly created dummy object
-         */
-        static User create(const std::string& name, const std::string& pw);
+    /**
+      * Is this a dummy object or not?
+      */
+    bool isDummy() const {
+        return dummy;
+    }
 
-        /**
-         * Create a user entry and initialize it from the supplied
-         * JSON structure:
-         *
-         *       {
-         *            "n" : "username",
-         *            "sha512" : {
-         *                "h" : "base64 encoded sha512 hash of the password",
-         *                "s" : "base64 encoded salt",
-         *                "i" : iteration-count
-         *            },
-         *            "sha256" : {
-         *                "h" : "base64 encoded sha256 hash of the password",
-         *                "s" : "base64 encoded salt",
-         *                "i" : iteration-count
-         *            },
-         *            "sha1" : {
-         *                "h" : "base64 encoded sha1 hash of the password",
-         *                "s" : "base64 encoded salt",
-         *                "i" : iteration-count
-         *            },
-         *            "plain" : "base64 encoded hex version of sha1 hash
-         *                       of plain text password"
-         *       }
-         *
-         * @param obj the object containing the JSON description
-         * @throws std::runtime_error if there is a syntax error
-         */
-        static User create(const cJSON* obj);
+    /**
+     * Get the password metadata used for the requested mechanism
+     *
+     * @param mech the mechanism to retrieve the metadata for
+     * @return the passwod metadata
+     * @throws std::illegal_arguement if the mechanism isn't supported
+     */
+    const PasswordMetaData& getPassword(const Mechanism& mech) const;
 
-        /**
-         * Construct a dummy user object that may be used in authentication
-         *
-         * @param name username
-         * @param mech generate just the password for this mechanism
-         *
-         * @return a newly created dummy object
-         */
-        static User createDummy(const std::string& name,
-                                const Mechanism& mech);
+    /**
+     * Generate a JSON encoding of this object (it is primarily used
+     * from the test suite to generate a user database)
+     */
+    unique_cJSON_ptr to_json() const;
 
-        /**
-         * Set the default iteration count to use (may be overridden by
-         * the cbsasl property function.
-         *
-         * @param count the new iteration count to use
-         */
-        static void setDefaultHmacIterationCount(int count);
-    };
+    /**
+     * Generate a textual representation of the user object (in JSON)
+     */
+    std::string to_string() const;
+
+protected:
+    friend class UserFactory;
+
+    void generateSecrets(const Mechanism& mech, const std::string& passwd);
+
+    std::map<Mechanism, PasswordMetaData> password;
+
+    std::string username;
+
+    bool dummy;
+};
+
+/**
+ * The UserFactory class is used to generate a User Object from a
+ * username and plain text password.
+ */
+class UserFactory {
+public:
+    /**
+     * Construct a new user object
+     *
+     * @param name username
+     * @param pw password
+     *
+     * @return a newly created dummy object
+     */
+    static User create(const std::string& name, const std::string& pw);
+
+    /**
+     * Create a user entry and initialize it from the supplied
+     * JSON structure:
+     *
+     *       {
+     *            "n" : "username",
+     *            "sha512" : {
+     *                "h" : "base64 encoded sha512 hash of the password",
+     *                "s" : "base64 encoded salt",
+     *                "i" : iteration-count
+     *            },
+     *            "sha256" : {
+     *                "h" : "base64 encoded sha256 hash of the password",
+     *                "s" : "base64 encoded salt",
+     *                "i" : iteration-count
+     *            },
+     *            "sha1" : {
+     *                "h" : "base64 encoded sha1 hash of the password",
+     *                "s" : "base64 encoded salt",
+     *                "i" : iteration-count
+     *            },
+     *            "plain" : "base64 encoded hex version of sha1 hash
+     *                       of plain text password"
+     *       }
+     *
+     * @param obj the object containing the JSON description
+     * @throws std::runtime_error if there is a syntax error
+     */
+    static User create(const cJSON* obj);
+
+    /**
+     * Construct a dummy user object that may be used in authentication
+     *
+     * @param name username
+     * @param mech generate just the password for this mechanism
+     *
+     * @return a newly created dummy object
+     */
+    static User createDummy(const std::string& name, const Mechanism& mech);
+
+    /**
+     * Set the default iteration count to use (may be overridden by
+     * the cbsasl property function.
+     *
+     * @param count the new iteration count to use
+     */
+    static void setDefaultHmacIterationCount(int count);
+};
 }
