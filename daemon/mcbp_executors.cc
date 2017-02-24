b82f8af24ed4993c7e9dafb0289a7da4e862bcfc
Trond Norbye <trond.norbye@gmail.com>
2017-02-24 10:21:25 +0100

    Refactor: Remove duplicate check for dcp.open
    The check is performed by the validator
    
    Change-Id: Icabf55aa324dba5ee023df906f8adea0853c0dab
    Reviewed-on: http://review.couchbase.org/74230
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Harrison <james.harrison@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- daemon/mcbp_executors.cc
+++ daemon/mcbp_executors.cc
@@ -967,45 +967,42 @@ static void tap_checkpoint_end_executor(McbpConnection* c, void* packet) {
 static void dcp_open_executor(McbpConnection* c, void* packet) {
     auto* req = reinterpret_cast<protocol_binary_request_dcp_open*>(packet);
 
-    if (c->getBucketEngine()->dcp.open == NULL) {
-        mcbp_write_packet(c, PROTOCOL_BINARY_RESPONSE_NOT_SUPPORTED);
-    } else {
-        ENGINE_ERROR_CODE ret = c->getAiostat();
-        c->setAiostat(ENGINE_SUCCESS);
-        c->setEwouldblock(false);
-        c->setSupportsDatatype(true);
-        uint32_t flags = ntohl(req->message.body.flags);
+    ENGINE_ERROR_CODE ret = c->getAiostat();
+    c->setAiostat(ENGINE_SUCCESS);
+    c->setEwouldblock(false);
+    c->setSupportsDatatype(true);
+    uint32_t flags = ntohl(req->message.body.flags);
 
-        if (ret == ENGINE_SUCCESS) {
-            ret = c->getBucketEngine()->dcp.open(c->getBucketEngineAsV0(), c->getCookie(),
-                                                 req->message.header.request.opaque,
-                                                 ntohl(req->message.body.seqno),
-                                                 flags,
-                                                 (void*)(req->bytes +
-                                                         sizeof(req->bytes)),
-                                                 ntohs(
-                                                     req->message.header.request.keylen));
-        }
+    if (ret == ENGINE_SUCCESS) {
+        ret = c->getBucketEngine()->dcp.open(c->getBucketEngineAsV0(),
+                                             c->getCookie(),
+                                             req->message.header.request.opaque,
+                                             ntohl(req->message.body.seqno),
+                                             flags,
+                                             (void*)(req->bytes +
+                                                     sizeof(req->bytes)),
+                                             ntohs(
+                                                 req->message.header.request.keylen));
+    }
 
-        switch (ret) {
-        case ENGINE_SUCCESS:
-            c->setDcpXattrAware((flags & DCP_OPEN_INCLUDE_XATTRS) != 0);
-            c->setDcpNoValue((flags & DCP_OPEN_NO_VALUE) != 0);
-            audit_dcp_open(c);
-            mcbp_write_packet(c, PROTOCOL_BINARY_RESPONSE_SUCCESS);
-            break;
+    switch (ret) {
+    case ENGINE_SUCCESS:
+        c->setDcpXattrAware((flags & DCP_OPEN_INCLUDE_XATTRS) != 0);
+        c->setDcpNoValue((flags & DCP_OPEN_NO_VALUE) != 0);
+        audit_dcp_open(c);
+        mcbp_write_packet(c, PROTOCOL_BINARY_RESPONSE_SUCCESS);
+        break;
 
-        case ENGINE_DISCONNECT:
-            c->setState(conn_closing);
-            break;
+    case ENGINE_DISCONNECT:
+        c->setState(conn_closing);
+        break;
 
-        case ENGINE_EWOULDBLOCK:
-            c->setEwouldblock(true);
-            break;
+    case ENGINE_EWOULDBLOCK:
+        c->setEwouldblock(true);
+        break;
 
-        default:
-            mcbp_write_packet(c, engine_error_2_mcbp_protocol_error(ret));
-        }
+    default:
+        mcbp_write_packet(c, engine_error_2_mcbp_protocol_error(ret));
     }
 }
 
