e9d5c55d6751c0168f8aaf406efbce495dcc0e2d
Trond Norbye <trond.norbye@gmail.com>
2017-03-27 11:44:05 +0200

    Log if the engine method fails
    Change-Id: I2bcabd81d39c53566160a52807d789ea7d079548
    Reviewed-on: http://review.couchbase.org/75849
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- daemon/protocol/mcbp/engine_wrapper.cc
+++ daemon/protocol/mcbp/engine_wrapper.cc
@@ -15,14 +15,24 @@
  *   limitations under the License.
  */
 #include "engine_wrapper.h"
+#include <utilities/protocol2text.h>
 
 ENGINE_ERROR_CODE bucket_unknown_command(McbpConnection* c,
                                          protocol_binary_request_header* request,
                                          ADD_RESPONSE response) {
-    return c->getBucketEngine()->unknown_command(c->getBucketEngineAsV0(),
-                                                 c->getCookie(),
-                                                 request, response,
-                                                 c->getDocNamespace());
+    auto ret = c->getBucketEngine()->unknown_command(c->getBucketEngineAsV0(),
+                                                     c->getCookie(),
+                                                     request,
+                                                     response,
+                                                     c->getDocNamespace());
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(c,
+                 "%u: %s %s return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str(),
+                 memcached_opcode_2_text(c->getCmd()));
+    }
+    return ret;
 }
 
 void bucket_item_set_cas(McbpConnection* c, item* it, uint64_t cas) {
@@ -36,22 +46,45 @@ void bucket_reset_stats(McbpConnection* c) {
 
 ENGINE_ERROR_CODE bucket_get_engine_vb_map(McbpConnection* c,
                                            engine_get_vb_map_cb callback) {
-    return c->getBucketEngine()->get_engine_vb_map(c->getBucketEngineAsV0(),
-                                                   c->getCookie(), callback);
+    auto ret = c->getBucketEngine()->get_engine_vb_map(
+            c->getBucketEngineAsV0(), c->getCookie(), callback);
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(c,
+                 "%u: %s bucket_get_engine_vb_map return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+
+    return ret;
 }
 
 bool bucket_get_item_info(McbpConnection* c, const item* item_,
                           item_info* item_info_) {
-    return c->getBucketEngine()->get_item_info(c->getBucketEngineAsV0(),
-                                               c->getCookie(), item_,
-                                               item_info_);
+    auto ret = c->getBucketEngine()->get_item_info(
+            c->getBucketEngineAsV0(), c->getCookie(), item_, item_info_);
+    if (!ret) {
+        LOG_INFO(c,
+                 "%u: %s bucket_get_item_info failed",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+
+    return ret;
 }
 
 bool bucket_set_item_info(McbpConnection* c, item* item_,
                           const item_info* item_info_) {
-    return c->getBucketEngine()->set_item_info(c->getBucketEngineAsV0(),
-                                               c->getCookie(), item_,
-                                               item_info_);
+    auto ret = c->getBucketEngine()->set_item_info(
+            c->getBucketEngineAsV0(), c->getCookie(), item_, item_info_);
+
+    if (!ret) {
+        LOG_INFO(c,
+                 "%u: %s bucket_set_item_info failed",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+
+    return ret;
 }
 
 ENGINE_ERROR_CODE bucket_store(McbpConnection* c,
@@ -59,8 +92,21 @@ ENGINE_ERROR_CODE bucket_store(McbpConnection* c,
                                uint64_t* cas,
                                ENGINE_STORE_OPERATION operation,
                                DocumentState document_state) {
-    return c->getBucketEngine()->store(c->getBucketEngineAsV0(), c->getCookie(),
-                                       item_, cas, operation, document_state);
+    auto ret = c->getBucketEngine()->store(c->getBucketEngineAsV0(),
+                                           c->getCookie(),
+                                           item_,
+                                           cas,
+                                           operation,
+                                           document_state);
+
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(c,
+                 "%u: %s bucket_store return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+
+    return ret;
 }
 
 ENGINE_ERROR_CODE bucket_remove(McbpConnection* c,
@@ -68,9 +114,19 @@ ENGINE_ERROR_CODE bucket_remove(McbpConnection* c,
                                 uint64_t* cas,
                                 uint16_t vbucket,
                                 mutation_descr_t* mut_info) {
-    return c->getBucketEngine()->remove(c->getBucketEngineAsV0(),
-                                        c->getCookie(), key, cas, vbucket,
-                                        mut_info);
+    auto ret = c->getBucketEngine()->remove(c->getBucketEngineAsV0(),
+                                            c->getCookie(),
+                                            key,
+                                            cas,
+                                            vbucket,
+                                            mut_info);
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(c,
+                 "%u: %s bucket_remove return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+    return ret;
 }
 
 ENGINE_ERROR_CODE bucket_get(McbpConnection* c,
@@ -78,8 +134,19 @@ ENGINE_ERROR_CODE bucket_get(McbpConnection* c,
                              const DocKey& key,
                              uint16_t vbucket,
                              DocumentState document_state) {
-    return c->getBucketEngine()->get(c->getBucketEngineAsV0(), c->getCookie(),
-                                     item_, key, vbucket, document_state);
+    auto ret = c->getBucketEngine()->get(c->getBucketEngineAsV0(),
+                                         c->getCookie(),
+                                         item_,
+                                         key,
+                                         vbucket,
+                                         document_state);
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(c,
+                 "%u: %s bucket_get return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+    return ret;
 }
 
 cb::EngineErrorItemPair bucket_get_if(McbpConnection* c,
@@ -87,8 +154,16 @@ cb::EngineErrorItemPair bucket_get_if(McbpConnection* c,
                                       uint16_t vbucket,
                                       std::function<bool(
                                           const item_info&)> filter) {
-    return c->getBucketEngine()->get_if(c->getBucketEngineAsV0(),
-                                        c->getCookie(), key, vbucket, filter);
+    auto ret = c->getBucketEngine()->get_if(
+            c->getBucketEngineAsV0(), c->getCookie(), key, vbucket, filter);
+
+    if (ret.first == cb::engine_errc::disconnect) {
+        LOG_INFO(c,
+                 "%u: %s bucket_get_if return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+    return ret;
 }
 
 ENGINE_ERROR_CODE bucket_get_locked(McbpConnection& c,
@@ -96,18 +171,35 @@ ENGINE_ERROR_CODE bucket_get_locked(McbpConnection& c,
                                     const DocKey& key,
                                     uint16_t vbucket,
                                     uint32_t lock_timeout) {
-    return c.getBucketEngine()->get_locked(c.getBucketEngineAsV0(),
-                                           c.getCookie(),
-                                           item_, key, vbucket, lock_timeout);
+    auto ret = c.getBucketEngine()->get_locked(c.getBucketEngineAsV0(),
+                                               c.getCookie(),
+                                               item_,
+                                               key,
+                                               vbucket,
+                                               lock_timeout);
+
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(&c,
+                 "%u: %s bucket_get_locked return ENGINE_DISCONNECT",
+                 c.getId(),
+                 c.getDescription().c_str());
+    }
+    return ret;
 }
 
 ENGINE_ERROR_CODE bucket_unlock(McbpConnection& c,
                                 const DocKey& key,
                                 uint16_t vbucket,
                                 uint64_t cas) {
-    return c.getBucketEngine()->unlock(c.getBucketEngineAsV0(),
-                                       c.getCookie(),
-                                       key, vbucket, cas);
+    auto ret = c.getBucketEngine()->unlock(
+            c.getBucketEngineAsV0(), c.getCookie(), key, vbucket, cas);
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(&c,
+                 "%u: %s bucket_unlock return ENGINE_DISCONNECT",
+                 c.getId(),
+                 c.getDescription().c_str());
+    }
+    return ret;
 }
 
 void bucket_release_item(McbpConnection* c, item* it) {
@@ -123,9 +215,22 @@ ENGINE_ERROR_CODE bucket_allocate(McbpConnection* c,
                                   const rel_time_t exptime,
                                   uint8_t datatype,
                                   uint16_t vbucket) {
-    return c->getBucketEngine()->allocate(c->getBucketEngineAsV0(),
-                                          c->getCookie(), it, key, nbytes,
-                                          flags, exptime, datatype, vbucket);
+    auto ret = c->getBucketEngine()->allocate(c->getBucketEngineAsV0(),
+                                              c->getCookie(),
+                                              it,
+                                              key,
+                                              nbytes,
+                                              flags,
+                                              exptime,
+                                              datatype,
+                                              vbucket);
+    if (ret == ENGINE_DISCONNECT) {
+        LOG_INFO(c,
+                 "%u: %s bucket_allocate return ENGINE_DISCONNECT",
+                 c->getId(),
+                 c->getDescription().c_str());
+    }
+    return ret;
 }
 
 std::pair<cb::unique_item_ptr, item_info> bucket_allocate_ex(McbpConnection& c,
@@ -136,9 +241,23 @@ std::pair<cb::unique_item_ptr, item_info> bucket_allocate_ex(McbpConnection& c,
                                                              const rel_time_t exptime,
                                                              uint8_t datatype,
                                                              uint16_t vbucket) {
-    return c.getBucketEngine()->allocate_ex(c.getBucketEngineAsV0(),
-                                            c.getCookie(), key, nbytes,
-                                            priv_nbytes,
-                                            flags, exptime, datatype,
-                                            vbucket);
+    try {
+        return c.getBucketEngine()->allocate_ex(c.getBucketEngineAsV0(),
+                                                c.getCookie(),
+                                                key,
+                                                nbytes,
+                                                priv_nbytes,
+                                                flags,
+                                                exptime,
+                                                datatype,
+                                                vbucket);
+    } catch (const cb::engine_error& err) {
+        if (err.code() == cb::engine_errc::disconnect) {
+            LOG_INFO(&c,
+                     "%u: %s bucket_allocate_ex return ENGINE_DISCONNECT",
+                     c.getId(),
+                     c.getDescription().c_str());
+        }
+        throw err;
+    }
 }
