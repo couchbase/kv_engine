bc12c616e9268aa9ee232b1033c76a31287dddbb
Trond Norbye <trond.norbye@gmail.com>
2017-04-11 10:05:46 +0200

    Add unique_ptr for X509 ptr in cb::openssl
    Change-Id: I5d9e31c44db28330032a0945b0b9cebef358e7b0
    Reviewed-on: http://review.couchbase.org/76616
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- include/memcached/openssl.h
+++ include/memcached/openssl.h
@@ -1,6 +1,20 @@
 /* -*- Mode: C; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
-#ifndef MEMCACHED_OPENSSL_H
-#define MEMCACHED_OPENSSL_H
+/*
+ *     Copyright 2017 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#pragma once
 
 /*
  * Unfortunately Apple deprecated the use of OpenSSL in (at least) Mavericks
@@ -16,8 +30,21 @@
 #pragma clang diagnostic ignored "-Wdeprecated-declarations"
 #endif
 
+#include <memory>
+
 #include <openssl/ssl.h>
 #include <openssl/bio.h>
 #include <openssl/err.h>
 
-#endif
+namespace cb {
+namespace openssl {
+struct X509deletor {
+    void operator()(X509* cert) {
+        X509_free(cert);
+    }
+};
+
+using unique_x509_ptr = std::unique_ptr<X509, X509deletor>;
+}
+}
+
