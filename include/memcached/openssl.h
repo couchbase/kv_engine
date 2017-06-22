d09690927da04406ba577b1bc535617bc81cb3e1
Trond Norbye <trond.norbye@gmail.com>
2017-06-22 13:41:42 +0200

    Remove apple-specific hack to nuke deprecation warnings for OpenSSL
    Change-Id: I14b787ee95352c71ccdab8fa23d215d11ab1b422
    Reviewed-on: http://review.couchbase.org/79881
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- include/memcached/openssl.h
+++ include/memcached/openssl.h
@@ -16,20 +16,6 @@
  */
 #pragma once
 
-/*
- * Unfortunately Apple deprecated the use of OpenSSL in (at least) Mavericks
- * and added attributes to the methods in the header causing the code to
- * emit a ton of warnings. I guess I should wrap all of the methods we're
- * using into another library so that I don't have to disable the deprecation
- * warnings for other code. We know of this one, but we don't want to make
- * an apple specific version (I guess the other option would be to drop the
- * support for ssl on mac ;-)
- */
-#if defined(__APPLE__) && defined(__clang__)
-#pragma clang diagnostic push
-#pragma clang diagnostic ignored "-Wdeprecated-declarations"
-#endif
-
 #include <memory>
 
 #include <openssl/ssl.h>
