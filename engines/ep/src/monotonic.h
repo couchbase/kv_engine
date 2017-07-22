73881633bccb3a507ec69935898d268aaa9987d6
Chris Hillery <ceej@couchbase.com>
2017-07-22 00:54:54 -0700

    Revert "Monotonic: Add atomic variant and policy control" Revert "Use AtomicMonotonic for ActiveStream::lastReadSeqno"
    This reverts commits db38f28ea7d3d7f2b02c4680a830b128eb69f9a7 and
    6762af9a382dc27fee5bfe424eea65b317312403, which were breaking the build
    on a number of platforms.
    
    Change-Id: I4971103fe0c413f5b6851ee1a23e5e1aec0e9ae4
    Reviewed-on: http://review.couchbase.org/81271
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- engines/ep/src/monotonic.h
+++ engines/ep/src/monotonic.h
@@ -17,47 +17,16 @@
 
 #pragma once
 
-#include <atomic>
 #include <limits>
-#include <stdexcept>
-#include <string>
-
-/// Policy class for handling non-monotonic updates by simply ignoring them.
-template <class T>
-struct IgnorePolicy {
-    void nonMonotonic(const T& curValue, const T& newValue) {
-        // Ignore the update.
-    }
-};
-
-/// Policy class for handling non-monotonic updates by throwing std::logic_error
-template <class T>
-struct ThrowExceptionPolicy {
-    void nonMonotonic(const T& curValue, const T& newValue) {
-        throw std::logic_error("Monotonic<> invariant failed: new value (" +
-                               std::to_string(newValue) +
-                               ") is not greater than current value (" +
-                               std::to_string(curValue) + ")");
-    }
-};
 
 /**
- * Monotonic is a class template for simple types T. It provides guarantee
+ * Monotonic is a class template for simple types T. It provides guarentee
  * of updating the class objects by only values that are greater than what is
- * contained at the time of update - i.e. the object must be
- * Strictly Increasing.
- *
- * Note: This is not atomic/thread-safe. If you need thread-safely, see
- * AtomicMonotonic.
- *
- * @tparam T value type used to represent the value.
- * @tparam OrderReversePolicy Policy class which controls the behaviour if
- *         an operation would break the monotonic invariant.
+ * contained at the time of update
+ * Note: This is not atomic/thread-safe
  */
-template <typename T,
-          template <class> class OrderReversedPolicy =
-                  IgnorePolicy>
-class Monotonic : public OrderReversedPolicy<T> {
+template <typename T>
+class Monotonic {
 public:
     Monotonic(const T val = std::numeric_limits<T>::min()) : val(val) {
     }
@@ -68,8 +37,6 @@ public:
     Monotonic& operator=(const Monotonic<T>& other) {
         if (val < other.val) {
             val = other.val;
-        } else {
-            OrderReversedPolicy<T>::nonMonotonic(val, other);
         }
         return *this;
     }
@@ -77,8 +44,6 @@ public:
     Monotonic& operator=(const T& v) {
         if (val < v) {
             val = v;
-        } else {
-            OrderReversedPolicy<T>::nonMonotonic(val, v);
         }
         return *this;
     }
@@ -88,59 +53,10 @@ public:
     }
 
     /* Can be used to lower the value */
-    void reset(T desired) {
-        val = desired;
+    void reset(const T& v) {
+        val = v.val;
     }
 
 private:
     T val;
-};
-
-/**
- * Variant of the Monotonic class, except that the type T is wrapped in
- * std::atomic, so all updates are atomic. T must be TriviallyCopyable.
- */
-template <typename T, template <class> class OrderReversedPolicy = IgnorePolicy>
-class AtomicMonotonic : public OrderReversedPolicy<T> {
-public:
-    AtomicMonotonic(T val = std::numeric_limits<T>::min()) : val(val) {
-    }
-
-    AtomicMonotonic(const AtomicMonotonic<T>& other) : val(other.val) {
-    }
-
-    AtomicMonotonic& operator=(T desired) {
-        while (true) {
-            T current = val.load();
-            if (current < desired) {
-                if (val.compare_exchange_weak(current, desired)) {
-                    break;
-                }
-            } else {
-                OrderReversedPolicy<T>::nonMonotonic(current, desired);
-                break;
-            }
-        }
-        return *this;
-    }
-
-    operator T() const {
-        return val;
-    }
-
-    T load() const {
-        return val;
-    }
-
-    void store(T desired) {
-        operator=(desired);
-    }
-
-    /* Can be used to lower the value */
-    void reset(T desired) {
-        val = desired;
-    }
-
-private:
-    std::atomic<T> val;
 };
