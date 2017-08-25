d05f06556276dbbb437966574d152bba10c465d3
Trond Norbye <trond.norbye@gmail.com>
2017-08-25 10:28:52 +0200

    Remove unused #cmakedefines
    We checked a bunch of headers to define a macro, but we never
    tested the macro.
    
    Change-Id: I9d487a9d6953e23a2bf4f7a0099999e2d6911fc3
    Reviewed-on: http://review.couchbase.org/82731
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- engines/ep/src/config.cmake.h
+++ engines/ep/src/config.cmake.h
@@ -1,27 +1,15 @@
 #pragma once
 
 /* Header files */
-#cmakedefine HAVE_ALLOCA_H ${HAVE_ALLOCA_H}
 #cmakedefine HAVE_ARPA_INET_H ${HAVE_ARPA_INET_H}
-#cmakedefine HAVE_ATOMIC_H ${HAVE_ATOMIC_H}
-#cmakedefine HAVE_MACH_MACH_TIME_H ${HAVE_MACH_MACH_TIME_H}
-#cmakedefine HAVE_MEMORY ${HAVE_MEMORY}
 #cmakedefine HAVE_NETDB_H ${HAVE_NETDB_H}
-#cmakedefine HAVE_NETINET_IN_H ${HAVE_NETINET_IN_H}
 #cmakedefine HAVE_NETINET_IN_H ${HAVE_NETINET_IN_H}
 #cmakedefine HAVE_NETINET_TCP_H ${HAVE_NETINET_TCP_H}
 #cmakedefine HAVE_POLL_H ${HAVE_POLL_H}
-#cmakedefine HAVE_SYSEXITS_H ${HAVE_SYSEXITS_H}
 #cmakedefine HAVE_SYS_SOCKET_H ${HAVE_SYS_SOCKET_H}
 #cmakedefine HAVE_SYS_TIME_H ${HAVE_SYS_TIME_H}
 #cmakedefine HAVE_SCHED_H ${HAVE_SCHED_H}
-#cmakedefine HAVE_TR1_MEMORY ${HAVE_TR1_MEMORY}
-#cmakedefine HAVE_TR1_UNORDERED_MAP ${HAVE_TR1_UNORDERED_MAP}
 #cmakedefine HAVE_UNISTD_H ${HAVE_UNISTD_H}
-#cmakedefine HAVE_UNISTD_H ${HAVE_UNISTD_H}
-
-#cmakedefine HAVE_WINSOCK2_H ${HAVE_WINSOCK2_H}
-#cmakedefine HAVE_WS2TCPIP_H ${HAVE_WS2TCPIP_H}
 
 /* various */
 #define VERSION "${EP_ENGINE_VERSION}"
