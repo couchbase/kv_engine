couchbase/kv_engine

commit 8bf142736895169aeaa67936df02fffde61b76c9

MB-21650: Prevent false sharing of frequently modified memory stats
We record the memory usage, memory overhead and number of Items per
bucket, in the EPStats object. These stats are very frequenly updated
(on every memory allocation/deallocation, and every Item
creation/destruction), and they are updated from all threads. This can
cause false sharing between these values if they co-exist in the same
cache line.

A recent change (66882e8 - MB-20852 [17/N]: Serialize VB state
changes) added a new element to EPStats which resulted in the layout
of that class changing such that memory stat variables (memOverhead,
numItem, totalMemory) ended up on the same cache line. This resulted
in a signficant regression (1.9M op/s -> 1.4M ops) in the performance
of the 'kv_max_ops_reads_512_avg_ops_hera_kv' test, due to false
sharing between these variables.

To address this, ensure that each variable is placed on its own cache
line to prevent false sharing, by using the CachelinePadded template
class.

Change-Id: I8316637a7a0c6fd05fd6f6ba24a1df44c43390c5
Reviewed-on: http://review.couchbase.org/69951
Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
Tested-by: buildbot <build@couchbase.com>

src/ep_engine.cc
@@ -1906,7 +1906,7 @@ extern "C" {
 
         if (MemoryTracker::trackingMemoryAllocations()) {
             engine->getEpStats().memoryTrackerEnabled.store(true);
-            engine->getEpStats().totalMemory.store(inital_tracking->load());
+            engine->getEpStats().totalMemory->store(inital_tracking->load());
         }
         delete inital_tracking;
 

src/objectregistry.cc
@@ -143,8 +143,8 @@ void ObjectRegistry::onCreateItem(const Item *pItem)
    EventuallyPersistentEngine *engine = th->get();
    if (verifyEngine(engine)) {
        EPStats &stats = engine->getEpStats();
-       stats.memOverhead.fetch_add(pItem->size() - pItem->getValMemSize());
-       stats.numItem++;
+       stats.memOverhead->fetch_add(pItem->size() - pItem->getValMemSize());
+       ++(*stats.numItem);
    }
 }
 
@@ -153,8 +153,8 @@ void ObjectRegistry::onDeleteItem(const Item *pItem)
    EventuallyPersistentEngine *engine = th->get();
    if (verifyEngine(engine)) {
        EPStats &stats = engine->getEpStats();
-       stats.memOverhead.fetch_sub(pItem->size() - pItem->getValMemSize());
-       stats.numItem--;
+       stats.memOverhead->fetch_sub(pItem->size() - pItem->getValMemSize());
+       ++(*stats.numItem);
    }
 }
 
@@ -187,7 +187,7 @@ bool ObjectRegistry::memoryAllocated(size_t mem) {
         return false;
     }
     EPStats &stats = engine->getEpStats();
-    stats.totalMemory.fetch_add(mem);
+    stats.totalMemory->fetch_add(mem);
     return true;
 }
 
@@ -200,6 +200,6 @@ bool ObjectRegistry::memoryDeallocated(size_t mem) {
         return false;
     }
     EPStats &stats = engine->getEpStats();
-    stats.totalMemory.fetch_sub(mem);
+    stats.totalMemory->fetch_sub(mem);
     return true;
 }

src/checkpoint.cc
@@ -111,11 +111,11 @@ Checkpoint::~Checkpoint() {
     LOG(EXTENSION_LOG_INFO,
         "Checkpoint %" PRIu64 " for vbucket %d is purged from memory",
         checkpointId, vbucketId);
-    stats.memOverhead.fetch_sub(memorySize());
-    if (stats.memOverhead.load() >= GIGANTOR) {
+    stats.memOverhead->fetch_sub(memorySize());
+    if (stats.memOverhead->load() >= GIGANTOR) {
         LOG(EXTENSION_LOG_WARNING,
             "Checkpoint::~Checkpoint: stats.memOverhead (which is %" PRId64
-            ") is greater than %" PRId64, uint64_t(stats.memOverhead.load()),
+            ") is greater than %" PRId64, uint64_t(stats.memOverhead->load()),
             uint64_t(GIGANTOR));
     }
 }
@@ -249,11 +249,11 @@ queue_dirty_t Checkpoint::queueDirty(const queued_item &qi,
             size_t newEntrySize = qi->getNKey() + sizeof(index_entry) +
                                   sizeof(queued_item);
             memOverhead += newEntrySize;
-            stats.memOverhead.fetch_add(newEntrySize);
-            if (stats.memOverhead.load() >= GIGANTOR) {
+            stats.memOverhead->fetch_add(newEntrySize);
+            if (stats.memOverhead->load() >= GIGANTOR) {
                 LOG(EXTENSION_LOG_WARNING,
                     "Checkpoint::queueDirty: stats.memOverhead (which is %" PRId64
-                    ") is greater than %" PRId64, uint64_t(stats.memOverhead.load()),
+                    ") is greater than %" PRId64, uint64_t(stats.memOverhead->load()),
                     uint64_t(GIGANTOR));
             }
         }
@@ -365,10 +365,10 @@ size_t Checkpoint::mergePrevCheckpoint(Checkpoint *pPrevCheckpoint) {
     setSnapshotStartSeqno(getLowSeqno());
 
     memOverhead += newEntryMemOverhead;
-    stats.memOverhead.fetch_add(newEntryMemOverhead);
+    stats.memOverhead->fetch_add(newEntryMemOverhead);
     LOG(EXTENSION_LOG_WARNING,
         "Checkpoint::mergePrevCheckpoint: stats.memOverhead (which is %" PRId64
-        ") is greater than %" PRId64, uint64_t(stats.memOverhead.load()),
+        ") is greater than %" PRId64, uint64_t(stats.memOverhead->load()),
         uint64_t(GIGANTOR));
     return numNewItems;
 }

src/checkpoint.h
@@ -339,11 +339,11 @@ public:
         numMetaItems(0),
         memOverhead(0),
         effectiveMemUsage(0) {
-        stats.memOverhead.fetch_add(memorySize());
-        if (stats.memOverhead.load() >= GIGANTOR) {
+        stats.memOverhead->fetch_add(memorySize());
+        if (stats.memOverhead->load() >= GIGANTOR) {
             LOG(EXTENSION_LOG_WARNING,
                 "Checkpoint::Checkpoint: stats.memOverhead (which is %" PRId64
-                ") is greater than %" PRId64, uint64_t(stats.memOverhead.load()),
+                ") is greater than %" PRId64, uint64_t(stats.memOverhead->load()),
                 uint64_t(GIGANTOR));
         }
     }

src/ep.cc
@@ -304,7 +304,7 @@ EventuallyPersistentStore::EventuallyPersistentStore(
     size_t num_vbs = config.getMaxVbuckets();
     vb_mutexes = new Mutex[num_vbs];
 
-    stats.memOverhead = sizeof(EventuallyPersistentStore);
+    *stats.memOverhead = sizeof(EventuallyPersistentStore);
 
     if (config.getConflictResolutionType().compare("lww") == 0) {
         conflictResolver.reset(new LastWriteWinsResolution());

src/ep_engine.h
@@ -887,7 +887,8 @@ protected:
      */
     ENGINE_ERROR_CODE memoryCondition() {
         // Do we think it's possible we could free something?
-        bool haveEvidenceWeCanFreeMemory(stats.getMaxDataSize() > stats.memOverhead);
+        bool haveEvidenceWeCanFreeMemory =
+            (stats.getMaxDataSize() > stats.memOverhead->load());
         if (haveEvidenceWeCanFreeMemory) {
             // Look for more evidence by seeing if we have resident items.
             VBucketCountVisitor countVisitor(*this, vbucket_state_active);

src/stats.h
@@ -25,6 +25,7 @@
 #include <map>
 
 #include "atomic.h"
+#include <platform/cacheline_padded.h>
 #include <platform/histogram.h>
 #include <platform/processclock.h>
 #include "memory_tracker.h"
@@ -177,9 +178,9 @@ public:
 
     size_t getTotalMemoryUsed() {
         if (memoryTrackerEnabled.load()) {
-            return totalMemory.load();
+            return totalMemory->load();
         }
-        return currentSize.load() + memOverhead.load();
+        return currentSize.load() + memOverhead->load();
     }
 
     bool decrDiskQueueSize(size_t decrementBy) {
@@ -310,11 +311,11 @@ public:
     //! Total size of StoredVal memory overhead
     AtomicValue<size_t> storedValOverhead;
     //! Amount of memory used to track items and what-not.
-    AtomicValue<size_t> memOverhead;
+    cb::CachelinePadded<AtomicValue<size_t>> memOverhead;
     //! Total number of Item objects
-    AtomicValue<size_t> numItem;
+    cb::CachelinePadded<AtomicValue<size_t>> numItem;
     //! The total amount of memory used by this bucket (From memory tracking)
-    AtomicValue<size_t> totalMemory;
+    cb::CachelinePadded<AtomicValue<size_t>> totalMemory;
     //! True if the memory usage tracker is enabled.
     AtomicValue<bool> memoryTrackerEnabled;
     //! Whether or not to force engine shutdown.

src/stored-value.cc
@@ -365,7 +365,7 @@ void HashTable::resize(size_t newSize) {
         return;
     }
 
-    stats.memOverhead.fetch_sub(memorySize());
+    stats.memOverhead->fetch_sub(memorySize());
     ++numResizes;
 
     // Set the new size so all the hashy stuff works.
@@ -389,7 +389,7 @@ void HashTable::resize(size_t newSize) {
     cb_free(values);
     values = newValues;
 
-    stats.memOverhead.fetch_add(memorySize());
+    stats.memOverhead->fetch_add(memorySize());
 }
 
 static size_t distance(size_t a, size_t b) {

commit 8eb8ddb522bfe9e0fc7febfa648895a42cf92c23

MB-25897: ObjectRegistry: decrement numItem when Items are deleted
ObjectRegistry::onDeleteItem was incorrectly incrementing numItem
instead of decrementing.

This is a regression since 8bf14273
(http://review.couchbase.org/#/c/69951/8), introduced in 4.6.0.

Change to decrement; and add regression test.

Change-Id: Id5af53c66c434848523dce0faded2890a408c571
Reviewed-on: http://review.couchbase.org/83016
Well-Formed: Build Bot <build@couchbase.com>
Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
Tested-by: Build Bot <build@couchbase.com>

engines/ep/CMakeLists.txt
@@ -285,6 +285,7 @@ ADD_EXECUTABLE(ep-engine_ep_unit_tests
                tests/module_tests/memory_tracker_test.cc
                tests/module_tests/mock_hooks_api.cc
                tests/module_tests/mutation_log_test.cc
+               tests/module_tests/objectregistry_test.cc
                tests/module_tests/mutex_test.cc
                tests/module_tests/stats_test.cc
                tests/module_tests/storeddockey_test.cc

engines/ep/src/ep_engine.cc
@@ -5334,7 +5334,9 @@ ENGINE_ERROR_CODE EventuallyPersistentEngine::setVBucketState(
 }
 
 EventuallyPersistentEngine::~EventuallyPersistentEngine() {
-    kvBucket->deinitialize();
+    if (kvBucket) {
+        kvBucket->deinitialize();
+    }
     LOG(EXTENSION_LOG_NOTICE, "~EPEngine: Completed deinitialize.");
     kvBucket.reset();
     LOG(EXTENSION_LOG_NOTICE, "~EPEngine: Deleted KvBucket.");

engines/ep/src/objectregistry.cc
@@ -159,7 +159,7 @@ void ObjectRegistry::onDeleteItem(const Item *pItem)
    if (verifyEngine(engine)) {
        EPStats &stats = engine->getEpStats();
        stats.memOverhead->fetch_sub(pItem->size() - pItem->getValMemSize());
-       ++(*stats.numItem);
+       --(*stats.numItem);
    }
 }
 

engines/ep/tests/mock/mock_synchronous_ep_engine.cc
@@ -25,9 +25,8 @@
 
 #include <string>
 
-SynchronousEPEngine::SynchronousEPEngine(const std::string& extra_config)
+SynchronousEPEngine::SynchronousEPEngine(std::string extra_config)
     : EventuallyPersistentEngine(get_mock_server_api) {
-
     // Tests may need to create multiple failover table entries, so allow that
     maxFailoverEntries = 5;
 

engines/ep/tests/mock/mock_synchronous_ep_engine.h
@@ -32,7 +32,7 @@
  */
 class SynchronousEPEngine : public EventuallyPersistentEngine {
 public:
-    SynchronousEPEngine(const std::string& extra_config);
+    SynchronousEPEngine(std::string extra_config = {});
 
     void setKVBucket(std::unique_ptr<KVBucket> store);
     void setDcpConnMap(std::unique_ptr<DcpConnMap> dcpConnMap);

engines/ep/tests/module_tests/objectregistry_test.cc
@@ -1,0 +1,64 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2017 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include "config.h"
+
+#include "objectregistry.h"
+
+#include "item.h"
+#include "test_helpers.h"
+#include "tests/mock/mock_synchronous_ep_engine.h"
+
+#include <gtest/gtest.h>
+
+class ObjectRegistryTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        ObjectRegistry::onSwitchThread(&engine);
+    }
+    void TearDown() override {
+        ObjectRegistry::onSwitchThread(nullptr);
+    }
+
+    SynchronousEPEngine engine;
+};
+
+// Check that constructing & destructing an Item is correctly tracked in
+// EpStats::numItem via ObjectRegistry::on{Create,Delete}Item.
+TEST_F(ObjectRegistryTest, NumItem) {
+    ASSERT_EQ(0, *engine.getEpStats().numItem);
+
+    {
+        auto item = make_item(0, makeStoredDocKey("key"), "value");
+        EXPECT_EQ(1, *engine.getEpStats().numItem);
+    }
+    EXPECT_EQ(0, *engine.getEpStats().numItem);
+}
+
+// Check that constructing & destructing an Item is correctly tracked in
+// EpStats::memOverhead via ObjectRegistry::on{Create,Delete}Item.
+TEST_F(ObjectRegistryTest, MemOverhead) {
+    ASSERT_EQ(0, *engine.getEpStats().memOverhead);
+
+    {
+        auto item = make_item(0, makeStoredDocKey("key"), "value");
+        // Currently just checking the overhead is non-zero; could expand
+        // to calculate expected size based on the Item's size.
+        EXPECT_NE(0, *engine.getEpStats().memOverhead);
+    }
+    EXPECT_EQ(0, *engine.getEpStats().memOverhead);
+}