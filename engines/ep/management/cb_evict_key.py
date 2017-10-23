couchbase/kv_engine

commit 4df093d4e1e7c65158c8539604b6f1907951ae15

MB-23377: Move GetValue ownership for multi fetch
GetValues currently have a raw pointer to the blob which they are
associated with. Really they should own this and pass on ownership when
required to ensure that resources are properly cleaned up.

However in the case of multi bg fetches multiple VBucketBGFetchItems may
have GetValues which refer the same underlying blob. Therefore in this
case there is an n X 1 relationship between VBucketBGFetchItems and
underlying blobs (the things we are fetching). Therefore it would be
incorrect for a VBucketBGFetchItem to own something which owns one of
these blobs.

This patch moves the ownership of GetValues from VBucketBGFetchItems to
vb_fetch_item_ctx_ts which typically has a  vector of
VBucketBGFetchItems. The VBucketBGFetchItems then hold a pointer to the
GetValue.

Change-Id: I48b2dc8535f0f7eb2beb9eaa9ed504d1b5a2b7ca
Reviewed-on: http://review.couchbase.org/78598
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

engines/ep/src/couch-kvstore/couch-kvstore.cc
@@ -549,9 +549,7 @@ void CouchKVStore::getMulti(uint16_t vb, vb_bgfetch_queue_t &itms) {
                    couchstore_strerror(errCode), vb, numItems);
         st.numGetFailure += numItems;
         for (auto& item : itms) {
-            for (auto& fetch : item.second.bgfetched_list) {
-                fetch->value.setStatus(ENGINE_NOT_MY_VBUCKET);
-            }
+            item.second.value.setStatus(ENGINE_NOT_MY_VBUCKET);
         }
         return;
     }
@@ -583,9 +581,7 @@ void CouchKVStore::getMulti(uint16_t vb, vb_bgfetch_queue_t &itms) {
                    couchstore_strerror(errCode),
                    couchkvstore_strerrno(db, errCode).c_str(), vb);
         for (auto& item : itms) {
-            for (const auto& fetch : item.second.bgfetched_list) {
-                fetch->value.setStatus(couchErr2EngineErr(errCode));
-            }
+            item.second.value.setStatus(couchErr2EngineErr(errCode));
         }
     }
     closeDatabaseHandle(db);
@@ -2302,6 +2298,8 @@ int CouchKVStore::getMultiCb(Db *db, DocInfo *docinfo, void *ctx) {
         st.numGetFailure++;
     }
 
+    bg_itm_ctx.value = std::move(returnVal);
+
     returnVal.setStatus(cbCtx->cks.couchErr2EngineErr(errCode));
 
     bool return_val_ownership_transferred = false;
@@ -2309,7 +2307,7 @@ int CouchKVStore::getMultiCb(Db *db, DocInfo *docinfo, void *ctx) {
         return_val_ownership_transferred = true;
         // populate return value for remaining fetch items with the
         // same seqid
-        fetch->value = returnVal;
+        fetch->value = &bg_itm_ctx.value;
         st.readTimeHisto.add(
                 std::chrono::duration_cast<std::chrono::microseconds>(
                         ProcessClock::now() - fetch->initTime)

engines/ep/src/bgfetcher.cc
@@ -100,8 +100,8 @@ void BgFetcher::clearItems(VBucket::id_type vbId,
                            vb_bgfetch_queue_t& itemsToFetch) {
     for (auto& fetch : itemsToFetch) {
         // every fetched item belonging to the same key shares
-        // a single data buffer, just delete it from the first fetched item
-        fetch.second.bgfetched_list.front()->delValue();
+        // a single data buffer, just delete it from the shared context
+        delete fetch.second.value.getValue();
     }
 }
 

engines/ep/src/ep_vb.cc
@@ -80,8 +80,8 @@ ENGINE_ERROR_CODE EPVBucket::completeBGFetchForSingleItem(
         const DocKey& key,
         const VBucketBGFetchItem& fetched_item,
         const ProcessClock::time_point startTime) {
-    ENGINE_ERROR_CODE status = fetched_item.value.getStatus();
-    Item* fetchedValue = fetched_item.value.getValue();
+    ENGINE_ERROR_CODE status = fetched_item.value->getStatus();
+    Item* fetchedValue = fetched_item.value->getValue();
     { // locking scope
         ReaderLockHolder rlh(getStateLock());
         auto hbl = ht.getLockedBucket(key);
@@ -434,6 +434,7 @@ size_t EPVBucket::queueBGFetchItem(const DocKey& key,
         bgfetch_itm_ctx.isMetaOnly = false;
     }
 
+    fetch->value = &bgfetch_itm_ctx.value;
     bgfetch_itm_ctx.bgfetched_list.push_back(std::move(fetch));
 
     bgFetcher->addPendingVB(getId());

engines/ep/src/kv_bucket.cc
@@ -1369,7 +1369,7 @@ void KVBucket::completeBGFetch(const DocKey& key,
 
         VBucketPtr vb = getVBucket(vbucket);
         if (vb) {
-            VBucketBGFetchItem item{gcb.val, cookie, init, isMeta};
+            VBucketBGFetchItem item{&gcb.val, cookie, init, isMeta};
             ENGINE_ERROR_CODE status =
                     vb->completeBGFetchForSingleItem(key, item, startTime);
             engine.notifyIOComplete(item.cookie, status);

engines/ep/src/kvstore.h
@@ -44,7 +44,7 @@ public:
     VBucketBGFetchItem(const void* c, bool meta_only)
         : cookie(c), initTime(ProcessClock::now()), metaDataOnly(meta_only) {
     }
-    VBucketBGFetchItem(const GetValue& value_,
+    VBucketBGFetchItem(GetValue* value_,
                        const void* c,
                        const ProcessClock::time_point& init_time,
                        bool meta_only)
@@ -56,12 +56,7 @@ public:
 
     ~VBucketBGFetchItem() {}
 
-    void delValue() {
-        delete value.getValue();
-        value.setValue(NULL);
-    }
-
-    GetValue value;
+    GetValue* value;
     const void * cookie;
     ProcessClock::time_point initTime;
     bool metaDataOnly;
@@ -92,6 +87,7 @@ struct vb_bgfetch_item_ctx_t {
     }
     std::list<std::unique_ptr<VBucketBGFetchItem>> bgfetched_list;
     bool isMetaOnly;
+    GetValue value;
 };
 
 typedef std::unordered_map<StoredDocKey, vb_bgfetch_item_ctx_t> vb_bgfetch_queue_t;

engines/ep/src/warmup.cc
@@ -304,6 +304,7 @@ static bool batchWarmupCallback(uint16_t vbId,
             bg_itm_ctx.isMetaOnly = false;
             bg_itm_ctx.bgfetched_list.emplace_back(
                     std::make_unique<VBucketBGFetchItem>(nullptr, false));
+            bg_itm_ctx.bgfetched_list.back()->value = &bg_itm_ctx.value;
         }
 
         c->epstore->getROUnderlying(vbId)->getMulti(vbId, items2fetch);
@@ -319,7 +320,7 @@ static bool batchWarmupCallback(uint16_t vbId,
             std::unique_ptr<VBucketBGFetchItem> fetchedItem(
                     std::move(bg_itm_ctx.bgfetched_list.back()));
             if (applyItem) {
-                GetValue &val = fetchedItem->value;
+                GetValue& val = *fetchedItem->value;
                 if (val.getStatus() == ENGINE_SUCCESS) {
                     // NB: callback will delete the GetValue's Item
                     c->cb.callback(val);
@@ -339,8 +340,8 @@ static bool batchWarmupCallback(uint16_t vbId,
                 }
             } else {
                 // Providing that the status is SUCCESS, delete the Item
-                if (fetchedItem->value.getStatus() == ENGINE_SUCCESS) {
-                    delete fetchedItem->value.getValue();
+                if (fetchedItem->value->getStatus() == ENGINE_SUCCESS) {
+                    delete fetchedItem->value->getValue();
                 }
                 c->skipped++;
             }

commit 4e9cc030f453b1d75a964a83226ab5ea967ec69a

MB-26390: CouchKVStore::getMultiCb: set non-zero status correctly
If a couchstore file becomes corrupted such that a document cannot be
fetched from disk using getMulti (returning CHECKSUM_FAIL or any other
non-zero status), then instead of logging and returning a error to the
user (TMPFAIL), KV-Engine incorrectly returns ENOENT - i.e. document
doesn't exist.

The issue is that the status code was set *after* the rest of GetValue
had already been assigned elsewhere, meaning a non-zero status code
was lost. The bug was introduced by commit
4df093d4e1e7c65158c8539604b6f1907951ae15, which refactored GetMulti
ownership.

Change-Id: I70f4f3eba48e453ec3e5aba15821f0f041f45928
Reviewed-on: http://review.couchbase.org/84363
Well-Formed: Build Bot <build@couchbase.com>
Reviewed-by: Jim Walker <jim@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

engines/ep/management/cb_evict_key.py
@@ -1,0 +1,27 @@
+#!/usr/bin/env python
+
+"""
+Evict the specified key from memory.
+
+Copyright (c) 2017 Couchbase, Inc
+"""
+
+import mc_bin_client
+import sys
+
+HOST = '127.0.0.1'
+PORT = 12000
+
+if len(sys.argv) < 5:
+    msg = ('Usage: {} <user> <password> <bucket> <vbid> <key>'.format(
+        sys.argv[0]))
+    print >> sys.stderr, msg
+    sys.exit(1)
+
+client = mc_bin_client.MemcachedClient(host=HOST, port=PORT)
+client.sasl_auth_plain(user=sys.argv[1], password=sys.argv[2])
+client.bucket_select(sys.argv[3])
+client.vbucketId = int(sys.argv[4])
+
+key = sys.argv[5]
+print client.evict_key(key)

engines/ep/src/couch-kvstore/couch-kvstore.cc
@@ -2323,16 +2323,13 @@ int CouchKVStore::getMultiCb(Db *db, DocInfo *docinfo, void *ctx) {
     vb_bgfetch_item_ctx_t& bg_itm_ctx = (*qitr).second;
     GetMetaOnly meta_only = bg_itm_ctx.isMetaOnly;
 
-    GetValue returnVal;
-    couchstore_error_t errCode = cbCtx->cks.fetchDoc(db, docinfo, returnVal,
-                                                     cbCtx->vbId, meta_only);
+    couchstore_error_t errCode = cbCtx->cks.fetchDoc(
+            db, docinfo, bg_itm_ctx.value, cbCtx->vbId, meta_only);
     if (errCode != COUCHSTORE_SUCCESS && (meta_only == GetMetaOnly::No)) {
         st.numGetFailure++;
     }
 
-    bg_itm_ctx.value = std::move(returnVal);
-
-    returnVal.setStatus(cbCtx->cks.couchErr2EngineErr(errCode));
+    bg_itm_ctx.value.setStatus(cbCtx->cks.couchErr2EngineErr(errCode));
 
     bool return_val_ownership_transferred = false;
     for (auto& fetch : bg_itm_ctx.bgfetched_list) {

engines/ep/tests/module_tests/kvstore_test.cc
@@ -701,6 +701,7 @@ TEST_F(CouchKVStoreErrorInjectionTest, get_docinfo_by_id) {
         EXPECT_CALL(ops, pread(_, _, _, _, _)).Times(3).RetiresOnSaturation();
         gv = kvstore->get(items.front().getKey(), 0);
     }
+    EXPECT_EQ(ENGINE_TMPFAIL, gv.getStatus());
 }
 
 /**
@@ -722,6 +723,7 @@ TEST_F(CouchKVStoreErrorInjectionTest, get_open_doc_with_docinfo) {
         EXPECT_CALL(ops, pread(_, _, _, _, _)).Times(5).RetiresOnSaturation();
         gv = kvstore->get(items.front().getKey(), 0);
     }
+    EXPECT_EQ(ENGINE_TMPFAIL, gv.getStatus());
 }
 
 /**
@@ -745,6 +747,7 @@ TEST_F(CouchKVStoreErrorInjectionTest, getMulti_docinfos_by_id) {
         kvstore->getMulti(0, itms);
 
     }
+    EXPECT_EQ(ENGINE_TMPFAIL, itms[items.at(0).getKey()].value.getStatus());
 }
 
 
@@ -766,6 +769,7 @@ TEST_F(CouchKVStoreErrorInjectionTest, getMulti_open_doc_with_docinfo) {
 
         EXPECT_EQ(1, kvstore->getKVStoreStat().numGetFailure);
     }
+    EXPECT_EQ(ENGINE_TMPFAIL, itms[items.at(0).getKey()].value.getStatus());
 }
 
 /**