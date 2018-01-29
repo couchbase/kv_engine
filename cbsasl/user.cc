4f7b07d885b2a2066bcb5f297af6774aba697920
Trond Norbye <trond.norbye@gmail.com>
2018-01-29 13:47:04 +0100

    gcc7 warnings: algorithm may be uninitialized
    The algorithm variable was set in the switch statement
    handling all of the legal values for the scoped enum,
    but the function could have been called with invalid
    input.
    
    Change-Id: I0833ed46ad02fa1dc57b88212bfb83a8b945d2c6
    Reviewed-on: http://review.couchbase.org/88495
    Reviewed-by: Tim Bradgate <tim.bradgate@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- cbsasl/user.cc
+++ cbsasl/user.cc
@@ -196,7 +196,7 @@ void cb::sasl::User::generateSecrets(const Mechanism& mech,
 
     std::vector<uint8_t> salt;
     std::string encodedSalt;
-    cb::crypto::Algorithm algorithm;
+    cb::crypto::Algorithm algorithm = cb::crypto::Algorithm::MD5;
 
     switch (mech) {
     case Mechanism::SCRAM_SHA512:
@@ -214,6 +214,15 @@ void cb::sasl::User::generateSecrets(const Mechanism& mech,
     case Mechanism::PLAIN:
     case Mechanism::UNKNOWN:
         throw std::logic_error("cb::cbsasl::User::generateSecrets invalid algorithm");
+    }
+
+    if (algorithm == cb::crypto::Algorithm::MD5) {
+        // gcc7 complains that algorithm may have been uninitialized when we
+        // used it below. This would happen if the user provided a mech
+        // which isn't handled above. If that happens we should just
+        // throw an exception.
+        throw std::invalid_argument(
+                "cb::sasl::User::generateSecrets: invalid mechanism provided");
     }
 
     if (salt.empty()) {
