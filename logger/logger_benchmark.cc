21c12a726866ff2602b29d4ca9f01e770c3baee5
Trond Norbye <trond.norbye@gmail.com>
2018-02-09 11:00:34 +0100

    Add logger benchmark
    Add a small micro benchmark for the logger testing 3 different scenarios:
    
    1) GetLogger - Test the cost of fetching the logger object (means copying
                   the shared pointer)
    
    2) LogToLoggerWithDisabeldLogLevel - Test the cost of the log statements
                   we have in our code when the logger will drop the log
                   statement becuase the log level isn't enabled (note:
                   this does _NOT_ include the cost it could be to generate
                   the parameters passed in!)
    
    3) LogToLoggerWithEnabledLogLevel - Test the cost of adding the log
                   statement into the queue of statements to pass off to the
                   async logger. Note that the async logger will drop all
                   of them. The test will probably return "incorrect" results
                   if we manage to fill up the queue between the two threads.
    
    Run on (8 X 2500 MHz CPU s)
    2018-02-09 11:41:04
    ---------------------------------------------------------------------------------
    Benchmark                                          Time           CPU Iterations
    ---------------------------------------------------------------------------------
    GetLogger/threads:1                               18 ns         18 ns   38555385
    GetLogger/threads:2                               46 ns         92 ns    7759112
    GetLogger/threads:4                               50 ns        198 ns    3510752
    GetLogger/threads:8                               53 ns        414 ns    1910896
    LogToLoggerWithDisabeldLogLevel/threads:1         26 ns         26 ns   26377668
    LogToLoggerWithDisabeldLogLevel/threads:2         57 ns        114 ns    6165690
    LogToLoggerWithDisabeldLogLevel/threads:4         76 ns        304 ns    2282880
    LogToLoggerWithDisabeldLogLevel/threads:8         59 ns        462 ns    1506520
    LogToLoggerWithEnabledLogLevel/threads:1         100 ns        100 ns    7002031
    LogToLoggerWithEnabledLogLevel/threads:2         125 ns        249 ns    2677172
    LogToLoggerWithEnabledLogLevel/threads:4         115 ns        458 ns    1610576
    LogToLoggerWithEnabledLogLevel/threads:8          87 ns        681 ns    1083992
    
    Change-Id: I040327e1be9dca10a219df726d530d5b942d01dd
    Reviewed-on: http://review.couchbase.org/89170
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>

---
+++ logger/logger_benchmark.cc
@@ -0,0 +1,122 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2018 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <benchmark/benchmark.h>
+
+#include <logger/logger.h>
+#include <iostream>
+
+/**
+ * Benchmark the cost of grabbing the logger (which means checking
+ * for it's existence and copy a shared pointer).
+ */
+void GetLogger(benchmark::State& state) {
+    if (state.thread_index == 0) {
+        cb::logger::createBlackholeLogger();
+    }
+
+    while (state.KeepRunning()) {
+        auto logger = cb::logger::get();
+        benchmark::DoNotOptimize(logger);
+    }
+
+    if (state.thread_index == 0) {
+        cb::logger::shutdown();
+    }
+}
+
+BENCHMARK(GetLogger)->ThreadRange(1, 8);
+
+/**
+ * Benchmark the cost of logging to a level which is dropped.
+ * We're currently using the async logging (which is the one
+ * we'll be using "in production".. except that we don't have
+ * the file-backend writing to the disk
+ */
+void LogToLoggerWithDisabledLogLevel(benchmark::State& state) {
+    if (state.thread_index == 0) {
+        cb::logger::Config config{};
+        config.cyclesize = 2048;
+        config.buffersize = 8192;
+        config.sleeptime = 1;
+        config.unit_test = true;
+        config.console = false;
+
+        auto init = cb::logger::initialize(config);
+        if (init) {
+            std::cerr << "Failed to initialize logger: " << *init;
+            return;
+        }
+
+        cb::logger::get()->set_level(spdlog::level::level_enum::warn);
+    }
+
+    while (state.KeepRunning()) {
+        LOG_TRACE("Foo");
+    }
+
+    if (state.thread_index == 0) {
+        cb::logger::shutdown();
+    }
+}
+
+BENCHMARK(LogToLoggerWithDisabledLogLevel)->ThreadRange(1, 8);
+
+/**
+ * Benchmark the cost of logging to a level which is enabled (moved
+ * to the sink).
+ * We're currently using the async logging (which is the one
+ * we'll be using "in production".. except that we don't have
+ * the file-backend writing to the disk
+ */
+void LogToLoggerWithEnabledLogLevel(benchmark::State& state) {
+    if (state.thread_index == 0) {
+        cb::logger::Config config{};
+        config.cyclesize = 2048;
+        config.buffersize = 8192;
+        config.sleeptime = 1;
+        config.unit_test = true;
+        config.console = false;
+
+        auto init = cb::logger::initialize(config);
+        if (init) {
+            std::cerr << "Failed to initialize logger: " << *init;
+            return;
+        }
+
+        cb::logger::get()->set_level(spdlog::level::level_enum::trace);
+    }
+
+    while (state.KeepRunning()) {
+        LOG_TRACE("Foo");
+    }
+
+    if (state.thread_index == 0) {
+        cb::logger::shutdown();
+    }
+}
+
+BENCHMARK(LogToLoggerWithEnabledLogLevel)->ThreadRange(1, 8);
+
+int main(int argc, char** argv) {
+    ::benchmark::Initialize(&argc, argv);
+    if (::benchmark::ReportUnrecognizedArguments(argc, argv)) {
+        return EXIT_FAILURE;
+    }
+    ::benchmark::RunSpecifiedBenchmarks();
+    return EXIT_SUCCESS;
+}
