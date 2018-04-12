54f917d80074039670db17009f004fd7fb9eea21
Daniel Owen <owend@couchbase.com>
2018-04-12 10:41:16 +0100

    MB-29238: Use domain instead of source for memcached audit events
    Change the memcached audit events to have a user_id made up from
    {domain, user} as opposed to the previous {source, user}.
    
    Change-Id: I02b7e2027d2cc4777386ca7550de8c503a083389
    Reviewed-on: http://review.couchbase.org/92613
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- daemon/mcaudit.cc
+++ daemon/mcaudit.cc
@@ -86,10 +86,10 @@ static unique_cJSON_ptr create_memcached_audit_object(const Connection* c) {
 
     cJSON_AddStringToObject(root, "peername", c->getPeername().c_str());
     cJSON_AddStringToObject(root, "sockname", c->getSockname().c_str());
-    cJSON *source = cJSON_CreateObject();
-    cJSON_AddStringToObject(source, "source", "memcached");
-    cJSON_AddStringToObject(source, "user", c->getUsername());
-    cJSON_AddItemToObject(root, "real_userid", source);
+    cJSON *domain = cJSON_CreateObject();
+    cJSON_AddStringToObject(domain, "domain", "memcached");
+    cJSON_AddStringToObject(domain, "user", c->getUsername());
+    cJSON_AddItemToObject(root, "real_userid", domain);
 
     return unique_cJSON_ptr(root);
 }
