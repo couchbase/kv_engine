7cdbc465a2943ec9ff2295eee2a5f0de284fe489
Tim Bradgate <tim.bradgate@couchbase.com>
2018-04-25 14:54:44 +0100

    MB-29377: Change return signature of cb::logger::get()
    Change the return signature of getting the logger object
    to return a direct pointer to the object. This has a notable
    affect on the logger benchmark results, in the case where we are
    A) getting the logger or B) logging at a level which is disabled.
    
    Before change:
    
    Run on (8 X 2300 MHz CPU s)
    2018-04-25 15:37:36
    ----------------------------------------------------------------------------------------------
    Benchmark                                                       Time           CPU Iterations
    ----------------------------------------------------------------------------------------------
    LoggerBench_Blackhole/GetLogger/threads:1                      13 ns         13 ns   50902427
    LoggerBench_Blackhole/GetLogger/threads:16                     38 ns        320 ns    2124144
    LoggerBench/LogToLoggerWithDisabledLogLevel/threads:1          13 ns         13 ns   50811533
    LoggerBench/LogToLoggerWithDisabledLogLevel/threads:16         54 ns        483 ns    1284736
    LoggerBench/LogToLoggerWithEnabledLogLevel/threads:1          475 ns        473 ns    1485288
    LoggerBench/LogToLoggerWithEnabledLogLevel/threads:16         606 ns       1065 ns     550688
    
    After change:
    
    Run on (8 X 2300 MHz CPU s)
    2018-04-25 15:37:36
    ----------------------------------------------------------------------------------------------
    Benchmark                                                       Time           CPU Iterations
    ----------------------------------------------------------------------------------------------
    LoggerBench_Blackhole/GetLogger/threads:1                       2 ns          2 ns  335836112
    LoggerBench_Blackhole/GetLogger/threads:16                      0 ns          3 ns  220889872
    LoggerBench/LogToLoggerWithDisabledLogLevel/threads:1           2 ns          2 ns  334633624
    LoggerBench/LogToLoggerWithDisabledLogLevel/threads:16          0 ns          4 ns  156165040
    LoggerBench/LogToLoggerWithEnabledLogLevel/threads:1          472 ns        471 ns    1490142
    LoggerBench/LogToLoggerWithEnabledLogLevel/threads:16         423 ns       1075 ns     590576
    
    Change-Id: I82ebd5c1776ff0efad6b9316e1ff35a20887892f
    Reviewed-on: http://review.couchbase.org/93286
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@gmail.com>
    Tested-by: Build Bot <build@couchbase.com>

--- logger/logger.h
+++ logger/logger.h
@@ -15,6 +15,20 @@
  *   limitations under the License.
  */
 
+/*
+ *   A note on the thread safety of the logger API:
+ *
+ *   The API is thread safe unless the underlying logger object is changed
+ * during runtime. This means some methods can only be safely called if the
+ * caller guarantees no other threads exist and/or are calling the logging
+ * functions.
+ *
+ *   The caveat being we should not change the underlying logger object during
+ * run-time, the exception to this is during the initial memcached startup,
+ * where we are running in a single thread at the point we switch from console
+ * logging to file logging.
+ */
+
 #pragma once
 
 #include <logger/visibility.h>
@@ -57,6 +71,8 @@ struct LOGGER_PUBLIC_API Config {
  *
  * The default level for the created logger is set to INFO
  *
+ * See note about thread safety at the top of the file
+ *
  * @param logger_settings the configuration for the logger
  * @return optional error message if something goes wrong
  */
@@ -70,6 +86,8 @@ boost::optional<std::string> initialize(const Config& logger_settings);
  * don't need any output (but may call methods who tries
  * to fetch the logger)
  *
+ * See note about thread safety at the top of the file
+ *
  * @throws std::bad_alloc
  * @throws spdlog::spdlog_ex if an error occurs creating the logger
  *                           (if it already exists for instance)
@@ -80,6 +98,8 @@ void createBlackholeLogger();
 /**
  * Initialize the logger with the logger which logs to the console
  *
+ * See note about thread safety at the top of the file
+ *
  * @throws std::bad_alloc
  * @throws spdlog::spdlog_ex if an error occurs creating the logger
  */
@@ -88,9 +108,26 @@ void createConsoleLogger();
 
 /**
  * Get the underlying logger object
+ *
+ * See note about thread safety at the top of the file.
+ *
+ * This will return null if a logger has not been
+ * initialized through one of the following:
+ *
+ * - initialize()
+ * - createBlackholeLogger()
+ * - createConsoleLogger()
  */
 LOGGER_PUBLIC_API
-std::shared_ptr<spdlog::logger> get();
+spdlog::logger* get();
+
+/**
+ * Reset the underlying logger object
+ *
+ * See note about thread safety at the top of the file
+ */
+LOGGER_PUBLIC_API
+void reset();
 
 LOGGER_PUBLIC_API
 EXTENSION_LOGGER_DESCRIPTOR& getLoggerDescriptor();
