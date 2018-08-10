744442c236572daf74be263453f3ccf743434c91
Jim Walker <jim@couchbase.com>
2018-08-10 14:45:29 +0100

    MB-30865: [1/n] Add unsigned_leb128 encode/decoder and tests
    Add a decoder and encoder (with tests) to cb::mcbp namespace.
    
    These methods are intended for use with reading from the network
    and encoding outgoing packets (and even disk meta-data).
    
    Change-Id: I5e7306cac89eac57bb3314f38bc904431aa82945
    Reviewed-on: http://review.couchbase.org/98128
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

---
+++ include/mcbp/protocol/unsigned_leb128.h
@@ -0,0 +1,114 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2018 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+#pragma once
+
+#include <platform/sized_buffer.h>
+#include <array>
+#include <gsl/gsl>
+#include <type_traits>
+
+namespace cb {
+namespace mcbp {
+
+/**
+ * Helper code for encode and decode of LEB128 values.
+ * - mcbp encodes collection-ID as an unsigned LEB128
+ * - see https://en.wikipedia.org/wiki/LEB128
+ */
+
+/**
+ * decode_unsigned_leb128 returns the decoded uint<T> and the index of the last
+ * byte of the leb128 within buf.
+ * @param buf buffer containing a leb128 encoded value (of size T)
+ * @returns std::pair first is the decoded value and second a buffer for the
+ *          remaining data (size will be 0 for no more data)
+ */
+template <class T>
+typename std::enable_if<std::is_unsigned<T>::value,
+                        std::pair<T, cb::const_byte_buffer>>::type
+decode_unsigned_leb128(cb::const_byte_buffer buf) {
+    T rv = buf[0] & 0x7full;
+    size_t end = 0;
+    if ((buf[0] & 0x80) == 0x80ull) {
+        T shift = 7;
+        // shift in the remaining data
+        for (end = 1; end < buf.size(); end++) {
+            rv |= (buf[end] & 0x7full) << shift;
+            if ((buf[end] & 0x80ull) == 0) {
+                break; // no more
+            }
+            shift += 7;
+        }
+
+        // We should of stopped for a stop byte, not the end of the buffer
+        if (end == buf.size()) {
+            throw std::invalid_argument("decode_unsigned_leb128: no stop byte");
+        }
+    }
+    // Return the decoded value and a buffer for any remaining data
+    return {rv,
+            cb::const_byte_buffer{buf.data() + end + 1,
+                                  buf.size() - (end + 1)}};
+}
+
+// Empty, non specialised version of the decoder class
+template <class T, class Enable = void>
+class unsigned_leb128 {};
+
+/**
+ * For encoding a unsigned T leb128, class constructs from a T value and
+ * provides a const_byte_buffer for access to the encoded
+ */
+template <class T>
+class unsigned_leb128<
+        T,
+        typename std::enable_if<std::is_unsigned<T>::value>::type> {
+public:
+    unsigned_leb128(T in) {
+        do {
+            // extract 7 bits from in
+            auto byte = gsl::narrow_cast<uint8_t>(in & 0x7full);
+            in >>= 7;
+
+            // In has more data?
+            if (in != 0) {
+                byte |= 0x80;
+            }
+
+            // Assign byte
+            data[size] = byte;
+
+            // Increase the size
+            size++;
+        } while (in != 0);
+    }
+
+    cb::const_byte_buffer get() const {
+        return {data.data(), size};
+    }
+
+private:
+    // Larger T may need a larger array
+    static_assert(sizeof(T) <= 8, "Class is only valid for uint 8/16/64");
+
+    // array large enough to store uint64_t::max
+    std::array<uint8_t, 10> data{};
+    uint8_t size{0};
+};
+
+} // namespace mcbp
+} // namespace cb
---
+++ tests/unsigned_leb128/unsigned_leb128_test.cc
@@ -0,0 +1,143 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2018 Couchbase, Inc.
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <gtest/gtest.h>
+#include <mcbp/protocol/unsigned_leb128.h>
+
+#include <limits>
+#include <random>
+
+template <class T>
+class UnsignedLeb128 : public ::testing::Test {};
+
+using MyTypes = ::testing::Types<uint8_t, uint16_t, uint32_t, uint64_t>;
+TYPED_TEST_CASE(UnsignedLeb128, MyTypes);
+
+TYPED_TEST(UnsignedLeb128, EncodeDecode0) {
+    cb::mcbp::unsigned_leb128<TypeParam> zero(0);
+    EXPECT_EQ(1, zero.get().size());
+    EXPECT_EQ(0, zero.get().data()[0]);
+    auto rv = cb::mcbp::decode_unsigned_leb128<TypeParam>(zero.get());
+    EXPECT_EQ(0, rv.first);
+    EXPECT_EQ(0, rv.second.size()); // All input consumed
+}
+
+TYPED_TEST(UnsignedLeb128, EncodeDecodeMax) {
+    cb::mcbp::unsigned_leb128<TypeParam> max(
+            std::numeric_limits<TypeParam>::max());
+    auto rv = cb::mcbp::decode_unsigned_leb128<TypeParam>(max.get());
+    EXPECT_EQ(std::numeric_limits<TypeParam>::max(), rv.first);
+    EXPECT_EQ(0, rv.second.size());
+}
+
+// Input has the MSbit set for every byte
+TYPED_TEST(UnsignedLeb128, EncodeDecode0x80) {
+    TypeParam value = 0;
+    for (size_t i = 0; i < sizeof(TypeParam); i++) {
+        value |= 0x80ull << (i * 8);
+    }
+    cb::mcbp::unsigned_leb128<TypeParam> leb(value);
+    auto rv = cb::mcbp::decode_unsigned_leb128<TypeParam>(leb.get());
+    EXPECT_EQ(value, rv.first);
+    EXPECT_EQ(0, rv.second.size());
+}
+
+TYPED_TEST(UnsignedLeb128, EncodeDecodeRandomValue) {
+    std::mt19937_64 twister(sizeof(TypeParam));
+    auto value = gsl::narrow_cast<TypeParam>(twister());
+    cb::mcbp::unsigned_leb128<TypeParam> leb(value);
+    auto rv = cb::mcbp::decode_unsigned_leb128<TypeParam>(leb.get());
+    EXPECT_EQ(value, rv.first);
+    EXPECT_EQ(0, rv.second.size());
+}
+
+TYPED_TEST(UnsignedLeb128, EncodeDecodeValues) {
+    std::vector<uint64_t> values = {1,
+                                    10,
+                                    100,
+                                    255,
+                                    256,
+                                    1000,
+                                    10000,
+                                    65535,
+                                    65536,
+                                    100000,
+                                    1000000,
+                                    100000000,
+                                    4294967295,
+                                    4294967296,
+                                    1000000000000};
+
+    for (auto v : values) {
+        if (v <= std::numeric_limits<TypeParam>::max()) {
+            cb::mcbp::unsigned_leb128<TypeParam> leb(
+                    gsl::narrow_cast<TypeParam>(v));
+            auto rv = cb::mcbp::decode_unsigned_leb128<TypeParam>(leb.get());
+            EXPECT_EQ(v, rv.first);
+            EXPECT_EQ(0, rv.second.size());
+        }
+    }
+}
+
+TYPED_TEST(UnsignedLeb128, EncodeDecodeMultipleValues) {
+    std::mt19937_64 twister(sizeof(TypeParam));
+    std::vector<uint8_t> data;
+    std::vector<TypeParam> values;
+    const int iterations = 10;
+
+    // Encode
+    for (int n = 0; n < iterations; n++) {
+        values.push_back(gsl::narrow_cast<TypeParam>(twister()));
+        cb::mcbp::unsigned_leb128<TypeParam> leb(values.back());
+        for (auto c : leb.get()) {
+            data.push_back(c);
+        }
+    }
+
+    std::pair<TypeParam, cb::const_byte_buffer> decoded = {0, {data}};
+    int index = 0;
+
+    // Decode
+    do {
+        decoded = cb::mcbp::decode_unsigned_leb128<TypeParam>(decoded.second);
+        EXPECT_EQ(values[index], decoded.first);
+        index++;
+    } while (decoded.second.size() != 0);
+    EXPECT_EQ(iterations, index);
+}
+
+TYPED_TEST(UnsignedLeb128, DecodeInvalidInput) {
+    // Encode a value and then break the value by removing the stop-byte
+    std::mt19937_64 twister(sizeof(TypeParam));
+    auto value = gsl::narrow_cast<TypeParam>(twister());
+    cb::mcbp::unsigned_leb128<TypeParam> leb(value);
+
+    // Take a copy of the const encoded value for modification
+    std::vector<uint8_t> data;
+    for (auto c : leb.get()) {
+        data.push_back(c);
+    }
+
+    // Set the MSbit of the MSB so it's no longer a stop-byte
+    data.back() |= 0x80ull;
+
+    try {
+        cb::mcbp::decode_unsigned_leb128<TypeParam>({data});
+        FAIL() << "Decode didn't throw";
+    } catch (const std::invalid_argument&) {
+    }
+}
