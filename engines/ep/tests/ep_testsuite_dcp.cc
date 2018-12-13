d762da858ef405b6676b54530e7bc93c66a525c7
Christopher Farman <christopher.farman@couchbase.com>
2018-12-13 17:16:06 +0000

    MB-32099: Change ephemeral eviction policy for cursor dropping tests
    This patch attempts to fix the intermittent cv failure, caused by the
    pager running and deleting items on an ephemeral bucket. This would
    cause an unexpected deletion opcode intermittently. This was
    identified on both "test dcp cursor dropping" and "test dcp cursor
    dropping (replication)", but not "test dcp cursor dropping backfill".
    Seeming as these are similar tests, if we see any CV failures like
    this in the future on the backfill test, the same fix should be
    applied to it too.
    
    Change-Id: I5a3f6320b6615a0d4866c54dfe30345989689ebb
    Reviewed-on: http://review.couchbase.org/102806
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/tests/ep_testsuite_dcp.cc
+++ engines/ep/tests/ep_testsuite_dcp.cc
@@ -7140,7 +7140,8 @@ BaseTestCase testsuite_testcases[] = {
                     create at least 1000 items when our residency
                     ratio gets to 90%. See test body for more details. */
                  "cursor_dropping_lower_mark=60;cursor_dropping_upper_mark=70;"
-                 "chk_remover_stime=1;max_size=6291456;chk_max_items=8000;",
+                 "chk_remover_stime=1;max_size=6291456;chk_max_items=8000;"
+                 "ephemeral_full_policy=fail_new_data",
 
                  // TODO RDB: Cannot store any item (ENGINE_ENOMEM).
                  // Needs to resize 'max_size' to consider RocksDB
@@ -7155,7 +7156,8 @@ BaseTestCase testsuite_testcases[] = {
                     create at least 1000 items when our residency
                     ratio gets to 90%. See test body for more details. */
                  "cursor_dropping_lower_mark=60;cursor_dropping_upper_mark=70;"
-                 "chk_remover_stime=1;max_size=6291456;chk_max_items=8000;",
+                 "chk_remover_stime=1;max_size=6291456;chk_max_items=8000;"
+                 "ephemeral_full_policy=fail_new_data",
 
                  // TODO RDB: Cannot store any item (ENGINE_ENOMEM).
                  // Needs to resize 'max_size' to consider RocksDB
