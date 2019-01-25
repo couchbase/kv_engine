1137d5643516f5a3e49744b97fd1266d801ccea5
Christopher Farman <christopher.farman@couchbase.com>
2019-01-25 10:53:51 +0000

    Update output options of JenkinsLogSearch script
    This commit adds a choice for the format of the output
    of the result of the search.
    --format (-f) has options 'plain' (the default), 'log-line' and
    'jira'.
    
    Change-Id: I2402c3874ddded0fd5ad52b6b0947ab89610f999
    Reviewed-on: http://review.couchbase.org/104075
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- scripts/jenkins_console_log_search.py
+++ scripts/jenkins_console_log_search.py
@@ -7,10 +7,11 @@ It results in printing a list of links to builds that match the search
 As the requests package is not included within kv, you will need to either
 download this package yourself or reference the one included inside
 couchbase-cli.
+This is currently limited to searching for log patterns contained within
+one line of the logs, as the search checks line-by-line.
 """
 
 import argparse
-import json
 import requests
 import sys
 import time
@@ -31,17 +32,25 @@ argParser.add_argument('--job', '-j', type=str,
 argParser.add_argument('--search', '-s', type=str,
                        help='The string to search the logs for', required=True)
 argParser.add_argument('--build-no', '-b', type=int,
-                       help='The build number of cv job to check backwards '
-                            'from. 0 (default) fetches latest build number',
-                       default=0)
+                       help='The build number of cv job to check backwards from. '
+                            '0 (default) fetches latest build number', default=0)
 argParser.add_argument('--no-of-builds', '-n', type=int,
                        help='The number of builds to check back', default=100)
+argParser.add_argument('--format', '-f', default="plain", type=str,
+                       help="Select the format to print results. "
+                            "Available formats are: "
+                            "plain (default), log-line, jira")
 
 args = argParser.parse_args()
 job = 'job/' + args.job + '/'
 
+availableFormats = ["plain", "log-line", "jira"]
+outputFormat = args.format.lower()
+assert outputFormat in availableFormats, "%r format is not supported" % outputFormat
+
 consoleText = '/consoleText/'
-resultURLs = []
+resultURLs = {}
+failedBuildNums = []
 
 if args.build_no == 0:
     # need to fetch the latest build number
@@ -50,20 +59,54 @@ if args.build_no == 0:
     args.build_no = j['number']
 
 print("Searching for:", ('"' + args.search + '"'), "in console logs of job:",
-      args.job, "between build", args.build_no - (args.no_of_builds - 1), "and",
-      args.build_no, file=sys.stderr)
+      args.job, "between build", args.build_no - (args.no_of_builds - 1),
+      "and", args.build_no, file=sys.stderr)
 
 start_time = time.time()
 for i in range(0, args.no_of_builds):
+    lines = []
     print('\r >>> Current progress: {}   '.format(str(i)), end='',
     flush=True, file=sys.stderr)
     r = requests.get(serverURL + job + str(args.build_no-i) + consoleText)
-    result = r.text.find(args.search)
-    if result != -1:
-        resultURLs.append(serverURL + job + str(args.build_no-i) + '/console/')
+    if r.status_code != 200:
+        failedBuildNums.append(args.build_no-i)
+    for line in r.text.split('\n'):
+        result = line.find(args.search)
+        if result != -1:
+            lines.append(line)
+    if lines:
+        resultURLs[serverURL + job + str(args.build_no-i) + '/console/'] = lines
 
-print('\r Completed search in', (time.time() - start_time), 's',
-      file=sys.stderr)
+print('\r Completed search in', (time.time() - start_time), 's', file=sys.stderr)
+if failedBuildNums:
+    print("Failed log request on build(s) no:", failedBuildNums, file=sys.stderr)
 
-for url in resultURLs:
-    print(url)
+# Ensure above prints actually print before results (and not mangled inside results)
+sys.stderr.flush()
+
+if not resultURLs:
+    # Empty results, did not find any matches
+    print("No matches found")
+elif outputFormat == 'jira':
+    # Print in a JIRA format
+    print("{panel:title=Search for", ('"' + args.search + '"'),
+          "in console logs of job", args.job, "between build no",
+          args.build_no - (args.no_of_builds - 1), "and", args.build_no, '}')
+    for url in resultURLs:
+        print('[', url, ']', sep="")
+        print('{noformat}')
+        for line in resultURLs[url]:
+            print(line)
+        print('{noformat}')
+    print("{panel}")
+elif outputFormat == "log-line":
+    # Print findings with log line attached
+    for url in resultURLs:
+        print(url, end=" : ")
+        for line in resultURLs[url]:
+            print(line, end=", ")
+        print('\n', end="")
+else:  # outputFormat == "plain"
+    # Print findings normally
+    for url in resultURLs:
+        print(url)
