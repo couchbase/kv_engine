faa37755ada1032a42ce46e0b8fd2c97616321ca
Trond Norbye <trond.norbye@gmail.com>
2019-03-14 14:16:52 +0100

    Remove unused cmake checks from ep-engine
    Change-Id: Ia46260fc9b15cd4b1d9b5f8e0dc2f8937943d4f0
    Reviewed-on: http://review.couchbase.org/106193
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/src/config.cmake.h
+++ engines/ep/src/config.cmake.h
@@ -2,13 +2,6 @@
 
 /* Header files */
 #cmakedefine HAVE_ARPA_INET_H ${HAVE_ARPA_INET_H}
-#cmakedefine HAVE_NETDB_H ${HAVE_NETDB_H}
-#cmakedefine HAVE_NETINET_IN_H ${HAVE_NETINET_IN_H}
-#cmakedefine HAVE_NETINET_TCP_H ${HAVE_NETINET_TCP_H}
-#cmakedefine HAVE_POLL_H ${HAVE_POLL_H}
-#cmakedefine HAVE_SYS_SOCKET_H ${HAVE_SYS_SOCKET_H}
-#cmakedefine HAVE_SYS_TIME_H ${HAVE_SYS_TIME_H}
-#cmakedefine HAVE_UNISTD_H ${HAVE_UNISTD_H}
 
 /* various */
 #define VERSION "${EP_ENGINE_VERSION}"
