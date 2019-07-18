0e392761099b08a2e424ea05643dead92b891440
Trond Norbye <trond.norbye@gmail.com>
2019-07-18 13:59:42 +0200

    Remove unused include in rbac/privilege_database.cc
    Change-Id: Iaff379dc3edbd0598df989576221ba70f9302983
    Reviewed-on: http://review.couchbase.org/112148
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>

--- rbac/privilege_database.cc
+++ rbac/privilege_database.cc
@@ -19,7 +19,6 @@
 #include <folly/Synchronized.h>
 #include <nlohmann/json.hpp>
 #include <platform/dirutils.h>
-#include <platform/rwlock.h>
 #include <strings.h>
 #include <utilities/logtags.h>
 #include <atomic>
