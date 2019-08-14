f94fbdac31c731735e229a5198e6c5d9a538ae83
James Harrison <james.harrison@couchbase.com>
2019-08-14 16:12:31 +0100

    Prepare cli_auth_utils for use by cbvbucketctl
    Minor @wraps addition to support potential future signature inspection.
    
    Rename {max,min} to {max,min}_args to avoid shadowing builtin.
    
    Make it possible to exclude the allBuckets option if not appropriate.
    
    Adds some minor comments to document the decorator flow.
    
    Change-Id: I769434dc7d12ada583f14c5fc1783ff8918b5a5d
    Reviewed-on: http://review.couchbase.org/113343
    Tested-by: James Harrison <james.harrison@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/management/cli_auth_utils.py
+++ engines/ep/management/cli_auth_utils.py
@@ -9,33 +9,45 @@ import clitool
 import mc_bin_client
 import memcacheConstants
 
+from functools import wraps
+
 
 def cmd_decorator(f):
-    """Decorate a function with code to authenticate based on 1-3
-    additional arguments."""
+    """Decorate a function with code to authenticate based on
+    the following additional arguments passed by keyword:
 
+    bucketName
+    username
+    password
+    passwordFromStdin - if true, user will be prompted for password on stdin
+    """
+
+    @wraps(f)
     def g(*args, **kwargs):
+
+        # check arguments are suitable for wrapped func
         mc = args[0]
         spec = inspect.getargspec(f)
-        max = len(spec.args)
+        max_args = len(spec.args)
         defaults = len(spec.defaults) if spec.defaults else 0
-        min = max - defaults
+        min_args = max_args - defaults
 
-        if len(args) < min:
+        if len(args) < min_args:
             print(("Error: too few arguments - command "
                                   "expected a minimum of %s but was passed "
                                   "%s: %s"
-                                  % (min - 1, len(args) - 1, list(args[1:]))), file=sys.stderr)
+                                  % (min_args - 1, len(args) - 1, list(args[1:]))), file=sys.stderr)
             sys.exit(2)
 
         if spec.varargs is None:
-            if len(args) > max:
+            if len(args) > max_args:
                 print(("Error: too many arguments - command "
                                       "expected a maximum of %s but was passed "
                                       "%s: %s"
-                                      % (max - 1, len(args) - 1, list(args[1:]))), file=sys.stderr)
+                                      % (max_args - 1, len(args) - 1, list(args[1:]))), file=sys.stderr)
                 sys.exit(2)
 
+        # extract auth and bucket parameters (not passed to wrapped function)
         bucket = kwargs.pop('bucketName', None)
         username = kwargs.pop('username', None) or bucket
         cli_password = kwargs.pop('password', None)
@@ -46,7 +58,7 @@ def cmd_decorator(f):
 
         password = cli_password or stdin_password or env_password
 
-
+        # try to auth
         if username is not None or password is not None:
             bucket = bucket or 'default'
             username = username or bucket
@@ -58,11 +70,14 @@ def cmd_decorator(f):
                        .format(username, bucket))
                 sys.exit(1)
 
+        # HELO
         mc.enable_xerror()
         mc.enable_collections()
         mc.hello("{0} {1}".format(os.path.split(sys.argv[0])[1],
                                 os.getenv("EP_ENGINE_VERSION",
                                           "unknown version")))
+
+        # call function for one or all buckets
         try:
             if kwargs.pop('allBuckets', None):
                 buckets = mc.list_buckets()
@@ -79,17 +94,17 @@ def cmd_decorator(f):
                 f(*args, **kwargs)
         except mc_bin_client.ErrorEaccess:
             print("No access to bucket:{0} - permission denied "
-                   "or bucket does not exist.".format(bucket))
+                  "or bucket does not exist.".format(bucket))
             sys.exit(1)
 
     return g
 
 
-
-def get_authed_clitool(extraUsage=""):
+def get_authed_clitool(extraUsage="", allBuckets=True):
     c = clitool.CliTool(extraUsage)
 
-    c.addFlag('-a', 'allBuckets', 'iterate over all buckets')
+    if allBuckets:
+        c.addFlag('-a', 'allBuckets', 'iterate over all buckets')
     c.addOption('-b', 'bucketName', 'the bucket to get stats from (Default: default)')
     c.addOption('-u', 'username', 'the user as which to authenticate (Default: bucketName)')
     c.addOption('-p', 'password', 'the password for the bucket if one exists')
