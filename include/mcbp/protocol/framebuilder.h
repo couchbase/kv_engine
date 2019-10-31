fe7cc82f53c7e45aaf22dab5f97121ef0fbb06bb
Trond Norbye <trond.norbye@gmail.com>
2019-10-31 10:31:54 +0100

    Allow FrameBuilders to use a cb::const_char_buffer for backing store
    Change-Id: I7416e53f69adba683524687c63998a2e355e357e
    Reviewed-on: http://review.couchbase.org/117195
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- include/mcbp/protocol/framebuilder.h
+++ include/mcbp/protocol/framebuilder.h
@@ -139,6 +139,9 @@ public:
         moveAndInsert(existing, val, move_size);
         req->setExtlen(gsl::narrow<uint8_t>(val.size()));
     }
+    void setExtras(cb::const_char_buffer val) {
+        setExtras({reinterpret_cast<const uint8_t*>(val.data()), val.size()});
+    }
 
     /**
      * Insert/replace the Key section and move the value section to the new
@@ -229,6 +232,13 @@ protected:
  */
 class RequestBuilder : public FrameBuilder<Request> {
 public:
+    explicit RequestBuilder(cb::const_char_buffer backing,
+                            bool initialized = false)
+        : RequestBuilder({reinterpret_cast<uint8_t*>(
+                                  const_cast<char*>(backing.data())),
+                          backing.size()},
+                         initialized) {
+    }
     explicit RequestBuilder(cb::byte_buffer backing, bool initialized = false)
         : FrameBuilder<Request>(backing, initialized) {
     }
@@ -242,6 +252,13 @@ public:
  */
 class ResponseBuilder : public FrameBuilder<Response> {
 public:
+    explicit ResponseBuilder(cb::const_char_buffer backing,
+                             bool initialized = false)
+        : ResponseBuilder({reinterpret_cast<uint8_t*>(
+                                   const_cast<char*>(backing.data())),
+                           backing.size()},
+                          initialized) {
+    }
     explicit ResponseBuilder(cb::byte_buffer backing, bool initialized = false)
         : FrameBuilder<Response>(backing, initialized) {
     }
