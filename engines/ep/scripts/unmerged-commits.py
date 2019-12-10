33fb1ab2c9787f55555e5f7edea38807b3dbc371
Dave Rigby <daver@couchbase.com>
2019-12-10 10:56:12 +0000

    unmerged-commits.py: Add 6.5.0 branch
    Add 6.5.0 to the unmerged-commits script. This should be merged into
    mad-hatter; but no other branches should merge into 6.5.0 (i.e it has
    no upstream branches).
    
    Change-Id: I18d4c27c4941375f3c95528e0e3038ca029b9b3d
    Reviewed-on: http://review.couchbase.org/119664
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>

--- engines/ep/scripts/unmerged-commits.py
+++ engines/ep/scripts/unmerged-commits.py
@@ -17,26 +17,46 @@ class bcolors:
         WARNING = ''
         ENDC = ''
 
-# Branches to check for unmerged patches. Each toplevel element is a series
-# of branches (ordered by ancestory) which should be merged into each other.
-# i.e. the oldest supported branch to the newest, which is the order
-# patches should be merged.
-branches = (('couchbase/watson_ep',
-             'couchbase/spock'),
-            ('couchbase/watson_mc',
-             'couchbase/spock'),
-            ('couchbase/spock',
-             'couchbase/vulcan'),
-            ('couchbase/vulcan',
-             'couchbase/alice'),
-            ('couchbase/alice',
-             'couchbase/mad-hatter'),
-            ('couchbase/mad-hatter',
-             'couchbase/master'))
+# Sequences of branches to check for unmerged patches. Each toplevel
+# element is a series of branches (ordered by ancestory) which should
+# be merged into each other.  i.e. the oldest supported branch to the
+# newest, which is the order patches should be merged.
+#
+# There is a single sequence of branches for branches representing
+# whole release "trains" - for example vulcan is a train of (5.5.0,
+# 5.5.1, 5.5.2) and should be kept merged into alice (6.0.0, 6.0.1,
+# 6.0.2, ...), as new maintenance releases come along.
+#
+# However, we also have specific branches for a single release
+# (e.g. 6.5.0) are of limited lifespan - once 6.5.0 has shipped the
+# branch will not change and future fixes from say alice which need to
+# be included in 6.5.x should be merged into the release train branch
+# (e.g. mad-hatter).
+#
+# As such, there are multiple (currently two) sequence of branches -
+# one for the main release trains and (currently) one for 6.5.0 in
+# particular.
+sequences = (
+    # main release train sequence
+    {('couchbase/watson_ep',
+      'couchbase/spock'),
+     ('couchbase/watson_mc',
+      'couchbase/spock'),
+     ('couchbase/spock',
+      'couchbase/vulcan'),
+     ('couchbase/vulcan',
+      'couchbase/alice'),
+     ('couchbase/alice',
+      'couchbase/mad-hatter'),
+     ('couchbase/mad-hatter',
+      'couchbase/master')},
+    # 6.5.0 specific branch
+    {('couchbase/6.5.0',
+      'couchbase/mad-hatter')})
 
 total_unmerged = 0
-for series in branches:
-    for downstream, upstream in zip(series, series[1:]):
+for sequence in sequences:
+    for (downstream, upstream) in sequence:
         commits = subprocess.check_output(['git', 'cherry', '-v',
                                            upstream, downstream])
         count = len(commits.splitlines())
