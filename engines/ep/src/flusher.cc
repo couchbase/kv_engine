couchbase/kv_engine

commit 1f64b646719dacba8aa78b1101647a56ae94bbb8

MB-36744: Use VBReadyQueue in Flusher
The current method for flushing vBuckets is to iterate through them
by Vbid in order of priority. This can cause us to flush a vBucket with
newer mutations before a vBucket with older mutations. This can be
problematic with durability as we may require persistence of a prepare
and this could be delayed by newer writes which logically should not
be flushed first. Change the Flusher to use the VBReadyQueue to flush
vBuckets based on mutation age.

This give a small ~10% perf bump in the durability persist to majority
throughput test ycsb_workloada_4node_thr_durability3d_new.test. Old
runs were ~7500 ops/s and with this patch ~8300ops/s

The more important difference is tail latency of storage_age.
Storage_age is the stat that tracks time taken from VBucket::queueDirty
to the flusher visiting an Item. Stats from before:
    P50    - 77-155ms
    P99    - 819-851ms
    P99.9  - 1114-1179ms
    P99.99 - 1245-1310ms

Stats from after:
    P50    - 55-139ms
    P99    - 720-753ms
    P99.9  - 851-884ms
    P99.99 - 917-950ms

This shows a fairer method of scheduling the flusher which is important
for durability as Persist level prepares can only be satisfied after we
have flushed the given prepare.

Change-Id: I6e7053bc53349a7eff132a49b2313329739053b6
Reviewed-on: http://review.couchbase.org/117261
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

engines/ep/src/flusher.cc
@@ -206,31 +206,32 @@ bool Flusher::step(GlobalTask *task) {
         task->snooze(INT_MAX);
         return true;
 
-    case State::Running:
+    case State::Running: {
         // Start by putting ourselves back to sleep once step() completes.
         // If a new VB is notified (or a VB is re-notified after it is processed
         // in the loop below) then that will cause the task to be re-awoken.
         task->snooze(INT_MAX);
 
-        flushVB();
+        auto more = flushVB();
 
         if (_state == State::Running) {
             /// If there's still work to do for this shard, wake up the Flusher
             /// to run again.
-            const bool shouldWakeUp =
-                    !canSnooze() || (shard->highPriorityCount.load() > 0);
+            const bool shouldSnooze = hpVbs.empty() &&
+                                      shard->highPriorityCount.load() == 0 &&
+                                      !more;
 
             // Testing hook
             if (stepPreSnoozeHook) {
                 stepPreSnoozeHook();
             }
 
-            if (shouldWakeUp) {
+            if (!shouldSnooze) {
                 task->updateWaketime(std::chrono::steady_clock::now());
             }
         }
         return true;
-
+    }
     case State::Stopping:
         EP_LOG_DEBUG(
                 "Flusher::step: stopping flusher (write of all dirty items)");
@@ -250,26 +251,17 @@ bool Flusher::step(GlobalTask *task) {
 }
 
 void Flusher::completeFlush() {
-    while(!canSnooze()) {
-        flushVB();
+    // Flush all of our vBuckets
+    while (flushVB()) {
     }
 }
 
-void Flusher::flushVB(void) {
-    // If the low-priority vBucket queue is empty, see if there's any
-    // pending mutations - and if so re-populate the low pri queue.
-    if (lpVbs.empty()) {
-        if (hpVbs.empty()) {
-            doHighPriority = false;
-        }
-        bool inverse = true;
-        if (pendingMutation.compare_exchange_strong(inverse, false)) {
-            for (auto vbid : shard->getVBucketsSortedByState()) {
-                lpVbs.push(vbid);
-            }
-        }
+bool Flusher::flushVB() {
+    if (hpVbs.empty()) {
+        doHighPriority = false;
     }
 
+    // Search for any high priority vBuckets to flush.
     if (!doHighPriority && shard->highPriorityCount.load() > 0) {
         for (auto vbid : shard->getVBuckets()) {
             VBucketPtr vb = store->getVBucket(vbid);
@@ -283,25 +275,39 @@ void Flusher::flushVB(void) {
         }
     }
 
-    if (hpVbs.empty() && lpVbs.empty()) {
-        EP_LOG_DEBUG("Flusher::flushVB: Trying to flush but no vbuckets exist");
-        return;
-    } else if (!hpVbs.empty()) {
+    // Flush a high priority vBucket if applicable
+    if (!hpVbs.empty()) {
         Vbid vbid = hpVbs.front();
         hpVbs.pop();
         if (store->flushVBucket(vbid).first) {
             // More items still available, add vbid back to pending set.
             hpVbs.push(vbid);
         }
-    } else {
-        if (doHighPriority && --numHighPriority == 0) {
-            doHighPriority = false;
-        }
-        Vbid vbid = lpVbs.front();
-        lpVbs.pop();
-        if (store->flushVBucket(vbid).first) {
-            // More items still available, add vbid back to pending set.
-            lpVbs.push(vbid);
-        }
+
+        // Return false (don't re-wake) if the lpVbs is empty (i.e. nothing to
+        // do on our next iteration). If another vBucket joins this queue after
+        // then it will wake the task.
+        return !lpVbs.empty();
     }
+
+    // Below here we are flushing low priority vBuckets
+    if (doHighPriority && --numHighPriority == 0) {
+        // Now we flush a number of low priority vBuckets equal to the number of
+        // high priority vBuckets that we just flushed (or until we run out)
+        doHighPriority = false;
+    }
+
+    Vbid vbid;
+    if (!lpVbs.popFront(vbid)) {
+        // Return no more so we don't rewake the task
+        return false;
+    }
+
+    if (store->flushVBucket(vbid).first) {
+        // More items still available, add vbid back to pending set.
+        lpVbs.pushUnique(vbid);
+    }
+
+    // Return more (as we may have low priority vBuckets to flush)
+    return true;
 }

engines/ep/src/ep_bucket.cc
@@ -203,7 +203,7 @@ public:
 
     void callback(Vbid& vb) override {
         if (shard->getBucket(vb)) {
-            shard->getFlusher()->notifyFlushEvent();
+            shard->getFlusher()->notifyFlushEvent(vb);
         }
     }
 

engines/ep/src/flusher.h
@@ -18,6 +18,7 @@
 
 #include "executorthread.h"
 #include "utility.h"
+#include "vb_ready_queue.h"
 
 #include <memcached/vbucket.h>
 
@@ -49,14 +50,13 @@ public:
 
     const char * stateName() const;
 
-    void notifyFlushEvent(void) {
-        // By setting pendingMutation to true we are guaranteeing that the given
-        // flusher will iterate the entire vbuckets under its shard from the
-        // begining and flush for all mutations
-        bool disable = false;
-        if (pendingMutation.compare_exchange_strong(disable, true)) {
-            wake();
+    void notifyFlushEvent(Vbid vbid) {
+        if (!lpVbs.pushUnique(vbid)) {
+            // Something is already in the queue, no need to wake the flusher
+            return;
         }
+
+        wake();
     }
     void setTaskId(size_t newId) { taskId = newId; }
 
@@ -76,16 +76,17 @@ private:
 
     bool transitionState(State to);
     bool validTransition(State to) const;
-    void flushVB();
+
+    /**
+     * Flush a single vBucket
+     * @return true if there is more work to do
+     */
+    bool flushVB();
     void completeFlush();
     void initialize();
     void schedule_UNLOCKED();
 
     const char* stateName(State st) const;
-
-    bool canSnooze(void) {
-        return lpVbs.empty() && hpVbs.empty() && !pendingMutation.load();
-    }
 
     EPBucket* store;
     std::atomic<State> _state;
@@ -97,7 +98,7 @@ private:
 
     std::atomic<bool> forceShutdownReceived;
     std::queue<Vbid> hpVbs;
-    std::queue<Vbid> lpVbs;
+    VBReadyQueue lpVbs;
     bool doHighPriority;
     size_t numHighPriority;
     std::atomic<bool> pendingMutation;

engines/ep/src/kv_bucket.cc
@@ -2553,7 +2553,7 @@ std::ostream& operator<<(std::ostream& os, const KVBucket::Position& pos) {
 void KVBucket::notifyFlusher(const Vbid vbid) {
     KVShard* shard = vbMap.getShardByVbId(vbid);
     if (shard) {
-        shard->getFlusher()->notifyFlushEvent();
+        shard->getFlusher()->notifyFlushEvent(vbid);
     } else {
         throw std::logic_error("KVBucket::notifyFlusher() : shard null for " +
                                vbid.to_string());

engines/ep/tests/module_tests/flusher_test.cc
@@ -69,13 +69,12 @@ TEST_F(FlusherTest, MissingWakeupBeforeSnooze) {
     // Setup: Mark a vBucket as active, and notify a pending mutation on a
     // vBucket.
     engine->getKVBucket()->setVBucketState(Vbid(0), vbucket_state_active);
-    flusher->notifyFlushEvent();
 
     // Setup: Set the testing hook in Flusher::step() to trigger another notify
     // event just before we have decided to snooze (but before snooze() is
     // actually called).
     flusher->stepPreSnoozeHook = [this]() {
-        this->flusher->notifyFlushEvent();
+        this->flusher->notifyFlushEvent(Vbid(0));
     };
 
     // Test: Run the flusher task. This should flush the oustanding setVBstate

commit 2a368c39481ff4d42c6f755bd7d185b9a57554ca

MB-37294: Avoid starvation of low-pri VBs in Flusher::flushVB()
+Summary+

Commit 1f64b646719dacba8aa78b1101647a56ae94bbb8 modified the Flusher
to use VBReadyQueue to manage the low-priority VBuckets waiting to be
flushed. However, this change introduced a starvation issue for
low-priority vBuckets if there are outstanding high-priority vBuckets
which are still awaiting seqno_persisence.

+Details+

Consider the following scenario:

1. At least one SEQNO_PERSISTENCE request is outstanding -
   VBucket::hpVBReqs is non-empty for at least one vBucket.

2. This vbucket does not yet have the seqno requested - for example
   it's a replica vBucket and memory usage is high and replication to
   it has been throttled.
3. At least one other (low priority) vBucket is awaiting flushing.

The consequence is that the low-priority vBucket will never get
flushed (not until the high priority vBucket completes it's
seqno_persistence). This can lead to livelock - if we actually _could_
flush the low-priority vBucket(s) then that would allow
CheckpointMemory to be freed (expelling / removing closed,
unreferenced checkpoints).

The actual problem is the logic in Flusher::flushVB(). This is a
(needlessly?) complex function, but the high level logic involves
switching between two modes (Flusher::doHighPriority):

A) While there are no outstanding SEQNO_PERSISTENCE vBuckets for this
   shard, flush the next vBucket in lpVbs (low priority). Reschedule
   (return true) if there's any more low-priority VBs to flush.

B) If there are outstanding SEQNO_PERSISTENCE vBuckets for this shard:
   1) If hpVBs (high priority vbs) is empty, populate the hpVBs queue
      with all vBuckets with outstanding SEQNO_PERSISTENCE requests.

   2) Flush the next vb from hpVBs, Reschdule (return true) if there
      are any more items in hpVBs.

   3) Once all outstanding SEQNO_PERSISTENCE vBuckets have been
      flushed, allow an equal number of low-priority vBuckets to be
      flushed before retrying the outstanding SEQNO_PERSISTENCE vBuckets.

Step B.3 is crucial to avoid starvation of the low-priority queue -
without this step, then a single slow VBucket with an outstanding
SEQNO_PERSISTENCE request can prevent _all_ other vBuckets in the
shard from being flushed.

However, when the aforementioned path to use VBReadyQueue was
introduced, it inadvertantly prevented step B.3 from actually
occurring.

This is because when re-entering Flusher::flushVB after flushing the
last high priority VB from hpVBs (i.e. after step B.2), we incorrectly
only check if hpVbs.empty() is true, and if so set doHighPriority to
false - i.e. going back to mode A.

The fix is restore the logic from before the patch - only switch back
to mode A (doHighPriority=false) when both low and high priority VB
queues are empty.

Change-Id: Ic8438f33d8905ab2b0f13ff6c6d8b55eb97b8534
Fixes: 1f64b646719d ("MB-36744: Use VBReadyQueue in Flusher")

engines/ep/src/flusher.cc
@@ -257,7 +257,7 @@ void Flusher::completeFlush() {
 }
 
 bool Flusher::flushVB() {
-    if (hpVbs.empty()) {
+    if (lpVbs.empty() && hpVbs.empty()) {
         doHighPriority = false;
     }
 