3a956004c8f3263a952869817435c35fadb2fee5
Trond Norbye <trond.norbye@gmail.com>
2020-01-07 13:17:20 +0100

    Cleanup: Remove TapConnect test
    This is already tested in testapp_not_supported.cc
    
    Change-Id: Ia4cf259467c2cee4f3fd738eea787e93a66a1577
    Reviewed-on: http://review.couchbase.org/120081
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>

--- tests/testapp/testapp_tests.cc
+++ tests/testapp/testapp_tests.cc
@@ -480,21 +480,6 @@ TEST_P(McdTestappTest, Config_ValidateInvalidJSON) {
                                   Status::Einval);
 }
 
-// Test to ensure that if a Tap Connect is requested we respond with
-// Status::NotSupported
-TEST_P(McdTestappTest, TapConnect) {
-    std::vector<uint8_t> blob(1024);
-    RequestBuilder builder({blob.data(), blob.size()});
-    builder.setMagic(cb::mcbp::Magic::ClientRequest);
-    builder.setOpcode(ClientOpcode::TapConnect_Unsupported);
-    builder.setOpaque(0xdeadbeef);
-    safe_send(builder.getFrame()->getFrame());
-    safe_recv_packet(blob);
-    mcbp_validate_response_header(*reinterpret_cast<Response*>(blob.data()),
-                                  ClientOpcode::TapConnect_Unsupported,
-                                  Status::NotSupported);
-}
-
 static uint64_t get_session_ctrl_token() {
     Request request = {};
     request.setMagic(Magic::ClientRequest);
