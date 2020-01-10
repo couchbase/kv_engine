efc63dad274801f0dcdb6e3cafb09a3511a8add7
Trond Norbye <trond.norbye@gmail.com>
2020-01-10 06:51:28 +0100

    MB-37449: Remove support for mcd cmd line option
    -t, -R and -v may all be set through the configuration
    file.
    
    Change-Id: I7763f439c315bc713231aa9bc17925c470967b04
    Reviewed-on: http://review.couchbase.org/120201
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Daniel Owen <owend@couchbase.com>

--- daemon/cmdline.cc
+++ daemon/cmdline.cc
@@ -21,49 +21,26 @@ static void usage() {
 void parse_arguments(int argc, char** argv) {
     struct option long_options[] = {
             {"config", required_argument, nullptr, 'C'},
-            {"threads", required_argument, nullptr, 't'},
-            {"verbosity", no_argument, nullptr, 'v'},
-            {"requests_per_event", required_argument, nullptr, 'R'},
             {"help", no_argument, nullptr, 'h'},
             {nullptr, 0, nullptr, 0}};
 
     int cmd;
 
-    int verbosity = 0;
-    int threads = 0;
-    int requests = 0;
-
     // Tell getopt to restart the parsing (if we used getopt before calling
     // this method)
     optind = 1;
 
-    while ((cmd = getopt_long(argc, argv, "C:t:vR:h", long_options, nullptr)) !=
+    while ((cmd = getopt_long(argc, argv, "C:h", long_options, nullptr)) !=
            EOF) {
         switch (cmd) {
         case 'C':
             config_file = optarg;
             break;
 
-        case 't':
-            try {
-                threads = std::stoi(optarg);
-            } catch (const std::exception& e) {
-                LOG_ERROR(R"(Failed to parse "{}": {})", optarg, e.what());
-            }
-            break;
-        case 'v':
-            ++verbosity;
-            break;
-        case 'R':
-            try {
-                requests = std::stoi(optarg);
-            } catch (const std::exception& e) {
-                LOG_ERROR(R"(Failed to parse "{}": {})", optarg, e.what());
-            }
-            break;
         case 'h':
             usage();
             std::exit(EXIT_SUCCESS);
+
         default:
             usage();
             std::exit(EXIT_FAILURE);
@@ -77,19 +54,6 @@ void parse_arguments(int argc, char** argv) {
     }
 
     load_config_file(config_file.c_str(), Settings::instance());
-
-    if (verbosity > 0) {
-        Settings::instance().setVerbose(verbosity);
-    }
-
-    if (threads > 0) {
-        Settings::instance().setNumWorkerThreads(threads);
-    }
-
-    if (requests > 0) {
-        Settings::instance().setRequestsPerEventNotification(
-                requests, EventPriority::Default);
-    }
 }
 
 const std::string& get_config_file() {
