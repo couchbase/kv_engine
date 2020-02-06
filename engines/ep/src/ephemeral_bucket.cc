couchbase/kv_engine

commit b807b8a01d01ee2ff1b8b06b0e4ca54e7e128b1a

Fix clang analyzer warnings in our ephemeral HPR task and opentracing
Fix clang analyzer warnings in NotifyHighPriorityReqTask::run() of
ephemeral_bucket.cc and OpenTracingThread::run() of opentracing.cc
due to a std::map and std::vector respectively not being left in a
defined stated after a std::move().

Change-Id: I87d1cd36d898f3801bd186872627dad4109d8e76
Reviewed-on: http://review.couchbase.org/121617
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Richard de Mellow <richard.demellow@couchbase.com>

engines/ep/src/ephemeral_bucket.cc
@@ -340,7 +340,7 @@ bool EphemeralBucket::NotifyHighPriorityReqTask::run() {
         /* It is necessary that the toNotifyLock is not held while
            actually notifying. */
         std::lock_guard<std::mutex> lg(toNotifyLock);
-        notifyQ = std::move(toNotify);
+        std::swap(notifyQ, toNotify);
     }
 
     for (auto& notify : notifyQ) {
@@ -358,7 +358,7 @@ bool EphemeralBucket::NotifyHighPriorityReqTask::run() {
     bool scheduleSoon = false;
     {
         std::lock_guard<std::mutex> lg(toNotifyLock);
-        if (toNotify.size()) {
+        if (!notifyQ.empty()) {
             scheduleSoon = true;
         }
     }

daemon/opentracing.cc
@@ -110,7 +110,8 @@ void OpenTracingThread::run() {
 
         // move the entries over to another vector so the clients don't have
         // to wait while I'm working
-        auto entries = std::move(contexts);
+        std::vector<CookieTraceContext> entries{};
+        std::swap(contexts, entries);
 
         // Release the lock to the internal variables while I process the
         // batch of trace elements

commit d09c34285ffaf849b25063d7ee8a452e0e6d65de

Fix incorrect change from toNotify to notifyQ
Fix incorrect change from toNotify to notifyQ in
NotifyHighPriorityReqTask::run(), which would cause us to change how
frequently we run NotifyHighPriorityReqTask.

Bug introduced by patch b807b8a01d01ee2ff1b8b06b0e4ca54e7e128b1a.

Change-Id: Iae8242c2af6bb9ba2a7044b4db7785a999c9c83f
Reviewed-on: http://review.couchbase.org/121745
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

engines/ep/src/ephemeral_bucket.cc
@@ -358,7 +358,7 @@ bool EphemeralBucket::NotifyHighPriorityReqTask::run() {
     bool scheduleSoon = false;
     {
         std::lock_guard<std::mutex> lg(toNotifyLock);
-        if (!notifyQ.empty()) {
+        if (!toNotify.empty()) {
             scheduleSoon = true;
         }
     }