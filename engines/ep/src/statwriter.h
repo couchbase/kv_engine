dc35629e512a6c1ebeff6e981792152509a7fb0f
James Harrison <james.harrison@couchbase.com>
2020-03-10 16:12:31 +0000

    MB-38560: Modernise statwriter.h
    Includes using std::string_view for stat keys, using fmtlib over string
    concatenation in some cases, and structured bindings to unpack structs.
    
    Change-Id: I8ae55a423ebc44790657458da67666d01014cdf2
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/124595
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- engines/ep/src/statwriter.h
+++ engines/ep/src/statwriter.h
@@ -21,6 +21,8 @@
 
 #include <memcached/engine_common.h>
 #include <platform/histogram.h>
+#include <spdlog/fmt/fmt.h>
+
 #include <atomic>
 #include <optional>
 #include <sstream>
@@ -30,6 +32,35 @@
 using namespace std::string_view_literals;
 
 class EventuallyPersistentEngine;
+
+/**
+ * Helper method to get a Histogram bucket lower bound as a uint64_t.
+ *
+ * Histogram can be instantiated with types not immediately
+ * convertible to uint64_t (e.g., a std::chrono::duration);
+ * this helper avoids duplicating code to handle different
+ * instantiations.
+ */
+template <typename T, template <class> class Limits>
+uint64_t getBucketMin(const typename Histogram<T, Limits>::value_type& bin) {
+    return bin->start();
+}
+
+inline uint64_t getBucketMin(const MicrosecondHistogram::value_type& bin) {
+    return bin->start().count();
+}
+
+/**
+ * Helper method to get a Histogram bucket upper bound as a uint64_t.
+ */
+template <typename T, template <class> class Limits>
+uint64_t getBucketMax(const typename Histogram<T, Limits>::value_type& bin) {
+    return bin->end();
+}
+
+inline uint64_t getBucketMax(const MicrosecondHistogram::value_type& bin) {
+    return bin->end().count();
+}
 
 template <typename T>
 void add_casted_stat(std::string_view k,
@@ -56,126 +87,93 @@ void add_casted_stat(std::string_view k,
     add_casted_stat(k, v.load(), add_stat, cookie);
 }
 
+/**
+ * Convert a histogram into add stat calls for each bucket and for the mean.
+ */
 template <typename T>
-inline void add_casted_histo_stat(const char* k,
+inline void add_casted_histo_stat(std::string_view k,
                                   const T& v,
                                   const AddStatFn& add_stat,
                                   const void* cookie) {
     if (v.getValueCount() > 0) {
-        std::string meanKey(k);
-        meanKey += "_mean";
-        add_casted_stat(
-                meanKey.c_str(), std::round(v.getMean()), add_stat, cookie);
+        fmt::memory_buffer buf;
+        format_to(buf, "{}_mean", k);
+        add_casted_stat({buf.data(), buf.size()},
+                        std::round(v.getMean()),
+                        add_stat,
+                        cookie);
 
         HdrHistogram::Iterator iter{v.getHistogramsIterator()};
         while (auto result = v.getNextBucketLowHighAndCount(iter)) {
-            if (std::get<2>(*result) > 0) {
-                std::string newKey(k);
-                newKey += "_" + std::to_string(std::get<0>(*result)) + "," +
-                          std::to_string(std::get<1>(*result));
+            auto [lower, upper, count] = *result;
+            if (count > 0) {
+                buf.resize(0);
+                format_to(buf, "{}_{},{}", k, lower, upper);
                 add_casted_stat(
-                        newKey.c_str(), std::get<2>(*result), add_stat, cookie);
+                        {buf.data(), buf.size()}, count, add_stat, cookie);
             }
         }
     }
 }
 
-inline void add_casted_stat(const char* k,
+inline void add_casted_stat(std::string_view k,
                             const HdrHistogram& v,
                             const AddStatFn& add_stat,
                             const void* cookie) {
     add_casted_histo_stat<HdrHistogram>(k, v, add_stat, cookie);
 }
 
-inline void add_casted_stat(const char* k,
+inline void add_casted_stat(std::string_view k,
                             const Hdr1sfMicroSecHistogram& v,
                             const AddStatFn& add_stat,
                             const void* cookie) {
     add_casted_histo_stat<Hdr1sfMicroSecHistogram>(k, v, add_stat, cookie);
 }
 
-inline void add_casted_stat(const char* k,
+inline void add_casted_stat(std::string_view k,
                             const Hdr2sfMicroSecHistogram& v,
                             const AddStatFn& add_stat,
                             const void* cookie) {
     add_casted_histo_stat<Hdr2sfMicroSecHistogram>(k, v, add_stat, cookie);
 }
 
-inline void add_casted_stat(const char* k,
+inline void add_casted_stat(std::string_view k,
                             const Hdr1sfInt32Histogram& v,
                             const AddStatFn& add_stat,
                             const void* cookie) {
     add_casted_histo_stat<Hdr1sfInt32Histogram>(k, v, add_stat, cookie);
 }
 
-inline void add_casted_stat(const char* k,
+inline void add_casted_stat(std::string_view k,
                             const HdrUint8Histogram& v,
                             const AddStatFn& add_stat,
                             const void* cookie) {
     add_casted_histo_stat<HdrUint8Histogram>(k, v, add_stat, cookie);
 }
 
-/// @cond DETAILS
-/**
- * Convert a histogram into a bunch of calls to add stats.
- */
 template <typename T, template <class> class Limits>
-struct histo_stat_adder {
-    histo_stat_adder(const char* k, const AddStatFn& a, const void* c)
-        : prefix(k), add_stat(a), cookie(c) {
-    }
-
-    void operator()(const std::unique_ptr<HistogramBin<T, Limits>>& b) {
-        std::stringstream ss;
-        ss << prefix << "_" << b->start() << "," << b->end();
-        add_casted_stat(ss.str().c_str(), b->count(), add_stat, cookie);
-    }
-    const char *prefix;
-    AddStatFn add_stat;
-    const void *cookie;
-};
-
-// Specialization for UnsignedMicroseconds - needs count() calling to get
-// a raw integer which can be printed.
-template <>
-inline void histo_stat_adder<UnsignedMicroseconds, cb::duration_limits>::
-operator()(const MicrosecondHistogram::value_type& b) {
-    std::stringstream ss;
-    ss << prefix << "_" << b->start().count() << "," << b->end().count();
-
-    add_casted_stat(ss.str().c_str(), b->count(), add_stat, cookie);
-}
-/// @endcond
-
-template <typename T, template <class> class Limits>
-void add_casted_stat(const char* k,
-                     const Histogram<T, Limits>& v,
+void add_casted_stat(std::string_view k,
+                     const Histogram<T, Limits>& hist,
                      const AddStatFn& add_stat,
                      const void* cookie) {
-    histo_stat_adder<T, Limits> a(k, add_stat, cookie);
-    std::for_each(v.begin(), v.end(), a);
+    fmt::memory_buffer buf;
+    for (const auto& bin : hist) {
+        auto lower = getBucketMin(bin);
+        auto upper = getBucketMax(bin);
+        auto count = bin->count();
+        buf.resize(0);
+        format_to(buf, "{}_{},{}", k, lower, upper);
+        add_casted_stat({buf.data(), buf.size()}, count, add_stat, cookie);
+    }
 }
 
 template <typename P, typename T>
 void add_prefixed_stat(P prefix,
-                       const char* nm,
-                       T val,
+                       std::string_view name,
+                       const T& val,
                        const AddStatFn& add_stat,
                        const void* cookie) {
-    std::stringstream name;
-    name << prefix << ":" << nm;
-
-    add_casted_stat(name.str().c_str(), val, add_stat, cookie);
-}
-
-template <typename P, typename T, template <class> class Limits>
-void add_prefixed_stat(P prefix,
-                       const char* nm,
-                       Histogram<T, Limits>& val,
-                       const AddStatFn& add_stat,
-                       const void* cookie) {
-    std::stringstream name;
-    name << prefix << ":" << nm;
-
-    add_casted_stat(name.str().c_str(), val, add_stat, cookie);
+    fmt::memory_buffer buf;
+    format_to(buf, "{}:{}", prefix, name);
+    add_casted_stat({buf.data(), buf.size()}, val, add_stat, cookie);
 }
