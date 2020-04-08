2a7913f81eeb8326775da00c74c05cd0e5e60bc5
Trond Norbye <trond.norbye@gmail.com>
2020-04-08 09:32:02 +0200

    MB-38656: Remove "empty" test
    We don't upgrade any MetaData information as part of compaction
    anymore (compaction pass a NIL pointer to the edit docinfo
    hook).
    
    Change-Id: I73022646e99b4d54e3039adf3c39087c3f57f8da
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/125211
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- engines/ep/tests/module_tests/kvstore_test.cc
+++ engines/ep/tests/module_tests/kvstore_test.cc
@@ -1789,67 +1789,6 @@ TEST_F(CouchstoreTest, testV2WriteRead) {
     gc.callback(gv);
 }
 
-static int testCompactionUpgradeHook(DocInfo** info, const sized_buf* item) {
-    // Examine the metadata of the doc, we expect that the first compaction
-    // upgraded us to V1
-    EXPECT_EQ(MetaDataFactory::createMetaData((*info)->rev_meta)
-                      ->getVersionInitialisedFrom(),
-              MetaData::Version::V1);
-    return 0;
-}
-
-TEST_F(CouchstoreTest, testV2CompactionUpgrade) {
-    // Ensure CAS, exptime and flags are set to something.
-    auto datatype = PROTOCOL_BINARY_DATATYPE_JSON; // lies, but non-zero
-    StoredDocKey key = makeStoredDocKey("key");
-    queued_item item(std::make_unique<Item>(key,
-                                            0x01020304 /*flags*/,
-                                            0xaa00bb11, /*expiry*/
-                                            "value",
-                                            5,
-                                            datatype,
-                                            0xf00fcafe11225566ull));
-
-    EXPECT_NE(0, datatype); // make sure we writing non-zero values
-
-    // Write an item with forced (valid) V2 meta
-    MockCouchRequest::MetaData meta;
-    meta.cas = 0xf00fcafe11225566ull;
-    meta.expiry = 0xaa00bb11;
-    meta.flags = 0x01020304;
-    meta.ext1 = FLEX_META_CODE;
-    meta.ext2 = datatype;
-    meta.legacyDeleted = 1;
-
-    kvstore->begin(std::make_unique<TransactionContext>(vbid));
-    auto* request = kvstore->setAndReturnRequest(item);
-
-    // Force the meta to be V2
-    request->writeMetaData(meta, MockCouchRequest::MetaData::sizeofV2);
-
-    // Commit it
-    kvstore->commit(flush);
-
-    CompactionConfig config;
-    compaction_ctx cctx(config, 0);
-
-    cctx.expiryCallback = std::make_shared<ExpiryCallback>();
-    EXPECT_TRUE(kvstore->compactDB(&cctx));
-
-    // Now use the test dhook
-    EXPECT_TRUE(kvstore->compactDBInternal(&cctx, testCompactionUpgradeHook));
-
-    // Read back successful, the extra byte will of been dropped.
-    MockedGetCallback<GetValue> gc;
-    EXPECT_CALL(gc, status(ENGINE_SUCCESS));
-    EXPECT_CALL(gc, cas(htonll(0xf00fcafe11225566ull)));
-    EXPECT_CALL(gc, expTime(htonl(0xaa00bb11)));
-    EXPECT_CALL(gc, flags(0x01020304));
-    EXPECT_CALL(gc, datatype(protocol_binary_datatype_t(meta.ext2)));
-    GetValue gv = kvstore->get(DiskDocKey{key}, Vbid(0));
-    gc.callback(gv);
-}
-
 class CouchKVStoreMetaData : public ::testing::Test {
 };
 
