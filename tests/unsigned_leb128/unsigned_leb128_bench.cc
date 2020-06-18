2c0e4ea23c7881f24534183d8f7cb09398f6e9d5
Jim Walker <jim@couchbase.com>
2020-06-18 15:49:36 +0100

    MB-39594: Add a benchmark for leb128 decoding
    Add code which runs the leb128<uint32> decoder with gbench.
    
    Current benchmark values from developer system:
    
    > ./memcached_unsigned_leb128_bench
    Run on (12 X 2600 MHz CPU s)
    2020-06-22 16:10:03
    -------------------------------------------------------------------------------
    Benchmark                                        Time           CPU Iterations
    -------------------------------------------------------------------------------
    bench_unsigned_leb128_decode/1/0                29 ns         29 ns   24847454
    bench_unsigned_leb128_decode/2/0                31 ns         31 ns   23322450
    bench_unsigned_leb128_decode/3/0                32 ns         32 ns   21737105
    bench_unsigned_leb128_decode/4/0                35 ns         35 ns   19941259
    bench_unsigned_leb128_decode/5/0                37 ns         37 ns   18591606
    bench_unsigned_leb128_decodeNoThrow/1/0         25 ns         25 ns   28532415
    bench_unsigned_leb128_decodeNoThrow/2/0         26 ns         26 ns   26174878
    bench_unsigned_leb128_decodeNoThrow/3/0         29 ns         29 ns   23662558
    bench_unsigned_leb128_decodeNoThrow/4/0         31 ns         31 ns   21680687
    bench_unsigned_leb128_decodeNoThrow/5/0         34 ns         34 ns   20061790
    
    Change-Id: I0d4e1e187c1ac3784a13a0b6b7f14d059203efe3
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/130892
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Jim Walker <jim@couchbase.com>

---
+++ tests/unsigned_leb128/unsigned_leb128_bench.cc
@@ -0,0 +1,121 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2020 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#include <mcbp/protocol/unsigned_leb128.h>
+#include <array>
+
+#include <benchmark/benchmark.h>
+
+// Create input for the test, a leb128 prefixed 'key'
+static std::vector<uint8_t> makeLebPrefixedBuffer(uint32_t prefix,
+                                                  size_t keylen) {
+    cb::mcbp::unsigned_leb128<uint32_t> leb(prefix);
+    std::vector<uint8_t> buffer;
+    for (auto c : leb) {
+        buffer.push_back(c);
+    }
+    for (size_t ii = 0; ii < keylen; ii++) {
+        buffer.push_back('k');
+    }
+    return buffer;
+}
+
+// For the given size return the range that can be encoded in that size
+std::pair<uint32_t, uint32_t> getTestRange(size_t encodedBytes) {
+    switch (encodedBytes) {
+    case 1:
+        return {0, 127};
+    case 2:
+        return {128, 16383};
+    case 3:
+        return {16384, 2097151};
+    case 4:
+        return {2097152, 268435455};
+    case 5:
+        return {268435456, 4294967295};
+    default:
+        throw std::invalid_argument("getTestRange invalid argument");
+    }
+}
+
+const size_t numberOfInputs = 1000;
+
+static void bench_unsigned_leb128_decode(benchmark::State& state) {
+    auto range = getTestRange(state.range(0));
+    std::array<std::vector<uint8_t>, numberOfInputs> buffers;
+    size_t value = range.first;
+    for (auto& b : buffers) {
+        b = makeLebPrefixedBuffer(value, state.range(1));
+        if (value == range.second) {
+            value = range.first;
+        } else {
+            value++;
+        }
+    }
+
+    auto itr = buffers.begin();
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(
+                cb::mcbp::decode_unsigned_leb128<uint32_t>({*itr}));
+        itr++;
+        if (itr == buffers.end()) {
+            itr = buffers.begin();
+        }
+    }
+}
+
+static void bench_unsigned_leb128_decodeNoThrow(benchmark::State& state) {
+    auto range = getTestRange(state.range(0));
+    std::array<std::vector<uint8_t>, numberOfInputs> buffers;
+    size_t value = range.first;
+    for (auto& b : buffers) {
+        b = makeLebPrefixedBuffer(value, state.range(1));
+        if (value == range.second) {
+            value = range.first;
+        } else {
+            value++;
+        }
+    }
+
+    auto itr = buffers.begin();
+    while (state.KeepRunning()) {
+        benchmark::DoNotOptimize(cb::mcbp::decode_unsigned_leb128<uint32_t>(
+                {*itr}, cb::mcbp::Leb128NoThrow{}));
+        itr++;
+        if (itr == buffers.end()) {
+            itr = buffers.begin();
+        }
+    }
+}
+
+static void generateBenchmarkArguments(benchmark::internal::Benchmark* b) {
+    // Test inputs.
+    std::array<int, 5> lebSizes = {{1, 2, 3, 4, 5}};
+    for (auto size : lebSizes) {
+        // Second input is the 'keylen', how many bytes are in the buffer after
+        // the leb128 encoded integer. For now keep the second argument fixed as
+        // 0 as it doesn't affect the current decode - we may in the future want
+        // to change the key len
+        b->Args({size, 0});
+    }
+}
+
+BENCHMARK(bench_unsigned_leb128_decode)->Apply(generateBenchmarkArguments);
+BENCHMARK(bench_unsigned_leb128_decodeNoThrow)
+        ->Apply(generateBenchmarkArguments);
+
+BENCHMARK_MAIN();
