986503b6910a24ef98f21faffa22520d2daef883
James Harrison <james.harrison@couchbase.com>
2020-06-23 11:00:47 +0100

    MB-40081: Revert "MB-37159: Enable DelVBucketConcurrentOperationsTest test"
    This reverts commit 78bdb85fdd1b1a45eb1975c0f2734fb7ad68915c.
    
    The DelVBucketConcurrentOperationsTest test intermittently fails in
    KV-Engine CV. This revert disables the test under magma only.
    
    Change-Id: I3bc01477f15bb9526fd614127e53bae2f4902871
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/131170
    Tested-by: James Harrison <james.harrison@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- engines/ep/tests/module_tests/kvstore_test.cc
+++ engines/ep/tests/module_tests/kvstore_test.cc
@@ -2090,6 +2090,11 @@ TEST_P(KVStoreParamTest, TestDataStoredInTheRightVBucket) {
 // Expect ThreadSanitizer to pick this.
 // Rocks has race condition issues
 TEST_P(KVStoreParamTestSkipRocks, DelVBucketConcurrentOperationsTest) {
+    // MB-37159: Skipping for magma as this has been seen to timeout
+    if (kvstoreConfig->getBackend() == "magma") {
+        return;
+    }
+
     std::atomic<bool> stop{false};
     bool okToDelete{false};
     uint32_t deletes{0};
