couchbase/kv_engine

commit 4d160d1902afbb25678442480fe575d291984886

MB-23086: Disassociate ExecutorThread::run from any engine
The purpose of this commit is to ensure that the ExecutorThread doesn't
use a deleted engine. In this commit, the use of a deleted engine is
inert because it's just a pointer that is never dereferenced, however
that will change when ObjectRegistry needs the engine's arena.

To achieve this change the ExecutorThread::run so that is explicitly
allocates to no engine (using NonBucketAllocationGuard). Thus for the
majority of the run loop, ObjectRegistry stores null and only for a
small number of places should the thread place an engine in
ObjectRegistry.

1) When a task is invoked.

Switching to the engine is now done by using a new GlobalTask::execute
method which uses a BucketAllocationGuard to switch to the task's
engine. The execute method is now what calls the virtual run() method.

2) When a task is deleted.

In general tasks are deleted (actually destructed and deallocated) in
two places.

a) Within the ExecutorThread by calling ExecutorPool::cancel with
   eraseTask:true.
b) Within ~EventuallyPersistentEngine as the engine takes sole ownership
   of its tasks.

This commit changes case a) by using the BucketAllocationGuard around
the call to shared_ptr<GlobalTask>::reset(). Given the destruction of
an engine cannot occur until all tasks are destructed, it is safe to
switch to the engine before the task.reset(), after the reset the engine
can be deleted, but is no longer dereferenced.

Change-Id: Ied2ef3cb0adabe8634a3829260a4fc7562eedad5
Reviewed-on: http://review.couchbase.org/108349
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Jim Walker <jim@couchbase.com>

engines/ep/src/executorpool.cc
@@ -244,7 +244,6 @@ TaskQueue *ExecutorPool::_nextTask(ExecutorThread &t, uint8_t tick) {
 }
 
 TaskQueue *ExecutorPool::nextTask(ExecutorThread &t, uint8_t tick) {
-    NonBucketAllocationGuard guard;
     TaskQueue *tq = _nextTask(t, tick);
     return tq;
 }
@@ -322,6 +321,11 @@ bool ExecutorPool::_cancel(size_t taskId, bool eraseTask) {
                                    "cancel() on it");
         }
         taskLocator.erase(itr);
+        {
+            // Account the task reset to its engine
+            BucketAllocationGuard guard(task->getEngine());
+            task.reset();
+        }
         tMutex.notify_all();
     } else { // wake up the task from the TaskQ so a thread can safely erase it
              // otherwise we may race with unregisterTaskable where a unlocated

engines/ep/src/executorthread.cc
@@ -109,9 +109,13 @@ void ExecutorThread::stop(bool wait) {
 
 void ExecutorThread::run() {
     EP_LOG_DEBUG("Thread {} running..", getName());
+    // The run loop should not account to any bucket, only once inside a task
+    // will accounting be back on. GlobalTask::execute will switch to the bucket
+    // and ExecutorPool::cancel will guard the task delete clause with the
+    // BucketAllocationGuard to account task deletion to the bucket.
+    NonBucketAllocationGuard guard;
 
     for (uint8_t tick = 1;; tick++) {
-        resetCurrentTask();
 
         if (state != EXECUTOR_RUNNING) {
             break;
@@ -120,17 +124,10 @@ void ExecutorThread::run() {
         updateCurrentTime();
         if (TaskQueue *q = manager->nextTask(*this, tick)) {
             manager->startWork(taskType);
-            EventuallyPersistentEngine *engine = currentTask->getEngine();
-
-            // Not all tasks are associated with an engine, only switch
-            // for those that do.
-            if (engine) {
-                ObjectRegistry::onSwitchThread(engine);
-            }
 
             if (currentTask->isdead()) {
                 manager->doneWork(taskType);
-                manager->cancel(currentTask->uid, true);
+                cancelCurrentTask(*manager);
                 continue;
             }
 
@@ -184,7 +181,7 @@ void ExecutorThread::run() {
 
             // Now Run the Task ....
             currentTask->setState(TASK_RUNNING, TASK_SNOOZED);
-            bool again = currentTask->run();
+            bool again = currentTask->execute();
 
             // Task done, log it ...
             const std::chrono::steady_clock::duration runtime(
@@ -206,7 +203,7 @@ void ExecutorThread::run() {
 
             // Check if task is run once or needs to be rescheduled..
             if (!again || currentTask->isdead()) {
-                manager->cancel(currentTask->uid, true);
+                cancelCurrentTask(*manager);
             } else {
                 // if a task has not set snooze, update its waketime to now
                 // before rescheduling for more accurate timing histograms
@@ -223,12 +220,11 @@ void ExecutorThread::run() {
                         curTaskDescr,
                         currentTask->getId(),
                         to_ns_since_epoch(currentTask->getWaketime()).count());
+                resetCurrentTask();
             }
             manager->doneWork(taskType);
         }
     }
-    // Thread is about to terminate - disassociate it from any engine.
-    ObjectRegistry::onSwitchThread(nullptr);
 
     state = EXECUTOR_DEAD;
 }
@@ -285,4 +281,10 @@ const std::string ExecutorThread::getStateName() {
     default:
         return std::string("dead");
     }
+}
+
+void ExecutorThread::cancelCurrentTask(ExecutorPool& manager) {
+    auto uid = currentTask->uid;
+    resetCurrentTask();
+    manager.cancel(uid, true);
 }

engines/ep/src/executorthread.h
@@ -103,8 +103,6 @@ public:
 
     void schedule(ExTask &task);
 
-    void reschedule(ExTask &task);
-
     void wake(ExTask &task);
 
     // Changes this threads' current task to the specified task
@@ -143,6 +141,7 @@ public:
     }
 
 protected:
+    void cancelCurrentTask(ExecutorPool& manager);
 
     cb_thread_t thread;
     ExecutorPool *manager;

engines/ep/src/fakes/fake_executorpool.h
@@ -242,7 +242,7 @@ private:
             rescheduled = false;
             return;
         }
-        rescheduled = currentTask->run();
+        rescheduled = currentTask->execute();
     }
 
     TaskQueue& queue;

engines/ep/src/globaltask.cc
@@ -67,6 +67,12 @@ GlobalTask::~GlobalTask() {
     }
 }
 
+bool GlobalTask::execute() {
+    // Invoke run with the engine as the target for alloc/dalloc
+    BucketAllocationGuard guard(engine);
+    return run();
+}
+
 void GlobalTask::snooze(const double secs) {
     if (secs == INT_MAX) {
         setState(TASK_SNOOZED, TASK_RUNNING);

engines/ep/src/globaltask.h
@@ -70,18 +70,8 @@ public:
 
     virtual ~GlobalTask();
 
-    /**
-     * The invoked function when the task is executed.
-     *
-     * If the task wants to run again, it should return true - it will be
-     * added back onto the ready queue and scheduled according to it's
-     * priority. To run again but at a later time, call snooze() specifying
-     * how long to sleep before it should be scheduled again.
-     * If the task is complete (and should never be run again), return false.
-     *
-     * @return Whether or not this task should be rescheduled
-     */
-    virtual bool run() = 0;
+    /// execute the task and return true if it should be rescheduled
+    bool execute();
 
     /**
      * Gives a description of this task.
@@ -240,6 +230,19 @@ public:
     static std::array<TaskId, static_cast<int>(TaskId::TASK_COUNT)> allTaskIds;
 
 protected:
+    /**
+     * The invoked function when the task is executed.
+     *
+     * If the task wants to run again, it should return true - it will be
+     * added back onto the ready queue and scheduled according to it's
+     * priority. To run again but at a later time, call snooze() specifying
+     * how long to sleep before it should be scheduled again.
+     * If the task is complete (and should never be run again), return false.
+     *
+     * @return Whether or not this task should be rescheduled
+     */
+    virtual bool run() = 0;
+
     /**
      * Wake up a task, setting it's wakeTime to "now".
      *

engines/ep/src/kv_bucket.cc
@@ -2454,11 +2454,11 @@ void KVBucket::wakeUpCheckpointRemover() {
 }
 
 void KVBucket::runDefragmenterTask() {
-    defragmenterTask->run();
+    defragmenterTask->execute();
 }
 
 void KVBucket::runItemFreqDecayerTask() {
-    itemFreqDecayerTask->run();
+    itemFreqDecayerTask->execute();
 }
 
 bool KVBucket::runAccessScannerTask() {

engines/ep/src/objectregistry.cc
@@ -221,4 +221,12 @@ NonBucketAllocationGuard::~NonBucketAllocationGuard() {
     th->set(engine);
 }
 
+BucketAllocationGuard::BucketAllocationGuard(EventuallyPersistentEngine* engine)
+    : previous(ObjectRegistry::onSwitchThread(engine, true)) {
+}
+
+BucketAllocationGuard::~BucketAllocationGuard() {
+    ObjectRegistry::onSwitchThread(previous);
+}
+
 #endif

commit 186710c4e0658c19d1b23feba4f309e064ec0e88

MB-40517: ExecutorPool::cancel must be able to call schedule
The task cancel path can call schedule, this certainly happens for tasks
which have the final reference to a VBucket. The task destructs and
in-turn destructs the VBucket, which wants to schedule a clean-up task.
Prior to this fix, a deadlock occurred because schedule was called
whilst the ExecutorPool mutex was held (because we were inside cancel).

This fixes a regression introduced by 4d160d19.

Change-Id: Ia0b71a5f96efdb4813d65e9f6fea83ef008b8d8e
Reviewed-on: http://review.couchbase.org/c/kv_engine/+/133137
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>

engines/ep/src/executorpool.cc
@@ -253,12 +253,12 @@ void ExecutorPool::doneWork(task_type_t taskType) {
     }
 }
 
-bool ExecutorPool::_cancel(size_t taskId, bool eraseTask) {
+ExTask ExecutorPool::_cancel(size_t taskId, bool remove) {
     LockHolder lh(tMutex);
     auto itr = taskLocator.find(taskId);
     if (itr == taskLocator.end()) {
         EP_LOG_DEBUG("Task id {} not found", uint64_t(taskId));
-        return false;
+        return {};
     }
 
     ExTask task = itr->second.first;
@@ -266,11 +266,11 @@ bool ExecutorPool::_cancel(size_t taskId, bool eraseTask) {
                  task->getDescription(),
                  task->getId(),
                  task->getTaskable().getName(),
-                 eraseTask ? "final erase" : "!");
+                 remove ? "removed task" : "");
 
     task->cancel(); // must be idempotent, just set state to dead
 
-    if (eraseTask) { // only internal threads can erase tasks
+    if (remove) { // only internal threads can remove tasks
         if (!task->isdead()) {
             throw std::logic_error("ExecutorPool::_cancel: task '" +
                                    task->getDescription() +
@@ -278,24 +278,32 @@ bool ExecutorPool::_cancel(size_t taskId, bool eraseTask) {
                                    "cancel() on it");
         }
         taskLocator.erase(itr);
-        {
-            // Account the task reset to its engine
-            BucketAllocationGuard guard(task->getEngine());
-            task.reset();
-        }
         tMutex.notify_all();
     } else { // wake up the task from the TaskQ so a thread can safely erase it
              // otherwise we may race with unregisterTaskable where a unlocated
              // task runs in spite of its bucket getting unregistered
         itr->second.second->wake(task);
     }
-    return true;
+    return task;
 }
 
-bool ExecutorPool::cancel(size_t taskId, bool eraseTask) {
-    NonBucketAllocationGuard guard;
-    bool rv = _cancel(taskId, eraseTask);
-    return rv;
+bool ExecutorPool::cancel(size_t taskId, bool remove) {
+    ExTask task;
+    {
+        NonBucketAllocationGuard guard;
+        task = _cancel(taskId, remove);
+    }
+
+    // Critical to the structure of this function is that task (a shared_ptr)
+    // is destructed 1) after ExecutorPool::tMutex is released (which happens
+    // when _cancel returns) and 2) with the memory tracking associated back to
+    // the callers engine, which happens when the above _cancel scope ends.
+    //
+    // 1) The mutex must not be held because if the task object referenced from
+    // the shared_ptr does destruct, it is allowed to call other ExecutorPool
+    // methods, e.g. call schedule(my_cleanup_task); and we must not hold the
+    // lock for that path.
+    return task != nullptr;
 }
 
 bool ExecutorPool::_wake(size_t taskId) {

engines/ep/src/executorpool.h
@@ -111,7 +111,16 @@ public:
         return isHiPrioQset ? hpTaskQ[curTaskType] : lpTaskQ[curTaskType];
     }
 
-    bool cancel(size_t taskId, bool eraseTask=false);
+    /**
+     * cancel the task with taskId and optionally remove from taskLocator.
+     * Removing from the taskLocator will eventually trigger deletion of the
+     * task as references to the shared_ptr are dropped.
+     *
+     * @param taskId Task to cancel
+     * @param remove true if the task should be removed from taskLocator
+     * @return true if the task was found and cancelled
+     */
+    bool cancel(size_t taskId, bool remove = false);
 
     bool wake(size_t taskId);
 
@@ -234,7 +243,16 @@ protected:
     virtual ~ExecutorPool();
 
     TaskQueue* _nextTask(ExecutorThread &t, uint8_t tick);
-    bool _cancel(size_t taskId, bool eraseTask=false);
+
+    /**
+     * see cancel() for detail
+     *
+     * @param taskId Task to cancel
+     * @param remove true if the task should be removed from taskLocator
+     * @return if the task is located it is returned
+     */
+    ExTask _cancel(size_t taskId, bool remove = false);
+
     bool _wake(size_t taskId);
     virtual bool _startWorkers();
 

engines/ep/tests/module_tests/executorpool_test.cc
@@ -358,3 +358,116 @@ TEST_F(SingleThreadedExecutorPoolTest, ignore_duplicate_schedule) {
 
     pool->cancel(taskId, true);
 }
+
+class ScheduleOnDestruct {
+public:
+    ScheduleOnDestruct(ExecutorPool& pool,
+                       Taskable& taskable,
+                       ExecutorThread& thread)
+        : pool(pool), taskable(taskable), thread(thread) {
+    }
+    ~ScheduleOnDestruct();
+
+    ExecutorPool& pool;
+    Taskable& taskable;
+    ExecutorThread& thread;
+};
+
+class ScheduleOnDestructTask : public GlobalTask {
+public:
+    ScheduleOnDestructTask(Taskable& t,
+                           TaskId taskId,
+                           double sleeptime,
+                           bool completeBeforeShutdown,
+                           ExecutorPool& pool,
+                           Taskable& taskable,
+                           ExecutorThread& thread)
+        : GlobalTask(t, taskId, sleeptime, completeBeforeShutdown),
+          scheduleOnDestruct(pool, taskable, thread) {
+    }
+
+    bool run() override {
+        return false;
+    }
+
+    std::string getDescription() override {
+        return "ScheduleOnDestructTask";
+    }
+
+    std::chrono::microseconds maxExpectedDuration() override {
+        return std::chrono::seconds(60);
+    }
+
+    ScheduleOnDestruct scheduleOnDestruct;
+};
+
+class StopTask : public GlobalTask {
+public:
+    StopTask(Taskable& t,
+             TaskId taskId,
+             double sleeptime,
+             bool completeBeforeShutdown,
+             ExecutorThread& thread)
+        : GlobalTask(t, taskId, sleeptime, completeBeforeShutdown),
+          thread(thread) {
+    }
+
+    bool run() override {
+        thread.stop(false);
+        return false;
+    }
+
+    std::string getDescription() override {
+        return "StopTask";
+    }
+
+    std::chrono::microseconds maxExpectedDuration() override {
+        return std::chrono::seconds(60);
+    }
+
+    ExecutorThread& thread;
+};
+
+ScheduleOnDestruct::~ScheduleOnDestruct() {
+    // Schedule StopTask from the destructor. This task will ensure that
+    // ::run terminates its inner run-loop.
+    ExTask task = std::make_shared<StopTask>(
+            taskable, TaskId::ItemPager, 10, true, thread);
+    // MB-40517: This deadlocked
+    pool.schedule(task);
+    pool.wake(task->getId());
+}
+
+/*
+ * At least one object in KV-engine (VBucket) requires to schedule a new
+ * task from its destructor - and that destructor can be called from a task's
+ * destructor. Thus it's a requirement of ExecutorPool/KV that a task
+ * destructing within the scope of ExecutorPool::cancel() can safely call
+ * ExecutorPool::schedule(). MB-40517 broke this as a double lock (deadlock)
+ * occurred.
+ *
+ * This test simulates the requirements with the following:
+ * 1) schedule the "ScheduleOnDestructTask", a task which will schedule another
+ *    task when it destructs.
+ * 2) Use the real ExecutorThread and run the task
+ * 3) ~ScheduleOnDestructTask is called from ExecutorThread::run
+ * 4) From the destructor, schedule a new task, StopTask. When this task runs it
+ *    calls into ExeuctorThread::stop so the run-loop terminates and the test
+ *    can finish.
+ * Without the MB-40517 fix, step 4 would deadlock because the ExecutorPool
+ * lock is already held by the calling thread.
+ */
+TEST_F(SingleThreadedExecutorPoolTest, cancel_can_schedule) {
+    ExecutorThread thr(pool, NONIO_TASK_IDX, "cancel_can_schedule");
+    ExTask task = std::make_shared<ScheduleOnDestructTask>(
+            taskable, TaskId::ItemPager, 10, true, *pool, taskable, thr);
+
+    auto id = task->getId();
+    ASSERT_EQ(id, pool->schedule(task));
+    task.reset(); // only the pool has a reference
+
+    // Wake our task and call run. The task is a 'one-shot' task which will
+    // then be cancelled, which as part of cancel calls schedule
+    pool->wake(id);
+    thr.run();
+}