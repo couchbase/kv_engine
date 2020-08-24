couchbase/kv_engine

commit d13d1ff74f3b87af1c89545199e6b5285bdf0f5f

MB-38283: Stop PassiveStream repeatedly logging ENOMEM
PassiveStream would previously log each time it tried to
processMessage, quickly filling logs. Make it instead log when first
backing off, and when it resumes processing.

Change-Id: Icc73ca9429a307140882a083b2c435bc489379ed
Reviewed-on: http://review.couchbase.org/c/kv_engine/+/124383
Reviewed-by: Dave Rigby <daver@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>
Well-Formed: Build Bot <build@couchbase.com>

engines/ep/src/dcp/passive_stream.cc
@@ -681,7 +681,8 @@ ENGINE_ERROR_CODE PassiveStream::processMessage(
                 std::string("PassiveStream::processMessage: "
                             "Message type not supported"));
     }
-    if (ret != ENGINE_SUCCESS) {
+    // ENOMEM logging is handled by maybeLogMemoryState
+    if (ret != ENGINE_SUCCESS && ret != ENGINE_ENOMEM) {
         log(spdlog::level::level_enum::warn,
             "{} Got error '{}' while trying to process "
             "{} with seqno:{}",
@@ -692,6 +693,10 @@ ENGINE_ERROR_CODE PassiveStream::processMessage(
     } else {
         handleSnapshotEnd(vb, *message->getBySeqno());
     }
+
+    maybeLogMemoryState(cb::to_engine_errc(ret),
+                        taskToString[messageType],
+                        message->getItem()->getBySeqno());
 
     return ret;
 }
@@ -1230,6 +1235,27 @@ void PassiveStream::log(spdlog::level::level_enum severity,
                     std::string{passiveStreamLoggingPrefix}.append(fmt).data(),
                     args...);
         }
+    }
+}
+
+void PassiveStream::maybeLogMemoryState(cb::engine_errc status,
+                                        const std::string& msgType,
+                                        int64_t seqno) {
+    bool previousNoMem = isNoMemory.load();
+    if (status == cb::engine_errc::no_memory && !previousNoMem) {
+        log(spdlog::level::level_enum::warn,
+            "{} Got error '{}' while trying to process "
+            "{} with seqno:{}",
+            vb_,
+            cb::to_string(status),
+            msgType,
+            seqno);
+        isNoMemory.store(true);
+    } else if (status == cb::engine_errc::success && previousNoMem) {
+        log(spdlog::level::level_enum::info,
+            "{} PassiveStream resuming after no-memory backoff",
+            vb_);
+        isNoMemory.store(false);
     }
 }
 

engines/ep/src/dcp/passive_stream.h
@@ -232,6 +232,24 @@ protected:
              Args... args) const;
 
     /**
+     * Log when the stream backs off or resumes processing items.
+     *
+     * Determines if the stream has backed off or resumed based on
+     * the passed status. Assumes no_memory indicates the stream will
+     * backoff, and that success indicates the stream has resumed
+     * processing.
+     *
+     * @param status last error code returned by the engine when processing
+     *               a message
+     * @param msgType string indicating the type of the last message processed
+     *                ("mutation", "deletion", "expiration", "prepare")
+     * @param seqno seqno of last message processed
+     */
+    void maybeLogMemoryState(cb::engine_errc status,
+                             const std::string& msgType,
+                             int64_t seqno);
+
+    /**
      * Notifies the consumer connection that the stream has items ready to be
      * pick up.
      */
@@ -317,4 +335,8 @@ protected:
      * messages in the buffer.
      */
     std::function<void()> processBufferedMessages_postFront_Hook;
+
+    // Flag indicating if the most recent call to processMessage
+    // backed off due to ENOMEM. Only used for limiting logging
+    std::atomic<bool> isNoMemory{false};
 };

commit b1dea0bf5338f6c89cdf45cece76fe4b2ceb8eaf

MB-41024: PassiveStream: Don't handleSnapshotEnd if message not processed
If a Prepare is received when under memory pressure it is initially
rejected and queued, that the snapshot end should _not_ be notified to
the PDM. Otherwise when the queued message is later processed, we will
end up notifying the snapshot end a second time and throw a Monotonic
logic_error exception when attempting to push the same seqno to the
PDM::receivedSnapshotEnds

Fixes: d13d1ff74 ("MB-38283: Stop PassiveStream repeatedly logging ENOMEM")

Change-Id: I44cc88027b28a80fd85f3b6687fea7b748152e68
Reviewed-on: http://review.couchbase.org/c/kv_engine/+/134816
Well-Formed: Build Bot <build@couchbase.com>
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Jim Walker <jim@couchbase.com>

engines/ep/src/dcp/passive_stream.cc
@@ -681,15 +681,17 @@ ENGINE_ERROR_CODE PassiveStream::processMessage(
                 std::string("PassiveStream::processMessage: "
                             "Message type not supported"));
     }
-    // ENOMEM logging is handled by maybeLogMemoryState
-    if (ret != ENGINE_SUCCESS && ret != ENGINE_ENOMEM) {
-        log(spdlog::level::level_enum::warn,
-            "{} Got error '{}' while trying to process "
-            "{} with seqno:{}",
-            vb_,
-            cb::to_string(cb::to_engine_errc(ret)),
-            taskToString[messageType],
-            message->getItem()->getBySeqno());
+    if (ret != ENGINE_SUCCESS) {
+        // ENOMEM logging is handled by maybeLogMemoryState
+        if (ret != ENGINE_ENOMEM) {
+            log(spdlog::level::level_enum::warn,
+                "{} Got error '{}' while trying to process "
+                "{} with seqno:{}",
+                vb_,
+                cb::to_string(cb::to_engine_errc(ret)),
+                taskToString[messageType],
+                message->getItem()->getBySeqno());
+        }
     } else {
         handleSnapshotEnd(vb, *message->getBySeqno());
     }

engines/ep/tests/module_tests/dcp_durability_stream_test.cc
@@ -34,6 +34,7 @@
 #include "../mock/mock_checkpoint_manager.h"
 #include "../mock/mock_dcp_consumer.h"
 #include "../mock/mock_dcp_producer.h"
+#include "../mock/mock_replicationthrottle.h"
 #include "../mock/mock_stream.h"
 #include "../mock/mock_synchronous_ep_engine.h"
 
@@ -1964,15 +1965,7 @@ queued_item DurabilityPassiveStreamTest::makeAndReceiveDcpPrepare(
 
     // The consumer receives snapshot-marker [seqno, seqno]
     uint32_t opaque = 0;
-    SnapshotMarker marker(opaque,
-                          vbid,
-                          seqno,
-                          seqno,
-                          snapshotMarkerFlags,
-                          hcs /*HCS*/,
-                          {} /*maxVisibleSeqno*/,
-                          {} /*streamId*/);
-    stream->processMarker(&marker);
+    makeAndProcessSnapshotMarker(opaque, seqno, snapshotMarkerFlags, hcs);
 
     queued_item qi = makePendingItem(
             key, "value", Requirements(level, Timeout::Infinity()));
@@ -1991,6 +1984,21 @@ queued_item DurabilityPassiveStreamTest::makeAndReceiveDcpPrepare(
                       nullptr,
                       cb::mcbp::DcpStreamId{})));
     return qi;
+}
+void DurabilityPassiveStreamTest::makeAndProcessSnapshotMarker(
+        uint32_t opaque,
+        uint64_t seqno,
+        uint64_t snapshotMarkerFlags,
+        const boost::optional<uint64_t>& hcs) {
+    SnapshotMarker marker(opaque,
+                          vbid,
+                          seqno,
+                          seqno,
+                          snapshotMarkerFlags,
+                          hcs /*HCS*/,
+                          {} /*maxVisibleSeqno*/,
+                          {} /*streamId*/);
+    stream->processMarker(&marker);
 }
 
 void DurabilityPassiveStreamTest::testReceiveDcpPrepare() {
@@ -4010,6 +4018,69 @@ TEST_P(DurabilityPassiveStreamTest, AllowedDuplicatePreparesSetOnDiskSnap) {
                       DocKeyEncodesCollectionId::No,
                       nullptr,
                       cb::mcbp::DcpStreamId{})));
+}
+
+// Regression test for MB-41024: check that if a Prepare is received when under
+// memory pressure and it is initially rejected and queued, that the snapshot
+// end is not notified twice to PDM.
+TEST_P(DurabilityPassiveStreamTest,
+       MB_41024_PrepareAtSnapEndWithMemoryPressure) {
+    if (ephemeralFailNewData()) {
+        // This configuration disconnects if a replication stream would
+        // go over the high watermark - so test not applicable.
+        return;
+    }
+
+    // Send initial snapshot marrker.
+    uint32_t opaque = 0;
+    auto prepareSeqno = 1;
+    // Require a disk snapshot so PassiveStream::handleSnapshotEnd() notifies
+    // the PDM even if we haven't successfully processed a Prepare.
+    makeAndProcessSnapshotMarker(
+            opaque, prepareSeqno, MARKER_FLAG_DISK | MARKER_FLAG_CHK);
+
+    // Setup - send prepare. This should initially be rejected, by virtue
+    // of it value size which would make mem_used greater than
+    // bucket quota * replicationThrottleThreshold.
+    std::string largeValue(1024 * 1024 * 10, '\0');
+    using namespace cb::durability;
+    auto prepare =
+            makePendingItem(makeStoredDocKey("key"),
+                            largeValue,
+                            Requirements(Level::Majority, Timeout::Infinity()));
+    prepare->setBySeqno(prepareSeqno);
+
+    // We *don't* want the replication throttle to kick in before we attempt
+    // to process the prepare, so configure the MockReplicationThrottle to
+    // just return "Process" when called during the messageReceived call
+    // below.
+    using namespace ::testing;
+    EXPECT_CALL(engine->getMockReplicationThrottle(), getStatus())
+            .WillOnce(Return(ReplicationThrottle::Status::Process));
+
+    // Message received by stream. Too large for current mem_used so should
+    // be buffered and return TMPFAIL
+    EXPECT_EQ(ENGINE_TMPFAIL,
+              stream->messageReceived(std::make_unique<MutationConsumerMessage>(
+                      prepare,
+                      opaque,
+                      IncludeValue::Yes,
+                      IncludeXattrs::Yes,
+                      IncludeDeleteTime::No,
+                      IncludeDeletedUserXattrs::Yes,
+                      DocKeyEncodesCollectionId::No,
+                      nullptr,
+                      cb::mcbp::DcpStreamId{})));
+
+    // Increase replicationThrottleThreshold to allow mutation to be processed.
+    engine->getConfiguration().setReplicationThrottleThreshold(100);
+
+    // Test: now process the buffered message. This would previously throw a
+    // Monotonic logic_error exception when attempting to push the same
+    // seqno to the PDM::receivedSnapshotEnds
+    uint32_t processedBytes = 0;
+    EXPECT_EQ(all_processed,
+              stream->processBufferedMessages(processedBytes, 1));
 }
 
 void DurabilityPromotionStreamTest::SetUp() {

engines/ep/tests/module_tests/dcp_durability_stream_test.h
@@ -103,6 +103,16 @@ public:
 
 protected:
     /**
+     * Creates a SNAPSHOT_MARKER message with range [seqno,seqno] and
+     * processes it on the DCP stream.
+     */
+    void makeAndProcessSnapshotMarker(
+            uint32_t opaque,
+            uint64_t seqno,
+            uint64_t snapshotMarkerFlags = MARKER_FLAG_MEMORY | MARKER_FLAG_CHK,
+            const boost::optional<uint64_t>& hcs = 0);
+
+    /**
      * Creates a DCP_PREPARE message (with a preceeding SNAPSHOT_MARKER), and
      * processes it on the DCP stream.
      * Returns the SyncWrite prepare item.

engines/ep/tests/module_tests/evp_store_single_threaded_test.h
@@ -299,6 +299,10 @@ public:
         return std::get<0>(GetParam()).find("ephemeral") != std::string::npos;
     }
 
+    bool ephemeralFailNewData() const {
+        return ephemeral() && std::get<1>(GetParam()) == "fail_new_data";
+    }
+
     bool fullEviction() const {
         return persistent() && std::get<1>(GetParam()) == "full_eviction";
     }

engines/ep/tests/module_tests/test_helpers.cc
@@ -68,7 +68,7 @@ queued_item makeCommittedviaPrepareItem(StoredDocKey key, std::string value) {
 }
 
 queued_item makePendingItem(StoredDocKey key,
-                            std::string value,
+                            const std::string& value,
                             cb::durability::Requirements reqs) {
     queued_item qi{new Item(key, 0, 0, value.data(), value.size())};
     qi->setPendingSyncWrite(reqs);

engines/ep/tests/module_tests/test_helpers.h
@@ -42,7 +42,7 @@ Item make_item(
 
 /// Make a queued_item representing a pending SyncWrite.
 queued_item makePendingItem(StoredDocKey key,
-                            std::string value,
+                            const std::string& value,
                             cb::durability::Requirements reqs = {
                                     cb::durability::Level::Majority, {}});
 