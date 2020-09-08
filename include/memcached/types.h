3343603ffc8285fcbbbffeba8111db716f251885
Trond Norbye <trond.norbye@gmail.com>
2020-09-08 10:43:19 +0200

    Remove local WIN32_LEAN_AND_MEAN definition
    This is set by TLM these days
    
    Change-Id: I31c371f43786bac562e6361aa73e724d4a26d2a7
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/135742
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- include/memcached/types.h
+++ include/memcached/types.h
@@ -9,14 +9,7 @@
 
 #ifdef WIN32
 // Need DWORD and ssize_t (used to be defined in platform/platform.h
-#ifndef WIN32_LEAN_AND_MEAN
-#define DO_UNDEF_WIN32_LEAN_AND_MEAN
-#define WIN32_LEAN_AND_MEAN
-#endif
 #include <folly/portability/Windows.h>
-#ifdef DO_UNDEF_WIN32_LEAN_AND_MEAN
-#undef WIN32_LEAN_AND_MEAN
-#endif
 typedef SSIZE_T ssize_t;
 typedef unsigned int useconds_t;
 #else
