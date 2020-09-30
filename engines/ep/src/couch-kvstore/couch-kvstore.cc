debeb02d2c8e2474f8a02c5cf08a9fba96b89403
Trond Norbye <trond.norbye@gmail.com>
2020-09-30 09:30:54 +0200

    Disable concurrent flusher and compaction in couch-kvstore
    In MB-41747 it looks like on_disk_prepares is corrupt.
    
    The value gets incremented as part of storing an object to
    disk, and decremented as part of compaction when it gets
    purged. One of the changes we've done lately is that we
    now run the flusher and compaction in parallel so lets start
    by disabling that.
    
    Change-Id: Icf39aa4dcfcca29671d8afcd4b42f402369d6050
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/137197
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: James Harrison <james.harrison@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/src/couch-kvstore/couch-kvstore.cc
+++ engines/ep/src/couch-kvstore/couch-kvstore.cc
@@ -46,6 +46,8 @@
 #include <shared_mutex>
 #include <utility>
 
+#undef ENABLE_CONCURRENT_COMPACTION_AND_FLUSHER
+
 static int bySeqnoScanCallback(Db* db, DocInfo* docinfo, void* ctx);
 static int byIdScanCallback(Db* db, DocInfo* docinfo, void* ctx);
 
@@ -1033,8 +1035,10 @@ bool CouchKVStore::compactDBInternal(std::unique_lock<std::mutex>& vbLock,
                 "CouchKVStore::compactDB: droppedKeyCb must be set ");
     }
 
+#ifdef ENABLE_CONCURRENT_COMPACTION_AND_FLUSHER
     // we may run the first part of the compaction without exclusive access
     vbLock.unlock();
+#endif
 
     auto* def_iops = statCollectingFileOpsCompaction.get();
     std::chrono::steady_clock::time_point start =
@@ -1251,6 +1255,7 @@ bool CouchKVStore::compactDBInternal(std::unique_lock<std::mutex>& vbLock,
     // but to avoid doing that "forever" we'll give up after 10 times
     // and then hold the lock (and block the flusher) to make sure we catch
     // up.
+#ifdef ENABLE_CONCURRENT_COMPACTION_AND_FLUSHER
     vbLock.lock();
     for (int ii = 0; ii < 10; ++ii) {
         concurrentCompactionUnitTestHook();
@@ -1266,6 +1271,7 @@ bool CouchKVStore::compactDBInternal(std::unique_lock<std::mutex>& vbLock,
     }
 
     concurrentCompactionUnitTestHook();
+#endif
 
     // Block any writers, do the final catch up and swap the file
     tryToCatchUpDbFile(*compactdb,
@@ -1571,7 +1577,11 @@ StorageProperties CouchKVStore::getStorageProperties() {
                          StorageProperties::EfficientVBDeletion::Yes,
                          StorageProperties::PersistedDeletion::Yes,
                          StorageProperties::EfficientGet::Yes,
+#ifdef ENABLE_CONCURRENT_COMPACTION_AND_FLUSHER
                          StorageProperties::ConcurrentWriteCompact::Yes,
+#else
+                         StorageProperties::ConcurrentWriteCompact::No,
+#endif
                          StorageProperties::ByIdScan::Yes);
     return rv;
 }
