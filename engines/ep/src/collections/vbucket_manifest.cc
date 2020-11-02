couchbase/kv_engine

commit d0eec7ab2ecd07836fbba9926b116135dac43ddf

MB-16181: Change VB::Manifest stats to use fmt::memory_buffer
Switch from snprintf to fmtlib API

Change-Id: Ifbaa4648bb6aebc5d0adc2178824785726eab986
Reviewed-on: http://review.couchbase.org/c/kv_engine/+/137969
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Dave Rigby <daver@couchbase.com>

engines/ep/src/collections/vbucket_manifest.cc
@@ -846,21 +846,20 @@ void Manifest::decrementItemCount(CollectionID collection) const {
 bool Manifest::addCollectionStats(Vbid vbid,
                                   const void* cookie,
                                   const AddStatFn& add_stat) const {
-    try {
-        const int bsize = 512;
-        char buffer[bsize];
-        checked_snprintf(buffer, bsize, "vb_%d:collections", vbid.get());
-        add_casted_stat(buffer, map.size(), add_stat, cookie);
-        checked_snprintf(buffer, bsize, "vb_%d:manifest:uid", vbid.get());
-        add_casted_stat(buffer, manifestUid, add_stat, cookie);
-    } catch (const std::exception& e) {
-        EP_LOG_WARN(
-                "VB::Manifest::addCollectionStats {}, failed to build stats "
-                "exception:{}",
-                vbid,
-                e.what());
-        return false;
-    }
+    fmt::memory_buffer key;
+    fmt::memory_buffer value;
+
+    format_to(key, "vb_{}:collections", vbid.get());
+    format_to(value, "{}", map.size());
+    add_stat({key.data(), key.size()}, {value.data(), value.size()}, cookie);
+
+    key.resize(0);
+    value.resize(0);
+
+    format_to(key, "vb_{}:manifest:uid", vbid.get());
+    format_to(value, "{}", manifestUid);
+    add_stat({key.data(), key.size()}, {value.data(), value.size()}, cookie);
+
     for (const auto& entry : map) {
         if (!entry.second.addStats(
                     entry.first.to_string(), vbid, cookie, add_stat)) {
@@ -874,42 +873,45 @@ bool Manifest::addCollectionStats(Vbid vbid,
 bool Manifest::addScopeStats(Vbid vbid,
                              const void* cookie,
                              const AddStatFn& add_stat) const {
-    const int bsize = 512;
-    char buffer[bsize];
-    try {
-        checked_snprintf(buffer, bsize, "vb_%d:scopes", vbid.get());
-        add_casted_stat(buffer, scopes.size(), add_stat, cookie);
-        checked_snprintf(buffer, bsize, "vb_%d:manifest_uid", vbid.get());
-        add_casted_stat(buffer, manifestUid, add_stat, cookie);
-    } catch (const std::exception& e) {
-        EP_LOG_WARN(
-                "VB::Manifest::addScopeStats {}, failed to build stats "
-                "exception:{}",
-                vbid,
-                e.what());
-        return false;
-    }
+    fmt::memory_buffer key;
+    fmt::memory_buffer value;
+
+    format_to(key, "vb_{}:scopes", vbid.get());
+    format_to(value, "{}", scopes.size());
+    add_stat({key.data(), key.size()}, {value.data(), value.size()}, cookie);
+
+    key.resize(0);
+    value.resize(0);
+
+    format_to(key, "vb_{}:manifest:uid", vbid.get());
+    format_to(value, "{}", manifestUid);
+    add_stat({key.data(), key.size()}, {value.data(), value.size()}, cookie);
 
     // Dump the scopes container, which only stores scope-identifiers, so that
     // we have a key and value we include the iteration index as the value.
     int i = 0;
     for (auto sid : scopes) {
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s", vbid.get(), sid.to_string().c_str());
-        add_casted_stat(buffer, i++, add_stat, cookie);
+        key.resize(0);
+        value.resize(0);
+
+        format_to(key, "vb_{}:{}", vbid.get(), sid);
+        format_to(value, "{}", i++);
+        add_stat(
+                {key.data(), key.size()}, {value.data(), value.size()}, cookie);
     }
 
     // Dump all collections and how they map to a scope. Stats requires unique
     // keys, so cannot use the cid as the value (as key won't be unique), so
     // return anything from the entry for the value, we choose item count.
     for (const auto& [cid, entry] : map) {
-        checked_snprintf(buffer,
-                         bsize,
-                         "vb_%d:%s:%s:items",
-                         vbid.get(),
-                         entry.getScopeID().to_string().c_str(),
-                         cid.to_string().c_str());
-        add_casted_stat(buffer, entry.getItemCount(), add_stat, cookie);
+        key.resize(0);
+        value.resize(0);
+
+        format_to(
+                key, "vb_{}:{}:{}:items", vbid.get(), entry.getScopeID(), cid);
+        format_to(value, "{}", entry.getItemCount());
+        add_stat(
+                {key.data(), key.size()}, {value.data(), value.size()}, cookie);
     }
 
     return true;
@@ -1097,41 +1099,25 @@ bool Manifest::DroppedCollectionInfo::addStats(
         CollectionID cid,
         const void* cookie,
         const AddStatFn& add_stat) const {
-    try {
-        std::array<char, 512> buffer;
-        checked_snprintf(buffer.data(),
-                         buffer.size(),
-                         "vb_%d_%x:start",
-                         vbid.get(),
-                         uint32_t(cid));
-        add_casted_stat(buffer.data(), start, add_stat, cookie);
-        checked_snprintf(buffer.data(),
-                         buffer.size(),
-                         "vb_%d_%x:end",
-                         vbid.get(),
-                         uint32_t(cid));
-        add_casted_stat(buffer.data(), end, add_stat, cookie);
-        checked_snprintf(buffer.data(),
-                         buffer.size(),
-                         "vb_%d_%x:items",
-                         vbid.get(),
-                         uint32_t(cid));
-        add_casted_stat(buffer.data(), itemCount, add_stat, cookie);
-        checked_snprintf(buffer.data(),
-                         buffer.size(),
-                         "vb_%d_%x:disk",
-                         vbid.get(),
-                         uint32_t(cid));
-        add_casted_stat(buffer.data(), diskSize, add_stat, cookie);
-    } catch (const std::exception& e) {
-        EP_LOG_WARN(
-                "VB::Manifest::DroppedCollectionInfo::addStats {}, failed to "
-                "build stats "
-                "exception:{}",
-                vbid,
-                e.what());
-        return false;
-    }
+    fmt::memory_buffer prefix;
+    format_to(prefix, "vb_{}:{}", vbid.get(), cid);
+    const auto addStat = [&prefix, &add_stat, &cookie](const auto& statKey,
+                                                       auto statValue) {
+        fmt::memory_buffer key;
+        fmt::memory_buffer value;
+        format_to(key,
+                  "{}:{}",
+                  std::string_view{prefix.data(), prefix.size()},
+                  statKey);
+        format_to(value, "{}", statValue);
+        add_stat(
+                {key.data(), key.size()}, {value.data(), value.size()}, cookie);
+    };
+
+    addStat("start", start);
+    addStat("end", end);
+    addStat("items", itemCount);
+    addStat("disk", diskSize);
     return true;
 }
 

engines/ep/src/collections/manifest.cc
@@ -295,7 +295,7 @@ nlohmann::json Manifest::toJson(
         }
         if (!scope["collections"].empty() || visible) {
             scope["name"] = s.second.name;
-            scope["uid"] = fmt::format("{0:x}", s.first);
+            scope["uid"] = fmt::format("{0:x}", uint32_t(s.first));
             manifest["scopes"].push_back(scope);
         }
     }

engines/ep/src/collections/vbucket_manifest_entry.cc
@@ -64,54 +64,36 @@ bool Collections::VB::ManifestEntry::addStats(const std::string& cid,
                                               Vbid vbid,
                                               const void* cookie,
                                               const AddStatFn& add_stat) const {
-    try {
-        const int bsize = 512;
-        char buffer[bsize];
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:scope", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getScopeID().to_string(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:start_seqno", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getStartSeqno(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:high_seqno", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getHighSeqno(), add_stat, cookie);
-        checked_snprintf(buffer,
-                         bsize,
-                         "vb_%d:%s:persisted_high_seqno",
-                         vbid.get(),
-                         cid.c_str());
-        add_casted_stat(buffer, getPersistedHighSeqno(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:items", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getItemCount(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:disk_size", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getDiskSize(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:ops_get", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getOpsGet(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:ops_store", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getOpsStore(), add_stat, cookie);
-        checked_snprintf(
-                buffer, bsize, "vb_%d:%s:ops_delete", vbid.get(), cid.c_str());
-        add_casted_stat(buffer, getOpsDelete(), add_stat, cookie);
+    fmt::memory_buffer prefix;
+    format_to(prefix, "vb_{}:{}", vbid.get(), cid);
 
-        if (getMaxTtl()) {
-            checked_snprintf(
-                    buffer, bsize, "vb_%d:%s:maxTTL", vbid.get(), cid.c_str());
-            add_casted_stat(buffer, getMaxTtl().value().count(), add_stat, cookie);
-        }
-        return true;
-    } catch (const std::exception& error) {
-        EP_LOG_WARN(
-                "VB::ManifestEntry::addStats {}, failed to build stats, "
-                "exception:{}",
-                vbid,
-                error.what());
+    const auto addStat = [&prefix, &add_stat, &cookie](const auto& statKey,
+                                                       auto statValue) {
+        fmt::memory_buffer key;
+        fmt::memory_buffer value;
+        format_to(key,
+                  "{}:{}",
+                  std::string_view{prefix.data(), prefix.size()},
+                  statKey);
+        format_to(value, "{}", statValue);
+        add_stat(
+                {key.data(), key.size()}, {value.data(), value.size()}, cookie);
+    };
+
+    addStat("scope", getScopeID().to_string().c_str());
+    addStat("start_seqno", getStartSeqno());
+    addStat("high_seqno", getHighSeqno());
+    addStat("persisted_high_seqno", getPersistedHighSeqno());
+    addStat("items", getItemCount());
+    addStat("disk_size", getDiskSize());
+    addStat("ops_get", getOpsGet());
+    addStat("ops_store", getOpsStore());
+    addStat("ops_delete", getOpsDelete());
+    if (getMaxTtl()) {
+        addStat("maxTTL", getMaxTtl().value().count());
     }
-    return false;
+
+    return true;
 }
 
 std::ostream& Collections::VB::operator<<(

include/memcached/dockey.h
@@ -180,6 +180,8 @@ private:
     ScopeIDType value;
 };
 
+std::ostream& operator<<(std::ostream& os, const ScopeID& sid);
+
 using ScopeCollectionPair = std::pair<ScopeID, CollectionID>;
 
 /**

utilities/dockey.cc
@@ -21,12 +21,6 @@
 #include <sstream>
 #include <string_view>
 
-std::string CollectionID::to_string() const {
-    std::stringstream sstream;
-    sstream << "0x" << std::hex << value;
-    return sstream.str();
-}
-
 bool CollectionID::isReserved(CollectionIDType value) {
     return value >= System && value <= Reserved7;
 }
@@ -39,9 +33,19 @@ std::ostream& operator<<(std::ostream& os, const CollectionID& cid) {
     return os << "0x" << std::hex << uint32_t(cid);
 }
 
+std::string CollectionID::to_string() const {
+    std::stringstream sstream;
+    sstream << *this;
+    return sstream.str();
+}
+
+std::ostream& operator<<(std::ostream& os, const ScopeID& sid) {
+    return os << "0x" << std::hex << uint32_t(sid);
+}
+
 std::string ScopeID::to_string() const {
     std::stringstream sstream;
-    sstream << "0x" << std::hex << value;
+    sstream << *this;
     return sstream.str();
 }
 

utilities/test_manifest.cc
@@ -44,7 +44,7 @@ CollectionsManifest& CollectionsManifest::add(const ScopeEntry::Entry& entry) {
 
     nlohmann::json jsonEntry;
     std::stringstream ss;
-    ss << std::hex << entry.uid;
+    ss << std::hex << uint32_t(entry.uid);
 
     jsonEntry["name"] = entry.name;
     jsonEntry["uid"] = ss.str();
@@ -95,23 +95,17 @@ CollectionsManifest& CollectionsManifest::remove(
         const ScopeEntry::Entry& entry) {
     updateUid();
     std::stringstream sid;
-    sid << std::hex << entry.uid;
-    bool removed = false;
+    sid << std::hex << uint32_t(entry.uid);
     for (auto itr = json["scopes"].begin(); itr != json["scopes"].end();
          itr++) {
         if ((*itr)["name"] == entry.name && (*itr)["uid"] == sid.str()) {
             json["scopes"].erase(itr);
-            removed = true;
-            break;
+            return *this;
         }
     }
 
-    if (!removed) {
-        throw std::invalid_argument(
-                "CollectionsManifest::remove(scope) did not remove anything");
-    }
-
-    return *this;
+    throw std::invalid_argument(
+            "CollectionsManifest::remove(scope) did not remove anything");
 }
 
 CollectionsManifest& CollectionsManifest::remove(
@@ -122,7 +116,6 @@ CollectionsManifest& CollectionsManifest::remove(
     std::stringstream cid;
     cid << std::hex << uint32_t(collectionEntry.uid);
 
-    bool removed = false;
     // Iterate on all scopes, find the one matching the passed scopeEntry
     for (auto itr = json["scopes"].begin(); itr != json["scopes"].end();
          itr++) {
@@ -135,36 +128,34 @@ CollectionsManifest& CollectionsManifest::remove(
                 if ((*citr)["name"] == collectionEntry.name &&
                     (*citr)["uid"] == cid.str()) {
                     (*itr)["collections"].erase(citr);
-                    removed = true;
-                    break;
+                    return *this;
                 }
             }
             break;
         }
     }
 
-    if (!removed) {
-        throw std::invalid_argument(
-                "CollectionsManifest::remove(collection) did not remove "
-                "anything");
-    }
-
-    return *this;
+    throw std::invalid_argument(
+            "CollectionsManifest::remove(collection) did not remove "
+            "anything");
 }
 
 CollectionsManifest& CollectionsManifest::rename(
         const ScopeEntry::Entry& scopeEntry, const std::string& newName) {
     updateUid();
     std::stringstream sidString;
-    sidString << std::hex << scopeEntry.uid;
+    sidString << std::hex << uint32_t(scopeEntry.uid);
     for (auto& scope : json["scopes"]) {
         if (scope["name"] == scopeEntry.name &&
             scope["uid"] == sidString.str()) {
             scope["name"] = newName;
-            break;
+            return *this;
         }
     }
-    return *this;
+
+    throw std::invalid_argument(
+            "CollectionsManifest::rename(scope) did not rename "
+            "anything");
 }
 
 CollectionsManifest& CollectionsManifest::rename(
@@ -173,9 +164,8 @@ CollectionsManifest& CollectionsManifest::rename(
         const std::string& newName) {
     updateUid();
     std::stringstream sidString, cidString;
-    sidString << std::hex << scopeEntry.uid;
+    sidString << std::hex << uint32_t(scopeEntry.uid);
     cidString << std::hex << uint32_t(collectionEntry.uid);
-
     for (auto& scope : json["scopes"]) {
         if (scope["name"] == scopeEntry.name &&
             scope["uid"] == sidString.str()) {
@@ -188,7 +178,9 @@ CollectionsManifest& CollectionsManifest::rename(
             }
         }
     }
-    return *this;
+    throw std::invalid_argument(
+            "CollectionsManifest::rename(collection) did not rename "
+            "anything");
 }
 
 bool CollectionsManifest::exists(const CollectionEntry::Entry& collectionEntry,
@@ -196,7 +188,7 @@ bool CollectionsManifest::exists(const CollectionEntry::Entry& collectionEntry,
     std::stringstream cid;
     cid << std::hex << uint32_t(collectionEntry.uid);
     std::stringstream sid;
-    sid << std::hex << scopeEntry.uid;
+    sid << std::hex << uint32_t(scopeEntry.uid);
 
     for (auto itr = json["scopes"].begin(); itr != json["scopes"].end();
          itr++) {
@@ -217,7 +209,7 @@ bool CollectionsManifest::exists(const CollectionEntry::Entry& collectionEntry,
 
 bool CollectionsManifest::exists(const ScopeEntry::Entry& scopeEntry) const {
     std::stringstream sid;
-    sid << std::hex << scopeEntry.uid;
+    sid << std::hex << uint32_t(scopeEntry.uid);
     for (auto itr = json["scopes"].begin(); itr != json["scopes"].end();
          itr++) {
         if ((*itr)["name"] == scopeEntry.name && (*itr)["uid"] == sid.str()) {

commit e94bfc4909fb996ec2f4734729e03ba071b25618

MB-16181: VB::Manifest logging incorrect
Logging issue introduced with commit d0eec7a as ScopeID ostream
operator was added, the VB::Manifest logging stopped working.

This commit restores those log messages.

Change-Id: I58e18f1273b8379ce27e5a4f96e171c0bdd02865
Reviewed-on: http://review.couchbase.org/c/kv_engine/+/139200
Reviewed-by: James Harrison <james.harrison@couchbase.com>
Tested-by: Jim Walker <jim@couchbase.com>

engines/ep/src/collections/vbucket_manifest.cc
@@ -257,7 +257,7 @@ void Manifest::addCollection(const WriteHandle& wHandle,
 
     EP_LOG_INFO(
             "collections: {} adding collection:[name:{},id:{}] to "
-            "scope:{:#x}, "
+            "scope:{}, "
             "maxTTL:{} {}, "
             "replica:{}, seqno:{}, manifest:{:#x}",
             vb.getId(),
@@ -339,7 +339,7 @@ void Manifest::dropCollection(WriteHandle& wHandle,
             vb.getSaveDroppedCollectionCallback(cid, wHandle, itr->second));
 
     EP_LOG_INFO(
-            "collections: {} drop of collection:{} from scope:{:#x}"
+            "collections: {} drop of collection:{} from scope:{}"
             ", replica:{}, seqno:{}, manifest:{:#x} tombstone:{}",
             vb.getId(),
             cid,
@@ -401,7 +401,7 @@ void Manifest::addScope(const WriteHandle& wHandle,
             std::move(item), optionalSeqno, {}, wHandle, {});
 
     EP_LOG_INFO(
-            "collections: {} added scope:name:{},id:{:#x} "
+            "collections: {} added scope:name:{},id:{} "
             "replica:{}, seqno:{}, manifest:{:#x}",
             vb.getId(),
             scopeName,
@@ -455,7 +455,7 @@ void Manifest::dropScope(const WriteHandle& wHandle,
     }
 
     EP_LOG_INFO(
-            "collections: {} dropped scope:id:{:#x} "
+            "collections: {} dropped scope:id:{} "
             "replica:{}, seqno:{}, manifest:{:#x}",
             vb.getId(),
             sid,