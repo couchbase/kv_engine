3dd61a54f571b5b8528555f79dc1502282e449cc
Trond Norbye <trond.norbye@gmail.com>
2020-11-07 11:48:43 +0100

    Remove leftovers from compile support with SunPRO
    We don't support these compilers anymore
    
    Change-Id: Ia84e84d636c94b4fbb9ffdebaff0cec05e36775b
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/139774
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- include/memcached/visibility.h
+++ include/memcached/visibility.h
@@ -6,11 +6,7 @@
  * via shared libraries. GNU does not mind.
  * so MEMCACHED_PUBLIC_CLASS had to be added
  */
-
-#if (defined(__SUNPRO_C) && (__SUNPRO_C >= 0x550)) || (defined(__SUNPRO_CC) && (__SUNPRO_CC >= 0x550))
-#define MEMCACHED_PUBLIC_API __global
-#define MEMCACHED_PUBLIC_CLASS __global
-#elif defined __GNUC__
+#if defined __GNUC__
 #define MEMCACHED_PUBLIC_API __attribute__ ((visibility("default")))
 #define MEMCACHED_PUBLIC_CLASS __attribute__((visibility("default")))
 #elif defined(_MSC_VER)
