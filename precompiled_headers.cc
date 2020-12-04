6f01216bc2a9995272bba607925c3cbf11e5fe28
Ben Huddleston <ben.huddleston@couchbase.com>
2020-12-04 17:04:51 +0000

    MB-43677: Add PCH Object Library
    Add an object library containing a pre-compiled header that includes
    headers commonly used throughout the codebase. This commit is a
    proof of concept that use pre-compiled headers in various targets
    throughout the code base.
    
    Before:
    
    Analyzing build trace from 'noOptimize.bin'...
    **** Time summary:
    Compilation (1065 times):
      Parsing (frontend):         3093.3 s
      Codegen & opts (backend):    832.9 s
    
    After:
    
      Analyzing build trace from 'initial.bin'...
    **** Time summary:
    Compilation (1073 times):
      Parsing (frontend):         2847.3 s
      Codegen & opts (backend):    826.4 s
    
    Change-Id: I182fa5d0058849e02ff8d473e8477aa51d5d999f
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/155349
    Tested-by: Ben Huddleston <ben.huddleston@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

---
+++ precompiled_headers.cc
@@ -0,0 +1,12 @@
+/*
+ *     Copyright 2021-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+// This file is required to build the precompiled header object libraries
+// (kv_pch et. al.) because object libraries need some source to compile.
---
+++ precompiled_headers.h
@@ -0,0 +1,30 @@
+/*
+ *     Copyright 2021-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+#pragma once
+
+#include <nlohmann/json.hpp>
+
+// Used by most unit test files.
+#include <folly/portability/GTest.h>
+
+// Used throughout the codebase
+#include <folly/SharedMutex.h>
+#include <folly/Synchronized.h>
+
+// MB-46844:
+// Included by collections/vbucket_manifest.h, which in turn included
+// by 50+ other files.
+// Consider changing collections/vbucket_manifest.h to use pimpl for
+// Manifest::map which would avoid the need to include F14Map.h.
+#include <folly/container/F14Map.h>
+
+#include <map>
+#include <string>
