2d2168ba47595b0409e70777daf9d8bc76262a59
Dave Rigby <daver@couchbase.com>
2021-02-09 14:51:26 +0000

    MB-43818: Add kv_symbolize_backtrace.py
    Script to convert addresses from backtraces printed via
    print_backtrace() to symbol names.
    
    Accepts input from stdin or files provided on the command line; for
    each line checks to see if it matches the backtrace format and if so
    invokes gdb to symbolise the address.
    
    If the line doesn't match then simply passes that line through - as
    such this can be used as a filter on a complete log.
    
    Also accepts --source-line flag to print the source file and line
    details. This is omitted by default as it makes a very verbose output.
    
    Example usage:
    
        cat memcached.log | grep CRITICAL | ./kv_symbolize_backtrace.py
    
    Change-Id: I68d839e23703a402dd93fd99686854f14c53d3b0
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/145545
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ scripts/kv_symbolize_backtrace.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+"""Script to convert addresses from backtraces printed via print_backtrace()
+to symbol names.
+Accepts input from stdin or files provided on the command line; for each line
+checks to see if it matches the backtrace format and if so invokes gdb to
+symbolise the address.
+
+If the line doesn't match then simply passes that line through - as such this
+can be used as a filter on a complete log.
+
+Example usage:
+
+    cat memcached.log | grep CRITICAL | ./kv_symbolize_backtrace.py
+
+Note: Spawns GDB once for each matched line (as it must read the correct
+binary) so can be slow with large numbers of lines. In such as case just pass
+the script the specific lines to symbolify.
+"""
+
+from __future__ import print_function
+import argparse
+import fileinput
+import re
+import subprocess
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--source-line', action='store_true',
+                        help='Include source file + line details')
+    parser.add_argument('files', metavar='FILE', nargs='*',
+                        help='files to read, if empty, stdin is used')
+    args = parser.parse_args()
+
+    # Match a stack frame of the form, extracting the module and address;
+    # also capturing text before and after match
+    #     /lib64/libc.so.6(clone+0x6d) [0x7f77c18db000+0xfe8dd]
+    pattern = re.compile(
+        '(.*?\s+)([\w/\.\+]+)\([^\)]*\) \[(0x[0-9a-f]+)\+(0x[0-9a-f]+)\](.*?)')
+
+    for line in fileinput.input(
+            files=args.files if len(args.files) > 0 else ('-',)):
+        match = pattern.search(line)
+        if match:
+            (pre, binary, base_addr, offset, post) = match.groups()
+            # For relocatable binaries we should use the offset address, but
+            # for the main executable binary we should use base+offset.
+            # As a heuristic, Assume base > 0x400000 is relocable (0x400000 is
+            # normal base address for memcached on CentOS7).
+            if int(base_addr, 0) > 0x400000:
+                addr = int(offset, 0)
+            else:
+                addr = int(base_addr, 0) + int(offset, 0)
+            gdb_args = ['gdb', binary, '--batch', '-ex',
+                        'info symbol ' + hex(addr)]
+            if args.source_line:
+                gdb_args += ['-ex', 'printf "\t"', '-ex',
+                             'info line *' + hex(addr)]
+            out = subprocess.check_output(gdb_args, universal_newlines=True)
+            print(pre, out.strip(), post)
+        else:
+            print(line, end='')
