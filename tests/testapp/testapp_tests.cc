7f7b401e8de946934b118e5ad6524e9e1b87760a
James Harrison <00jamesh@gmail.com>
2021-06-11 12:13:27 +0100

    MB-46853: Include frameExtras in Request bodyLen validation
    FrameExtraslen was erroneously not included when validating that all
    the expected fields fit within the received bodyLen.
    
    Change-Id: I225b19e63fc17638433b19fac0316d99f2aa4d1b
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/155608
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Well-Formed: Restriction Checker

--- tests/testapp/testapp_tests.cc
+++ tests/testapp/testapp_tests.cc
@@ -48,6 +48,7 @@
 #include <platform/dirutils.h>
 #include <platform/socket.h>
 #include <platform/string_hex.h>
+#include <protocol/connection/frameinfo.h>
 #include <fstream>
 #include <gsl/gsl>
 
@@ -1012,6 +1013,65 @@ TEST_F(TestappTest, CollectionsSelectBucket) {
                               to_string(e.getReason());
         }
     }
+}
+
+/**
+ * Encode a Alt Request (0x08) HELO command with framing extras but
+ * too short total length field.
+ *
+ * See MB-46853.
+ */
+class MalformedHelloCommand : public BinprotHelloCommand {
+public:
+    MalformedHelloCommand() : BinprotHelloCommand("client") {
+        // To repro the issue, It does not matter _what_ frame infos are
+        // present, as long as there are two and they are valid.
+        addFrameInfo(BarrierFrameInfo());
+        addFrameInfo(BarrierFrameInfo());
+
+        // _NO_ Features are advertised; the value should be empty.
+    }
+
+    void encode(std::vector<uint8_t>& buf) const override {
+        BinprotHelloCommand::encode(buf);
+        // At this point
+        //   BodyLen = frameExtras + extras + key + value
+        //      8     =     2       +   0    +  6  +  0
+        auto& hdr = *reinterpret_cast<cb::mcbp::Request*>(buf.data());
+        auto realBodyLen = hdr.getBodylen();
+        EXPECT_EQ(8, realBodyLen);
+        // shorten the bodyLen by two bytes. This _should_ fail validation
+        // because now frameExtras + extras + key > BodyLen.
+        // This would mean they key would extend past the end of the
+        // request.
+        // MB-46853 - this was accepted because frameExtras were not counted
+        // leaving extras + key <= BodyLen
+        hdr.setBodylen(realBodyLen - 2);
+    }
+};
+
+TEST_F(TestappTest, MB_46853_TotalBodyLengthValidation) {
+    auto& conn = getConnection();
+
+    MalformedHelloCommand cmd;
+    // MB-46853: memcached segfault, connection reset.
+    // Good behaviour: validation catches malformed packet,
+    //                 memcached fine, connection reset.
+    try {
+        conn.execute(cmd);
+        // connection should be closed, reading the response should throw.
+        // Fail if that didn't happen.
+        FAIL();
+    } catch (const std::system_error& e) {
+        EXPECT_EQ(ECONNRESET, e.code().value());
+    }
+
+    // this will fail if memcached died because of the previous request.
+    conn.reconnect();
+
+    BinprotHelloCommand followup("StillAlive?");
+    const auto rsp = BinprotHelloResponse(conn.execute(followup));
+    EXPECT_EQ(cb::mcbp::Status::Success, rsp.getStatus());
 }
 
 INSTANTIATE_TEST_CASE_P(
