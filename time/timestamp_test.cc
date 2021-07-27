a6a7b3ceedd37a7f4eb3721558326d55c6330996
Trond Norbye <trond.norbye@gmail.com>
2021-07-27 12:10:40 +0200

    Refactor: Use gtest for timestamp_test
    Change-Id: I33ccd24e82f250d251aed72da5394046220ed7a8
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/158269
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Richard de Mellow <richard.demellow@couchbase.com>

--- time/timestamp_test.cc
+++ time/timestamp_test.cc
@@ -1,4 +1,3 @@
-/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 /*
  *     Copyright 2015-Present Couchbase, Inc.
  *
@@ -8,38 +7,23 @@
  *   software will be governed by the Apache License, Version 2.0, included in
  *   the file licenses/APL2.txt.
  */
+#include <folly/portability/GTest.h>
+#include <folly/portability/Stdlib.h>
+
 #include <memcached/isotime.h>
-#include <string.h>
 #include <chrono>
-#include <iomanip>
-#include <iostream>
-#include <sstream>
-#include <stdlib.h>
 
-static char env[80];
-
-int main()
-{
+/// Verify that a given input format generates the correct output (with
+/// timezone offset)
+TEST(IsoTime, TestFormat) {
     time_t now = 1426239360;
-    std::string expected("2015-03-13T02:36:00.000000-07:00");
-    std::string timezone("TZ=America/Los_Angeles");
+    const std::string expected("2015-03-13T02:36:00.000000-07:00");
 
 #ifdef _MSC_VER
-    timezone.assign("TZ=PST8PDT");
+    setenv("TZ", "PST8PDT", 1);
+#else
+    setenv("TZ", "America/Los_Angeles", 1);
 #endif
-
-    strcpy(env, timezone.c_str());
-    putenv(env);
-
     tzset();
-
-    std::string timestamp = ISOTime::generatetimestamp(now, 0);
-    if (timestamp != expected) {
-        std::cerr << "Comparison failed" << std::endl
-                  << "  Expected [" << expected << "]" << std::endl
-                  << "  got      [" << timestamp << "]" << std::endl;
-        return EXIT_FAILURE;
-    }
-
-    return EXIT_SUCCESS;
+    EXPECT_EQ(expected, ISOTime::generatetimestamp(now, 0));
 }
