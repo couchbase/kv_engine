0ad45241d04df929f395bb886f97f251397ce6e7
James Harrison <00jamesh@gmail.com>
2021-08-03 13:57:07 +0100

    MB-47092: Separate datatype stats from other aggregated stats
    Preliminary work for moving datatype stats to the high cardinality
    Prometheus endpoint, while leaving other more important stats in the
    low cardinality endpoint.
    
    This patch does not alter cbstats or Prometheus output.
    
    Change-Id: Ie18c86a5b5caab8013ed87c3de07b182580c734b
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/158647
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>

--- engines/ep/src/kv_bucket.h
+++ engines/ep/src/kv_bucket.h
@@ -28,6 +28,7 @@
 class DurabilityCompletionTask;
 class ReplicationThrottle;
 class VBucketCountVisitor;
+class DatatypeStatVisitor;
 namespace Collections {
 class Manager;
 }
@@ -802,6 +803,14 @@ protected:
             const VBucketCountVisitor& pending,
             const VBucketCountVisitor& dead,
             const BucketStatCollector& collector);
+
+    /**
+     * Helper method used by getAggregatedVBucketStats to output aggregated
+     * datatype stats.
+     */
+    void appendDatatypeStats(const DatatypeStatVisitor& active,
+                             const DatatypeStatVisitor& replica,
+                             const BucketStatCollector& collector);
 
     /**
      * Returns the callback function to be invoked when SyncWrite(s) have been
