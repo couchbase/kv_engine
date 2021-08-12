22d1c5bd9d19e2bd99b36a4a79f0e947d52bfa9e
Dave Rigby <daver@couchbase.com>
2021-08-12 12:32:12 +0100

    MB-47790: Propagate KVStore::scan errors from DCPBackfillDisk
    If a KVStore detects corruption during a scan, it fails and an error
    is returned to the caller. However in the case of DCPBackfillDisk, it
    did not check for such an error and instead incorrectly assumed the
    scan had completed successfully. This resulted in a rebalance hang as
    no more mutations were sent by the Producer to complete the backfill.
    
    Address this by correctly propogating the error back to the DCP stream
    by setting it to dead(BACKFILL_FAIL).
    
    Change-Id: I8ce618bceaf8e140adb0a94f6c746440b913202f
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/159767
    Reviewed-by: Jim Walker <jim@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

---
+++ engines/ep/tests/mock/mock_kvstore.h
@@ -0,0 +1,180 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2021 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+#pragma once
+
+#include "kvstore.h"
+#include "kvstore_config.h"
+#include "rollback_result.h"
+#include "vb_commit.h"
+#include "vbucket_bgfetch_item.h"
+#include "vbucket_state.h"
+#include <folly/portability/GMock.h>
+
+class KVBucket;
+struct TransactionContext;
+
+class MockKVStore : public KVStore {
+public:
+    MockKVStore();
+    ~MockKVStore() override;
+
+    MOCK_METHOD(void, deinitialize, (), (override));
+    MOCK_METHOD(void,
+                addStats,
+                (const AddStatFn& add_stat,
+                 const void* c,
+                 const std::string& args),
+                (override));
+    MOCK_METHOD(bool,
+                getStat,
+                (std::string_view name, size_t& value),
+                (const, override));
+    MOCK_METHOD(GetStatsMap,
+                getStats,
+                (gsl::span<const std::string_view> keys),
+                (const, override));
+    MOCK_METHOD(void,
+                addTimingStats,
+                (const AddStatFn& add_stat, const void* c),
+                (override));
+    MOCK_METHOD(bool, commit, (VB::Commit & commitData), (override));
+    MOCK_METHOD(void, rollback, (), (override));
+    MOCK_METHOD(StorageProperties, getStorageProperties, (), (override));
+    MOCK_METHOD(void, set, (queued_item item), (override));
+    MOCK_METHOD(GetValue,
+                get,
+                (const DiskDocKey& key, Vbid vb, ValueFilter filter),
+                (override));
+    MOCK_METHOD(GetValue,
+                getWithHeader,
+                (const KVFileHandle& kvFileHandle,
+                 const DiskDocKey& key,
+                 Vbid vb,
+                 ValueFilter filter),
+                (override));
+    MOCK_METHOD(void, setMaxDataSize, (size_t size), (override));
+    MOCK_METHOD(void,
+                getMulti,
+                (Vbid vb, vb_bgfetch_queue_t& itms),
+                (override));
+    MOCK_METHOD(void,
+                getRange,
+                (Vbid vb,
+                 const DiskDocKey& startKey,
+                 const DiskDocKey& endKey,
+                 ValueFilter filter,
+                 const GetRangeCb& cb),
+                (override));
+    MOCK_METHOD(void, del, (queued_item item), (override));
+    MOCK_METHOD(void, delVBucket, (Vbid vbucket, uint64_t fileRev), (override));
+    MOCK_METHOD(std::vector<vbucket_state*>,
+                listPersistedVbuckets,
+                (),
+                (override));
+    MOCK_METHOD(bool,
+                snapshotVBucket,
+                (Vbid vbucketId, const vbucket_state& vbstate),
+                (override));
+    MOCK_METHOD(bool,
+                compactDB,
+                (std::unique_lock<std::mutex> & vbLock,
+                 std::shared_ptr<CompactionContext> c),
+                (override));
+    MOCK_METHOD(void,
+                abortCompactionIfRunning,
+                (std::unique_lock<std::mutex> & vbLock, Vbid vbid),
+                (override));
+    MOCK_METHOD(vbucket_state*, getCachedVBucketState, (Vbid vbid), (override));
+    MOCK_METHOD(vbucket_state,
+                getPersistedVBucketState,
+                (Vbid vbid),
+                (override));
+    MOCK_METHOD(size_t, getNumPersistedDeletes, (Vbid vbid), (override));
+    MOCK_METHOD(DBFileInfo, getDbFileInfo, (Vbid dbFileId), (override));
+    MOCK_METHOD(DBFileInfo, getAggrDbFileInfo, (), (override));
+    MOCK_METHOD(size_t, getItemCount, (Vbid vbid), (override));
+    MOCK_METHOD(RollbackResult,
+                rollback,
+                (Vbid vbid,
+                 uint64_t rollbackseqno,
+                 std::unique_ptr<RollbackCB>),
+                (override));
+    MOCK_METHOD(void, pendingTasks, (), (override));
+    MOCK_METHOD(cb::engine_errc,
+                getAllKeys,
+                (Vbid vbid,
+                 const DiskDocKey& start_key,
+                 uint32_t count,
+                 std::shared_ptr<StatusCallback<const DiskDocKey&>> cb),
+                (override));
+    MOCK_METHOD(bool, supportsHistoricalSnapshots, (), (const, override));
+    MOCK_METHOD(std::unique_ptr<BySeqnoScanContext>,
+                initBySeqnoScanContext,
+                (std::unique_ptr<StatusCallback<GetValue>> cb,
+                 std::unique_ptr<StatusCallback<CacheLookup>> cl,
+                 Vbid vbid,
+                 uint64_t startSeqno,
+                 DocumentFilter options,
+                 ValueFilter valOptions,
+                 SnapshotSource source),
+                (override));
+    MOCK_METHOD(std::unique_ptr<ByIdScanContext>,
+                initByIdScanContext,
+                (std::unique_ptr<StatusCallback<GetValue>> cb,
+                 std::unique_ptr<StatusCallback<CacheLookup>> cl,
+                 Vbid vbid,
+                 const std::vector<ByIdRange>& ranges,
+                 DocumentFilter options,
+                 ValueFilter valOptions),
+                (override));
+    MOCK_METHOD(scan_error_t, scan, (BySeqnoScanContext & sctx), (override));
+    MOCK_METHOD(scan_error_t, scan, (ByIdScanContext & sctx), (override));
+    MOCK_METHOD(std::unique_ptr<KVFileHandle>,
+                makeFileHandle,
+                (Vbid vbid),
+                (override));
+    MOCK_METHOD((std::pair<bool, Collections::VB::PersistedStats>),
+                getCollectionStats,
+                (const KVFileHandle& kvFileHandle, CollectionID collection),
+                (override));
+    MOCK_METHOD((std::pair<bool, Collections::KVStore::Manifest>),
+                getCollectionsManifest,
+                (Vbid vbid),
+                (override));
+    MOCK_METHOD(
+            (std::pair<bool,
+                       std::vector<Collections::KVStore::DroppedCollection>>),
+            getDroppedCollections,
+            (Vbid vbid),
+            (override));
+    MOCK_METHOD(const KVStoreConfig&, getConfig, (), (const, override));
+    MOCK_METHOD(void,
+                setStorageThreads,
+                (ThreadPoolConfig::StorageThreadCount num),
+                (override));
+    MOCK_METHOD(uint64_t, prepareToDeleteImpl, (Vbid vbid), (override));
+    MOCK_METHOD(void, prepareToCreateImpl, (Vbid vbid), (override));
+
+    /**
+     * Helper function to replace the existing read-only KVStore in the given
+     * bucket & shard with a new MockKVStore instance. Returns a reference to
+     * the created mock.
+     */
+    static MockKVStore& replaceROKVStoreWithMock(KVBucket& bucket,
+                                                 size_t shardId);
+};
---
+++ engines/ep/tests/module_tests/backfill_disk_test.cc
@@ -0,0 +1,99 @@
+/* -*- Mode: C++; tab-width: 4; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+/*
+ *     Copyright 2021 Couchbase, Inc
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ */
+
+/**
+ * Tests for DCPBackfillDisk class.
+ */
+
+#include "dcp/backfill-manager.h"
+#include "dcp/response.h"
+#include "evp_store_single_threaded_test.h"
+#include "test_helpers.h"
+#include "tests/mock/mock_dcp_producer.h"
+#include "tests/mock/mock_kvstore.h"
+#include "tests/mock/mock_stream.h"
+
+using namespace ::testing;
+
+/// Test fixture for DCPBackfillDisk class tests.
+class DCPBackfillDiskTest : public SingleThreadedEPBucketTest {};
+
+/**
+ * Regression test for MB-47790 - if a backfill fails during the scan() phase
+ * due to disk issues, the stream should be closed (and not left stuck at
+ * the last read seqno).
+ */
+TEST_F(DCPBackfillDiskTest, ScanDiskError) {
+    // Store an items, create new checkpoint and flush so we have something to
+    // backfill from disk
+    setVBucketStateAndRunPersistTask(vbid, vbucket_state_active);
+    store_item(vbid, makeStoredDocKey("key1"), "value");
+    flushAndRemoveCheckpoints(vbid);
+
+    // Setup expectations on mock KVStore - expect to initialise the scan
+    // context, then a scan() call which we cause to fail, followed by destroy
+    // of scan context.
+    auto& mockKVStore = MockKVStore::replaceROKVStoreWithMock(*store, 0);
+    EXPECT_CALL(mockKVStore, initBySeqnoScanContext(_, _, _, _, _, _, _))
+            .Times(1);
+    EXPECT_CALL(mockKVStore, scan(An<BySeqnoScanContext&>()))
+            .WillOnce(Return(scan_failed));
+
+    // Create producer now we have items only on disk.
+    auto producer = std::make_shared<MockDcpProducer>(
+            *engine, cookie, "test-producer", 0 /*flags*/, false /*startTask*/);
+
+    auto stream =
+            std::make_shared<MockActiveStream>(engine.get(),
+                                               producer,
+                                               DCP_ADD_STREAM_FLAG_DISKONLY,
+                                               0,
+                                               *engine->getVBucket(vbid),
+                                               0,
+                                               1,
+                                               0,
+                                               0,
+                                               0,
+                                               IncludeValue::Yes,
+                                               IncludeXattrs::Yes,
+                                               IncludeDeletedUserXattrs::No,
+                                               std::string{});
+    stream->setActive();
+    ASSERT_TRUE(stream->isBackfilling());
+
+    // Initialise the backfill of this VBucket (performs initial scan but
+    // doesn't read any data yet).
+    auto& bfm = producer->getBFM();
+    ASSERT_EQ(backfill_success, bfm.backfill());
+    auto backfillRemaining = stream->getNumBackfillItemsRemaining();
+    ASSERT_TRUE(backfillRemaining);
+
+    // Test - run backfill scan step. Backfill should fail early as scan()
+    // has been configured to return scan_failed.
+    bfm.backfill();
+
+    // Verify state - stream should have been marked dead, and EndStreamResponse
+    // added to ready queue indicating the disk backfill failed.
+    EXPECT_FALSE(stream->isActive());
+    ASSERT_EQ(1, stream->public_readyQSize());
+    auto response = stream->public_popFromReadyQ();
+    EXPECT_EQ(DcpResponse::Event::StreamEnd, response->getEvent());
+    auto* streamEndResp = dynamic_cast<StreamEndResponse*>(response.get());
+    ASSERT_NE(nullptr, streamEndResp);
+    EXPECT_EQ(cb::mcbp::DcpStreamEndStatus::BackfillFail,
+              streamEndResp->getFlags());
+}
