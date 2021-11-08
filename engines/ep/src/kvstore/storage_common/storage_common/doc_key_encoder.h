89f2e7ddaa1d6d2bbcb258b6dc3b1e48fb0a0fa0
Ben Huddleston <ben.huddleston@couchbase.com>
2021-11-08 15:41:11 +0000

    MB-48684: Move encodeDocKey to storage_common
    So that it can be re-used in magma
    
    Change-Id: I18e2a64a494e298d4ef03cd937361c1338b67194
    Reviewed-on: http://review.couchbase.org/c/kv_engine/+/165258
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

---
+++ engines/ep/src/kvstore/storage_common/storage_common/doc_key_encoder.h
@@ -0,0 +1,36 @@
+/*
+ *   Copyright 2021-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+#pragma once
+
+#include <mcbp/protocol/unsigned_leb128.h>
+
+#include <string>
+
+std::string encodeDocKey(std::string_view key,
+                         std::string_view collection,
+                         bool prepare) {
+    std::string ret;
+
+    if (prepare) {
+        auto leb128 = cb::mcbp::unsigned_leb128<uint64_t>(2);
+        ret.append(leb128.begin(), leb128.end());
+    }
+
+    auto cid = 0;
+    if (!collection.empty()) {
+        cid = stoull(std::string(collection));
+    }
+    auto leb128 = cb::mcbp::unsigned_leb128<uint64_t>(cid);
+    ret.append(leb128.begin(), leb128.end());
+    ret.append(key);
+
+    return ret;
+}
