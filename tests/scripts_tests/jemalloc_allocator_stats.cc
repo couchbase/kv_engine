7f8f271289b99f42762031bcbf74761f95298a88
Jim Walker <jim@couchbase.com>
2021-12-16 11:33:55 +0000

    Remove leak from jemalloc_allocator_stats
    If this program is compiled with ASAN we may see CV failures
    
    Change-Id: I97ddc33710c34c93a7c2b7f271104ca65849492d
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/167750
    Tested-by: Dave Rigby <daver@couchbase.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- tests/scripts_tests/jemalloc_allocator_stats.cc
+++ tests/scripts_tests/jemalloc_allocator_stats.cc
@@ -3,14 +3,23 @@
 #include <platform/cb_malloc.h>
 #include <jemalloc/jemalloc.h>
 
+#include <array>
+
 int main() {
-    for (size_t i = 1; i < 1000; i++) {
-        void* x = cb_malloc(i * 100);
-        (void)x;
+    std::array<void*, 1000> allocations;
+    // allocate a variety of different sizes via cb_malloc (so that they do go
+    // via jemalloc when configured)
+    for (size_t i = 0; i < 1000; i++) {
+        allocations[i] = cb_malloc(i * 100);
     }
 
     // Dump allocator statistics to stderr.
     je_malloc_stats_print(nullptr, nullptr, nullptr);
+
+    // And clean up to avoid ASAN warnings
+    for (size_t i = 0; i < 1000; i++) {
+        cb_free(allocations[i]);
+    }
 
     return 0;
 }
