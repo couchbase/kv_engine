7b771c4cce5eb7d832fccca3acd7b5a6a83d4ee0
Douglas <douglas.3598032@couchbase.com>
2022-03-28 14:50:58 +0100

    Refactor: avoid duplication of getBucket() call
    connection.getBucket() is already defined as a variable 'bucket'.
    Use this instead of repeating the call.
    
    Change-Id: Iaefaffdb3d19c786d7487ade16f2b75a655482e0
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/172902
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- daemon/protocol/mcbp/stats_context.cc
+++ daemon/protocol/mcbp/stats_context.cc
@@ -762,7 +762,7 @@ cb::engine_errc StatsCommandContext::commandComplete() {
         // stats for these.
         break;
     default:
-        ++connection.getBucket().responseCounters[int(
+        ++bucket.responseCounters[int(
                 cb::mcbp::to_status(cb::engine_errc(command_exit_code)))];
         break;
     }
