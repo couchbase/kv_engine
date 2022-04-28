9018ab17b1a543dda7333dd963e2a56e2f16f517
Ben Huddleston <ben.huddleston@couchbase.com>
2022-04-28 15:31:03 +0100

    [BP] MB-51968: Remove DCP stats from stats.json
    We potentially spike memory usage by hundreds of MB to grab DCP stats
    when we have (tens/hundreds of) thousands of streams and the stats are
    not particularly useful as they aren't even captured in a cbcollect.
    
    Change-Id: I75660786813597c2f676faca16df9d22a56fa006
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/174452
    Well-Formed: Restriction Checker
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/src/kv_bucket.cc
+++ engines/ep/src/kv_bucket.cc
@@ -1236,7 +1236,6 @@ void KVBucket::snapshotStats(bool shuttingDown) {
     cb::tracing::Traceable traceable;
     bool rv = engine.getStats(&traceable, {}, {}, snapshot_add_stat) ==
                       cb::engine_errc::success;
-    engine.doDcpStatsInner(nullptr, snapshot_add_stat, {});
 
     nlohmann::json snapshotStats(statsMap);
     if (rv && shuttingDown) {
