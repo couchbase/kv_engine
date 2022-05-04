0b32603c93712c6dd13a7614de8173f00ed5c5df
Douglas <douglas.3598032@couchbase.com>
2022-05-04 14:41:03 +0100

    MB-51958: Disable STItemPagerTest.ReplicaEvictedBeforeActive
    Following frequent failures of the test, disable it while it is
    rewritten.
    
    Change-Id: I0fd5d6a68d21e460f1d34c16918e68437196dbfb
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/174460
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/tests/module_tests/item_pager_test.cc
+++ engines/ep/tests/module_tests/item_pager_test.cc
@@ -1095,7 +1095,8 @@ TEST_P(STItemPagerTest, EvictBGFetchedDeletedItem) {
     checkItemStatePostFetch(false /*expect item to not exist*/);
 }
 
-TEST_P(STItemPagerTest, ReplicaEvictedBeforeActive) {
+// See MB-51958, disabled due to consistent failures
+TEST_P(STItemPagerTest, DISABLED_ReplicaEvictedBeforeActive) {
     // MB-40531 test that the item pager does not evict from active vbuckets
     // if evicting from replicas can reclaim enough memory to reach the low
     // water mark (determined when the item pager first runs).
