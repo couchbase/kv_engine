f1b852f5e0c24cffbb0a07da34b8f5c359b31ae4
Richard de Mellow <richard.demellow@couchbase.com>
2022-05-13 11:56:16 +0100

    Modernize genconfig.cc to make it easier to read
    1. Use auto keyword where applicable
    2. Use for each style loops where applicable to reduce the need to use
       iterators.
    3. Move to use fmt over std::string stream and std::err, as it's clear
       to read when deciphering what will be rendered to a string.
    4. Replace .compare() with == or !=
    5. Generated code will static_cast<> over c style casting
    
    Change-Id: I9ca3dc78b5151cb0f7caf391b21d759f570281b9
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/174792
    Tested-by: Richard de Mellow <richard.demellow@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/tools/genconfig.cc
+++ engines/ep/tools/genconfig.cc
@@ -11,22 +11,21 @@
 
 #include "utilities/readfile.h"
 
+#include <fmt/format.h>
 #include <json_utilities.h>
-#include <platform/dirutils.h>
 #include <nlohmann/json.hpp>
-#include <string>
-#include <sstream>
-#include <iostream>
+#include <platform/dirutils.h>
 #include <fstream>
 #include <map>
+#include <string>
 
-std::stringstream prototypes;
-std::stringstream initialization;
-std::stringstream implementation;
-std::stringstream addStatImplementation;
+std::string prototypes;
+std::string initialization;
+std::string implementation;
+std::string addStatImplementation;
 
-typedef std::string (*getValidatorCode)(const std::string&,
-                                        const nlohmann::json&);
+using getValidatorCode = std::string (*)(const std::string&,
+                                         const nlohmann::json&);
 
 std::map<std::string, getValidatorCode> validators;
 std::map<std::string, std::string> datatypes;
@@ -36,8 +35,10 @@ static std::string getDatatype(const std::string& key,
     auto ret = json["type"].get<std::string>();
     auto iter = datatypes.find(ret);
     if (iter == datatypes.end()) {
-        std::cerr << "Invalid datatype specified for \"" << key << "\": " << ret
-                  << std::endl;
+        fmt::print(stderr,
+                   "Invalid datatype specified for \"{}\": {}\n",
+                   key,
+                   ret);
         exit(1);
     }
 
@@ -53,10 +54,11 @@ static std::string getRangeValidatorCode(const std::string& key,
     auto min = first->find("min");
     auto max = first->find("max");
     if (min == first->end() && max == first->end()) {
-        std::cerr << "Incorrect syntax for a range validator specified for"
-                  << "\"" << key << "\"." << std::endl
-                  << "You need at least one of a min or a max clause."
-                  << std::endl;
+        fmt::print(
+                stderr,
+                "Incorrect syntax for a range validator specified for\"{}\".\n"
+                "You need at least one of a min or a max clause.\n",
+                key);
         exit(1);
     }
 
@@ -65,9 +67,10 @@ static std::string getRangeValidatorCode(const std::string& key,
         !(min->type() == nlohmann::json::value_t::number_integer ||
           min->type() == nlohmann::json::value_t::number_unsigned ||
           min->type() == nlohmann::json::value_t::number_float)) {
-        std::cerr << "Incorrect datatype for the range validator specified for "
-                  << "\"" << key << "\"." << std::endl
-                  << "Only numbers are supported." << std::endl;
+        fmt::print(stderr,
+                   "Incorrect datatype for the range validator specified for "
+                   "\"{}\"\nOnly numbers are supported.\n",
+                   key);
         exit(1);
     }
 
@@ -78,9 +81,10 @@ static std::string getRangeValidatorCode(const std::string& key,
           max->type() == nlohmann::json::value_t::number_float ||
           (max->type() == nlohmann::json::value_t::string &&
            max->get<std::string>() == "NUM_CPU"))) {
-        std::cerr << "Incorrect datatype for the range validator specified for "
-                  << "\"" << key << "\"." << std::endl
-                  << "Only numbers are supported." << std::endl;
+        fmt::print(stderr,
+                   "Incorrect datatype for the range validator specified for "
+                   "\"{}\"\nOnly numbers are supported.\n",
+                   key);
         exit(1);
     }
 
@@ -130,9 +134,8 @@ static std::string getRangeValidatorCode(const std::string& key,
         }
     }
 
-    std::string out = "(new " + validator_type + "())->min(" + mins +
-                      ")->max(" + maxs + ")";
-    return out;
+    return fmt::format(
+            "(new {}())->min({})->max({})", validator_type, mins, maxs);
 }
 
 static std::string getEnumValidatorCode(const std::string& key,
@@ -142,33 +145,37 @@ static std::string getEnumValidatorCode(const std::string& key,
     auto first = validator.begin();
 
     if (first->type() != nlohmann::json::value_t::array) {
-        std::cerr << "Incorrect enum value for " << key
-                  << ".  Array of values is required." << std::endl;
+        fmt::print(
+                stderr,
+                "Incorrect enum value for {}.  Array of values is required.\n",
+                key);
         exit(1);
     }
 
     if (first->empty()) {
-        std::cerr << "At least one validator enum element is required (" << key
-                  << ")" << std::endl;
+        fmt::print(stderr,
+                   "At least one validator enum element is required ({})\n",
+                   key);
         exit(1);
     }
 
-    std::stringstream ss;
-    ss << "(new EnumValidator())";
+    std::string out("(new EnumValidator())");
 
     for (auto& obj : *first) {
         if (obj.type() != nlohmann::json::value_t::string) {
-            std::cerr << "Incorrect validator for " << key
-                      << ", all enum entries must be strings." << std::endl;
+            fmt::print(stderr,
+                       "Incorrect validator for {}, all enum entries must be "
+                       "strings.\n",
+                       key);
             exit(1);
         }
-        ss << "\n\t\t->add(\"" << obj.get<std::string>() << "\")";
+        out += fmt::format("\n\t\t->add(\"{}\")", obj.get<std::string>());
     }
-    return ss.str();
+    return out;
 }
 
 static void initialize() {
-    const char* header = R"(/*
+    std::string_view header(R"(/*
  *     Copyright 2020-Present Couchbase, Inc.
  *
  *   Use of this software is governed by the Business Source License included
@@ -181,11 +188,11 @@ static void initialize() {
 // ###########################################
 // # DO NOT EDIT! THIS IS A GENERATED FILE
 // ###########################################
-)";
+)");
 
-    prototypes << header;
-
-    implementation << header << R"(
+    prototypes += header;
+    implementation += header;
+    implementation += std::string_view{R"(
 #include "configuration.h"
 #include "configuration_impl.h"
 #include <platform/sysinfo.h>
@@ -194,7 +201,7 @@ static void initialize() {
 
 using namespace std::string_literals;
 
-)";
+)"};
 
     validators["range"] = getRangeValidatorCode;
     validators["enum"] = getEnumValidatorCode;
@@ -210,7 +217,7 @@ static bool isReadOnly(const nlohmann::json& json) {
     try {
         return !cb::jsonGet<bool>(json, "dynamic");
     } catch (const nlohmann::json::exception& e) {
-        std::cerr << e.what() << std::endl;
+        fmt::print(stderr, "{}\n", e.what());
         exit(1);
     }
     return false;
@@ -256,8 +263,7 @@ static std::string getValidator(const std::string& key,
 
     // Abort early if the validator is bad
     if (validator->size() != 1) {
-        std::cerr << "Only one validator can be specified for " << key
-                  << std::endl;
+        fmt::print(stderr, "Only one validator can be specified for {}\n", key);
         exit(1);
     }
 
@@ -268,8 +274,10 @@ static std::string getValidator(const std::string& key,
     std::map<std::string, getValidatorCode>::iterator iter;
     iter = validators.find(first.key());
     if (iter == validators.end()) {
-        std::cerr << "Unknown validator specified for \"" << key << "\": \""
-                  << first->get<std::string>() << "\"" << std::endl;
+        fmt::print(stderr,
+                   "Unknown validator specified for \"{}\": \"{}\"\n",
+                   key,
+                   first->get<std::string>());
         exit(1);
     }
 
@@ -295,18 +303,18 @@ static std::string getRequirements(const std::string& key,
         return "";
     }
 
-    std::ostringstream ss;
-
-    ss << "(new Requirement)\n";
+    std::string out("(new Requirement)\n");
 
     for (auto req : requirements->items()) {
         auto reqKey = req.key();
 
         auto reqParam = params.find(key);
         if (reqParam == params.end()) {
-            std::cerr << "Required parameter \"" << reqKey
-                      << "\" for parameter \"" << key << "\" does not exist"
-                      << std::endl;
+            fmt::print(stderr,
+                       "Required parameter \"{}\" for parameter \"{}\" does "
+                       "not exist\n",
+                       reqKey,
+                       key);
             exit(1);
         }
 
@@ -339,108 +347,101 @@ static std::string getRequirements(const std::string& key,
                     "getRequirements(): binary value not supported");
         }
 
-        ss << "        ->add(\"" << reqKey << "\", (" << type << ")" << value
-           << ")";
+        out += fmt::format("        ->add(\"{}\",({}){})", reqKey, type, value);
     }
 
-    return ss.str();
+    return out;
 }
 
 static std::string getGetterPrefix(const std::string& str) {
-    if (str.compare("bool") == 0) {
-        return "is";
-    } else {
-        return "get";
-    }
+    return str == "bool" ? "is" : "get";
 }
 
 static std::string getCppName(const std::string& str) {
-    std::stringstream ss;
+    std::string out;
     bool doUpper = true;
 
-    std::string::const_iterator iter;
-    for (iter = str.begin(); iter != str.end(); ++iter) {
-        if (*iter == '_') {
+    for (const char& c : str) {
+        if (c == '_') {
             doUpper = true;
         } else {
             if (doUpper) {
-                ss << (char)toupper(*iter);
+                out += toupper(c);
                 doUpper = false;
             } else {
-                ss << (char)*iter;
+                out += c;
             }
         }
     }
-    return ss.str();
+    return out;
 }
 
 static void generate(const nlohmann::json& params, const std::string& key) {
-    std::string cppName = getCppName(key);
+    auto cppName = getCppName(key);
 
     auto json = params[key];
-    std::string type = getDatatype(key, json);
-    std::string defaultVal = json["default"].get<std::string>();
+    auto type = getDatatype(key, json);
+    auto defaultVal = json["default"].get<std::string>();
 
-    if (defaultVal.compare("max") == 0 || defaultVal.compare("min") == 0) {
-        if (type.compare("std::string") != 0) {
-            std::stringstream ss;
-            ss << "std::numeric_limits<" << type << ">::" << defaultVal << "()";
-            defaultVal = ss.str();
-        }
+    if ((defaultVal == "max" || defaultVal == "min") && type != "std::string") {
+        defaultVal =
+                fmt::format("std::numeric_limits<{}>::{}()", type, defaultVal);
     }
 
-    std::string validator = getValidator(key, json);
-    std::string requirements = getRequirements(key, json, params);
+    auto validator = getValidator(key, json);
+    auto requirements = getRequirements(key, json, params);
 
     // Generate prototypes
-    prototypes << "    " << type << " " << getGetterPrefix(type) << cppName
-               << "() const;" << std::endl;
+    prototypes += fmt::format(
+            "    {} {}{}() const;\n", type, getGetterPrefix(type), cppName);
     const auto dynamic = !isReadOnly(json);
 
     if (dynamic) {
-        prototypes << "    void set" << cppName << "(const " << type
-                   << " &nval);" << std::endl;
+        prototypes +=
+                fmt::format("    void set{}(const {} &nval);\n", cppName, type);
     }
 
     // Generate initialization code
-    initialization << "    addParameter(\"" << key << "\", " << std::boolalpha;
-    if (type == "std::string") {
-        initialization << "\"" << defaultVal << "\"s, ";
-    } else {
-        initialization << type << "(" << defaultVal << "), ";
-    }
-    initialization << dynamic << ");" << std::endl;
+    initialization += fmt::format(
+            "    addParameter(\"{}\", {}, {});\n",
+            key,
+            type == "std::string"
+                    ? fmt::format("\"{}\"s", defaultVal)
+                    : fmt::format("static_cast<{}>({})", type, defaultVal),
+            dynamic);
 
     if (!validator.empty()) {
-        initialization << "    setValueValidator(\"" << key << "\", "
-                       << validator << ");" << std::endl;
+        initialization += fmt::format(
+                "    setValueValidator(\"{}\", {});\n", key, validator);
     }
     if (!requirements.empty()) {
-        initialization << "    setRequirements(\"" << key << "\", "
-                       << requirements << ");" << std::endl;
+        initialization += fmt::format(
+                "    setRequirements(\"{}\", {});\n", key, requirements);
     }
     if (hasAliases(json)) {
-        for (std::string alias : getAliases(json)) {
-            initialization << "    addAlias(\"" << key << "\", \"" << alias
-                           << "\");" << std::endl;
+        for (const auto& alias : getAliases(json)) {
+            initialization +=
+                    fmt::format("    addAlias(\"{}\", \"{}\");\n", key, alias);
         }
     }
 
     // Generate the getter
-    implementation << type << " Configuration::" << getGetterPrefix(type)
-                   << cppName << "() const {" << std::endl
-                   << "    return "
-                   << "getParameter<" << datatypes[type] << ">(\"" << key
-                   << "\");" << std::endl
-                   << "}" << std::endl;
-
+    implementation += fmt::format(
+            "{} Configuration::{}{}() const {{\n    "
+            "return getParameter<{}>(\"{}\");\n}}\n",
+            type,
+            getGetterPrefix(type),
+            cppName,
+            datatypes[type],
+            key);
     if (!isReadOnly(json)) {
         // generate the setter
-        implementation << "void Configuration::set" << cppName << "(const "
-                       << type << " &nval) {" << std::endl
-                       << "    setParameter(\"" << key << "\", nval);"
-                       << std::endl
-                       << "}" << std::endl;
+        implementation += fmt::format(
+                "void Configuration::set{}(const {} &nval) {{\n"
+                "    setParameter(\"{}\", nval);\n}}\n",
+                cppName,
+                type,
+                key);
     }
 
     // collect all the aliases
@@ -451,10 +452,11 @@ static void generate(const nlohmann::json& params, const std::string& key) {
     // and the main config param name
     names.push_back(key);
     // add to the definition of Configuration::addStat
-    for (std::string name : names) {
-        addStatImplementation << "    "
-                              << "maybeAddStat(collector, Key::ep_" << name
-                              << ", \"" << name << "\"sv);" << std::endl;
+    for (const auto& name : names) {
+        addStatImplementation += fmt::format(
+                "    maybeAddStat(collector, Key::ep_{}, \"{}\"sv);\n",
+                name,
+                name);
     }
 }
 
@@ -464,14 +466,15 @@ static void generate(const nlohmann::json& params, const std::string& key) {
  */
 int main(int argc, char **argv) {
     if (argc < 4) {
-        std::cerr << "Usage: " << argv[0] << " "
-                  << "<input config file> <header> <source>\n";
+        fmt::print(stderr,
+                   "Usage: {} <input config file> <header> <source>\n",
+                   argv[0]);
         return 1;
     }
 
-    const char* file = argv[1];
-    const char* header = argv[2];
-    const char* source = argv[3];
+    const std::string file = argv[1];
+    const std::string header = argv[2];
+    const std::string source = argv[3];
 
     initialize();
 
@@ -479,13 +482,13 @@ int main(int argc, char **argv) {
     try {
         json = nlohmann::json::parse(readFile(file));
     } catch (const nlohmann::json::exception& e) {
-        std::cerr << "Failed to parse JSON. e.what()=" << e.what() << std::endl;
+        fmt::print(stderr, "Failed to parse JSON. e.what()=\n", e.what());
         return 1;
     }
 
     auto params = json.find("params");
     if (params == json.end()) {
-        std::cerr << "FATAL: could not find \"params\" section" << std::endl;
+        fmt::print(stderr, "FATAL: could not find \"params\" section\n");
         return 1;
     }
 
@@ -495,20 +498,20 @@ int main(int argc, char **argv) {
 
     std::ofstream headerfile(header);
     if (!headerfile.is_open()) {
-        std::cerr << "Unable to create header file : " << header << std::endl;
+        fmt::print(stderr, "Unable to create header file : {}\n", header);
         return 1;
     }
-    headerfile << prototypes.str();
+    headerfile << prototypes;
     headerfile.close();
 
     std::ofstream implfile(source);
     if (!implfile.is_open()) {
-        std::cerr << "Unable to create source file : " << header << std::endl;
+        fmt::print(stderr, "Unable to create source file : {}\n", header);
         return 1;
     }
-    implfile << implementation.str() << std::endl
+    implfile << implementation << std::endl
              << "void Configuration::initialize() {" << std::endl
-             << initialization.str() << "}" << std::endl
+             << initialization << "}" << std::endl
              << std::endl
              << "void Configuration::addStats(const BucketStatCollector& "
                 "collector) const {"
@@ -516,7 +519,7 @@ int main(int argc, char **argv) {
              << "    using namespace cb::stats;" << std::endl
              << "    using namespace std::string_view_literals;" << std::endl
              << "    std::lock_guard<std::mutex> lh(mutex);" << std::endl
-             << addStatImplementation.str() << "}" << std::endl;
+             << addStatImplementation << "}" << std::endl;
     implfile.close();
 
     return 0;
