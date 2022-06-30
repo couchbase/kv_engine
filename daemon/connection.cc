d1880021a4094272a665aade15a8a4b721538088
Trond Norbye <trond.norbye@gmail.com>
2022-06-30 12:48:35 +0200

    [refactor] Use early return in tryToProgressDcpStream()
    Change-Id: I415b1414f128037b7b767277d3702fa440a31084
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/176915
    Reviewed-by: Dave Rigby <daver@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- daemon/connection.cc
+++ daemon/connection.cc
@@ -706,66 +706,70 @@ void Connection::tryToProgressDcpStream() {
                 "Connection::executeCommandsCallback(): no cookies "
                 "available!");
     }
-    if (cookies.front()->empty()) {
-        // make sure we reset the privilege context
-        cookies.front()->reset();
 
-        // MB-38007: We see an increase in rebalance time for
-        // "in memory" workloads when allowing DCP to fill up to
-        // 40MB (thats the default) batch size into the output buffer.
-        // We've not been able to figure out exactly _why_ this is
-        // happening and have assumptions that it may be caused
-        // that it doesn't align too much with the flow control being
-        // used. Before moving to bufferevent we would copy the entire
-        // message into kernel space before trying to read (and process)
-        // any input messages before trying to send the next one.
-        // It could be that it would be better at processing the
-        // incoming flow control messages instead of the current
-        // model where the input socket gets drained and put in
-        // userspace buffers, the send queue is tried to be drained
-        // before we do the callback and process the already queued
-        // input and generate more output before returning to the
-        // layer doing the actual IO.
-        std::size_t dcpMaxQSize =
-                (dcpFlowControlBufferSize == 0)
-                        ? Settings::instance().getMaxSendQueueSize()
-                        : 1024 * 1024;
-        bool more = (getSendQueueSize() < dcpMaxQSize);
-        if (type == Type::Consumer) {
-            // We want the consumer to perform some steps because
-            // it could be pending bufferAcks
-            numEvents = max_reqs_per_event;
-        }
-        while (more && numEvents > 0) {
-            const auto ret = getBucket().getDcpIface()->step(
-                    *cookies.front().get(),
-                    getBucket().shouldThrottleDcp(*this),
-                    *this);
-            switch (remapErrorCode(ret)) {
-            case cb::engine_errc::success:
-                more = (getSendQueueSize() < dcpMaxQSize);
-                --numEvents;
-                break;
-            case cb::engine_errc::throttled:
-            case cb::engine_errc::would_block:
-                more = false;
-                break;
-            default:
-                LOG_WARNING(R"({}: step returned {} - closing connection {})",
-                            getId(),
-                            cb::to_string(ret),
-                            getDescription());
-                if (ret == cb::engine_errc::disconnect) {
-                    setTerminationReason("Engine forced disconnect");
-                }
-                shutdown();
-                more = false;
+    // Currently working on an incomming packet
+    if (!cookies.front()->empty()) {
+        return;
+    }
+
+    // make sure we reset the privilege context
+    cookies.front()->reset();
+
+    // MB-38007: We see an increase in rebalance time for
+    // "in memory" workloads when allowing DCP to fill up to
+    // 40MB (thats the default) batch size into the output buffer.
+    // We've not been able to figure out exactly _why_ this is
+    // happening and have assumptions that it may be caused
+    // that it doesn't align too much with the flow control being
+    // used. Before moving to bufferevent we would copy the entire
+    // message into kernel space before trying to read (and process)
+    // any input messages before trying to send the next one.
+    // It could be that it would be better at processing the
+    // incoming flow control messages instead of the current
+    // model where the input socket gets drained and put in
+    // userspace buffers, the send queue is tried to be drained
+    // before we do the callback and process the already queued
+    // input and generate more output before returning to the
+    // layer doing the actual IO.
+    std::size_t dcpMaxQSize =
+            (dcpFlowControlBufferSize == 0)
+                    ? Settings::instance().getMaxSendQueueSize()
+                    : 1024 * 1024;
+    bool more = (getSendQueueSize() < dcpMaxQSize);
+    if (type == Type::Consumer) {
+        // We want the consumer to perform some steps because
+        // it could be pending bufferAcks
+        numEvents = max_reqs_per_event;
+    }
+    while (more && numEvents > 0) {
+        const auto ret = getBucket().getDcpIface()->step(
+                *cookies.front().get(),
+                getBucket().shouldThrottleDcp(*this),
+                *this);
+        switch (remapErrorCode(ret)) {
+        case cb::engine_errc::success:
+            more = (getSendQueueSize() < dcpMaxQSize);
+            --numEvents;
+            break;
+        case cb::engine_errc::throttled:
+        case cb::engine_errc::would_block:
+            more = false;
+            break;
+        default:
+            LOG_WARNING(R"({}: step returned {} - closing connection {})",
+                        getId(),
+                        cb::to_string(ret),
+                        getDescription());
+            if (ret == cb::engine_errc::disconnect) {
+                setTerminationReason("Engine forced disconnect");
             }
+            shutdown();
+            more = false;
         }
-        if (more && numEvents == 0) {
-            // We used the entire timeslice... schedule a new one
-            triggerCallback();
-        }
+    }
+    if (more && numEvents == 0) {
+        // We used the entire timeslice... schedule a new one
+        triggerCallback();
     }
 }
 
