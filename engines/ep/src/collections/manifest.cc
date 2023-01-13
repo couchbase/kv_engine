0278a4d4ec80b3b2e5b560209a205b28af1510ff
Jim Walker <jim@couchbase.com>
2023-01-13 13:16:44 +0000

    MB-54516: Remove superfluous comma from drop collection logging
    Change-Id: I2a646c54f7623c62a4890474c3b054461eed5baf
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/184973
    Well-Formed: Restriction Checker
    Tested-by: Jim Walker <jim@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>

--- engines/ep/src/collections/manifest.cc
+++ engines/ep/src/collections/manifest.cc
@@ -823,7 +823,7 @@ cb::engine_error Manifest::isSuccessor(const Manifest& successor) const {
                                     itr->second));
                 }
             } else {
-                EP_LOG_INFO("drop collection manifest:{:#x} cid:{}, sid:{}, ",
+                EP_LOG_INFO("drop collection manifest:{:#x} cid:{}, sid:{}",
                             successor.getUid(),
                             cid,
                             collection.sid);
