couchbase/kv_engine

commit b1d4dea8c85a105ce53f85b1d33558ecba18f31b

MB-20017: Add support for retrieving per-thread cpu usage
Use mcstat "threads details" or look in prometheus.

    ./mcstat --json 'threads details'
    {
      "details": {
        "89513": {
        "name": "memcached",
        "system": 110,
        "user": 1070
      },
    [... cut ...]

Change-Id: I4cc68bee2cc667673db7e82782d7d1744b34bed5
Reviewed-on: https://review.couchbase.org/c/kv_engine/+/184712
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: Jim Walker <jim@couchbase.com>
Reviewed-by: Dave Rigby <daver@couchbase.com>

daemon/stats.cc
@@ -16,16 +16,17 @@
 #include "memcached.h"
 #include "network_interface_manager.h"
 #include "settings.h"
-
 #include <fmt/chrono.h>
 #include <folly/Chrono.h>
+#include <logger/logger.h>
 #include <platform/cb_arena_malloc.h>
 #include <platform/timeutils.h>
+#include <sigar.h>
 #include <statistics/collector.h>
 #include <statistics/labelled_collector.h>
 #include <statistics/prometheus.h>
 #include <statistics/prometheus_collector.h>
-
+#include <utilities/string_utilities.h>
 #include <string_view>
 
 // add global stats
@@ -240,7 +241,31 @@ cb::engine_errc server_prometheus_stats(
         if (metricGroup == MetricGroup::Low) {
             server_global_stats(kvCollector);
             stats_audit(kvCollector);
-        } else if (isServerlessDeployment()) {
+        } else {
+            try {
+                sigar::iterate_threads([&kvCollector](auto tid,
+                                                      auto name,
+                                                      auto user,
+                                                      auto system) {
+                    auto thread_pool = get_thread_pool_name(name);
+                    kvCollector.addStat(cb::stats::Key::thread_cpu_usage,
+                                        user,
+                                        {{"tid", std::to_string(tid)},
+                                         {"thread_name", name},
+                                         {"thread_pool", thread_pool},
+                                         {"domain", "user"}});
+                    kvCollector.addStat(cb::stats::Key::thread_cpu_usage,
+                                        system,
+                                        {{"tid", std::to_string(tid)},
+                                         {"thread_name", name},
+                                         {"thread_pool", thread_pool},
+                                         {"domain", "system"}});
+                });
+            } catch (const std::exception& e) {
+                LOG_WARNING("sigar::iterate_threads: {}", e.what());
+            }
+        }
+        if (isServerlessDeployment()) {
             // include all metering metrics, without the "kv_" prefix
             server_prometheus_metering(collector);
         }

daemon/CMakeLists.txt
@@ -239,6 +239,7 @@ target_link_libraries(memcached_daemon
                       memcached_rbac
                       phosphor
                       xattr
+                      sigar
                       ${SNAPPY_LIBRARIES}
                       ${MALLOC_LIBRARIES}
                       ${LIBEVENT_LIBRARIES}

daemon/protocol/mcbp/stats_context.cc
@@ -25,6 +25,7 @@
 #include <daemon/stats_tasks.h>
 #include <executor/executorpool.h>
 #include <gsl/gsl-lite.hpp>
+#include <logger/logger.h>
 #include <mcbp/protocol/framebuilder.h>
 #include <mcbp/protocol/header.h>
 #include <memcached/stat_group.h>
@@ -33,9 +34,11 @@
 #include <phosphor/trace_log.h>
 #include <platform/cb_arena_malloc.h>
 #include <platform/checked_snprintf.h>
+#include <sigar.h>
 #include <statistics/cbstat_collector.h>
 #include <statistics/labelled_collector.h>
 #include <utilities/engine_errc_2_mcbp.h>
+#include <utilities/string_utilities.h>
 #include <chrono>
 #include <cinttypes>
 #include <stdexcept>
@@ -573,27 +576,67 @@ static cb::engine_errc stat_timings_executor(const std::string&,
     return cb::engine_errc::success;
 }
 
-static cb::engine_errc stat_threads_executor(const std::string&,
+static cb::engine_errc stat_threads_executor(const std::string& arg,
                                              Cookie& cookie) {
     auto& setting = Settings::instance();
 
-    append_stats(std::string{"num_frontend_threads"},
-                 std::to_string(setting.getNumWorkerThreads()),
-                 cookie);
-    append_stats(std::string{"num_reader_threads"},
-                 std::to_string(setting.getNumReaderThreads()),
-                 cookie);
-    append_stats(std::string{"num_writer_threads"},
-                 std::to_string(setting.getNumWriterThreads()),
-                 cookie);
-    append_stats(std::string{"num_auxio_threads"},
-                 std::to_string(setting.getNumAuxIoThreads()),
-                 cookie);
-    append_stats(std::string{"num_nonio_threads"},
-                 std::to_string(setting.getNumNonIoThreads()),
-                 cookie);
+    if (arg.empty()) {
+        append_stats(std::string{"num_frontend_threads"},
+                     std::to_string(setting.getNumWorkerThreads()),
+                     cookie);
+        append_stats(std::string{"num_reader_threads"},
+                     std::to_string(setting.getNumReaderThreads()),
+                     cookie);
+        append_stats(std::string{"num_writer_threads"},
+                     std::to_string(setting.getNumWriterThreads()),
+                     cookie);
+        append_stats(std::string{"num_auxio_threads"},
+                     std::to_string(setting.getNumAuxIoThreads()),
+                     cookie);
+        append_stats(std::string{"num_nonio_threads"},
+                     std::to_string(setting.getNumNonIoThreads()),
+                     cookie);
+        return cb::engine_errc::success;
+    }
 
-    return cb::engine_errc::success;
+    if (arg == "details") {
+        nlohmann::json json{
+                {"num_frontend_threads", setting.getNumWorkerThreads()},
+                {"num_reader_threads", setting.getNumReaderThreads()},
+                {"num_writer_threads", setting.getNumWriterThreads()},
+                {"num_auxio_threads", setting.getNumAuxIoThreads()},
+                {"num_nonio_threads", setting.getNumNonIoThreads()}};
+
+        try {
+            const auto pid = getpid();
+            sigar::iterate_threads([&json, &pid](auto tid,
+                                                 auto name,
+                                                 auto user,
+                                                 auto system) {
+                nlohmann::json entry = {{"user", user}, {"system", system}};
+                if (pid == tid) {
+                    entry["main"] = true;
+                }
+                if (!name.empty()) {
+                    entry["name"] = name;
+                    auto pool = get_thread_pool_name(name);
+                    if (!pool.empty()) {
+                        entry["pool"] = pool;
+                    }
+                }
+                json[std::to_string(tid)] = std::move(entry);
+            });
+        } catch (const std::exception& e) {
+            LOG_WARNING("{} - sigar::iterate_process_threads: {}",
+                        cookie.getConnectionId(),
+                        e.what());
+        }
+
+        append_stats("details", json.dump(), cookie);
+        return cb::engine_errc::success;
+    }
+
+    return cb::engine_errc::invalid_arguments;
 }
 
 static cb::engine_errc stat_tasks_all_executor(const std::string&,

tests/testapp/testapp_stats.cc
@@ -726,3 +726,20 @@ TEST_P(StatsTest, TestSettingAndGettingThreadCount) {
     }
     EXPECT_TRUE(seenThreadsKey);
 }
+
+TEST_P(StatsTest, ThreadDetails) {
+    nlohmann::json json;
+
+    adminConnection->stats(
+            [&json](const auto&, const auto& value) -> void {
+                json = nlohmann::json::parse(value);
+            },
+            "threads details");
+    EXPECT_FALSE(json.empty()) << "Expected a JSON payload to be returned";
+    EXPECT_TRUE(json.contains("num_auxio_threads"));
+    EXPECT_TRUE(json.contains("num_frontend_threads"));
+    EXPECT_TRUE(json.contains("num_nonio_threads"));
+    EXPECT_TRUE(json.contains("num_reader_threads"));
+    EXPECT_TRUE(json.contains("num_writer_threads"));
+    EXPECT_LT(5, json.size()) << "There should be some threads reported";
+}

utilities/string_utilities.cc
@@ -101,3 +101,30 @@ std::string to_string(cb::byte_buffer buf) {
 std::string to_string(cb::const_byte_buffer buf) {
     return std::string(reinterpret_cast<const char*>(buf.data()), buf.size());
 }
+
+std::string_view get_thread_pool_name(std::string_view threadname) {
+    using namespace std::string_view_literals;
+    std::string_view thread_pool;
+    auto prefix_end = threadname.find_first_of("0123456789");
+    if (prefix_end != threadname.npos) {
+        // Clean up the thread pool name by removing any
+        // uninteresting characters from the end of a non-empty
+        // prefix. We have different naming conventions between
+        // memcached, ep-engine and magma - some have no
+        // separator between pool name and thread number, some
+        // have ':' and others have '_'. However, we cannot
+        // simply include ':' and '_' in the prefix end search
+        // as they are also used in the middle of some thread
+        // names. If the last character is one of the possible
+        // separators then drop it.
+        if (prefix_end > 0) {
+            const auto last_prefix_char = threadname.at(prefix_end - 1);
+            const auto skip_chars = ":_"sv;
+            if (skip_chars.find(last_prefix_char) != skip_chars.npos) {
+                --prefix_end;
+            }
+        }
+        thread_pool = {threadname.data(), prefix_end};
+    }
+    return thread_pool;
+}

utilities/string_utilities.h
@@ -93,3 +93,15 @@ std::ostream& operator<<(std::ostream& ostr, const std::optional<T>& optional) {
 
     return ostr << "<none>";
 }
+
+/**
+ * Get the name of "our" thread pools from the name of a thread. Inside
+ * our system we use different schemes for our naming (some use: name:number,
+ * other use name_number etc). This method tries to pick out the "name" field
+ * from our known mappings, or an empty view if it doesn't look like one of
+ * our schemes.
+ *
+ * @param threadname the name of the thread
+ * @return the part of the name which represents the thread pool
+ */
+std::string_view get_thread_pool_name(std::string_view threadname);

commit 9f8a2c27946746b790f817e982307e1544f18941

MB-55143: scrape metering in MetricGroup::Low
Regression introduced in b1d4dea8c85a105ce53f85b1d33558ecba18f31b which
scraped the metric in both low and high causing duplicates.

Change-Id: Ia76efb1cb3b2f637e8ad8d571e390ddd60070c3c
Reviewed-on: https://review.couchbase.org/c/kv_engine/+/185938
Tested-by: Build Bot <build@couchbase.com>
Reviewed-by: James H <james.harrison@couchbase.com>

daemon/stats.cc
@@ -241,6 +241,10 @@ cb::engine_errc server_prometheus_stats(
         if (metricGroup == MetricGroup::Low) {
             server_global_stats(kvCollector);
             stats_audit(kvCollector);
+            if (isServerlessDeployment()) {
+                // include all metering metrics, without the "kv_" prefix
+                server_prometheus_metering(collector);
+            }
         } else {
             try {
                 sigar::iterate_threads([&kvCollector](auto tid,
@@ -264,10 +268,6 @@ cb::engine_errc server_prometheus_stats(
             } catch (const std::exception& e) {
                 LOG_WARNING("sigar::iterate_threads: {}", e.what());
             }
-        }
-        if (isServerlessDeployment()) {
-            // include all metering metrics, without the "kv_" prefix
-            server_prometheus_metering(collector);
         }
         BucketManager::instance().forEach([&kvCollector,
                                            metricGroup](Bucket& bucket) {

tests/statistics/prometheus_test.cc
@@ -154,8 +154,8 @@ TEST_F(PrometheusStatTest, MeteringIncludedInHighCardinality) {
     {
         auto metrics = getMetrics();
 
-        EXPECT_THAT(metrics.high, Contains(Key("boot_timestamp_seconds")));
-        EXPECT_THAT(metrics.high,
+        EXPECT_THAT(metrics.low, Contains(Key("boot_timestamp_seconds")));
+        EXPECT_THAT(metrics.low,
                     Not(Contains(Key("kv_boot_timestamp_seconds"))));
     }
 }