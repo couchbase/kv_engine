7f212ca29ec2b92a4951939b6003ee7a5dfa56b5
Paolo Cocchi <paolo.cocchi@couchbase.com>
2023-02-22 15:28:31 +0100

    MB-55507: Remove temporary diagnostic checkpoint dumps
    Change-Id: Ibc1b323bdc8d0b099764576a80d5e9c81ff66c14
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/187225
    Well-Formed: Restriction Checker
    Reviewed-by: Vesko Karaganev <vesko.karaganev@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- engines/ep/src/checkpoint_manager.cc
+++ engines/ep/src/checkpoint_manager.cc
@@ -852,23 +852,9 @@ bool CheckpointManager::queueDirty(
         // Could not queue into the current checkpoint as it already has a
         // duplicate item (and not permitted to de-dupe this item).
         if (vb.getState() != vbucket_state_active) {
-            // stderr goes to ns_server that drops memcached lines if too many.
-            // Let's dump only the open checkpoint, and the last closed one if
-            // it exists.
-            // Note: If we'll not get the needed info this way either, next move
-            // is to dump to memcached logs. I'm still trying to avoid that for
-            // now as I don't want to risk to lose memcached info by log
-            // wrapping.
-            if (checkpointList.size() > 1) {
-                const auto openIt = std::prev(checkpointList.end());
-                (*std::prev(openIt))->dump();
-            }
-            openCkpt->dump();
-
             // We shouldn't see this for non-active vBuckets; given the
             // original (active) vBucket on some other node should not have
             // put duplicate mutations in the same Checkpoint.
-
             const auto msg = fmt::format(
                     "CheckpointManager::queueDirty: Got status:{} when {} is "
                     "non-active:{}, item:[op:{}, seqno:{}], lastBySeqno:{}, "
