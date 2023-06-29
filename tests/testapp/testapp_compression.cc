44882f75ee47f4e2c58aa3cd6071698fb75101fa
Trond Norbye <trond.norbye@gmail.com>
2023-06-29 09:54:48 +0200

    [compression] Add extra unit tests
    Extend the unit tests to test operating on documents which
    are stored compressed on the server (append/prepend, subdoc
    and get)
    
    Change-Id: Ice0c4f0fb3a8e9f1109bb48afecef76ae9341a98
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/193215
    Tested-by: Build Bot <build@couchbase.com>
    Reviewed-by: Paolo Cocchi <paolo.cocchi@couchbase.com>

--- tests/testapp/testapp_compression.cc
+++ tests/testapp/testapp_compression.cc
@@ -16,8 +16,27 @@
 
 using namespace std::string_literals;
 
+/**
+ * The CompressionTest batch performs tests on a bucket with active
+ * compression set (and allows for testing operations where the data
+ * on the server is compressed).
+ */
 class CompressionTest : public TestappXattrClientTest {
+public:
+    static void SetUpTestCase() {
+        TestappXattrClientTest::SetUpTestCase();
+        xattr_value.resize(4096, 'z');
+        xattr_value.front() = '"';
+        xattr_value.back() = '"';
+    }
+
 protected:
+    void SetUp() override {
+        TestappXattrClientTest::SetUp();
+        mcd_env->getTestBucket().setCompressionMode(
+                *adminConnection, bucketName, "active");
+    }
+
     void waitForCompression() {
         bool compressed = false;
         using std::chrono::steady_clock;
@@ -42,7 +61,60 @@ protected:
         ASSERT_TRUE(compressed)
                 << "Timed out waiting for the document to be compressed";
     }
+
+    /**
+     * Upsert a document
+     *
+     * Helper function to update or insert a document with
+     * extended attributes and wait for the document to be compressed
+     *
+     * @param value The documents value
+     */
+    void upsert(std::string value) {
+        BinprotSubdocMultiMutationCommand cmd;
+        cmd.setKey(name);
+        cmd.setVBucket(Vbid{0});
+        cmd.addMutation(cb::mcbp::ClientOpcode::SubdocDictUpsert,
+                        SUBDOC_FLAG_XATTR_PATH,
+                        xattr_key,
+                        xattr_value);
+        cmd.addMutation(
+                cb::mcbp::ClientOpcode::Set, SUBDOC_FLAG_NONE, "", value);
+        cmd.addDocFlag(cb::mcbp::subdoc::doc_flag::Mkdoc);
+        auto rsp = userConnection->execute(cmd);
+        if (!rsp.isSuccess()) {
+            throw ConnectionError("Failed to upsert document", rsp);
+        }
+        waitForCompression();
+    }
+
+    void validateJsonField(std::string_view key,
+                           std::string_view value,
+                           bool xattr) {
+        BinprotSubdocCommand cmd;
+        cmd.setOp(cb::mcbp::ClientOpcode::SubdocGet);
+        cmd.setKey(name);
+        cmd.setPath(std::string{key});
+        if (xattr) {
+            cmd.addPathFlags(SUBDOC_FLAG_XATTR_PATH);
+        }
+        userConnection->sendCommand(cmd);
+        BinprotSubdocResponse resp;
+        userConnection->recvResponse(resp);
+        ASSERT_TRUE(resp.isSuccess()) << to_string(resp.getStatus());
+        EXPECT_EQ(value, resp.getValue());
+    }
+
+    void validateXAttrs() {
+        validateJsonField(xattr_key, xattr_value, true);
+    }
+
+    static const std::string xattr_key;
+    static std::string xattr_value;
 };
+
+const std::string CompressionTest::xattr_key = "meta.value";
+std::string CompressionTest::xattr_value;
 
 INSTANTIATE_TEST_SUITE_P(
         TransportProtocols,
@@ -57,47 +129,80 @@ INSTANTIATE_TEST_SUITE_P(
 /// attributes
 TEST_P(CompressionTest, MB57640) {
     TESTAPP_SKIP_FOR_OTHER_BUCKETS(BucketType::Couchbase);
-    mcd_env->getTestBucket().setCompressionMode(
-            *adminConnection, bucketName, "active");
-
-    EXPECT_EQ(0, userConnection->increment(name, 1));
-
-    const std::string xattr_key = "meta.value";
-    std::string xattr_value;
-    xattr_value.resize(4096, 'z');
-    xattr_value.front() = '"';
-    xattr_value.back() = '"';
-
-    // Add an attribute shich should be big enough to the document
-    // to be compressed
-    {
-        BinprotSubdocCommand cmd;
-        cmd.setOp(cb::mcbp::ClientOpcode::SubdocDictAdd);
-        cmd.setKey(name);
-        cmd.setPath(xattr_key);
-        cmd.setValue(xattr_value);
-        cmd.addPathFlags(SUBDOC_FLAG_XATTR_PATH | SUBDOC_FLAG_MKDIR_P);
-        const auto resp = userConnection->execute(cmd);
-        ASSERT_TRUE(resp.isSuccess()) << to_string(resp.getStatus());
-    }
-
-    // Wait for the document to become compressed
-    waitForCompression();
+    upsert("0");
 
     // Increment the counter
     EXPECT_EQ(1, userConnection->increment(name, 1));
 
     // Validate that the XATTRs is still intact
-    {
-        BinprotSubdocCommand cmd;
-        cmd.setOp(cb::mcbp::ClientOpcode::SubdocGet);
-        cmd.setKey(name);
-        cmd.setPath(xattr_key);
-        cmd.addPathFlags(SUBDOC_FLAG_XATTR_PATH);
-        userConnection->sendCommand(cmd);
-        BinprotSubdocResponse resp;
-        userConnection->recvResponse(resp);
-        ASSERT_TRUE(resp.isSuccess()) << to_string(resp.getStatus());
-        EXPECT_EQ(xattr_value, resp.getValue());
-    }
+    validateXAttrs();
+}
+
+/// Verify that Subdoc operations work works on a compressed document
+/// with extended attributes
+TEST_P(CompressionTest, SubdocLookupXattrOnCompressedDocument) {
+    TESTAPP_SKIP_FOR_OTHER_BUCKETS(BucketType::Couchbase);
+    upsert("0");
+
+    // The document should be compressed, verify that we can read the
+    // attributes
+    validateXAttrs();
+}
+
+/// Verify that Subdoc operations work works on a compressed document
+/// with extended attributes
+TEST_P(CompressionTest, SubdocLookupFieldOnCompressedDocument) {
+    TESTAPP_SKIP_FOR_OTHER_BUCKETS(BucketType::Couchbase);
+    upsert(R"({"foo" : "bar"})");
+
+    // The document should be compressed, verify that we can read the
+    // document fields by using subdoc
+    validateJsonField("foo", R"("bar")", false);
+}
+
+/// Verify that get works correctly:
+/// * If the document contains xattrs, the document needs to be inflated
+///   before being returned.
+/// * If there is no xattrs, the document should be returned compressed
+TEST_P(CompressionTest, GetReturnsCompressedData) {
+    TESTAPP_SKIP_FOR_OTHER_BUCKETS(BucketType::Couchbase);
+    Document doc;
+    doc.info.id = name;
+    doc.value.resize(4096, 'a');
+    doc.compress();
+    // By storing the document compressed, the server shouldn't need to compress
+    // it and just reuse the provided blob
+    userConnection->mutate(doc, Vbid{0}, MutationType::Set);
+    waitForCompression();
+
+    auto document = userConnection->get(name, Vbid{0});
+    EXPECT_EQ(cb::mcbp::Datatype::Snappy, document.info.datatype);
+    EXPECT_EQ(doc.value, document.value);
+
+    // Verify that if the document contains xattrs it gets inflated (and
+    // the xattrs stripped off)
+    std::string value(R"({"foo" : "bar"})");
+    upsert(value);
+    document = userConnection->get(name, Vbid{0});
+    EXPECT_EQ(cb::mcbp::Datatype::JSON, document.info.datatype);
+    EXPECT_EQ(value, document.value);
+}
+
+/// Verify that append work correctly on compressed document
+TEST_P(CompressionTest, AppendPrepend) {
+    TESTAPP_SKIP_FOR_OTHER_BUCKETS(BucketType::Couchbase);
+    upsert(R"(1234)");
+    Document document;
+    document.value = "{}";
+    document.info.id = name;
+    userConnection->mutate(document, Vbid{0}, MutationType::Append);
+    userConnection->mutate(document, Vbid{0}, MutationType::Prepend);
+    waitForCompression();
+    auto doc = userConnection->get(name, Vbid{0});
+    // The document contains XATTRS so it should be inflated before returned
+    EXPECT_EQ(cb::mcbp::Datatype::Raw, doc.info.datatype);
+    EXPECT_EQ("{}1234{}"s, doc.value);
+
+    // Validate that we didn't mess up the xattr's
+    validateXAttrs();
 }
