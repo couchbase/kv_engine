0a722f3c3aae895cc0e5129c3a7ffa68918786c4
Rohan Suri <rohan.suri@couchbase.com>
2023-10-27 17:31:43 +0530

    MB-59186 Add Magma Status::Code::RetryLater in switch block
    Linked magma change: https://review.couchbase.org/c/magma/+/199640/3
    
    Change-Id: If5cffa5db26ee8d6388f6f9e18ec8d9330e5e147
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/199649
    Tested-by: Rohan Suri <rohan.suri@couchbase.com>
    Reviewed-by: Dave Rigby <daver@couchbase.com>

--- engines/ep/src/kvstore/magma-kvstore/magma-kvstore.cc
+++ engines/ep/src/kvstore/magma-kvstore/magma-kvstore.cc
@@ -3195,6 +3195,7 @@ RollbackResult MagmaKVStore::rollback(Vbid vbid,
     case Status::NotFound:
     case Status::Cancelled:
     case Status::RetryCompaction:
+    case Status::RetryLater:
     case Status::NoAccess:
     case Status::RangeNotFound:
         logger->critical("MagmaKVStore::rollback Rollback {} status:{}",
