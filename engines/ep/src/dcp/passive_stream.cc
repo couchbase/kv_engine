9b8490d080104179aa532277f5dddc79c370ec3d
Paolo Cocchi <paolo.cocchi@couchbase.com>
2024-01-18 11:35:15 +0100

    MB-31869: Reference follow-up work for MB-60439
    Change-Id: I4c574e2fd12e3be189160965655df29f15f81bea
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/203919
    Tested-by: Paolo Cocchi <paolo.cocchi@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- engines/ep/src/dcp/passive_stream.cc
+++ engines/ep/src/dcp/passive_stream.cc
@@ -354,9 +354,10 @@ cb::engine_errc PassiveStream::messageReceived(
     case KVBucket::ReplicationThrottleStatus::Pause: {
         forceMessage(*dcpResponse).getError();
 
-        // @todo MB-31869: active-check for preventing that we consume memory
-        //  for dead streams are being removes. We can remove the check once we
-        //  get rid of the buffer
+        // @todo MB-60439: Review and possibly remove.
+        //  Legacy active check for preventing that we consume memory by
+        //  buffering message for dead streams are being removes. We can
+        //  probably remove the check once we get rid of the buffer.
         if (isActive()) {
             unackedBytes += ufc.release();
         }
