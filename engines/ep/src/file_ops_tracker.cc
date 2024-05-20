fd9846d4bcc047a8a9abb1382b599fd780c48bed
Vesko Karaganev <vesko.karaganev@couchbase.com>
2024-05-20 08:59:22 +0100

    [BP] MB-34155: Add FileOpsTracker for recording pending IO
    The FileOpsTracker class uses a thread-local store for recording
    pending IO requests. Any reads and writes that need to be tracked
    need to call FileOpsTracker::start with the appropriate request type.
    Once the IO has returned, FileOpsTracker::complete must be called to
    clear the state.
    
    An external observer can call FileOpsTracker::visitThreads to enumerate
    the pending IO requests on all known threads. The external observer can
    then filter those by thread type. To detect a stuck Reader or Writer
    thread, one can filter for these TaskTypes and check the
    FileOp::startTime.
    
    Calling of FileOpsTracker::start/complete will be done from within the
    KVStore.
    
    The intention is that should be sufficient to detect a stuck read or
    write that would affect a front-end thread in a way that is easy to
    reason about (e.g. a writer thread has spent more than 120s in a 4 KiB
    write).
    
    Change-Id: Ifaf172d5deccd467f4852399c09cb0459b617d84
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/215164
    Well-Formed: Restriction Checker
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Tested-by: Vesko Karaganev <vesko.karaganev@couchbase.com>

---
+++ engines/ep/src/file_ops_tracker.cc
@@ -0,0 +1,128 @@
+/*
+ *     Copyright 2024-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+#include "file_ops_tracker.h"
+
+#include "ep_time.h"
+#include "executor/globaltask.h"
+#include <folly/system/ThreadName.h>
+#include <gsl/gsl-lite.hpp>
+#include <platform/cb_arena_malloc.h>
+#include <memory>
+
+FileOp::FileOp(Type type) : FileOp(type, 0) {
+}
+
+FileOp::FileOp(Type type, size_t nbytes)
+    : type(type), nbytes(nbytes), startTime{ep_uptime_now()} {
+}
+
+FileOpsTrackerScopeGuard::FileOpsTrackerScopeGuard(FileOpsTracker& tracker)
+    : tracker(&tracker) {
+}
+
+FileOpsTrackerScopeGuard::~FileOpsTrackerScopeGuard() {
+    if (tracker) {
+        tracker->complete();
+    }
+}
+
+FileOpsTrackerScopeGuard::FileOpsTrackerScopeGuard(
+        FileOpsTrackerScopeGuard&& other)
+    : tracker(std::exchange(other.tracker, nullptr)) {
+}
+
+FileOpsTrackerScopeGuard& FileOpsTrackerScopeGuard::operator=(
+        FileOpsTrackerScopeGuard&& other) {
+    tracker = std::exchange(other.tracker, nullptr);
+    return *this;
+}
+
+/**
+ * Each tracked thread will have one of these objects.
+ * This object contains the currently executing FileOp and the type of thread
+ * that it is owned by. Together, these two values are sufficient to filter
+ * interesting threads and identify when one of those threads has become stuck.
+ */
+struct FileOpsTracker::ThreadSlot {
+    /// Initialises the slot with current state against the global arena.
+    ThreadSlot();
+    /// Deiniialises the slot against the global arena.
+    ~ThreadSlot();
+
+    /// The type of thread pool this thread belongs to.
+    const task_type_t threadType;
+    /// The name of the thread. Effectively const and allocated under NoArena.
+    std::string threadName;
+    /**
+     * The request currently pending on the thread.
+     * There will almost never be contention here, so use a regular mutex.
+     */
+    folly::Synchronized<FileOp, std::mutex> currentRequest;
+};
+
+FileOpsTracker::ThreadSlot::ThreadSlot()
+    : threadType(GlobalTask::getCurrentTaskType()),
+      threadName(),
+      currentRequest(FileOp{FileOp::Type::None}) {
+    cb::NoArenaGuard guard;
+    threadName = folly::getCurrentThreadName().value_or("unknown");
+}
+
+FileOpsTracker::ThreadSlot::~ThreadSlot() {
+    cb::NoArenaGuard guard;
+    std::exchange(threadName, {});
+}
+
+FileOpsTracker& FileOpsTracker::instance() {
+    static FileOpsTracker tracker;
+    return tracker;
+}
+
+FileOpsTracker::FileOpsTracker() {
+    cb::NoArenaGuard guard;
+    threadSlot =
+            std::make_unique<folly::ThreadLocal<ThreadSlot, FileOpsTracker>>();
+}
+
+FileOpsTracker::~FileOpsTracker() {
+    cb::NoArenaGuard guard;
+    threadSlot.reset();
+}
+
+FileOpsTracker::ThreadSlot& FileOpsTracker::getThreadSlot() {
+    cb::NoArenaGuard guard;
+    return **threadSlot;
+}
+
+void FileOpsTracker::start(const FileOp& op) {
+    Expects(op.type != FileOp::Type::None);
+    getThreadSlot().currentRequest = op;
+}
+
+FileOpsTrackerScopeGuard FileOpsTracker::startWithScopeGuard(const FileOp& op) {
+    start(op);
+    return {*this};
+}
+
+void FileOpsTracker::complete() {
+    getThreadSlot().currentRequest = FileOp{FileOp::Type::None};
+}
+
+void FileOpsTracker::visitThreads(
+        std::function<void(task_type_t, std::string_view, const FileOp&)>
+                visitor) {
+    for (auto&& slot : threadSlot->accessAllThreads()) {
+        auto op = slot.currentRequest.copy();
+        if (op.type != FileOp::Type::None) {
+            visitor(slot.threadType, slot.threadName, op);
+        }
+    }
+}
---
+++ engines/ep/tests/module_tests/file_ops_tracker_test.cc
@@ -0,0 +1,121 @@
+/*
+ *     Copyright 2024-Present Couchbase, Inc.
+ *
+ *   Use of this software is governed by the Business Source License included
+ *   in the file licenses/BSL-Couchbase.txt.  As of the Change Date specified
+ *   in that file, in accordance with the Business Source License, use of this
+ *   software will be governed by the Apache License, Version 2.0, included in
+ *   the file licenses/APL2.txt.
+ */
+
+#include <folly/portability/GTest.h>
+
+#include "ep_time.h"
+#include "executor/task_type.h"
+#include "file_ops_tracker.h"
+#include "tests/module_tests/thread_gate.h"
+#include <platform/cb_arena_malloc.h>
+#include <platform/cb_arena_malloc_client.h>
+#include <chrono>
+
+class FileOpsTrackerTest : public ::testing::Test {
+public:
+    void SetUp() override {
+        testClient = cb::ArenaMalloc::registerClient();
+        tracker = std::make_unique<FileOpsTracker>();
+    }
+
+    void TearDown() override {
+        // It's important that the thread locals in FileOpsTracker never
+        // allocate from the bucket's arena.
+        EXPECT_EQ(0, cb::ArenaMalloc::getPreciseAllocated(testClient));
+        cb::ArenaMalloc::unregisterClient(testClient);
+    }
+
+    using VisitorData = std::tuple<task_type_t, std::string, FileOp>;
+    std::vector<VisitorData> getRequests();
+
+    // Arena client tests can use to verify the FileOpsTracker did not allocate
+    // into another arena.
+    cb::ArenaMallocClient testClient;
+    std::unique_ptr<FileOpsTracker> tracker;
+};
+
+std::vector<FileOpsTrackerTest::VisitorData> FileOpsTrackerTest::getRequests() {
+    std::vector<FileOpsTrackerTest::VisitorData> v;
+    // cb::ArenaMallocGuard guard(testClient);
+    tracker->visitThreads([&v](auto type, auto name, auto req) {
+        // cb::NoArenaGuard guard2;
+        v.emplace_back(std::make_tuple(type, std::string(name), req));
+    });
+    return v;
+}
+
+TEST_F(FileOpsTrackerTest, BasicTest) {
+    ASSERT_EQ(0, getRequests().size());
+
+    {
+        cb::ArenaMallocGuard guard(testClient);
+        tracker->start(FileOp::open());
+    }
+    auto requests = getRequests();
+
+    ASSERT_EQ(1, requests.size());
+    auto [type, _, req] = requests[0];
+    EXPECT_EQ(NO_TASK_TYPE, type);
+    EXPECT_EQ(FileOp::Type::Open, req.type);
+    EXPECT_LE(std::chrono::seconds(0), ep_uptime_now() - req.startTime);
+    EXPECT_GT(std::chrono::seconds(5), ep_uptime_now() - req.startTime);
+
+    {
+        cb::ArenaMallocGuard guard(testClient);
+        tracker->complete();
+    }
+    EXPECT_EQ(0, getRequests().size());
+}
+
+TEST_F(FileOpsTrackerTest, ExitGuard) {
+    ASSERT_EQ(0, getRequests().size());
+
+    {
+        cb::ArenaMallocGuard guard(testClient);
+        auto scopeGuard = tracker->startWithScopeGuard(FileOp::read(1));
+        cb::NoArenaGuard guard2;
+        EXPECT_EQ(1, getRequests().size());
+    }
+
+    EXPECT_EQ(0, getRequests().size());
+}
+
+TEST_F(FileOpsTrackerTest, Interleaving) {
+    constexpr size_t NumThreads = 3;
+    constexpr auto Timeout = std::chrono::seconds(5);
+    ASSERT_EQ(0, getRequests().size());
+
+    ThreadGate tg(NumThreads);
+    ThreadGate waitForComplete(1);
+    std::vector<std::thread> threads;
+
+    for (size_t i = 0; i < NumThreads; i++) {
+        threads.emplace_back([&]() {
+            auto scopeGuard = tracker->startWithScopeGuard(FileOp::read(1));
+            tg.threadUp();
+            waitForComplete.waitFor(Timeout);
+        });
+    }
+
+    tg.waitFor(Timeout);
+    const auto requests = getRequests();
+
+    waitForComplete.threadUp();
+    for (auto&& t : threads) {
+        t.join();
+    }
+    EXPECT_EQ(0, getRequests().size());
+
+    EXPECT_EQ(3, requests.size());
+    for (size_t i = 0; i < NumThreads; i++) {
+        EXPECT_EQ(NO_TASK_TYPE, std::get<0>(requests[i]));
+        EXPECT_EQ(FileOp::Type::Read, std::get<2>(requests[i]).type);
+    }
+}
