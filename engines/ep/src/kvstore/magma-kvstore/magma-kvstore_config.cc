630a757884c6d1ca2d4723981e754a9c0388ac9a
Vesko Karaganev <vesko.karaganev@couchbase.com>
2024-05-22 13:45:47 +0100

    MB-34155: Wrap Magma's FileSystem interface
    This patch wraps Magma's FileSystem interfaces inside KV, allowing us to
    provide a modified implementation.
    
    This patch has no observable changes, but will allow us to wrap the APIs
    with FileOpsTracker.
    
    Change-Id: Ia5316736af6a3558ff9737e0220036a8f6fd0bb3
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/210578
    Tested-by: Vesko Karaganev <vesko.karaganev@couchbase.com>
    Reviewed-by: Jim Walker <jim@couchbase.com>

--- engines/ep/src/kvstore/magma-kvstore/magma-kvstore_config.cc
+++ engines/ep/src/kvstore/magma-kvstore/magma-kvstore_config.cc
@@ -14,6 +14,7 @@
 #include "configuration.h"
 #include "error_handler.h"
 #include "magma-kvstore.h"
+#include "magma-kvstore_fs.h"
 
 #include <memcached/server_core_iface.h>
 
@@ -127,6 +128,7 @@ MagmaKVStoreConfig::MagmaKVStoreConfig(Configuration& config,
     magmaSeqTreeIndexBlockSize = config.getMagmaSeqTreeIndexBlockSize();
     magmaKeyTreeDataBlockSize = config.getMagmaKeyTreeDataBlockSize();
     magmaKeyTreeIndexBlockSize = config.getMagmaKeyTreeIndexBlockSize();
+    magmaCfg.FS = getMagmaTrackingFileSystem(magmaCfg.GetDefaultFS());
 
     config.addValueChangedListener(
             "magma_enable_block_cache",
@@ -265,4 +267,15 @@ void MagmaKVStoreConfig::setMagmaFusionCacheSize(size_t bytes) {
     if (store) {
         store->setFusionCacheSize(bytes);
     }
+}
+
+void MagmaKVStoreConfig::setMakeDirectoryFn(magma::DirectoryConstructor fn) {
+    Expects(getenv("MEMCACHED_UNIT_TESTS") != nullptr);
+    magmaCfg.FS.MakeDirectory = fn;
+}
+
+void MagmaKVStoreConfig::setReadOnly(bool readOnly) {
+    setReadOnlyHook();
+    magmaCfg.ReadOnly = readOnly;
+    magmaCfg.FS = getMagmaTrackingFileSystem(magmaCfg.GetDefaultFS());
 }
