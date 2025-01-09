0a22615e5c1ef8bf5d415f3fc536c65252fe5f80
Trond Norbye <trond.norbye@gmail.com>
2025-01-09 11:35:31 +0100

    MB-64768: Don't use compression for encrypted audit log
    It cause our unit tests to break (as we don't write enough data
    to the files causing zlib to do a flush)
    
    Change-Id: I577e3df867ee18449e768acea30a5dd3fdd0315b
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/221676
    Reviewed-by: Vesko Karaganev <vesko.karaganev@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>

--- auditd/src/auditfile.cc
+++ auditd/src/auditfile.cc
@@ -204,7 +204,7 @@ bool AuditFile::open() {
 
     try {
         file = cb::crypto::FileWriter::create(
-                key, open_file_name, 8192, cb::crypto::Compression::ZLIB);
+                key, open_file_name, 8192, cb::crypto::Compression::None);
         LOG_INFO_CTX(
                 "Audit file",
                 {"encrypted", file->is_encrypted() ? "encrypted" : "plain"});
