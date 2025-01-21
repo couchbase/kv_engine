924a0a01fdc2b5c4985cffb808512cab8d6e02fc
Trond Norbye <trond.norbye@gmail.com>
2025-01-21 09:16:33 +0100

    clang-analyze: initialization value is never read
    Value stored to 'priString' during its initialization is never read
    
    Use the utility function format_as instead
    
    Change-Id: Id3971be90a70b3351ebaea51e535403c6603bc70
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/222187
    Reviewed-by: Jim Walker <jim@couchbase.com>
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>

--- engines/ep/src/connhandler.cc
+++ engines/ep/src/connhandler.cc
@@ -355,19 +355,7 @@ void ConnHandler::addStats(const AddStatFn& add_stat, CookieIface& c) {
         }
     }
     const auto priority = cookie.load()->getConnectionIface().getPriority();
-    const char* priString = "<INVALID>";
-    switch (priority) {
-    case ConnectionPriority::High:
-        priString = "high";
-        break;
-    case ConnectionPriority::Medium:
-        priString = "medium";
-        break;
-    case ConnectionPriority::Low:
-        priString = "low";
-        break;
-    }
-    addStat("priority", priString, add_stat, c);
+    addStat("priority", format_as(priority), add_stat, c);
     addStat(DcpControlKeys::FlatBuffersSystemEvents,
             areFlatBuffersSystemEventsEnabled(),
             add_stat,
