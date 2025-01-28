f26eaea86cddf3b972be7003495d18627ce5bc4f
Trond Norbye <trond.norbye@gmail.com>
2025-01-28 08:08:21 +0100

    Refactor: Move global variables to the top of the file
    Instead of being stuck in the middle of the various command
    handlers
    
    Change-Id: I9c4116afe28f9464dff42680ca03a3202bc06882
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/222482
    Tested-by: Trond Norbye <trond.norbye@couchbase.com>
    Reviewed-by: Vesko Karaganev <vesko.karaganev@couchbase.com>

--- daemon/mcbp_executors.cc
+++ daemon/mcbp_executors.cc
@@ -64,6 +64,28 @@
 #include <utilities/engine_errc_2_mcbp.h>
 #include <utilities/throttle_utilities.h>
 
+/**
+ * The handler function is used to handle and incomming packet (command or
+ * response).
+ * Each handler is provided with a Cookie object which contains all
+ * of the context information about the command/response.
+ *
+ * When called the entire packet is available.
+ */
+using HandlerFunction = std::function<void(Cookie&)>;
+
+/**
+ * A map between the request packets op-code and the function to handle
+ * the request message
+ */
+std::array<HandlerFunction, 0x100> handlers;
+
+/**
+ * A map between the response packets op-code and the function to handle
+ * the response message.
+ */
+std::array<HandlerFunction, 0x100> response_handlers;
+
 static void process_bin_get_meta(Cookie& cookie) {
     cookie.obtainContext<GetMetaCommandContext>(cookie).drive();
 }
@@ -135,28 +157,6 @@ static void disable_traffic_control_mode_executor(Cookie& cookie) {
                       c, TrafficControlMode::Disabled);
           }).drive();
 }
-
-/**
- * The handler function is used to handle and incomming packet (command or
- * response).
- * Each handler is provided with a Cookie object which contains all
- * of the context information about the command/response.
- *
- * When called the entire packet is available.
- */
-using HandlerFunction = std::function<void(Cookie&)>;
-
-/**
- * A map between the request packets op-code and the function to handle
- * the request message
- */
-std::array<HandlerFunction, 0x100> handlers;
-
-/**
- * A map between the response packets op-code and the function to handle
- * the response message.
- */
-std::array<HandlerFunction, 0x100> response_handlers;
 
 static void process_bin_unknown_packet(Cookie& cookie) {
     auto& connection = cookie.getConnection();
