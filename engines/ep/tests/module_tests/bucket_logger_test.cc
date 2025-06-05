41f9511ada429edb9905030f8e9702b9869c2c7c
Jim Walker <jim@couchbase.com>
2025-06-05 09:08:23 +0100

    MB-67075: Move PrefixLogger to BucketLoggerTest
    the BucketLoggerTest::TearDown recreates the logger so subsequent
    tests can run.
    
    Change-Id: I887bc1ddd8d19850a82c8a84aff909f46f944987
    Reviewed-on: https://review.couchbase.org/c/kv_engine/+/228782
    Reviewed-by: Faizan Alam <faizan.alam@couchbase.com>
    Reviewed-by: Trond Norbye <trond.norbye@couchbase.com>
    Tested-by: Build Bot <build@couchbase.com>

--- engines/ep/tests/module_tests/bucket_logger_test.cc
+++ engines/ep/tests/module_tests/bucket_logger_test.cc
@@ -308,3 +308,28 @@ TEST_F(BucketLoggerTest, RaceInFlushDueToPtrOwnership) {
     // has finished as it locks on the logger_map_mutex_ in spdlog::registry
     cb::logger::shutdown();
 }
+
+TEST_F(BucketLoggerTest, PrefixLogger) {
+    auto prefixLogger = std::make_shared<cb::logger::PrefixLogger>(
+            "prefix_logger", cb::logger::get());
+    prefixLogger->setPrefix(cb::logger::BasicJsonType({{"prefix", "test"}}),
+                            "prefix=test");
+
+    prefixLogger->log(spdlog::level::info, "my message");
+    prefixLogger->logWithContext(
+            spdlog::level::info, "my message", {{"key", "value"}});
+
+    cb::logger::shutdown();
+    files = cb::io::findFilesWithPrefix(config.filename);
+    ASSERT_EQ(1, files.size()) << "We should only have a single logfile";
+    EXPECT_EQ(
+            1,
+            countInFile(files.front(), "INFO my message {\"prefix\":\"test\"}"))
+            << getLogContents();
+    EXPECT_EQ(
+            1,
+            countInFile(
+                    files.front(),
+                    "INFO my message {\"prefix\":\"test\",\"key\":\"value\"}"))
+            << getLogContents();
+}
--- logger/logger_test_fixture.cc
+++ logger/logger_test_fixture.cc
@@ -102,28 +102,3 @@ std::string SpdloggerTest::getLogContents() {
 
     return ret;
 }
-
-TEST_F(SpdloggerTest, PrefixLogger) {
-    auto prefixLogger = std::make_shared<cb::logger::PrefixLogger>(
-            "prefix_logger", cb::logger::get());
-    prefixLogger->setPrefix(cb::logger::BasicJsonType({{"prefix", "test"}}),
-                            "prefix=test");
-
-    prefixLogger->log(spdlog::level::info, "my message");
-    prefixLogger->logWithContext(
-            spdlog::level::info, "my message", {{"key", "value"}});
-
-    cb::logger::shutdown();
-    files = cb::io::findFilesWithPrefix(config.filename);
-    ASSERT_EQ(1, files.size()) << "We should only have a single logfile";
-    EXPECT_EQ(
-            1,
-            countInFile(files.front(), "INFO my message {\"prefix\":\"test\"}"))
-            << getLogContents();
-    EXPECT_EQ(
-            1,
-            countInFile(
-                    files.front(),
-                    "INFO my message {\"prefix\":\"test\",\"key\":\"value\"}"))
-            << getLogContents();
-}
